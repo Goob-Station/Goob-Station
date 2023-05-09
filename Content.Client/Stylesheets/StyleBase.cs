034d6fc15705785f881c37f644a4fb4a775529ae
Rane <60792108+Elijahrane@users.noreply.github.com>
2023-05-09 10:42:20 -0400

    Add Simplified Chinese Font (#1)
    * Add Simplified Chinese Font
    
    * woops nice overwrite

--- Content.Client/Stylesheets/StyleBase.cs
+++ Content.Client/Stylesheets/StyleBase.cs
@@ -47,6 +47,7 @@ namespace Content.Client.Stylesheets
                 new []
                 {
                     "/Fonts/NotoSans/NotoSans-Regular.ttf",
+                    "/Fonts/NotoSans/NotoSans-SC.ttf",
                     "/Fonts/NotoSans/NotoSansSymbols-Regular.ttf",
                     "/Fonts/NotoSans/NotoSansSymbols2-Regular.ttf"
                 },
@@ -57,6 +58,7 @@ namespace Content.Client.Stylesheets
                 new []
                 {
                     "/Fonts/NotoSans/NotoSans-Italic.ttf",
+                    "/Fonts/NotoSans/NotoSans-SC.ttf",
                     "/Fonts/NotoSans/NotoSansSymbols-Regular.ttf",
                     "/Fonts/NotoSans/NotoSansSymbols2-Regular.ttf"
                 },
--- Content.Client/Stylesheets/StyleNano.cs
+++ Content.Client/Stylesheets/StyleNano.cs
@@ -31,7 +31,8 @@ namespace Content.Client.Stylesheets
                 {
                     $"/Fonts/NotoSans{ds}/NotoSans{ds}-{variation}.ttf",
                     $"/Fonts/NotoSans/NotoSansSymbols-{sv}.ttf",
-                    "/Fonts/NotoSans/NotoSansSymbols2-Regular.ttf"
+                    "/Fonts/NotoSans/NotoSansSymbols2-Regular.ttf",
+                    "/Fonts/NotoSans/NotoSans-SC.ttf",
                 },
                 size
             );
