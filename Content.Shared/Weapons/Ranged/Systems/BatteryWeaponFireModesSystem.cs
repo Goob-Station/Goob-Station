71cc67da0cc35e6c35b5b94a639d792f6d762f5f
CatMagic <135462166+CatMagic1@users.noreply.github.com>
2024-04-24 17:35:34 -0400

    durk kiss me on the lips

--- Content.Shared/Weapons/Ranged/Systems/BatteryWeaponFireModesSystem.cs
+++ Content.Shared/Weapons/Ranged/Systems/BatteryWeaponFireModesSystem.cs
@@ -109,10 +109,10 @@ public sealed class BatteryWeaponFireModesSystem : EntitySystem
                 _popupSystem.PopupClient(Loc.GetString("gun-set-fire-mode", ("mode", prototype.Name)), uid, user.Value);
             }
 
-        if (MagSprite != null)
+        if (component.MagSprite != null)
         {
             if (TryComp<MagazineVisuals>(uid, out var magVisuals)){
-                        magVisuals.MagState = MagSprite;
+                        magVisuals.MagState = component.MagSprite;
             }
         }
         }
