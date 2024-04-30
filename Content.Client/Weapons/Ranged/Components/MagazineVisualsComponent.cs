7adb6d97d74c880e1a6523b1edcccb5847a0b4e5
CatMagic <135462166+CatMagic1@users.noreply.github.com>
2024-04-30 19:35:07 -0400

    asdad

--- Content.Client/Weapons/Ranged/Components/MagazineVisualsComponent.cs
+++ Content.Client/Weapons/Ranged/Components/MagazineVisualsComponent.cs
@@ -5,8 +5,7 @@ namespace Content.Client.Weapons.Ranged.Components;
 /// <summary>
 /// Visualizer for gun mag presence; can change states based on ammo count or toggle visibility entirely.
 /// </summary>
-[RegisterComponent, Access(typeof(GunSystem))]
-[RegisterComponent, Access(typeof(BatteryGunFireModeVisuals))]
+[RegisterComponent, Access(typeof(GunSystem)), Access(typeof(BatteryGunFireModeVisuals))]
 public sealed partial class MagazineVisualsComponent : Component
 {
     /// <summary>
