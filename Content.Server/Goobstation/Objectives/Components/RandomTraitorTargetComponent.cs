8c05d3e9dbbd2718c2cdb50d5c2f8d7933cd4c39
Conchelle <mary@thughunt.ing>
2024-06-19 03:21:58 +0300

    Protecting another traitor's target

---
+++ Content.Server/Goobstation/Objectives/Components/RandomTraitorTargetComponent.cs
@@ -0,0 +1,9 @@
+﻿namespace Content.Server.Goobstation.Objectives.Components;
+
+/// <summary>
+/// Sets the target to protect a player that is targeted to kill by another traitor
+/// </summary>
+[RegisterComponent]
+public sealed partial class RandomTraitorTargetComponent : Component
+{
+}
