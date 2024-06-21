c3142fe7d805ace00eb44eae895bbb22da4c540d
Aidenkrz <aiden@djkraz.com>
2024-06-21 01:56:09 -0500

    Fix up that file schema.

--- Content.Shared/Goobstation/Supermatter/Components/SupermatterComponent.cs
+++ Content.Shared/Goobstation/Supermatter/Components/SupermatterComponent.cs
@@ -53,10 +53,10 @@ public sealed partial class SupermatterComponent : Component
     public SharedSupermatterSystem.SuperMatterSound? SmSound;
 
     [DataField("dustSound")]
-    public SoundSpecifier DustSound = new SoundPathSpecifier("/Audio/Supermatter/dust.ogg");
+    public SoundSpecifier DustSound = new SoundPathSpecifier("/Audio/Goobstation/Supermatter/dust.ogg");
 
     [DataField("delamSound")]
-    public SoundSpecifier DelamSound = new SoundPathSpecifier("/Audio/Supermatter/delamming.ogg");
+    public SoundSpecifier DelamSound = new SoundPathSpecifier("/Audio//Goobstation/Supermatter/delamming.ogg");
 
     [DataField("delamAlarm")]
     public SoundSpecifier DelamAlarm = new SoundPathSpecifier("/Audio/Machines/alarm.ogg");
