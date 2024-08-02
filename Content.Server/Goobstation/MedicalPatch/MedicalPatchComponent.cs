9e155c0a4f10bd1f2eb150b36555f950064e22f8
unknown <Administrator@DESKTOP-PMRIVVA.kommune.indresogn.no>
2024-08-02 15:50:56 +0200

    component summary and changed patch white list

--- Content.Server/Goobstation/MedicalPatch/MedicalPatchComponent.cs
+++ Content.Server/Goobstation/MedicalPatch/MedicalPatchComponent.cs
@@ -9,14 +9,27 @@ public sealed partial class MedicalPatchComponent : Component
     public string SolutionName = "drink";
     [DataField] // [ViewVariables(VVAccess.ReadWrite)]
     public FixedPoint2 TransferAmount = FixedPoint2.New(1);
+    /// <summary>
+    ///  if this is a single use patch, gets destroyed or replaced when empty or removed.
+    /// </summary>
     [DataField]
     public bool SingleUse = false;
+    /// <summary>
+    /// how often the patch shud transfer sulution
+    /// </summary>
     [DataField]
     public float UpdateTime = 1f;
+
     [DataField]
     public TimeSpan NextUpdate = TimeSpan.Zero;
+    /// <summary>
+    /// if any set ammount shud be transfered when the patch is attatched,
+    /// </summary>
     [DataField]
     public FixedPoint2 InjectAmmountOnAttatch = FixedPoint2.New(0);
+    /// <summary>
+    /// if a Percentage of the remaining soulution shud be transfered when attatched, use 0 - 100
+    /// </summary>
     [DataField]
     public FixedPoint2 InjectPercentageOnAttatch = FixedPoint2.New(0);
 }
