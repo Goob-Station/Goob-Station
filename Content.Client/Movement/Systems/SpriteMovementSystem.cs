4e5083ec35c936171ef38d83c8b498d45540fe1b
Aidenkrz <aiden@djkraz.com>
2024-08-02 12:34:00 -0500

    Fix sprite movement

--- Content.Client/Movement/Systems/SpriteMovementSystem.cs
+++ Content.Client/Movement/Systems/SpriteMovementSystem.cs
@@ -31,7 +31,7 @@ public sealed class SpriteMovementSystem : EntitySystem
         var moving = (SharedMoverController.GetNormalizedMovement(args.Entity.Comp.HeldMoveButtons) & MoveButtons.AnyDirection) != MoveButtons.None;
 
         var oldWalking = (SharedMoverController.GetNormalizedMovement(args.OldMovement) & MoveButtons.Walk) != MoveButtons.None;
-        var walking = (SharedMoverController.GetNormalizedMovement(args.Component.HeldMoveButtons) & MoveButtons.Walk) != MoveButtons.None;
+        var walking = (SharedMoverController.GetNormalizedMovement(args.Entity.Comp.HeldMoveButtons) & MoveButtons.Walk) != MoveButtons.None;
 
         if ((oldMoving == moving && oldWalking == walking) || !_spriteQuery.TryGetComponent(uid, out var sprite))
             return;
