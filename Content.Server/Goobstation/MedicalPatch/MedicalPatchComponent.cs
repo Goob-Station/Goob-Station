213b37d2022657a0533b8da13c5c5d5ae86a7d4e
unknown <Administrator@DESKTOP-PMRIVVA.kommune.indresogn.no>
2024-08-04 18:50:54 +0200

    sprites and cleanups

--- Content.Server/Goobstation/MedicalPatch/MedicalPatchComponent.cs
+++ Content.Server/Goobstation/MedicalPatch/MedicalPatchComponent.cs
@@ -7,13 +7,18 @@ public sealed partial class MedicalPatchComponent : Component
 {
     [DataField]
     public string SolutionName = "drink";
-    [DataField] // [ViewVariables(VVAccess.ReadWrite)]
+    [DataField]
     public FixedPoint2 TransferAmount = FixedPoint2.New(1);
     /// <summary>
     ///  if this is a single use patch, gets destroyed or replaced when empty or removed.
     /// </summary>
     [DataField]
     public bool SingleUse = false;
+    /// <summary>
+    ///  if single use what the Entity shud be replaced whit
+    /// </summary>
+    [DataField]
+    public string? TrashObject = "UsedMedicalPatch";
     /// <summary>
     /// how often the patch shud transfer sulution
     /// </summary>
