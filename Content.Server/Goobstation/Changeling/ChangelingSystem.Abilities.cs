060670ecf7ab7024efef056de56cb97302a4b81d
Fishbait <Fishbait@git.ml>
2024-08-11 12:28:34 +0200

    init

--- Content.Server/Goobstation/Changeling/ChangelingSystem.Abilities.cs
+++ Content.Server/Goobstation/Changeling/ChangelingSystem.Abilities.cs
@@ -463,6 +463,11 @@ public sealed partial class ChangelingSystem : EntitySystem
         if (!TryUseAbility(uid, comp, args))
             return;
 
+        if (TryComp<EyeComponent>(uid, out var eyes))
+        {
+            _eye.SetDrawLight(uid, !eyes.DrawLight);
+        }
+
         if (HasComp<FlashImmunityComponent>(uid))
         {
             _popup.PopupEntity(Loc.GetString("changeling-passive-active"), uid, uid);
