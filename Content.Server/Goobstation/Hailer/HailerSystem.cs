2ee273ad15f52f48acc3cda767f73778b38978da
starwolf <ramses@starwolves.io>
2024-08-26 21:13:24 +0200

    Create hail spam prevention.

--- Content.Server/Goobstation/Hailer/HailerSystem.cs
+++ Content.Server/Goobstation/Hailer/HailerSystem.cs
@@ -4,6 +4,9 @@ using Content.Shared.Inventory;
 using Content.Shared.Inventory.Events;
 using Robust.Shared.Audio.Systems;
 using Ra = System.Random;
+using Robust.Shared.Timing;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Robust.Shared.Serialization.TypeSerializers.Implementations;
 
 namespace Content.Shared.Hailer.EntitySystems;
 
@@ -11,6 +14,8 @@ public sealed class HailerSystem : EntitySystem
 {
     [Dependency] private readonly SharedActionsSystem _actionsSystem = default!;
     [Dependency] private readonly SharedAudioSystem _audio = default!;
+    [Dependency] private readonly IGameTiming _timing = default!;
+
 
     public override void Initialize()
     {
@@ -45,13 +50,23 @@ public sealed class HailerSystem : EntitySystem
         "/Audio/Goobstation/Hailer/freeze.ogg",
         "/Audio/Goobstation/Hailer/halt.ogg",
     ];
+    Dictionary<EntityUid, TimeSpan> _delays = new Dictionary<EntityUid, TimeSpan>();
+    TimeSpan _fixed_delay = TimeSpan.FromSeconds(2);
     private void OnHail(EntityUid uid, ActionsComponent component, ref HailerActionEvent args)
     {
         if (args.Handled)
             return;
-
+        // No hail spam check.
+        if (_delays.ContainsKey(uid))
+        {
+            if (_timing.CurTime < _delays[uid])
+            {
+                return;
+            }
+        }
         Ra r = new Ra();
         int rInt = r.Next(0, _sounds.Length);
         _audio.PlayPvs(_sounds[rInt], uid);
+        _delays[uid] = _timing.CurTime.Add(_fixed_delay);
     }
 }
