ec84bf1c34dea692aeda598b38eea0e0f8bee340
whateverusername0 <whateveremail>
2024-09-02 17:36:38 +1000

    sdjhvjsdfkvhsdf

--- Content.Shared/Store/Components/StoreComponent.cs
+++ Content.Shared/Store/Components/StoreComponent.cs
@@ -1,3 +1,4 @@
+using Content.Shared._White.StoreDiscount;
 using Content.Shared.FixedPoint;
 using Robust.Shared.Audio;
 using Robust.Shared.GameStates;
@@ -86,6 +87,11 @@ public sealed partial class StoreComponent : Component
     /// </summary>
     [DataField]
     public EntityUid? StartingMap;
+
+    // WD EDIT START
+    [DataField]
+    public SalesSpecifier Sales { get; private set; } = new();
+    // WD EDIT END
 
     #region audio
     /// <summary>
