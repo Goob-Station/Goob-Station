2963e76a4e205a0413e4f6e70d04936c234499e1
yglop <yglop@yandex.ru>
2024-09-23 18:47:51 +0500

    comment

--- Content.Server/Goobstation/WeaponRandomExplode/WeaponRandomExplodeComponent.cs
+++ Content.Server/Goobstation/WeaponRandomExplode/WeaponRandomExplodeComponent.cs
@@ -6,6 +6,10 @@ public sealed partial class WeaponRandomExplodeComponent : Component
     [DataField, AutoNetworkedField]
     public float explosionChance;
 
+    /// <summary>
+    /// if not filled - the explosion force will be 1.
+    /// if filled - the explosion force will be the current charge multiplied by this.
+    /// </summary>
     [DataField, AutoNetworkedField]
     public float multiplyByCharge;
 }
