b84b1e8a93d84e8dd4986e3b51b28a92e57291dd
username <113782077+whateverusername0@users.noreply.github.com>
2024-11-08 08:34:44 +1000

    upstream mind roles (#827)
    * help
    
    * help 2
    
    * hooray :firework:
    
    * shitfix
    
    * fucking renames
    
    ---------
    
    Co-authored-by: Errant <35878406+Errant-4@users.noreply.github.com>
    Co-authored-by: whateverusername0 <whateveremail>

--- Content.Server/_Goobstation/Heretic/EntitySystems/HereticSystem.cs
+++ Content.Server/_Goobstation/Heretic/EntitySystems/HereticSystem.cs
@@ -132,23 +132,7 @@ public sealed partial class HereticSystem : EntitySystem
         // pick one command staff
         predicates.Add(t => HasComp<CommandStaffComponent>(t));
 
-        // pick one secoff
-        predicates.Add(t =>
-            _prot.TryIndex<DepartmentPrototype>("Security", out var dept) // can we get sec jobs?
-            && _mind.TryGetMind(t, out var mindid, out _) // does it have a mind?
-            && TryComp<JobComponent>(mindid, out var jobc) && jobc.Prototype.HasValue // does it have a job?
-            && dept.Roles.Contains(jobc.Prototype!.Value)); // is that job being shitsec?
-
-        // pick one person from the same department
-        predicates.Add(t =>
-            _mind.TryGetMind(t, out var tmind, out _) && _mind.TryGetMind(ent, out var ownmind, out _) // get minds
-            && TryComp<JobComponent>(tmind, out var tjob) && tjob.Prototype.HasValue // get jobs
-            && TryComp<JobComponent>(ownmind, out var ownjob) && ownjob.Prototype.HasValue
-            && _prot.EnumeratePrototypes<DepartmentPrototype>() // compare jobs for all
-                .Where(d =>
-                    d.Roles.Contains(tjob.Prototype.Value)
-                    && d.Roles.Contains(ownjob.Prototype.Value)) // true = same department
-                .ToList().Count != 0);
+        // add more predicates here
 
         foreach (var predicate in predicates)
         {
