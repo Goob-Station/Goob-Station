5d4ec72472a50aeb8b97ab2693f8074d37491b96
BeeRobynn <robynthewarcrime@proton.me>
2024-11-12 23:40:01 +0000

    Revert "Fixed issues"
    This reverts commit ad788c7f8dc05b56799e0d01cb74525a99db85a9.

--- Content.Server/_Goobstation/Speech/EntitySystems/DementiaAccentSystem.cs
+++ Content.Server/_Goobstation/Speech/EntitySystems/DementiaAccentSystem.cs
@@ -1,12 +1,10 @@
 using System.Text.RegularExpressions;
 using Content.Server.Speech.Components;
-using Robust.Shared.Random;
 
 namespace Content.Server.Speech.EntitySystems;
 
 public sealed class DementiaAccentSystem : EntitySystem
 {
-    [Dependency] private readonly IRobustRandom _random = default!;
     [Dependency] private readonly ReplacementAccentSystem _replacement = default!;
 
     public override void Initialize()
@@ -20,26 +18,6 @@ public sealed class DementiaAccentSystem : EntitySystem
         var message = args.Message;
 
         message = _replacement.ApplyReplacements(message, "dementia");
-
-        // Prefix
-        if (_random.Prob(0.15f))
-        {
-            var pick = _random.Next(1, 3);
-
-            // Reverse sanitize capital
-            message = message[0].ToString().ToLower() + message.Remove(0, 1);
-            message = Loc.GetString($"accent-dementia-prefix-{pick}") + " " + message;
-        }
-
-        // Sanitize capital again, in case we substituted a word that should be capitalized
-        message = message[0].ToString().ToUpper() + message.Remove(0, 1);
-
-        // Suffixes
-        if (_random.Prob(0.3f))
-        {
-            var pick = _random.Next(1, 5);
-            message += Loc.GetString($"accent-dementia-suffix-{pick}");
-        }
 
         args.Message = message;
     }
