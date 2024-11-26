9bcd97dc90f34e6eb00816232b15c1f59cecdacf
username <113782077+whateverusername0@users.noreply.github.com>
2024-11-26 23:31:30 +1000

    Heretics update part 5 (#945)
    * i got that dawg in me
    
    * dasasdsaddas
    
    * jhkhkjj
    
    * my lazy ass is not doing the rest
    
    * finally.
    
    * finally.
    
    * jesus christ 1
    
    * help me
    
    * jesus christ 2
    
    * UAAAAWHGHHGG BUABHAUBHAUBHHGH
    
    * jesus christ 3
    
    * sdfjsdkghjdfkljbhsfuobd
    
    * 1984
    
    * ash lore update
    
    * THE CURSE OF 220
    
    * ultra violencce
    
    * fart
    
    * fdgfdgbfdbf
    
    * fgbdfgndfgn
    
    * it's raw propheting time
    
    * blbkblbkglbgkbgbgbgdfbfgbbb AAAAAAAAAAAAAAAAAAAAAAAAA
    
    * fdbfsgbybdf
    
    * bkbgkblkgbgbngfkb
    
    * void path part 1
    
    * REAL!!!!!!
    
    * it is working
    
    * ok looks good to me
    
    * random influence spawn
    
    * hey shitass, wanna see me bypass yaml linter?
    
    * RAAAAAAAAAAGH
    
    * dghgdfbsfdgbsdf
    
    * dfvsfvavsd
    
    * help
    
    * help
    
    * bbnvbnvmvbnmvbnmvbn
    
    * guiedbook
    
    * fhfdsghfgdhdfgh
    
    * AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
    
    * please
    
    * fucking
    die
    
    * vlkblvkbbgjhjfbhnbkvbnf A
    
    * i am malding
    
    * 6666 lines
    
    * FUCK
    
    * GOD SAVE MY SOUL!!
    
    * revert store shitassery
    
    * dfvdfgdf
    
    * Reapply "Uplink discounts (#580)"
    
    This reverts commit 55540db89a6ba1af06e81dbf82544bc8dc1d486d.
    
    * more uplink shittery revert
    
    * FUCK
    
    * based
    
    * dvsfv
    
    * dghfgghfgh
    
    * blnkbnbknlbnklb
    
    * :finnadie:
    
    * finally. :finnadie:
    
    * yeah
    
    * bnbnmbvnm
    
    * vblknknvblnkvblknklvbn
    
    * fuck
    
    * gaming
    
    * bnkb,nkvbnklvbknvb
    
    * final fixez
    
    * I AM GOING TO KILL MYSELF
    
    * finally
    
    * yay guidebook
    
    * ugh
    
    * fuck
    
    * fuck
    
    * bruh.
    
    * guidebook
    
    * bruh
    
    * source
    
    * f-
    
    * n lvjbl nvb;nkvb;lnkvl;bn
    
    * GACK
    
    * WHAT
    
    * g
    
    * gaming 2
    
    * gaming 2
    
    * docks
    
    * attribushions
    
    * balls
    
    * blade path cooking up...
    
    * more placeholders and shit
    
    * L
    
    * more placeholders holy shit!!!
    
    * W
    
    * W2
    
    * LMAOOOOOOOOO
    
    * huh?
    
    * fix checks, probably
    
    * W?
    
    * f
    
    * POWAH!!!
    
    * ai faction lulz
    
    ---------
    
    Co-authored-by: whateverusername0 <whateveremail>
    Co-authored-by: Piras314 <p1r4s@proton.me>

--- Content.Server/_Goobstation/Heretic/Abilities/HereticAbilitySystem.Ash.cs
+++ Content.Server/_Goobstation/Heretic/Abilities/HereticAbilitySystem.Ash.cs
@@ -80,7 +80,8 @@ public sealed partial class HereticAbilitySystem : EntitySystem
         if (!TryUseAbility(ent, args))
             return;
 
-        var lookup = _lookup.GetEntitiesInRange(ent, 5f);
+        var power = ent.Comp.CurrentPath == "Ash" ? ent.Comp.PathStage : 2.5f;
+        var lookup = _lookup.GetEntitiesInRange(ent, power);
 
         foreach (var look in lookup)
         {
@@ -92,18 +93,18 @@ public sealed partial class HereticAbilitySystem : EntitySystem
             {
                 if (flam.OnFire && TryComp<DamageableComponent>(ent, out var dmgc))
                 {
-                    // heals everything by 10 for each burning target
-                    _stam.TryTakeStamina(ent, -10);
+                    // heals everything by base + power for each burning target
+                    _stam.TryTakeStamina(ent, -(10 + power));
                     var dmgdict = dmgc.Damage.DamageDict;
                     foreach (var key in dmgdict.Keys)
-                        dmgdict[key] = -10f;
+                        dmgdict[key] -= 10f + power;
 
                     var dmgspec = new DamageSpecifier() { DamageDict = dmgdict };
                     _dmg.TryChangeDamage(ent, dmgspec, true, false, dmgc);
                 }
 
                 if (!flam.OnFire)
-                    _flammable.AdjustFireStacks(look, 5, flam, true);
+                    _flammable.AdjustFireStacks(look, power, flam, true);
 
                 if (TryComp<MobStateComponent>(look, out var mobstat))
                     if (mobstat.CurrentState == MobState.Critical)
@@ -132,7 +133,7 @@ public sealed partial class HereticAbilitySystem : EntitySystem
 
         // yeah. it just generates a ton of plasma which just burns.
         // lame, but we don't have anything fire related atm, so, it works.
-        var tilepos = _xform.GetGridOrMapTilePosition(ent, Transform(ent));
+        var tilepos = _transform.GetGridOrMapTilePosition(ent, Transform(ent));
         var enumerator = _atmos.GetAdjacentTileMixtures(Transform(ent).GridUid!.Value, tilepos, false, false);
         while (enumerator.MoveNext(out var mix))
         {
@@ -157,5 +158,7 @@ public sealed partial class HereticAbilitySystem : EntitySystem
         // fire immunity
         var flam = EnsureComp<FlammableComponent>(ent);
         flam.Damage = new(); // reset damage dict
+        // this does NOT protect you against lasers and whatnot. for now. when i figure out THIS STUPID FUCKING LIMB SYSTEM!!!
+        // regards.
     }
 }
