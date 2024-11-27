db83c711282f14d8a74fc20bccb5c476fb1a7d54
Aviu00 <aviu00@protonmail.com>
2024-11-27 21:37:20 +0300

    Tweak.

--- Content.Shared/Materials/MaterialStorageComponent.cs
+++ Content.Shared/Materials/MaterialStorageComponent.cs
@@ -69,7 +69,7 @@ public sealed partial class MaterialStorageComponent : Component
     public bool CanEjectStoredMaterials = true;
 
     // Goobstation
-    [DataField]
+    [DataField, ViewVariables(VVAccess.ReadWrite), AutoNetworkedField]
     public bool ConnectToSilo;
 }
 
