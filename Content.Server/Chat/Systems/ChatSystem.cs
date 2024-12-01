72594521256f1e371e35c6d145fc2acef37ab248
Piras314 <p1r4s@proton.me>
2024-12-01 11:32:32 -0500

    E

--- Content.Server/Chat/Systems/ChatSystem.cs
+++ Content.Server/Chat/Systems/ChatSystem.cs
@@ -64,7 +64,7 @@ public sealed partial class ChatSystem : SharedChatSystem
     public const int VoiceRange = 10; // how far voice goes in world units
     public const int WhisperClearRange = 2; // how far whisper goes while still being understandable, in world units
     public const int WhisperMuffledRange = 5; // how far whisper goes at all, in world units
-    public const string DefaultAnnouncementSound = "/Audio/_Goobstation/Announcements/violet.ogg";
+    public const string DefaultAnnouncementSound = "/Audio/_Goobstation/Announcements/attention.ogg";
 
     private bool _loocEnabled = true;
     private bool _deadLoocEnabled;
