c893bf7cf3c8aa1876d55bb4da4ba4757d69f888
Fishbait <Fishbait@git.ml>
2024-12-02 02:49:39 +0100

    files does seem to do nothing. deleted

--- Content.Client/_Goobstation/Blob/BlobMobSystem.cs
+++
@@ -1,8 +0,0 @@
-﻿using Content.Shared._Goobstation.Blob;
-
-namespace Content.Client._Goobstation.Blob;
-
-public sealed class BlobMobSystem : SharedBlobMobSystem
-{
-
-}
--- Content.Client/_Goobstation/Blob/ZombieBlobSystem.cs
+++
@@ -1,5 +0,0 @@
-﻿using Content.Shared._Goobstation.Blob;
-
-namespace Content.Client._Goobstation.Blob;
-
-public sealed class ZombieBlobSystem : SharedZombieBlobSystem;
