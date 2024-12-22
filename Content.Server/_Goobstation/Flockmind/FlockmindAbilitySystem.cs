8bf2f7fc362102566c43c6b14be5cb88ae68e351
Vlax <vlaxreborn@gmail.com>
2024-12-22 02:43:18 +0100

    hrhrh

--- Content.Server/_Goobstation/Flockmind/FlockmindAbilitySystem.cs
+++ Content.Server/_Goobstation/Flockmind/FlockmindAbilitySystem.cs
@@ -4,6 +4,7 @@ using Content.Shared.Damage;
 using Content.Shared.Damage.Systems;
 using Content.Shared.DoAfter;
 using Content.Shared.Flockmind;
+using Content.Server.Flockmind;
 using Content.Shared.Mind.Components;
 using Content.Shared.Mobs.Systems;
 using Content.Shared.Store.Components;
@@ -21,20 +22,44 @@ using Content.Shared.Localizations;
 using Robust.Shared.Audio;
 using Content.Shared.Mobs.Components;
 using Robust.Shared.Prototypes;
+using System.Linq;
+using Robust.Server.Audio;
 
 namespace Content.Server.Flockmind;
 
-public sealed partial class FlockmindAbilitySystem : EntitySystem
+public sealed partial class FlockmindSystem : EntitySystem
 {
-    [Dependency] private readonly FlashSystem _flash = default!;
-    [Dependency] private readonly SharedBodySystem _body = default!;
-    [Dependency] private readonly SharedStunSystem _stun = default!;
-    [Dependency] private readonly StaminaSystem _stamina = default!;
     public override void Initialize()
     {
         base.Initialize();
-
-        //SubscribeLocalEvent<FlockmindComponent, EventHereticOpenStore>(OnStore);
+        SubscribeLocalEvent<FlockmindComponent, EventSummonRift>(OnRiftSummon);
+        SubscribeLocalEvent<FlockmindComponent, EventRadioStun>(OnRadioStun);
     }
-}
 
+    #region Flockmind abilities
+
+    private void OnRiftSummon(EntityUid uid, FlockmindComponent comp, ref EventSummonRift args)
+    {
+        var transform = Transform(uid);
+        var rift = EntityManager.SpawnEntity("Rift", transform.Coordinates);
+        var audioSystem = EntityManager.System<AudioSystem>();
+        audioSystem.PlayFromEntity("/Audio/Effects/teleport_activate.ogg", rift);
+    }
+
+    private void OnRadioStun(EntityUid uid, FlockmindComponent comp, ref OnRadioStun args)
+    {
+        var radius = 15.0f;
+        var transform = Transform(uid);
+        var lookup = _lookup.GetEntitiesInRange(transform.Coordinates, radius);
+
+        foreach (var entity in lookup)
+        {
+            if (_inventory.TryGetSlotEntity(entity, "headset", out _))
+            {
+                _stun.TryStun(entity, TimeSpan.FromSeconds(5), true);
+            }
+        }
+    }
+
+    #endregion
+}
--- Content.Server/_Goobstation/Flockmind/FlockmindSystem.cs
+++ Content.Server/_Goobstation/Flockmind/FlockmindSystem.cs
@@ -1,12 +1,25 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Content.Shared.Mind;
+using Content.Server.Flash;
+using Robust.Server.Audio;
+using Robust.Shared.Audio;
+using Content.Shared.Mobs;
+using Content.Shared.Mobs.Systems;
+using Content.Shared.Nutrition.Components;
+using Content.Shared.Stunnable;
+using Content.Shared.Body.Systems;
+using Content.Shared.Damage.Systems;
+using Content.Shared.Inventory;
 
-namespace Content.Server._Goobstation.Flockmind
+namespace Content.Server.Flockmind
 {
-    public class FlockmindSystem
+    public sealed partial class FlockmindSystem : EntitySystem
     {
-        
+        [Dependency] private readonly EntityLookupSystem _lookup = default!;
+        [Dependency] private readonly FlashSystem _flash = default!;
+        [Dependency] private readonly SharedBodySystem _body = default!;
+        [Dependency] private readonly SharedStunSystem _stun = default!;
+        [Dependency] private readonly StaminaSystem _stamina = default!;
+        [Dependency] private readonly InventorySystem _inventory = default!;
+
     }
 }
