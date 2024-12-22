3ddedeae365e4826ff16cb56c458938b23aa8058
Raphael Bertoche <bertocheraphael@gmail.com>
2024-12-22 05:15:02 -0300

    Typo on comment

--- Content.Server/Antag/AntagSelectionSystem.cs
+++ Content.Server/Antag/AntagSelectionSystem.cs
@@ -249,7 +249,7 @@ public sealed partial class AntagSelectionSystem : GameRuleSystem<AntagSelection
         if (picking)
         {
             // Tries up to maxRetries times to assign antags.
-            // When any number of assignment fails, next iteration
+            // When any number of assignments fails, next iteration
             // gets new items to replace those.
             // Already selected or failed sessions are avoided.
             // It retries until it ends with no failures or up
