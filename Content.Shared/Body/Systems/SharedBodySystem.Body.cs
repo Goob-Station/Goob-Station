df1df8bd58b79146ce767a68fae06e81b75ff440
Aviu00 <93730715+Aviu00@users.noreply.github.com>
2024-12-23 17:27:41 +0300

    Evasion and wheelchair bound fix (#1239)

--- Content.Shared/Body/Systems/SharedBodySystem.Body.cs
+++ Content.Shared/Body/Systems/SharedBodySystem.Body.cs
@@ -462,7 +462,7 @@ public partial class SharedBodySystem
 
     private void OnStandAttempt(Entity<BodyComponent> ent, ref StandAttemptEvent args)
     {
-        if (ent.Comp.LegEntities.Count == 0 && ent.Comp.RequiredLegs > 0)
+        if (ent.Comp.LegEntities.Count < ent.Comp.RequiredLegs)
             args.Cancel();
     }
 
