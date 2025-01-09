WWhiteDreamProject/wwdpublic

[Tweak] Better Lizards (#164)
* [Tweak] Better Lizards

* digi legs

* resprited markings

* resprited body

* edit ftl

* fix missing sprites

Resources/Locale/en-US/_white/markings/reptilian.ftl
@@ -1,0 +1,74 @@
+# HEAD
+marking-LizardHeadGuilmon= Lizard, guilmon
+marking-LizardHeadGuilmon-guilmon = Main
+
+marking-LizardShoutEasternDragon= Lizard, eastern dragon
+marking-LizardShoutEasternDragon-eastern_dragon = Outer
+marking-LizardShoutEasternDragon-eastern_dragon_overlay = Inner
+
+marking-LizardShoutEasternDragonUsiki= Lizard, eastern dragon (Tentacle)
+marking-LizardShoutEasternDragonUsiki-eastern_dragon = Outer
+marking-LizardShoutEasternDragonUsiki-eastern_dragon_overlay = Inner
+marking-LizardShoutEasternDragonUsiki-eastern_dragon_usiki = Tentacle
+
+marking-LizardHeadCobraHood = Lizard, hood with ears (Cobra)
+marking-LizardHeadCobraHood-cobra_hood_with_ears = Outer
+marking-LizardHeadCobraHood-cobra_hood_with_ears_overlay = Inner
+
+marking-LizardHornsCurled-horns_curled_l = Left horn
+marking-LizardHornsCurled-horns_curled_r = Right horn
+
+marking-LizardHornsRam-horns_ram_l = Left horn
+marking-LizardHornsRam-horns_ram_r = Right horn
+
+marking-LizardHornsShort-horns_short_l = Left horn
+marking-LizardHornsShort-horns_short_r = Right horn
+
+marking-LizardHornsSimple-horns_simple_l = Left horn
+marking-LizardHornsSimple-horns_simple_r = Right horn
+
+marking-LizardHornsDouble-horns_double_l = Left horns
+marking-LizardHornsDouble-horns_double_r = Right horns
+
+marking-LizardHornsArgali-horns_argali_l = Left horn
+marking-LizardHornsArgali-horns_argali_r = Right horn
+
+marking-LizardHornsAyrshire-horns_ayrshire_l = Left Horn
+marking-LizardHornsAyrshire-horns_ayrshire_r = Right Horn
+
+marking-LizardHornsMyrsore-horns_myrsore_l = Left horn
+marking-LizardHornsMyrsore-horns_myrsore_r = Right horn
+
+marking-LizardHornsBighorn-horns_bighorn_l = Left horn
+marking-LizardHornsBighorn-horns_bighorn_r = Right horn
+
+# TAILS
+marking-SnakeLargeTail = Lizard, large snake tail
+marking-SnakeLargeTail-tail_snakelarge = Inner
+marking-SnakeLargeTail-tail_snakelarge_overlay = Outer
+
+# SPINES
+marking-LizardChestSpinesAquatic = Lizard, chest, spines (Aquatic)
+marking-LizardChestSpinesAquatic-spines_aquatic = Spikes
+marking-LizardChestSpinesAquaticWithoutTail = Lizard, chest, spines (Aquatic, without tail)
+marking-LizardChestSpinesAquaticWithoutTail-spines_aquatic = Spikes
+
+marking-LizardChestSpinesLong = Lizard, chest, spines (Long)
+marking-LizardChestSpinesLong-spines_long = Spikes
+marking-LizardChestSpinesLongWithoutTail = Lizard, chest, spines (Long, without tail)
+marking-LizardChestSpinesLongWithoutTail-spines_long = Spikes
+
+marking-LizardChestSpinesLongMembrane = Lizard, chest, spines (Long, Membrane)
+marking-LizardChestSpinesLongMembrane-spines_longmembrane = spines
+marking-LizardChestSpinesLongMembraneWithoutTail = Lizard, chest, spines (Long, Membrane, without tail)
+marking-LizardChestSpinesLongMembraneWithoutTail-spines_longmembrane = spines
+
+marking-LizardChestSpinesShort = Lizard, chest, spines (Short)
+marking-LizardChestSpinesShort-spines_short = Spikes
+marking-LizardChestSpinesShortWithoutTail = Lizard, chest, spines (Short, without tail)
+marking-LizardChestSpinesShortWithoutTail-spines_short = Spikes
+
+marking-LizardChestSpinesShortMembrane = Lizard, chest, spines (Short, Membrane)
+marking-LizardChestSpinesShortMembrane-spines_short = Spikes
+marking-LizardChestSpinesShortMembraneWithoutTail = Lizard, chest, spines (Short, Membrane, without tail)
+marking-LizardChestSpinesShortMembraneWithoutTail-spines_short = Spikes

Goob-Station/Goob-Station

Resources/Locale/en-US/_white/markings/reptilian.ftl
@@ -1,0 +1,74 @@
+# HEAD
+marking-LizardHeadGuilmon= Lizard, guilmon
+marking-LizardHeadGuilmon-guilmon = Main
+
+marking-LizardShoutEasternDragon= Lizard, eastern dragon
+marking-LizardShoutEasternDragon-eastern_dragon = Outer
+marking-LizardShoutEasternDragon-eastern_dragon_overlay = Inner
+
+marking-LizardShoutEasternDragonUsiki= Lizard, eastern dragon (Mustache)
+marking-LizardShoutEasternDragonUsiki-eastern_dragon = Outer
+marking-LizardShoutEasternDragonUsiki-eastern_dragon_overlay = Inner
+marking-LizardShoutEasternDragonUsiki-eastern_dragon_usiki = Tentacle
+
+marking-LizardHeadCobraHood = Lizard, hood with ears (Cobra)
+marking-LizardHeadCobraHood-cobra_hood_with_ears = Outer
+marking-LizardHeadCobraHood-cobra_hood_with_ears_overlay = Inner
+
+marking-LizardHornsCurled-horns_curled_l = Left horn
+marking-LizardHornsCurled-horns_curled_r = Right horn
+
+marking-LizardHornsRam-horns_ram_l = Left horn
+marking-LizardHornsRam-horns_ram_r = Right horn
+
+marking-LizardHornsShort-horns_short_l = Left horn
+marking-LizardHornsShort-horns_short_r = Right horn
+
+marking-LizardHornsSimple-horns_simple_l = Left horn
+marking-LizardHornsSimple-horns_simple_r = Right horn
+
+marking-LizardHornsDouble-horns_double_l = Left horns
+marking-LizardHornsDouble-horns_double_r = Right horns
+
+marking-LizardHornsArgali-horns_argali_l = Left horn
+marking-LizardHornsArgali-horns_argali_r = Right horn
+
+marking-LizardHornsAyrshire-horns_ayrshire_l = Left Horn
+marking-LizardHornsAyrshire-horns_ayrshire_r = Right Horn
+
+marking-LizardHornsMyrsore-horns_myrsore_l = Left horn
+marking-LizardHornsMyrsore-horns_myrsore_r = Right horn
+
+marking-LizardHornsBighorn-horns_bighorn_l = Left horn
+marking-LizardHornsBighorn-horns_bighorn_r = Right horn
+
+# TAILS
+marking-SnakeLargeTail = Lizard, large snake tail
+marking-SnakeLargeTail-tail_snakelarge = Inner
+marking-SnakeLargeTail-tail_snakelarge_overlay = Outer
+
+# SPINES
+marking-LizardChestSpinesAquatic = Lizard, chest, spines (Aquatic)
+marking-LizardChestSpinesAquatic-spines_aquatic = Spikes
+marking-LizardChestSpinesAquaticWithoutTail = Lizard, chest, spines (Aquatic, without tail)
+marking-LizardChestSpinesAquaticWithoutTail-spines_aquatic = Spikes
+
+marking-LizardChestSpinesLong = Lizard, chest, spines (Long)
+marking-LizardChestSpinesLong-spines_long = Spikes
+marking-LizardChestSpinesLongWithoutTail = Lizard, chest, spines (Long, without tail)
+marking-LizardChestSpinesLongWithoutTail-spines_long = Spikes
+
+marking-LizardChestSpinesLongMembrane = Lizard, chest, spines (Long, Membrane)
+marking-LizardChestSpinesLongMembrane-spines_longmembrane = spines
+marking-LizardChestSpinesLongMembraneWithoutTail = Lizard, chest, spines (Long, Membrane, without tail)
+marking-LizardChestSpinesLongMembraneWithoutTail-spines_longmembrane = spines
+
+marking-LizardChestSpinesShort = Lizard, chest, spines (Short)
+marking-LizardChestSpinesShort-spines_short = Spikes
+marking-LizardChestSpinesShortWithoutTail = Lizard, chest, spines (Short, without tail)
+marking-LizardChestSpinesShortWithoutTail-spines_short = Spikes
+
+marking-LizardChestSpinesShortMembrane = Lizard, chest, spines (Short, Membrane)
+marking-LizardChestSpinesShortMembrane-spines_short = Spikes
+marking-LizardChestSpinesShortMembraneWithoutTail = Lizard, chest, spines (Short, Membrane, without tail)
+marking-LizardChestSpinesShortMembraneWithoutTail-spines_short = Spikes