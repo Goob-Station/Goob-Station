ba557baee56126271c34d07ff88cb5a8469e3202
August Eymann <august.eymann@gmail.com>
2025-01-16 17:19:13 -0800

    started lockers oh god i dont know what im doing now

---
+++ Content.Shared/_Goobstation/PrisonerId/BrigLockerComponent.cs
@@ -0,0 +1,10 @@
+namespace Content.Shared._Goobstation.PrisonerId;
+
+/// <summary>
+/// This is used for...
+/// </summary>
+[RegisterComponent]
+public sealed partial class BrigLockerComponent : Component
+{
+    
+}
---
+++ Content.Shared/_Goobstation/PrisonerId/BrigLockerSystem.cs
@@ -0,0 +1,13 @@
+namespace Content.Shared._Goobstation.PrisonerId;
+
+/// <summary>
+/// This handles...
+/// </summary>
+public sealed class BrigLockerSystem : EntitySystem
+{
+    /// <inheritdoc/>
+    public override void Initialize()
+    {
+        
+    }
+}
