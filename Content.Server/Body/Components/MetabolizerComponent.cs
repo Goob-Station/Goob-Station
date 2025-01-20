116f871321198b2a2febbf29a58be57dcb2c174e
SX_7 <sn1.test.preria.2002@gmail.com>
2025-01-20 16:41:27 +0100

    Split processing of drugs 50/50, add metabolismscalemodifier

--- Content.Server/Body/Components/MetabolizerComponent.cs
+++ Content.Server/Body/Components/MetabolizerComponent.cs
@@ -81,5 +81,10 @@ namespace Content.Server.Body.Components
 
         [DataField("rateModifier")]
         public FixedPoint2 MetabolismRateModifier = 1.0;
+
+        // Preserves original behaviour, while allowing for optional scaling
+        // that's not tied to metabolization rate
+        [DataField("scaleModifier")]
+        public FixedPoint2 MetabolismScaleModifier = 1.0;
     }
 }
