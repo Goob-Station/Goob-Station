daf3fe00b511cf57a798ffa962fbfaa0329b0240
Ilya246 <ilyukarno@gmail.com>
2025-01-23 15:11:33 +0400

    do

--- Content.Server/Atmos/EntitySystems/GasTankSystem.cs
+++ Content.Server/Atmos/EntitySystems/GasTankSystem.cs
@@ -343,7 +343,7 @@ namespace Content.Server.Atmos.EntitySystems
 
                 // Let's cap the explosion, yeah?
                 // !1984
-                range = Math.Min(Math.Min(range, GasTankComponent.MaxExplosionRange), _maxExplosionRange);
+                range = Math.Min(Math.Min(range, component.MaxExplosionRange ?? 1f/0f), _maxExplosionRange); // Goobstation - Fix tank explosion cap shitcode
 
                 _explosions.TriggerExplosive(owner, radius: range);
 
