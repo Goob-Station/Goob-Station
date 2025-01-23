55f51628866ffbf11106b348b1a33b962410e5a6
Ignaz Kraft <ignaz.k@live.de>
2025-01-23 20:32:07 +0100

    test3

--- Content.Server/Medical/Components/HealthAnalyzerComponent.cs
+++ Content.Server/Medical/Components/HealthAnalyzerComponent.cs
@@ -1,4 +1,4 @@
-using Robust.Shared.Audio;
+using Content.Server.AbstractAnalyzer;
 using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom;
 
 namespace Content.Server.Medical.Components;
