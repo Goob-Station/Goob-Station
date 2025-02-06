8783d221e1f1ba75a1adb6346eefd6d0e37f8f37
Khundo <141230574+korguzenok@users.noreply.github.com>
2025-02-06 10:37:55 +0500

    Пацифизм и годмод после раунда (#57)
    * Peaceful round end (#761)
    
    * boom
    
    ---------
    
    Co-authored-by: Morb <14136326+Morb0@users.noreply.github.com>

---
+++ Content.Server/_Reserve/PeacefulRoundEnd/PeacefulRoundEndSystem.cs
@@ -0,0 +1,34 @@
+﻿using Content.Server.GameTicking;
+using Content.Shared.CombatMode.Pacification;
+using Content.Shared._Reserve.CCCVars;
+using Robust.Server.Player;
+using Robust.Shared.Configuration;
+using Content.Shared.Damage.Components;
+
+namespace Content.Server._Reserve.PeacefulRoundEnd;
+
+public sealed class PeacefulRoundEndSystem : EntitySystem
+{
+    [Dependency] private readonly IConfigurationManager _cfg = default!;
+    [Dependency] private readonly IPlayerManager _playerManager = default!;
+    
+    private bool _isEnabled = false;
+    
+    public override void Initialize()
+    {
+        base.Initialize();
+        _cfg.OnValueChanged(CCCVars.PeacefulRoundEnd, v => _isEnabled = v, true);
+        SubscribeLocalEvent<RoundEndTextAppendEvent>(OnRoundEnded);
+    }
+
+    private void OnRoundEnded(RoundEndTextAppendEvent ev)
+    {
+        if (!_isEnabled) return;
+        foreach (var session in _playerManager.Sessions)
+        {
+            if (!session.AttachedEntity.HasValue) continue;
+            EnsureComp<PacifiedComponent>(session.AttachedEntity.Value);
+            EnsureComp<GodmodeComponent>(session.AttachedEntity.Value);
+        }
+    }
+}
