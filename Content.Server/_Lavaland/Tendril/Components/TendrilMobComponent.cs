b3a0b662efb845e06cf34668abf3fc5c61eb7f8b
Rouden <149893554+Roudenn@users.noreply.github.com>
2025-02-08 16:43:24 +0300

    RUSSIAN COMMENATRY DETECTED, STARTING THE SELF-DESTRUCT MECHANISM!!!!!!!!
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- Content.Server/_Lavaland/Tendril/Components/TendrilMobComponent.cs
+++ Content.Server/_Lavaland/Tendril/Components/TendrilMobComponent.cs
@@ -1,7 +1,7 @@
 namespace Content.Server._Lavaland.Tendril.Components;
 
 /// <summary>
-/// Моб, созданный тендрилом. При смерти удаляется из списка его спавнов
+/// A mob created by a tendril. Upon death, it is removed from its spawn list
 /// </summary>
 [RegisterComponent]
 public sealed partial class TendrilMobComponent : Component
