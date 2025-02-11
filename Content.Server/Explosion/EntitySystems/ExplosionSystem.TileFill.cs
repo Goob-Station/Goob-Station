b350ffa38321f7af7268126f8bd66dfadf8ae6ac
TemporalOroboros <TemporalOroboros@gmail.com>
2025-02-10 19:16:20 -0800

    Purges uses of TransformComponent.WorldRotation (#34946)

--- Content.Server/Explosion/EntitySystems/ExplosionSystem.TileFill.cs
+++ Content.Server/Explosion/EntitySystems/ExplosionSystem.TileFill.cs
@@ -89,7 +89,7 @@ public sealed partial class ExplosionSystem
         {
             var xform = Transform(Comp<MapGridComponent>(referenceGrid.Value).Owner);
             spaceMatrix = xform.WorldMatrix;
-            spaceAngle = xform.WorldRotation;
+            spaceAngle = _transformSystem.GetWorldRotation(xform);
         }
 
         // is the explosion starting on a grid?
