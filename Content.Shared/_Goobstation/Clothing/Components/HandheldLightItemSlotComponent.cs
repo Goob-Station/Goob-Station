950bb7186fbf284925d29ba4c55e319265d8d2a0
starwolf <ramses@starwolves.io>
2025-02-13 22:54:33 +0100

    Implement desired changes. Add state.

--- Content.Shared/_Goobstation/Clothing/Components/HandheldLightItemSlotComponent.cs
+++ Content.Shared/_Goobstation/Clothing/Components/HandheldLightItemSlotComponent.cs
@@ -1,5 +1,6 @@
-
+using Content.Shared.Light.Components;
 using Robust.Shared.GameStates;
+using Robust.Shared.Serialization;
 
 namespace Content.Shared.Clothing;
 
@@ -9,8 +10,30 @@ namespace Content.Shared.Clothing;
 public sealed partial class HandheldLightItemSlotComponent : Component
 {
     [DataField, AutoNetworkedField]
-    public EntityUid? LightEntity;
-
+    public EntityUid? EntityActionReference;
+    // The HandHeldLight entity that is currently attached to the slot if any.
+    [DataField, AutoNetworkedField]
+    public EntityUid? AttachedLight;
     [DataField, AutoNetworkedField]
     public EntityUid? Wearer;
+    [DataField, AutoNetworkedField]
+    public string SlotName = "light";
+}
+
+[Serializable, NetSerializable]
+public sealed class HandheldLightItemSlotComponentState : ComponentState
+{
+    // If null no light is attached.
+    public LightState? State;
+
+    public HandheldLightItemSlotComponentState(LightState? lightState)
+    {
+        State = lightState;
+    }
+}
+[Serializable, NetSerializable]
+public sealed class LightState
+{
+    public bool Activated;
+    public HandheldLightPowerStates PowerState;
 }
--- Content.Shared/_Goobstation/Clothing/Systems/HandheldLightItemSlotSystem.cs
+++ Content.Shared/_Goobstation/Clothing/Systems/HandheldLightItemSlotSystem.cs
@@ -4,6 +4,7 @@ using Content.Shared.Light.Components;
 using Content.Shared.Containers.ItemSlots;
 using Content.Shared.Inventory.Events;
 using Robust.Shared.Containers;
+using Robust.Shared.GameStates;
 
 namespace Content.Shared._Goobstation.Clothing.Systems
 {
@@ -12,7 +13,7 @@ namespace Content.Shared._Goobstation.Clothing.Systems
     {
         [Dependency] private readonly SharedActionsSystem _actionsSystem = default!;
         [Dependency] private readonly ItemSlotsSystem _itemSlotsSystem = default!;
-
+        [Dependency] private readonly SharedAppearanceSystem _appearance = default!;
 
         public override void Initialize()
         {
@@ -23,46 +24,66 @@ namespace Content.Shared._Goobstation.Clothing.Systems
             SubscribeLocalEvent<HandheldLightItemSlotComponent, GotUnequippedEvent>(OnGotUnequipped);
             SubscribeLocalEvent<HandheldLightItemSlotComponent, EntRemovedFromContainerMessage>(OnLightRemoved);
             SubscribeLocalEvent<HandheldLightItemSlotComponent, EntInsertedIntoContainerMessage>(OnLightInserted);
-
+            SubscribeLocalEvent<HandheldLightItemSlotComponent, ComponentGetState>(OnGetState);
         }
+
+        private void OnGetState(EntityUid uid, HandheldLightItemSlotComponent component, ref ComponentGetState args)
+        {
+            if (component.AttachedLight == null)
+            {
+                args.State = new HandheldLightItemSlotComponentState(null);
+                return;
+            }
+            if (!_appearance.TryGetData(uid, HandheldLightVisuals.Power, out HandheldLightPowerStates value)
+            || !TryComp(uid, out HandheldLightComponent? lightComponent))
+                return;
+
+            args.State = new HandheldLightItemSlotComponentState(new LightState
+            {
+                Activated = lightComponent.Activated,
+                PowerState = value
+            });
+        }
+
         private void OnLightRemoved(EntityUid uid, HandheldLightItemSlotComponent component, EntRemovedFromContainerMessage args)
         {
+            if (args.Container.ID != component.SlotName || !TryComp(args.Entity, out HandheldLightComponent? lightComponent))
+                return;
+
             // If worn on head by the action taker then remove the securityhelmet.SecliteEntity action via RemoveAction.
-            if (component.Wearer != null && component.LightEntity != null)
-            {
-                _actionsSystem.RemoveAction(component.Wearer.Value, component.LightEntity);
-            }
+            if (component.Wearer != null && component.EntityActionReference != null)
+                _actionsSystem.RemoveAction(component.Wearer.Value, component.EntityActionReference);
+
+            component.AttachedLight = null;
         }
         private void OnLightInserted(EntityUid uid, HandheldLightItemSlotComponent component, EntInsertedIntoContainerMessage args)
         {
+            if (args.Container.ID != component.SlotName || !TryComp(args.Entity, out HandheldLightComponent? lightComponent))
+                return;
+
             // If worn on head by the action taker add the lightcomponent.toggleaction via addAction.
-            if (component.Wearer != null && TryComp(args.Entity, out HandheldLightComponent? lightComponent) && component.LightEntity != null)
-            {
-                _actionsSystem.AddAction(component.Wearer.Value, ref component.LightEntity, lightComponent.ToggleAction, args.Entity);
-            }
+            if (component.Wearer != null)
+                _actionsSystem.AddAction(component.Wearer.Value, ref component.EntityActionReference, lightComponent.ToggleAction, args.Entity);
+
+            component.AttachedLight = args.Entity;
         }
         private void OnGotEquipped(EntityUid uid, HandheldLightItemSlotComponent component, GotEquippedEvent args)
         {
-            var lightOption = _itemSlotsSystem.GetItemOrNull(uid, "light");
+            var lightOption = _itemSlotsSystem.GetItemOrNull(uid, component.SlotName);
             component.Wearer = args.Equipee;
 
-            if (lightOption != null &&
-              TryComp(lightOption, out HandheldLightComponent? lightComponent))
-            {
-                _actionsSystem.AddAction(args.Equipee, ref component.LightEntity, lightComponent.ToggleAction, lightOption.Value);
-            }
+            if (lightOption != null && TryComp(lightOption, out HandheldLightComponent? lightComponent))
+                _actionsSystem.AddAction(args.Equipee, ref component.EntityActionReference, lightComponent.ToggleAction, lightOption.Value);
         }
-
         private void OnGotUnequipped(EntityUid uid, HandheldLightItemSlotComponent component, GotUnequippedEvent args)
         {
-            var lightOption = _itemSlotsSystem.GetItemOrNull(uid, "light");
+            var lightOption = _itemSlotsSystem.GetItemOrNull(uid, component.SlotName);
             component.Wearer = null;
 
-            if (lightOption == null || component.LightEntity == null)
+            if (lightOption == null || component.EntityActionReference == null)
                 return;
 
-            _actionsSystem.RemoveAction(args.Equipee, component.LightEntity);
+            _actionsSystem.RemoveAction(args.Equipee, component.EntityActionReference);
         }
-
     }
 }
