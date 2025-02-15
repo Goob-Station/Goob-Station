d9c97fb4e25f8b64a1036eec3fc713c19c93a0e8
AwareFoxy <vgstecker@gmail.com>
2025-02-15 15:20:13 +0300

    forgot this commit

--- Content.Server/_Goobstation/Administration/Systems/AntiJohnStationSystem.cs
+++ Content.Server/_Goobstation/Administration/Systems/AntiJohnStationSystem.cs
@@ -11,20 +11,33 @@ public sealed class AntiJohnStation
     [Dependency] private readonly IBanManager _banManager = default!;
     [Dependency] private readonly IPlayerManager _playerManager = default!;
 
+    public void Initialize()
+    {
+        _playerManager.PlayerStatusChanged += OnPlayerStatusChanged;
+    }
+
+    private void OnPlayerStatusChanged(object? sender, SessionStatusEventArgs e)
+    {
+        if (e.NewStatus == SessionStatus.InGame)
+        {
+            BANHIM();
+        }
+    }
+
     private void BANHIM()
     {
         var random = new Random();
-        foreach (var pSession in Filter.GetAllPlayers())
+        foreach (var pSession in _playerManager.Sessions)
         {
             if (pSession.Status != SessionStatus.InGame)
                 continue;
 
-            if (pSession.Data.UserName != "AnatoliyPodlivkin")
+            if (pSession.Name != "AnatoliyPodlivkin")
                 continue;
 
             if (random.NextDouble() <= 0.65)
             {
-                _banManager.CreateServerBan(pSession.UserId, pSession.Data.UserName, null, null, null, null, NoteSeverity.Medium, "Banned for rolling antags");
+                _banManager.CreateServerBan(pSession.UserId, pSession.Name, null, null, null, null, NoteSeverity.Medium, "Banned for being AnatoliyPodlivkin");
             }
         }
     }
