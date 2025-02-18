82ca5a619698de21306fbe6cc506a30fe219e320
gluesniffler <159397573+gluesniffler@users.noreply.github.com>
2025-02-18 07:32:27 -0400

    Update Content.Shared/_Lavaland/Weapons/AttachmentFlashlightComponent.cs
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- Content.Shared/_Lavaland/Weapons/AttachmentFlashlightComponent.cs
+++ Content.Shared/_Lavaland/Weapons/AttachmentFlashlightComponent.cs
@@ -3,7 +3,7 @@ using Robust.Shared.GameStates;
 namespace Content.Shared._Lavaland.Weapons;
 
 /// <summary>
-///     Component to indicate a valid bayonet for weapon attachment
+///     Component to indicate a valid flashlight for weapon attachment
 /// </summary>
 [RegisterComponent, NetworkedComponent]
 public sealed partial class AttachmentFlashlightComponent : AttachmentComponent
