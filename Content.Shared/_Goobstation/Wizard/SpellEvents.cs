b177e6f986c868794b32bc13d3d3e378042d5cd6
Aviu00 <aviu00@protonmail.com>
2025-02-18 11:56:51 +0300

    Unnerf the nerfs.

--- Content.Shared/_Goobstation/Wizard/SpellEvents.cs
+++ Content.Shared/_Goobstation/Wizard/SpellEvents.cs
@@ -446,7 +446,7 @@ public sealed partial class SummonSimiansEvent : InstantActionEvent, ISpeakSpell
     public float Range = 1f;
 
     [DataField]
-    public int Amount = 3;
+    public int Amount = 4;
 
     [DataField]
     public Angle SpawnAngle = Angle.FromDegrees(40);
