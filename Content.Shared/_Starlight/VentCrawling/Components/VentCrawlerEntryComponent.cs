062484178c1d15ff562b91e9fa6691f5091cda0d
Fishbait <Fishbait@git.ml>
2025-02-19 13:43:07 +0100

    john

--- Content.Shared/_Starlight/VentCrawling/Components/VentCrawlerEntryComponent.cs
+++ Content.Shared/_Starlight/VentCrawling/Components/VentCrawlerEntryComponent.cs
@@ -3,5 +3,6 @@ namespace Content.Shared._Starlight.VentCrawling.Components;
 [RegisterComponent]
 public sealed partial class VentCrawlerEntryComponent : Component
 {
-    public const string HolderPrototypeId = "VentCrawlerHolder";
+    [DataField]
+    public string HolderPrototypeId = "VentCrawlerHolder";
 }
