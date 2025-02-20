54300f037e07489b87feffdbb69c5aa969fbf5a8
AwareFoxy <135021509+AwareFoxy@users.noreply.github.com>
2025-02-21 00:24:23 +0300

    Update publish_github_artifact.py

--- Tools/publish_github_artifact.py
+++ Tools/publish_github_artifact.py
@@ -14,8 +14,8 @@ VERSION = os.environ['GITHUB_SHA']
 # CONFIGURATION PARAMETERS
 # Forks should change these to publish to their own infrastructure.
 #
-ROBUST_CDN_URL = "https://cdn.simplestation.org/"
-FORK_ID = "goobstation-lrp"
+ROBUST_CDN_URL = "https://penis.reserve-station.space/"
+FORK_ID = "reserve"
 
 def main():
     print("Fetching artifact URL from API...")
