fdcdc16e8590902100f93434aa1f88607c381d1a
LuciferMkshelter <stepanteliatnik2022@gmail.com>
2025-02-21 17:11:56 +0200

    BUI cleanup

--- Content.Client/Labels/UI/HandRenamerBoundUserInterface.cs
+++ Content.Client/Labels/UI/HandRenamerBoundUserInterface.cs
@@ -1,49 +1,22 @@
 using Content.Client.Labels.RenameSystem;
 using Content.Shared.HandRenamer;
-using Robust.Client.UserInterface;
 
 namespace Content.Client.Labels.UI
 {
-    public sealed class HandRenamerBoundUserInterface : BoundUserInterface
+    public sealed class HandRenamerBoundUserInterface : HandLabelerBoundUserInterface
     {
-        [Dependency] private readonly IEntityManager _entManager = default!;
-
-        [ViewVariables]
-        private HandRenamerWindow? _window;
-
-        public HandRenamerBoundUserInterface(EntityUid owner, Enum uikey) : base(owner, uikey)
+        public HandRenamerBoundUserInterface(EntityUid owner, Enum uiKey) : base(owner, uiKey)
         {
-            IoCManager.InjectDependencies(this);
+
         }
 
-        protected override void Open()
-        {
-            base.Open();
-            _window = this.CreateWindow<HandRenamerWindow>();
-            if (_entManager.TryGetComponent(Owner, out HandRenamerComponent? renamer))
-            {
-                _window.SetMaxNameLength(renamer!.MaxNameChars);
-            }
-
-            _window.OnNameChanged += OnNameChanged;
-            Reload();
-        }
-
-        private void OnNameChanged(string newName)
+        protected override void OnLabelChanged(string newLabel)
         {
             if (_entManager.TryGetComponent(Owner, out HandRenamerComponent? component) &&
-                component.AssignedName.Equals(newName))
+                component.AssignedName.Equals(newLabel))
                 return;
 
-            SendPredictedMessage(new HandRenamerNameChangedMessage(newName));
-        }
-
-        public void Reload()
-        {
-            if (_window == null || !_entManager.TryGetComponent(Owner, out HandRenamerComponent? component))
-                return;
-
-            _window.SetCurrentName(component.AssignedName);
+            SendPredictedMessage(new HandRenamerNameChangedMessage(newLabel));
         }
     }
 }
