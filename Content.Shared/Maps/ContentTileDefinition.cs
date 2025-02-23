81c8c1d673b3d23a43c9efb7e452836f7713b052
metalgearsloth <31366439+metalgearsloth@users.noreply.github.com>
2025-02-23 18:23:00 +1100

    Roof data rework (#35388)

--- Content.Shared/Maps/ContentTileDefinition.cs
+++ Content.Shared/Maps/ContentTileDefinition.cs
@@ -133,11 +133,4 @@ namespace Content.Shared.Maps
         [DataField]
         public float TileRipResistance = 125f;
     }
-
-    [Flags]
-    public enum TileFlag : byte
-    {
-        None = 0,
-        Roof = 1 << 0,
-    }
 }
