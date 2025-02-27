bcd0fbd857ceff9698947a9c759822a8964383fe
Ilya246 <ilyukarno@gmail.com>
2025-02-27 15:25:01 +0400

    purge unrelated changes

--- Content.Client/Chat/Managers/IChatManager.cs
+++ Content.Client/Chat/Managers/IChatManager.cs
@@ -6,13 +6,12 @@ namespace Content.Client.Chat.Managers
     {
         void Initialize();
 
+        public void SendMessage(string text, ChatSelectChannel channel);
+
         /// <summary>
         ///     Will refresh perms.
         /// </summary>
         event Action PermissionsUpdated;
-
-        public void SendMessage(string text, ChatSelectChannel channel);
-
         public void UpdatePermissions();
     }
 }
--- Content.Shared/Body/Prototypes/BodyPrototypeSerializer.cs
+++ Content.Shared/Body/Prototypes/BodyPrototypeSerializer.cs
@@ -51,9 +51,6 @@ public sealed class BodyPrototypeSerializer : ITypeReader<BodyPrototype, Mapping
                     nodes.Add(new ErrorNode(value, $"Value is not a value data node"));
                     continue;
                 }
-                
-                if (organ.Value == "null" || organ.Value == null)
-                    continue;
 
                 if (!prototypes.TryIndex(organ.Value, out EntityPrototype? organPrototype))
                 {
