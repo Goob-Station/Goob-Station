72426e414770a4531f7a59b4015a710b3a7be7e9
gluesniffler <linebarrelerenthusiast@gmail.com>
2025-03-01 13:41:17 -0400

    IT WORKS

--- Content.Shared/StepTrigger/Components/StepTriggerComponent.cs
+++ Content.Shared/StepTrigger/Components/StepTriggerComponent.cs
@@ -38,7 +38,7 @@ public sealed partial class StepTriggerComponent : Component
     ///     Entities will only be triggered if their speed exceeds this limit.
     /// </summary>
     [DataField, AutoNetworkedField]
-    public float RequiredTriggeredSpeed = 3.5f;
+    public float RequiredTriggeredSpeed = 5.0f; // Tile Movement Change
 
     /// <summary>
     ///     If any entities occupy the blacklist on the same tile then steptrigger won't work.
