66700e0d8d1ca3c7c80c39872d4682e419703515
Fishbait <Fishbait@git.ml>
2025-03-04 12:03:25 +0100

    more cleanup

--- Content.Shared/_Goobstation/Bingle/BingleComponent.cs
+++ Content.Shared/_Goobstation/Bingle/BingleComponent.cs
@@ -11,8 +11,6 @@ public sealed partial class BingleComponent : Component
     [DataField]
     public bool Upgraded = false;
     [DataField]
-    public DamageSpecifier UpgradeDamage = default!;
-    [DataField]
     public bool Prime = false;
     [DataField]
     public EntityUid? MyPit;
