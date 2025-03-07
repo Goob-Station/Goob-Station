18196dbdd424c78c4dd45948b8646c616fcd34b9
SX_7 <sn1.test.preria.2002@gmail.com>
2025-03-07 18:32:31 +0100

    Dorms, maints, and buttons

--- Content.Server/_Goobstation/PlayerListener/DormNotifierMarkerComponent.cs
+++ Content.Server/_Goobstation/PlayerListener/DormNotifierMarkerComponent.cs
@@ -1,14 +1,14 @@
-﻿namespace Content.Server._Goobstation.PlayerListener;
+namespace Content.Server._Goobstation.PlayerListener;
 
 [RegisterComponent]
 public sealed partial class DormNotifierMarkerComponent : Component
 {
-    [ViewVariables(VVAccess.ReadWrite)]
+    [DataField, ViewVariables(VVAccess.ReadWrite)]
     public string Name = "";
 
     /// <summary>
     /// Tile range to check for players
     /// </summary>
-    [ViewVariables(VVAccess.ReadWrite)]
+    [DataField, ViewVariables(VVAccess.ReadWrite)]
     public float ProximityRadius = 1;
 }
