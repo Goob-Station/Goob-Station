d4d32db5799f7dfc2111274dc03668458581dab3
SX-7 <92227810+SX-7@users.noreply.github.com>
2025-03-08 08:23:14 +0100

    revert proofing

--- Content.Server/_Goobstation/PlayerListener/DormNotifierMarkerComponent.cs
+++ Content.Server/_Goobstation/PlayerListener/DormNotifierMarkerComponent.cs
@@ -3,14 +3,12 @@ namespace Content.Server._Goobstation.PlayerListener;
 [RegisterComponent]
 public sealed partial class DormNotifierMarkerComponent : Component
 {
-    [ViewVariables(VVAccess.ReadWrite)]
-    [DataField]
+    [DataField, ViewVariables(VVAccess.ReadWrite)]
     public string Name = "";
 
     /// <summary>
     /// Tile range to check for players
     /// </summary>
-    [ViewVariables(VVAccess.ReadWrite)]
-    [DataField]
+    [DataField, ViewVariables(VVAccess.ReadWrite)]
     public float ProximityRadius = 1;
 }
