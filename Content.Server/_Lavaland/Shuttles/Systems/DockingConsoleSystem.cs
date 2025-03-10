1da6c2ab166d4c7f882c7d6fa91db03d2aba9a00
AstroDogeDX <48888500+AstroDogeDX@users.noreply.github.com>
2025-03-10 18:17:58 +0000

    Point at the salv shuttle not the lavaland shuttle, in case for some reason it didn't load

--- Content.Server/_Lavaland/Shuttles/Systems/DockingConsoleSystem.cs
+++ Content.Server/_Lavaland/Shuttles/Systems/DockingConsoleSystem.cs
@@ -155,7 +155,7 @@ public sealed class DockingConsoleSystem : SharedDockingConsoleSystem
         _shuttle.FTLToDock(shuttle, Comp<ShuttleComponent>(shuttle), grid, priorityTag: docking.DockTag);
     }
 
-    private readonly ResPath _miningShuttlePath = new("/Maps/_Lavaland/mining.yml");
+    private readonly ResPath _miningShuttlePath = new("/Maps/Shuttles/mining.yml");
 
     /// <summary>
     /// Load a new mining shuttle if it still doesn't exist
