985dcb47071e065b7ac14c36d30890a015d904ed
August Eymann <august.eymann@gmail.com>
2025-03-10 15:43:17 -0700

    fuck this

---
+++ Content.Shared/_Goobstation/Camera/SharedCameraSystem.cs
@@ -0,0 +1,13 @@
+namespace Content.Shared._Goobstation.Camera;
+
+/// <summary>
+/// This handles...
+/// </summary>
+public sealed class SharedCameraSystem : EntitySystem
+{
+    /// <inheritdoc/>
+    public override void Initialize()
+    {
+        
+    }
+}
