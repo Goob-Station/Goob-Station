cc7af300efb079821d765c155925c432c7e54123
Conchelle <mary@thughunt.ing>
2025-03-11 22:58:30 +0200

    Congratulate user @aineias1 on sniping #2000 by swapping his movement inputs.

---
+++ Content.Shared/_Goobstation/MisandryBox/Smites/ToggleableSmiteSystem.cs
@@ -0,0 +1,24 @@
+﻿namespace Content.Shared._Goobstation.MisandryBox.Smites;
+
+public abstract class ToggleableSmiteSystem<T> : EntitySystem where T : Component
+{
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<T, ComponentInit>(OnInit);
+        SubscribeLocalEvent<T, ComponentShutdown>(OnShutdown);
+    }
+
+    private void OnInit(Entity<T> ent, ref ComponentInit args)
+    {
+        Set(ent.Owner);
+    }
+
+    private void OnShutdown(Entity<T> ent, ref ComponentShutdown args)
+    {
+        Set(ent.Owner);
+    }
+
+    public abstract void Set(EntityUid owner);
+}
