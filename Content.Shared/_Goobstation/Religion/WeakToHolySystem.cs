d3b7009c9b47b722763efca96c9937fefbced49e
SolsticeOfTheWinter <solsticeofthewinter@gmail.com>
2025-03-10 23:59:40 -1000

    Yeah uh hold that
    Signed-off-by: SolsticeOfTheWinter <solsticeofthewinter@gmail.com>

--- Content.Shared/_Goobstation/Religion/WeakToHolySystem.cs
+++ Content.Shared/_Goobstation/Religion/WeakToHolySystem.cs
@@ -1,29 +1,22 @@
-using Content.Shared.Popups;
-using Content.Shared.Mobs;
-using Robust.Shared.GameStates;
-using Robust.Shared.Prototypes;
-using Content.Shared.Damage.Prototypes;
-using System.Linq;
 using Content.Shared.Damage;
-using Robust.Shared.Audio.Systems;
-using Content.Shared.StepTrigger.Systems;
-using Content.Shared.Heretic;
-using Robust.Shared.Utility;
 using Content.Shared.Damage.Components;
-using Robust.Shared.Network;
-using Robust.Shared.Physics.Components;
-using Robust.Shared.Physics.Events;
 using Content.Shared.FixedPoint;
+using Content.Shared.Heretic;
+using Content.Shared.Popups;
+using Robust.Shared.Audio.Systems;
+using Robust.Shared.Network;
+using Robust.Shared.Physics.Events;
+using Robust.Shared.Prototypes;
 
 namespace Content.Shared._Goobstation.Religion;
 
 public sealed partial class WeakToHolySystem : EntitySystem
 {
-    [Dependency] private readonly INetManager _netManager = default!;
-    [Dependency] private readonly IPrototypeManager _prototypeManager = default!;
     [Dependency] private readonly SharedAudioSystem _audio = default!;
     [Dependency] private readonly DamageableSystem _damageableSystem = default!;
+    [Dependency] private readonly INetManager _netManager = default!;
     [Dependency] private readonly SharedPopupSystem _popupSystem = default!;
+    [Dependency] private readonly IPrototypeManager _prototypeManager = default!;
 
     public override void Initialize()
     {
@@ -41,8 +34,6 @@ public sealed partial class WeakToHolySystem : EntitySystem
         {
             _damageableSystem.ChangeDamageContainer(ent, "BiologicalMetaphysical");
         }
-
-
     }
 
 
@@ -51,16 +42,17 @@ public sealed partial class WeakToHolySystem : EntitySystem
     {
         var _heretic = EnsureComp<PassiveDamageComponent>(args.OtherEntity);
 
-        if (!HasComp<WeakToHolyComponent>(args.OtherEntity) && _heretic.Damage.DamageDict.TryGetValue("Holy", out var holy)) {
+        if (!HasComp<WeakToHolyComponent>(args.OtherEntity) &&
+            _heretic.Damage.DamageDict.TryGetValue("Holy", out var holy))
+        {
             return;
         }
 
         var oldValue = _heretic.DamageCap;
 
         _heretic.Damage.DamageDict.TryAdd("Holy", -10);
-        _heretic.DamageCap = new FixedPoint2(0); //why you no work
+        _heretic.DamageCap = new FixedPoint2(); //why you no work
         DirtyEntity(args.OtherEntity);
-
     }
 
     private void OnCollideEnd(EntityUid uid, HereticRitualRuneComponent component, ref EndCollideEvent args)
