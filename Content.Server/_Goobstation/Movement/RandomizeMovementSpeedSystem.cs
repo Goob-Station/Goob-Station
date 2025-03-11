74b943a2f6d867a3040b7ea18a1183366824f633
SolsticeOfTheWinter <solsticeofthewinter@gmail.com>
2025-03-10 23:46:23 -1000

    What am i cooking
    Signed-off-by: SolsticeOfTheWinter <solsticeofthewinter@gmail.com>

--- Content.Server/_Goobstation/Movement/RandomizeMovementSpeedSystem.cs
+++ Content.Server/_Goobstation/Movement/RandomizeMovementSpeedSystem.cs
@@ -1,6 +1,4 @@
-using Content.Server._Goobstation.Movement;
 using Content.Shared.Hands;
-using Content.Shared.Movement.Components;
 using Content.Shared.Movement.Systems;
 using Robust.Shared.Random;
 using Robust.Shared.Timing;
@@ -9,14 +7,16 @@ using Robust.Shared.Timing;
 // Once one of the smart people of the ivory tower help me I can get this working probably.
 
 
+namespace Content.Server._Goobstation.Movement;
+
 public sealed partial class RandomizeMovementSpeedSystem : EntitySystem
 {
-    [Dependency] private readonly MovementSpeedModifierSystem _speedModifierSystem = default!;
+    private static readonly TimeSpan ExecutionInterval = TimeSpan.FromSeconds(2);
     [Dependency] private readonly IRobustRandom _random = default!;
+    [Dependency] private readonly MovementSpeedModifierSystem _speedModifierSystem = default!;
     [Dependency] private readonly IGameTiming _timing = default!;
 
     private TimeSpan _nextExecutionTime = TimeSpan.Zero;
-    private static readonly TimeSpan ExecutionInterval = TimeSpan.FromSeconds(2);
 
     public override void Initialize()
     {
@@ -27,30 +27,34 @@ public sealed partial class RandomizeMovementSpeedSystem : EntitySystem
 
     public void OnItemInHand(EntityUid uid, RandomizeMovementspeedComponent comp, EquippedHandEvent args)
     {
-        // When the item is equipped, add the component to the player.
-        if (args.User != null)
-            EnsureComp<RandomizeMovementspeedComponent>(args.User);
+        EnsureComp<RandomizeMovementspeedComponent>(args.User);
     }
 
-    public void Update(float frameTime, EntityUid uid, RandomizeMovementspeedComponent comp, EquippedHandEvent args)
+    public void Update(float frameTime,
+        EntityUid uid,
+        RandomizeMovementspeedComponent comp,
+        ref RefreshMovementSpeedModifiersEvent args)
     {
-
         base.Update(frameTime);
 
         if (_timing.CurTime < _nextExecutionTime)
             return;
 
-        var modifier = _random.NextFloat(comp.Min, comp.Max);
         _nextExecutionTime = _timing.CurTime + ExecutionInterval;
 
+        var modifier = _random.NextFloat(comp.Min, comp.Max);
+        TryModifySpeed(uid, modifier, args);
+    }
+
+    public void TryModifySpeed(EntityUid uid, float modifier, RefreshMovementSpeedModifiersEvent args)
+    {
+        args.ModifySpeed(modifier);
+        _speedModifierSystem.RefreshMovementSpeedModifiers(uid);
     }
 
     public void OnUnequipped(EntityUid uid, RandomizeMovementspeedComponent comp, UnequippedHandEvent args)
     {
-        // Remove component when the item is unequipped.
         RemComp<RandomizeMovementspeedComponent>(args.User);
-        // Set to handled to prevent fuckery.
         args.Handled = true;
     }
-
 }
