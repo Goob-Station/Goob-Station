4939c1063f8039b3bbb3237329acd7ac251af4c8
August Eymann <august.eymann@gmail.com>
2025-03-12 02:41:07 -0700

    GOOB FLAVOR

--- Content.Shared/Weapons/Melee/Components/MeleeThrowOnHitComponent.cs
+++ Content.Shared/Weapons/Melee/Components/MeleeThrowOnHitComponent.cs
@@ -1,4 +1,5 @@
 using System.Numerics;
+using Content.Shared._Goobstation.Boomerang;
 using Robust.Shared.GameStates;
 using Robust.Shared.Physics.Components;
 using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom;
@@ -10,7 +11,7 @@ namespace Content.Shared.Weapons.Melee.Components;
 /// until it hits a wall or a time limit is exhausted.
 /// </summary>
 [RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
-[Access(typeof(MeleeThrowOnHitSystem))]
+[Access(typeof(MeleeThrowOnHitSystem), typeof(BoomerangSystem))] // Goobstation Edit
 public sealed partial class MeleeThrowOnHitComponent : Component
 {
     /// <summary>
