f4c051e3c98be01ca0c0eaf9a47279e4ed50c56a
SolsticeOfTheWinter <solsticeofthewinter@gmail.com>
2025-03-13 13:09:30 -1000

    I'm like albert einstein if he was autistic.
    Signed-off-by: SolsticeOfTheWinter <solsticeofthewinter@gmail.com>

--- Content.Server/_Goobstation/Movement/RandomizeMovementSpeedComponent.cs
+++ Content.Server/_Goobstation/Movement/RandomizeMovementSpeedComponent.cs
@@ -6,14 +6,12 @@ public sealed partial class RandomizeMovementspeedComponent : Component
     /// <summary>
     /// The minimum limit of the modifier.
     /// </summary>
-
     [DataField]
     public float Min { get; set; } = 0.6f;
 
     /// <summary>
     /// The max limit of the modifier.
     /// </summary>
-
     [DataField]
     public float Max { get; set; } = 1.6f;
 
@@ -23,6 +21,9 @@ public sealed partial class RandomizeMovementspeedComponent : Component
     [DataField]
     public float CurrentModifier { get; set; } = 1f;
 
+    /// <summary>
+    /// The Uid of the entity that picked up the item.
+    /// </summary>
     [DataField]
     public EntityUid EntityUid = default!;
 
--- Content.Server/_Goobstation/Movement/RandomizeMovementSpeedSystem.cs
+++ Content.Server/_Goobstation/Movement/RandomizeMovementSpeedSystem.cs
@@ -1,4 +1,4 @@
-using System.Diagnostics;
+using Content.Server.Bible;
 using Content.Shared.Hands;
 using Content.Shared.Movement.Systems;
 using Robust.Shared.Random;
@@ -25,24 +25,31 @@ public sealed class RandomizeMovementSpeedSystem : EntitySystem
 
     private void OnGotEquippedHand(Entity<RandomizeMovementspeedComponent> ent, ref GotEquippedHandEvent args)
     {
+        // Refresh the movement speed modifiers.
         _movementSpeedModifier.RefreshMovementSpeedModifiers(args.User);
+        // Get the Uid of the entity who picked up the item.
         GetEntityUid(ent, ref args);
     }
 
     private void OnGotUnequippedHand(Entity<RandomizeMovementspeedComponent> ent, ref GotUnequippedHandEvent args)
     {
+        // Refresh the movement speed modifiers.
         _movementSpeedModifier.RefreshMovementSpeedModifiers(args.User);
+        // Reset the user Uid.
         ent.Comp.EntityUid = default!;
     }
 
     private void GetEntityUid(Entity<RandomizeMovementspeedComponent> ent, ref GotEquippedHandEvent args)
     {
+        // Set the entity Uid field of the Component equal to the entity who picked up the item.
         ent.Comp.EntityUid = args.User;
     }
 
     private float GetMovementSpeedModifiers(RandomizeMovementspeedComponent comp)
     {
+        // Generate a modifier, which is a float between the minimum and maxiumum defined by the component.
         var modifier = _random.NextFloat(comp.Min, comp.Max);
+        // Return that modifier.
         return modifier;
 
     }
@@ -50,6 +57,7 @@ public sealed class RandomizeMovementSpeedSystem : EntitySystem
     {
         base.Update(frameTime);
 
+        // Check if it's time to execute again.
         if (_timing.CurTime < _nextExecutionTime)
             return;
 
@@ -58,26 +66,32 @@ public sealed class RandomizeMovementSpeedSystem : EntitySystem
         {
             foreach (var ent in EntityQuery<RandomizeMovementspeedComponent>())
             {
+                // Check if the user is capable of wielding a nullrod.
+                if (!HasComp<BibleUserComponent>(comp.EntityUid))
+                    return;
+
+                // Generate the new modifier.
                 var modifier = GetMovementSpeedModifiers(comp);
 
+                // Set the new modifier.
                 comp.CurrentModifier = modifier;
 
+                // Call the event, and refresh the movement speed modifiers.
                 _movementSpeedModifier.RefreshMovementSpeedModifiers(comp.EntityUid);
             }
         }
+        // Set the next execution time.
         _nextExecutionTime = _timing.CurTime + ExecutionInterval;
     }
 
 
     private static void OnRefreshMovementSpeedModifiers(EntityUid uid, RandomizeMovementspeedComponent  comp, ref HeldRelayedEvent<RefreshMovementSpeedModifiersEvent> args)
     {
+        // Set the variable modifier equal to the components current modifier field.
         var modifier = comp.CurrentModifier;
+        // Modify the speed of the entity according to the modifier.
         args.Args.ModifySpeed(modifier, modifier);
     }
 
 }
-
-// My best guess is that the Base.Update() in PassiveDamageSystem.cs is somehow activating the event to refresh modifiers.
-// Whenever I try to call it manually, it doesn't fucking work.
-// I want to hang myself.
 
