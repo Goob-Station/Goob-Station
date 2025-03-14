cfff731a041c2a1ed80a0530fc52076de6baf48e
Aiden <aiden@djkraz.com>
2025-03-13 20:19:00 -0500

    Redialing System because its funny (#2040)
    * Fuck You
    
    * Fix
    
    * Rules
    
    * oops mistell
    
    * evil chasm
    
    * rephrase
    
    * remove chasm lol

---
+++ Content.Shared/_Goobstation/Redial/MsgRedial.cs
@@ -0,0 +1,24 @@
+﻿using Lidgren.Network;
+using Robust.Shared.Network;
+using Robust.Shared.Serialization;
+
+namespace Content.Shared._Goobstation.Redial;
+
+public sealed class MsgRedial : NetMessage
+{
+    public override MsgGroups MsgGroup => MsgGroups.Core;
+
+    public string Address = string.Empty;
+
+    public override void ReadFromBuffer(NetIncomingMessage buffer, IRobustSerializer serializer)
+    {
+        Address = buffer.ReadString();
+    }
+
+    public override void WriteToBuffer(NetOutgoingMessage buffer, IRobustSerializer serializer)
+    {
+        buffer.Write(Address);
+    }
+
+    public override NetDeliveryMethod DeliveryMethod => NetDeliveryMethod.ReliableOrdered;
+}
