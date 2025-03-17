29dc0439418bba1bfe326c3083da5cc7334fe0e1
Aviu00 <aviu00@protonmail.com>
2025-03-17 14:49:50 +0300

    Imagine.

--- Content.Server/_Goobstation/Heretic/Abilities/HereticAbilitySystem.Rust.cs
+++ Content.Server/_Goobstation/Heretic/Abilities/HereticAbilitySystem.Rust.cs
@@ -524,6 +524,7 @@ public sealed partial class HereticAbilitySystem
                 _statusEffect.TryRemoveTime(uid, "Stun", reduction, status);
                 _statusEffect.TryRemoveTime(uid, "KnockedDown", reduction, status);
 
+                _statusEffect.TryRemoveStatusEffect(uid, "Pacified", status);
                 _statusEffect.TryRemoveStatusEffect(uid, "ForcedSleep", status);
                 _statusEffect.TryRemoveStatusEffect(uid, "SlowedDown", status);
                 _statusEffect.TryRemoveStatusEffect(uid, "BlurryVision", status);
