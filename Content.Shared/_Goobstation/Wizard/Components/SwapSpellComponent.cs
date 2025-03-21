bfafb788dd88f7c253db502e460c12c3f170496c
HawkeyeBlade <hawkeyewarframexd@gmail.com>
2025-03-22 02:59:50 +0800

    Update SwapSpellComponent.cs

--- Content.Shared/_Goobstation/Wizard/Components/SwapSpellComponent.cs
+++ Content.Shared/_Goobstation/Wizard/Components/SwapSpellComponent.cs
@@ -5,8 +5,6 @@ namespace Content.Shared._Goobstation.Wizard.Components;
 [RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
 public sealed partial class SwapSpellComponent : Component
 {
-    [DataField]
-    public bool AllowSecondaryTarget = true;
 
     [DataField, AutoNetworkedField]
     public EntityUid? SecondaryTarget;
