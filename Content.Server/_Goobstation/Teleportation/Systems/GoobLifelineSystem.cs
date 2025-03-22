0f7bf4e029ca9cdf116a07e753e1a7dcfb944ce2
Rouge2t7 <sarahoneill132@hotmail.com>
2025-03-22 16:22:44 +0000

    Fixes

--- Content.Server/_Goobstation/Teleportation/Systems/GoobLifelineSystem.cs
+++ Content.Server/_Goobstation/Teleportation/Systems/GoobLifelineSystem.cs
@@ -1,121 +1,64 @@
 using Content.Server._Goobstation.Teleportation.Components;
 using Content.Server.Explosion.EntitySystems;
 using Content.Server.Warps;
+using Content.Shared.Mobs.Components;
 using Content.Shared.Movement.Pulling.Systems;
 using Robust.Server.GameObjects;
 using Content.Shared.Rejuvenate;
-using Content.Shared.Hands;
-using Content.Shared.Hands.Components;
-using Content.Shared.Hands.EntitySystems;
+using Content.Shared.Standing;
 
 namespace Content.Server._Goobstation.Teleportation.EntitySystems;
 
 public sealed class GoobLifelineSystem : EntitySystem
 {
-    [Dependency] private readonly IEntityManager _entManager = default!;
     [Dependency] private readonly PullingSystem _pullingSystem = default!;
     [Dependency] private readonly TransformSystem _transform = default!;
 
     public override void Initialize()
     {
         base.Initialize();
-        SubscribeLocalEvent<WarpParentOnTriggerComponent, TriggerEvent>(HandleWarpParentTrigger);
-
-        var query = _entManager.AllEntityQueryEnumerator<WarpParentOnTriggerComponent>();
-        if (query.MoveNext(out var entity, out _))
-        {
-            ForcedLocation(entity);
-        }
+        SubscribeLocalEvent<WarpParentOnTriggerComponent, TriggerEvent>(OnTrigger);
     }
 
-    private void HandleWarpParentTrigger(EntityUid uid, WarpParentOnTriggerComponent component, TriggerEvent args)
+    private void OnTrigger(EntityUid uid, WarpParentOnTriggerComponent component, TriggerEvent args)
+    {
+        WarpParent(uid, component);
+        args.Handled = true;
+    }
+    private void WarpParent(EntityUid uid, WarpParentOnTriggerComponent component)
     {
         var location = FindWarpPoint(component.WarpLocation);
         if (location == null)
-        {
             return;
-        }
 
         if (!TryComp<TransformComponent>(uid, out var transform))
-        {
             return;
-        }
 
         var parentUid = transform.ParentUid;
-        if (parentUid == EntityUid.Invalid)
-        {
+        if (parentUid == EntityUid.Invalid || !HasComp<MobStateComponent>(parentUid))
             return;
-        }
 
-        // Drop items from hands
-        if (TryComp<HandsComponent>(parentUid, out var hands))
-        {
-            foreach (var hand in hands.Hands.Values)
-            {
-                var heldEntity = hand.HeldEntity;
-                if (heldEntity != null)
-                {
-                    var dropEvent = new VirtualItemDropAttemptEvent(parentUid, parentUid, heldEntity.Value, false);
-                    RaiseLocalEvent(parentUid, dropEvent);
-
-                    if (!dropEvent.Cancelled)
-                    {
-                        _entManager.GetComponent<TransformComponent>(heldEntity.Value).Coordinates = _entManager.GetComponent<TransformComponent>(parentUid).Coordinates;
-                        Get<SharedHandsSystem>().TryDrop(parentUid, hand);
-                    }
-                }
-            }
-        }
-
+        RaiseLocalEvent(parentUid, new DropHandItemsEvent());
         _pullingSystem.StopAllPulls(parentUid);
 
         var coords = _transform.GetMapCoordinates(location.Value);
-
         if (transform.MapID != coords.MapId)
         {
             _transform.SetMapCoordinates(parentUid, coords);
         }
-        else
-        {
-            _transform.SetMapCoordinates(parentUid, coords);
-        }
 
-        if (!TryComp<TransformComponent>(parentUid, out var parentTransform) || parentTransform.GridUid == EntityUid.Invalid)
-        {
-            _transform.AttachToGridOrMap(parentUid);
-        }
-
-        RaiseLocalEvent(parentUid, new RejuvenateEvent());
-
-        args.Handled = true;
+        QueueDel(uid);
     }
-
-    public void ForcedLocation(EntityUid entity)
-    {
-        if (!TryComp<WarpParentOnTriggerComponent>(entity, out var component))
-        {
-            return;
-        }
-
-        var location = FindWarpPoint(component.WarpLocation);
-        if (location == null)
-        {
-            return;
-        }
-
-        HandleWarpParentTrigger(entity, component, new TriggerEvent(entity));
-    }
-
     private EntityUid? FindWarpPoint(string location)
     {
-        var query = _entManager.AllEntityQueryEnumerator<WarpPointComponent, TransformComponent>();
-        while (query.MoveNext(out var uid, out var warp, out _))
+        var query = EntityQueryEnumerator<WarpPointComponent, TransformComponent>();
+
+        while (query.MoveNext(out var entity, out var warp, out var transform))
         {
             if (warp.Location == location)
-            {
-                return uid;
-            }
+                return entity;
         }
+
         return null;
     }
 }
