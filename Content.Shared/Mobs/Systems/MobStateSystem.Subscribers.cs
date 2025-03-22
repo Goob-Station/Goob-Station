3a0100553e3b9bd37fcc31d025608e67cbf00426
SlamBamActionman <83650252+SlamBamActionman@users.noreply.github.com>
2025-03-22 06:00:21 +0100

    Remove serverside StandingStateSystem (#35999)  + goob edits
    Refactor system

--- Content.Shared/Mobs/Systems/MobStateSystem.Subscribers.cs
+++ Content.Shared/Mobs/Systems/MobStateSystem.Subscribers.cs
@@ -186,6 +186,7 @@ public partial class MobStateSystem
             return;
 
         _blocker.UpdateCanMove(target); //update movement anytime a state changes
+        var ev = new DropHandItemsEvent(); // Goobstation
         switch (state)
         {
             case MobState.Alive:
@@ -194,13 +195,13 @@ public partial class MobStateSystem
                 break;
             case MobState.Critical:
                 _standing.Down(target);
-                RaiseLocalEvent(target, new DropHandItemsEvent()); // Goobstation
+                RaiseLocalEvent(target, ref ev); // Goobstation
                 _appearance.SetData(target, MobStateVisuals.State, MobState.Critical);
                 break;
             case MobState.Dead:
                 EnsureComp<CollisionWakeComponent>(target);
                 _standing.Down(target);
-                RaiseLocalEvent(target, new DropHandItemsEvent()); // Goobstation
+                RaiseLocalEvent(target, ref ev); // Goobstation
                 _appearance.SetData(target, MobStateVisuals.State, MobState.Dead);
                 break;
             case MobState.Invalid:
