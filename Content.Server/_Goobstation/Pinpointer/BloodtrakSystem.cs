4231dd4e7071f4a4932dc20b8429736a9b5d2cf0
SolsticeOfTheWinter <solsticeofthewinter@gmail.com>
2025-03-24 13:30:31 -1000

    bweh
    Signed-off-by: SolsticeOfTheWinter <solsticeofthewinter@gmail.com>

--- Content.Server/_Goobstation/Pinpointer/BloodtrakSystem.cs
+++ Content.Server/_Goobstation/Pinpointer/BloodtrakSystem.cs
@@ -1,23 +1,35 @@
+using System.Linq;
+using System.Numerics;
 using Content.Server.Forensics;
+using Content.Server.Shuttles.Events;
 using Content.Shared._Gobostation.Pinpointer;
 using Content.Shared._Goobstation.Pinpointer;
-using Content.Shared.Database;
-using Content.Shared.IdentityManagement;
 using Content.Shared.Interaction;
+using Content.Shared.Pinpointer;
 using Content.Shared.Tag;
+using Robust.Shared.Utility;
 
 namespace Content.Server._Goobstation.Pinpointer;
 
-public sealed partial class BloodtrakSystem : SharedBloodtrakSystem
+public sealed class BloodtrakSystem : SharedBloodtrakSystem
 {
     [Dependency] private readonly TagSystem _tag = default!;
     [Dependency] private readonly ForensicsSystem _forensicsSystem = default!;
+    [Dependency] private readonly SharedTransformSystem _transform = default!;
+    [Dependency] private readonly SharedAppearanceSystem _appearance = default!;
+
     private EntityQuery<ForensicsComponent> _forensicsQuery;
+    private EntityQuery<TransformComponent> _xformQuery;
 
     public override void Initialize()
     {
         base.Initialize();
+
+        _xformQuery = GetEntityQuery<TransformComponent>();
+
         SubscribeLocalEvent<BloodtrakComponent, AfterInteractEvent>(OnAfterInteract);
+        SubscribeLocalEvent<BloodtrakComponent, ActivateInWorldEvent>(OnActivate);
+        SubscribeLocalEvent<FTLCompletedEvent>(OnLocateTarget);
     }
 
     private EntityUid GetBloodTarget(EntityUid uid, BloodtrakComponent comp, AfterInteractEvent args)
@@ -74,5 +86,179 @@ public sealed partial class BloodtrakSystem : SharedBloodtrakSystem
         pinpointer.Target = target;
         if (pinpointer.IsActive)
             UpdateDirectionToTarget(uid, pinpointer);
+    }
+
+    public override bool TogglePinpointer(EntityUid uid, BloodtrakComponent? pinpointer = null)
+    {
+        if (!Resolve(uid, ref pinpointer))
+            return false;
+
+        var isActive = !pinpointer.IsActive;
+        SetActive(uid, isActive, pinpointer);
+        UpdateAppearance(uid, pinpointer);
+        return isActive;
+    }
+
+    private void UpdateAppearance(EntityUid uid, BloodtrakComponent pinpointer, AppearanceComponent? appearance = null)
+    {
+        if (!Resolve(uid, ref appearance))
+            return;
+        _appearance.SetData(uid, PinpointerVisuals.IsActive, pinpointer.IsActive, appearance);
+        _appearance.SetData(uid, PinpointerVisuals.TargetDistance, pinpointer.DistanceToTarget, appearance);
+    }
+
+    private void OnActivate(EntityUid uid, BloodtrakComponent component, ActivateInWorldEvent args)
+    {
+        if (args.Handled || !args.Complex)
+            return;
+
+        TogglePinpointer(uid, component);
+        LocateTarget(uid, component);
+
+        args.Handled = true;
+    }
+
+    private void OnLocateTarget(ref FTLCompletedEvent ev)
+    {
+        // This feels kind of expensive, but it only happens once per hyperspace jump
+
+        // todo: ideally, you would need to raise this event only on jumped entities
+        // this code update ALL pinpointers in game
+        var query = EntityQueryEnumerator<BloodtrakComponent>();
+
+        while (query.MoveNext(out var uid, out var pinpointer))
+        {
+            LocateTarget(uid, pinpointer);
+        }
+    }
+
+    private void LocateTarget(EntityUid uid, BloodtrakComponent component)
+    {
+        // try to find target from whitelist
+        if (component.IsActive && component.Component != null)
+        {
+            if (!EntityManager.ComponentFactory.TryGetRegistration(component.Component, out var reg))
+            {
+                Log.Error($"Unable to find component registration for {component.Component} for bloodtrak!");
+                DebugTools.Assert(false);
+                return;
+            }
+
+            var target = FindTargetFromComponent(uid, reg.Type);
+            SetTarget(uid, target, component);
+        }
+    }
+
+    public override void Update(float frameTime)
+    {
+        base.Update(frameTime);
+
+        // because target or pinpointer can move
+        // we need to update pinpointers arrow each frame
+        var query = EntityQueryEnumerator<BloodtrakComponent>();
+        while (query.MoveNext(out var uid, out var pinpointer))
+        {
+            UpdateDirectionToTarget(uid, pinpointer);
+        }
+    }
+
+    /// <summary>
+    ///     Try to find the closest entity from whitelist on a current map
+    ///     Will return null if can't find anything
+    /// </summary>
+    private EntityUid? FindTargetFromComponent(EntityUid uid, Type whitelist, TransformComponent? transform = null)
+    {
+        _xformQuery.Resolve(uid, ref transform, false);
+
+        if (transform == null)
+            return null;
+
+        // sort all entities in distance increasing order
+        var mapId = transform.MapID;
+        var l = new SortedList<float, EntityUid>();
+        var worldPos = _transform.GetWorldPosition(transform);
+
+        foreach (var (otherUid, _) in EntityManager.GetAllComponents(whitelist))
+        {
+            if (!_xformQuery.TryGetComponent(otherUid, out var compXform) || compXform.MapID != mapId)
+                continue;
+
+            var dist = (_transform.GetWorldPosition(compXform) - worldPos).LengthSquared();
+            l.TryAdd(dist, otherUid);
+        }
+
+        // return uid with a smallest distance
+        return l.Count > 0 ? l.First().Value : null;
+    }
+
+    /// <summary>
+    ///     Update direction from pinpointer to selected target (if it was set)
+    /// </summary>
+    protected override void UpdateDirectionToTarget(EntityUid uid, BloodtrakComponent? pinpointer = null)
+    {
+        if (!Resolve(uid, ref pinpointer))
+            return;
+
+        if (!pinpointer.IsActive)
+            return;
+
+        var target = pinpointer.Target;
+        if (target == null || !EntityManager.EntityExists(target.Value))
+        {
+            SetDistance(uid, Shared._Goobstation.Pinpointer.Distance.Unknown, pinpointer);
+            return;
+        }
+
+        var dirVec = CalculateDirection(uid, target.Value);
+        var oldDist = pinpointer.DistanceToTarget;
+        if (dirVec != null)
+        {
+            var angle = dirVec.Value.ToWorldAngle();
+            TrySetArrowAngle(uid, angle, pinpointer);
+            var dist = CalculateDistance(dirVec.Value, pinpointer);
+            SetDistance(uid, dist, pinpointer);
+        }
+        else
+        {
+            SetDistance(uid, Shared._Goobstation.Pinpointer.Distance.Unknown, pinpointer);
+        }
+        if (oldDist != pinpointer.DistanceToTarget)
+            UpdateAppearance(uid, pinpointer);
+    }
+
+    /// <summary>
+    ///     Calculate direction from pinUid to trgUid
+    /// </summary>
+    /// <returns>Null if failed to calculate distance between two entities</returns>
+    private Vector2? CalculateDirection(EntityUid pinUid, EntityUid trgUid)
+    {
+        var xformQuery = GetEntityQuery<TransformComponent>();
+
+        // check if entities have transform component
+        if (!xformQuery.TryGetComponent(pinUid, out var pin))
+            return null;
+        if (!xformQuery.TryGetComponent(trgUid, out var trg))
+            return null;
+
+        // check if they are on same map
+        if (pin.MapID != trg.MapID)
+            return null;
+
+        // get world direction vector
+        var dir = _transform.GetWorldPosition(trg, xformQuery) - _transform.GetWorldPosition(pin, xformQuery);
+        return dir;
+    }
+
+    private Shared._Goobstation.Pinpointer.Distance CalculateDistance(Vector2 vec, BloodtrakComponent pinpointer)
+    {
+        var dist = vec.Length();
+        if (dist <= pinpointer.ReachedDistance)
+            return Shared._Goobstation.Pinpointer.Distance.Reached;
+        else if (dist <= pinpointer.CloseDistance)
+            return Shared._Goobstation.Pinpointer.Distance.Close;
+        else if (dist <= pinpointer.MediumDistance)
+            return Shared._Goobstation.Pinpointer.Distance.Medium;
+        else
+            return Shared._Goobstation.Pinpointer.Distance.Far;
     }
 }
