b1d7d390f36ffc9d304353bf3ad5665392aa742e
Scruq445 <storchdamien@gmail.com>
2025-03-25 03:15:41 -0700

    make system for spawning base roundstart

---
+++ Content.Server/_Goobstation/Ghostbar/AntagBaseSystem.cs
@@ -0,0 +1,10 @@
+using Robust.Shared.Map
+
+const string AntagMapDIR = "Maps/_Goobstation/Nonstations/antagplanet.yml";
+private void OnRoundStart(RoundStartingEvent ev)
+{
+    var DirPath = new ResPath(AntagMapDIR);
+
+    if (_mapLoader.TryLoadMap(DirPath, out var MapStat, out _, new DeserializationOptions { InitializeMaps = true }))
+        _mapSystem.SetPaused(MapStat.Value.Comp.MapId, false);
+}
