c19149c47a7fe4f9a732601bac2bd7feb8212283
metalgearsloth <31366439+metalgearsloth@users.noreply.github.com>
2025-03-28 09:29:02 +1100

    Mob movement rewrite (#35931)
    * Conveyor optimisations
    
    - Optimise movement for moving stuff. Better flags + less resolves + slapped parallelrobustjob on it.
    - Sleeping for entities getting conveyed into walls.
    
    * Blocker version
    
    * Finish
    
    * Final
    
    * Fix conveyor power mispredict
    
    * Bagel save
    
    * Revert "Bagel save"
    
    This reverts commit 1b93fda81fb852d89b89b0beae0b80f8a61165f2.
    
    * Conveyor resave
    
    * Fix prediction
    
    * Mob movement rewrite
    
    * Bandaid
    
    * Working version
    
    * Tentatively working
    
    * Friction to fix cornering
    
    * More fixes
    
    * Revert bagel
    
    * Revert this
    
    * a
    
    * Reviewed
    
    * Funky re-save
    
    * Fix velocity
    
    * Table fix
    
    * Review
    
    * a

--- Content.Server/Movement/Systems/PullController.cs
+++ Content.Server/Movement/Systems/PullController.cs
@@ -37,6 +37,7 @@ using System.Numerics;
 using Content.Server.Movement.Components;
 using Content.Server.Physics.Controllers;
 using Content.Shared.ActionBlocker;
+using Content.Shared.Conveyor;
 using Content.Shared.Gravity;
 using Content.Shared.Input;
 using Content.Shared.Movement.Pulling.Components;
@@ -155,6 +156,12 @@ public sealed class PullController : VirtualController
             return false;
 
         var pulled = pullerComp.Pulling;
+
+        // See update statement; this thing overwrites so many systems, DOESN'T EVEN LERP PROPERLY.
+        // We had a throwing version but it occasionally had issues.
+        // We really need the throwing version back.
+        if (TryComp(pulled, out ConveyedComponent? conveyed) && conveyed.Conveying)
+            return false;
 
         if (!_pullableQuery.TryComp(pulled, out var pullable))
             return false;
@@ -286,6 +293,13 @@ public sealed class PullController : VirtualController
             if (!TryComp<PhysicsComponent>(pullableEnt, out var physics) ||
                 physics.BodyType == BodyType.Static ||
                 movingTo.MapId != pullableXform.MapID)
+            {
+                RemCompDeferred<PullMovingComponent>(pullableEnt);
+                continue;
+            }
+
+            // TODO: This whole thing is slop and really needs to be throwing again
+            if (TryComp(pullableEnt, out ConveyedComponent? conveyed) && conveyed.Conveying)
             {
                 RemCompDeferred<PullMovingComponent>(pullableEnt);
                 continue;
