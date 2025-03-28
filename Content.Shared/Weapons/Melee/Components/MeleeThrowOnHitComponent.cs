80eff0a3c211c5f4c831d6110f2a170f25568cf7
SolsticeOfTheWinter <solsticeofthewinter@gmail.com>
2025-03-28 11:18:53 -1000

    bweh
    Signed-off-by: SolsticeOfTheWinter <solsticeofthewinter@gmail.com>

--- Content.Shared/Weapons/Melee/Components/MeleeThrowOnHitComponent.cs
+++ Content.Shared/Weapons/Melee/Components/MeleeThrowOnHitComponent.cs
@@ -8,7 +8,7 @@ namespace Content.Shared.Weapons.Melee.Components;
 /// until it hits a wall or a time limit is exhausted.
 /// </summary>
 [RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
-[Access(typeof(MeleeThrowOnHitSystem), typeof(BoomerangSystem))] // Goobstation Edit
+// [Access(typeof(MeleeThrowOnHitSystem), typeof(BoomerangSystem))] // Goobstation Edit - No implicit access
 public sealed partial class MeleeThrowOnHitComponent : Component
 {
     /// <summary>
