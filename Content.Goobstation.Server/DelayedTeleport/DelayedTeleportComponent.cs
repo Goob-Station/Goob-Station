942849a218462623686396ad7dc69aa8a4f6c77b
SolsticeOfTheWinter <solsticeofthewinter@gmail.com>
2025-04-01 20:36:16 -1000

    Added documentation to delayed teleport comp
    Signed-off-by: SolsticeOfTheWinter <solsticeofthewinter@gmail.com>

--- Content.Goobstation.Server/DelayedTeleport/DelayedTeleportComponent.cs
+++ Content.Goobstation.Server/DelayedTeleport/DelayedTeleportComponent.cs
@@ -3,8 +3,24 @@ namespace Content.Goobstation.Server.DelayedTeleport;
 [RegisterComponent]
 public sealed partial class DelayedTeleportComponent : Component
 {
+    /// <summary>
+    /// The UID of the map that is being teleported to.
+    /// </summary>
+    [DataField]
     public EntityUid MapUid;
+    /// <summary>
+    /// The UID of the grid that is being teleported to, if added.
+    /// </summary>
+    [DataField]
     public EntityUid? GridUid;
+    /// <summary>
+    /// The delay before the teleport occurs.
+    /// </summary>
+    [DataField]
     public float Delay;
+    /// <summary>
+    /// The time since the component was applied.
+    /// </summary>
+    [DataField]
     public float Elapsed;
 }
