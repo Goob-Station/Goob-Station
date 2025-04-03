05a50a5c7806c705d7d43cc066f23882bdb562f0
LuciferMkshelter <stepanteliatnik2022@gmail.com>
2025-04-03 22:11:31 +0300

    texture fix, phoron dagger, infinite money glitch fix

--- Content.Goobstation.Server/NTR/Scan/BriefcaseScannerSystem.cs
+++ Content.Goobstation.Server/NTR/Scan/BriefcaseScannerSystem.cs
@@ -1,5 +1,6 @@
 using Content.Shared.DoAfter;
 using Content.Shared.Store;
+using Content.Shared.Popups;
 using Content.Shared.Store.Components;
 using Content.Shared.Interaction;
 using Content.Server.Store.Systems;
@@ -27,31 +28,15 @@ namespace Content.Goobstation.Server.NTR.Scan
             base.Initialize();
             SubscribeLocalEvent<BriefcaseScannerComponent, AfterInteractEvent>(OnAfterInteract);
             SubscribeLocalEvent<BriefcaseScannerComponent, BriefcaseScannerDoAfterEvent>(OnDoAfter);
-            SubscribeLocalEvent<StoreComponent, ActivatableUIOpenAttemptEvent>(OnStoreOpenAttempt);
         }
 
-        private void OnStoreOpenAttempt(EntityUid uid, StoreComponent component, ActivatableUIOpenAttemptEvent args)
-        { // copy pasted
-            if (!component.OwnerOnly)
-                return;
-            if (!_mind.TryGetMind(args.User, out var mindId, out var mind))
-                return;
-            component.AccountOwner ??= mind;
-            DebugTools.Assert(component.AccountOwner != null);
-
-            if (component.AccountOwner == mind)
-                return;
-            _popup.PopupEntity(Loc.GetString("store-not-account-owner", ("store", uid)), uid, args.User);
-            args.Cancel();
-        }
         private void OnAfterInteract(EntityUid uid, BriefcaseScannerComponent component, AfterInteractEvent args)
         {
             if (!args.CanReach || args.Target == null)
                 return;
-            // cant open the store if ur not ntr
             if (TryComp<StoreComponent>(uid, out var store) && store.OwnerOnly)
             {
-                if (!_mind.TryGetMind(args.User, out var mindId, out var mind) || store.AccountOwner != mind)
+                if (!_mind.TryGetMind(args.User, out var mindId, out _) || store.AccountOwner != mindId)
                 {
                     _popup.PopupEntity(Loc.GetString("store-not-account-owner", ("store", uid)), uid, args.User);
                     return;
@@ -83,17 +68,19 @@ namespace Content.Goobstation.Server.NTR.Scan
                 return;
             scannable.AlreadyScanned = true;
             //Dirty(target, scannable);
-            if (TryComp<StoreComponent>(uid, out var store))
+            if (TryComp<StoreComponent>(uid, out var store) && store.CurrencyWhitelist.Contains("NTLoyaltyPoint"))
             {
-                var currency = new Dictionary<string, FixedPoint2>
+                var points = scannable.Points;
+                if (points <= 0)
                 {
-                    { "NTLoyaltyPoint", FixedPoint2.New(scannable.Points) }
-                };
-
-                if (store.CurrencyWhitelist.Contains("NTLoyaltyPoint"))
+                    _popup.PopupEntity("womp womp", uid, args.User);
+                }
+                else
                 {
-                    _storeSystem.TryAddCurrency(currency, uid, store);
-                    DebugTools.Assert(store.Balance.ContainsKey("NTLoyaltyPoint"));
+                    _storeSystem.TryAddCurrency(new Dictionary<string, FixedPoint2>
+                    {
+                        { "NTLoyaltyPoint", FixedPoint2.New(points) }
+                    }, uid, store);
                 }
             }
 
