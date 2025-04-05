d37208146084fe09a79471c209725f7bdc5d2d76
pheenty <fedorlukin2006@gmail.com>
2025-04-05 18:16:37 +0700

    while we're at it fix abductor restrictions giving no messages

--- Content.Shared/_Shitmed/Restrict/SharedRestrictSystem.cs
+++ Content.Shared/_Shitmed/Restrict/SharedRestrictSystem.cs
@@ -1,4 +1,4 @@
-using Content.Shared.Interaction.Events;
+using Content.Shared.Interaction;
 using Content.Shared.Popups;
 using Content.Shared.Tag;
 using Content.Shared.Weapons.Melee.Events;
@@ -15,18 +15,19 @@ public sealed partial class SharedRestrictSystem : EntitySystem
     [Dependency] private readonly IGameTiming _timing = default!;
     public override void Initialize()
     {
-        SubscribeLocalEvent<RestrictInteractionByUserTagComponent, InteractionAttemptEvent>(OnAttemptInteract);
+        SubscribeLocalEvent<RestrictInteractionByUserTagComponent, BeforeRangedInteractEvent>(OnAttemptInteract);
         SubscribeLocalEvent<RestrictMeleeByUserTagComponent, AttemptMeleeEvent>(OnAttemptMelee);
         SubscribeLocalEvent<RestrictGunshotsByUserTagComponent, ShotAttemptedEvent>(OnAttemptGunshot);
     }
 
-    private void OnAttemptInteract(Entity<RestrictInteractionByUserTagComponent> ent, ref InteractionAttemptEvent args)
+    private void OnAttemptInteract(Entity<RestrictInteractionByUserTagComponent> ent, ref BeforeRangedInteractEvent args)
     {
-        if (!_tagSystem.HasAllTags(args.Uid, ent.Comp.Contains) || _tagSystem.HasAnyTag(args.Uid, ent.Comp.DoesntContain))
+        if (!_tagSystem.HasAllTags(args.User, ent.Comp.Contains) || _tagSystem.HasAnyTag(args.User, ent.Comp.DoesntContain))
         {
-            args.Cancelled = true;
             if (ent.Comp.Messages.Count != 0)
-                _popup.PopupClient(Loc.GetString(_random.Pick(ent.Comp.Messages)), args.Uid);
+                _popup.PopupClient(Loc.GetString(_random.Pick(ent.Comp.Messages)), args.User);
+
+            args.Handled = true;
         }
     }
 
@@ -34,9 +35,10 @@ public sealed partial class SharedRestrictSystem : EntitySystem
     {
         if(!_tagSystem.HasAllTags(args.User, ent.Comp.Contains) || _tagSystem.HasAnyTag(args.User, ent.Comp.DoesntContain))
         {
-            args.Cancelled = true;
             if(ent.Comp.Messages.Count != 0)
                 args.Message = Loc.GetString(_random.Pick(ent.Comp.Messages));
+
+            args.Cancelled = true;
         }
     }
 
@@ -44,13 +46,15 @@ public sealed partial class SharedRestrictSystem : EntitySystem
     {
         if(!_tagSystem.HasAllTags(args.User, ent.Comp.Contains) || _tagSystem.HasAnyTag(args.User, ent.Comp.DoesntContain))
         {
-            args.Cancel();
             var time = _timing.CurTime;
+
             if(ent.Comp.Messages.Count != 0 && time > ent.Comp.LastPopup + TimeSpan.FromSeconds(1))
             {
                 ent.Comp.LastPopup = time;
                 _popup.PopupClient(Loc.GetString(_random.Pick(ent.Comp.Messages)), args.User);
             }
+
+            args.Cancel();
         }
     }
 }
