038ee7863bf13f2996cd6c6fee0d0133b01ac41b
pheenty <fedorlukin2006@gmail.com>
2025-04-05 17:03:27 +0700

    split comp

--- Content.Shared/_Shitmed/Restrict/RestrictInteractionByUserTag.cs
+++ Content.Shared/_Shitmed/Restrict/RestrictInteractionByUserTag.cs
@@ -1,11 +1,10 @@
-using Content.Shared._Shitmed.Antags.Abductor;
 using Content.Shared.Tag;
 using Robust.Shared.GameStates;
 using Robust.Shared.Prototypes;
 
 namespace Content.Shared._Shitmed.Restrict;
 [RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
-public sealed partial class RestrictByUserTagComponent : Component
+public sealed partial class RestrictInteractionByUserTagComponent : Component
 {
     [DataField, AutoNetworkedField]
     public List<ProtoId<TagPrototype>> Contains = [];
@@ -15,15 +14,4 @@ public sealed partial class RestrictByUserTagComponent : Component
 
     [DataField, AutoNetworkedField]
     public List<string> Messages = [];
-
-    [DataField]
-    public bool BlockInteraction = true;
-
-    [DataField]
-    public bool BlockMelee = true;
-
-    [DataField]
-    public bool BlockGunshots = true;
-
-    public TimeSpan LastPopup;
 }
--- Content.Shared/_Shitmed/Restrict/SharedRestrictSystem.cs
+++ Content.Shared/_Shitmed/Restrict/SharedRestrictSystem.cs
@@ -15,16 +15,14 @@ public sealed partial class SharedRestrictSystem : EntitySystem
     [Dependency] private readonly IGameTiming _timing = default!;
     public override void Initialize()
     {
-        SubscribeLocalEvent<RestrictByUserTagComponent, AttemptMeleeEvent>(OnAttemptMelee);
-        SubscribeLocalEvent<RestrictByUserTagComponent, InteractionAttemptEvent>(OnAttemptInteract);
-        SubscribeLocalEvent<RestrictByUserTagComponent, ShotAttemptedEvent>(OnAttemptGunshot);
+        SubscribeLocalEvent<RestrictInteractionByUserTagComponent, InteractionAttemptEvent>(OnAttemptInteract);
+        SubscribeLocalEvent<RestrictMeleeByUserTagComponent, AttemptMeleeEvent>(OnAttemptMelee);
+        SubscribeLocalEvent<RestrictGunshotsByUserTagComponent, ShotAttemptedEvent>(OnAttemptGunshot);
     }
 
-    private void OnAttemptInteract(Entity<RestrictByUserTagComponent> ent, ref InteractionAttemptEvent args)
+    private void OnAttemptInteract(Entity<RestrictInteractionByUserTagComponent> ent, ref InteractionAttemptEvent args)
     {
-        if (ent.Comp.BlockInteraction
-        && (!_tagSystem.HasAllTags(args.Uid, ent.Comp.Contains)
-        || _tagSystem.HasAnyTag(args.Uid, ent.Comp.DoesntContain)))
+        if (!_tagSystem.HasAllTags(args.Uid, ent.Comp.Contains) || _tagSystem.HasAnyTag(args.Uid, ent.Comp.DoesntContain))
         {
             args.Cancelled = true;
             if (ent.Comp.Messages.Count != 0)
@@ -32,11 +30,9 @@ public sealed partial class SharedRestrictSystem : EntitySystem
         }
     }
 
-    private void OnAttemptMelee(Entity<RestrictByUserTagComponent> ent, ref AttemptMeleeEvent args)
+    private void OnAttemptMelee(Entity<RestrictMeleeByUserTagComponent> ent, ref AttemptMeleeEvent args)
     {
-        if(ent.Comp.BlockMelee
-        && (!_tagSystem.HasAllTags(args.User, ent.Comp.Contains)
-        || _tagSystem.HasAnyTag(args.User, ent.Comp.DoesntContain)))
+        if(!_tagSystem.HasAllTags(args.User, ent.Comp.Contains) || _tagSystem.HasAnyTag(args.User, ent.Comp.DoesntContain))
         {
             args.Cancelled = true;
             if(ent.Comp.Messages.Count != 0)
@@ -44,11 +40,9 @@ public sealed partial class SharedRestrictSystem : EntitySystem
         }
     }
 
-    private void OnAttemptGunshot(Entity<RestrictByUserTagComponent> ent, ref ShotAttemptedEvent args)
+    private void OnAttemptGunshot(Entity<RestrictGunshotsByUserTagComponent> ent, ref ShotAttemptedEvent args)
     {
-        if(ent.Comp.BlockGunshots &&
-        (!_tagSystem.HasAllTags(args.User, ent.Comp.Contains)
-        || _tagSystem.HasAnyTag(args.User, ent.Comp.DoesntContain)))
+        if(!_tagSystem.HasAllTags(args.User, ent.Comp.Contains) || _tagSystem.HasAnyTag(args.User, ent.Comp.DoesntContain))
         {
             args.Cancel();
             var time = _timing.CurTime;
