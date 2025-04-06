a574869a643c4754d8d6e5a55bdfd66b09e77098
otito <oplaton2303@gmail.com>
2025-04-06 14:14:53 -0300

    Mimery book first part complete and working

---
+++ Content.Shared/_Shitcode/Mimery/MimeryBookDoAfterEvent.cs
@@ -0,0 +1,10 @@
+using Content.Shared.DoAfter;
+using Robust.Shared.Serialization;
+
+namespace Content.Shared._Shitcode.Mimery;
+
+[Serializable, NetSerializable]
+public sealed partial class MimeryBookDoAfterEvent : SimpleDoAfterEvent
+{
+
+}
---
+++ Content.Shared/_Shitcode/Mimery/MimeryBookSystem.cs
@@ -0,0 +1,53 @@
+using Content.Shared.Actions;
+using Content.Shared.DoAfter;
+using Content.Shared.Interaction.Events;
+
+namespace Content.Shared._Shitcode.Mimery;
+
+public sealed class MimeryBookSystem : EntitySystem
+{
+    [Dependency] private readonly SharedDoAfterSystem _doAfter = default!;
+    [Dependency] private readonly SharedActionsSystem _actions = default!;
+
+    public override void Initialize()
+    {
+        SubscribeLocalEvent<MimeryBookComponent, UseInHandEvent>(OnUse);
+        SubscribeLocalEvent<MimeryBookComponent, MimeryBookDoAfterEvent>(OnDoAfter);
+    }
+
+
+    private void OnUse(Entity<MimeryBookComponent> ent, ref UseInHandEvent args)
+    {
+        AttmeptLearn(ent, args);
+    }
+    private void OnDoAfter(Entity<MimeryBookComponent> ent, ref MimeryBookDoAfterEvent args)
+    {
+        if (args.Handled ||  args.Cancelled)
+            return;
+        args.Handled = true;
+
+        Logger.Debug("granted action");
+        _actions.AddAction(args.Args.User, "ActionMimeryWall");
+
+    }
+
+    private void AttmeptLearn(Entity<MimeryBookComponent> ent, UseInHandEvent args)
+    {
+        var DoAfterEventArgs = new DoAfterArgs(EntityManager,
+            args.User,
+            ent.Comp.LearnTime,
+            new MimeryBookDoAfterEvent(),
+            ent,
+            target: ent)
+        {
+            BreakOnMove = true,
+            BreakOnDamage = true,
+            BreakOnDropItem = true,
+            NeedHand = true,
+            MultiplyDelay = true,
+        };
+
+        _doAfter.TryStartDoAfter(DoAfterEventArgs);
+    }
+
+}
