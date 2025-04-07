45a9414aa1b96b95c2a3fb694b40815c2020d6e2
GoobBot <uristmchands@proton.me>
2025-04-07 21:39:08 +0000

    chore: Automatically update REUSE headers

--- Content.Goobstation.Client/test.cs
+++ Content.Goobstation.Client/test.cs
@@ -1,5 +1,4 @@
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
-// SPDX-FileCopyrightText: 2025 Unknown <unknown@example.com>
 //
 // SPDX-License-Identifier: MIT
 
