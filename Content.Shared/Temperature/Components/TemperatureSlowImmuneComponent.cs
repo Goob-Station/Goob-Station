65c922aa71df6c4e42bafe4554494526ae02af56
thebiggestbruh <199992874+thebiggestbruh@users.noreply.github.com>
2025-04-07 21:26:57 +0000

    Apply automatic changes

--- Content.Shared/Temperature/Components/TemperatureSlowImmuneComponent.cs
+++ Content.Shared/Temperature/Components/TemperatureSlowImmuneComponent.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 thebiggestbruh <marcus2008stoke@gmail.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using Content.Shared.Temperature.Systems;
 
 namespace Content.Shared.Temperature.Components;
