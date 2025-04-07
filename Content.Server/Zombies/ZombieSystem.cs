c0b9a89aec4c397f16d5108d5c9895c2acbc4862
Aiden <28298836+Aidenkrz@users.noreply.github.com>
2025-04-07 15:21:57 -0500

    Update ZombieSystem.cs

--- Content.Server/Zombies/ZombieSystem.cs
+++ Content.Server/Zombies/ZombieSystem.cs
@@ -1,10 +1,3 @@
-// SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
-//
-// SPDX-License-Identifier: AGPL-3.0-or-later
-
-//
-// SPDX-License-Identifier: AGPL-3.0-or-later
-
 using System.Linq;
 using Content.Server.Actions;
 using Content.Server.Body.Systems;
