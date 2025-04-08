d92ffa5d764949c250a20acbdc0a6e9a0b290b11
GoobBot <uristmchands@proton.me>
2025-04-08 18:00:47 +0000

    chore: Automatically update REUSE headers

--- Content.Goobstation.Server/Implants/Components/SpaceNinjaImplantComponent.cs
+++ Content.Goobstation.Server/Implants/Components/SpaceNinjaImplantComponent.cs
@@ -1,3 +1,8 @@
+// SPDX-FileCopyrightText: 2025 Scruq445 <storchdamien@gmail.com>
+// SPDX-FileCopyrightText: 2025 Ted Lukin <66275205+pheenty@users.noreply.github.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 namespace Content.Goobstation.Server.Implants.Components;
 
 [RegisterComponent]
--- Content.Goobstation.Server/Implants/Systems/SpaceNinjaImplantSystem.cs
+++ Content.Goobstation.Server/Implants/Systems/SpaceNinjaImplantSystem.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 Scruq445 <storchdamien@gmail.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using Content.Goobstation.Server.Implants.Components;
 using Content.Shared.Implants;
 using Content.Shared.Ninja.Components;
