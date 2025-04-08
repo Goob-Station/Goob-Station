063e7812df080b7e539e5626988be6ef33763ba4
GoobBot <uristmchands@proton.me>
2025-04-08 08:08:26 +0000

    chore: Automatically update REUSE headers

--- Content.Shared/Weapons/Ranged/Systems/SharedGunSystem.Ballistic.cs
+++ Content.Shared/Weapons/Ranged/Systems/SharedGunSystem.Ballistic.cs
@@ -82,6 +82,7 @@
 // SPDX-FileCopyrightText: 2024 to4no_fix <156101927+chavonadelal@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2024 voidnull000 <18663194+voidnull000@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 pheenty <fedorlukin2006@gmail.com>
 // SPDX-FileCopyrightText: 2025 themias <89101928+themias@users.noreply.github.com>
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
