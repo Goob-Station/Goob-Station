0f4e04416c7713ddd24aea400208432c2a32d5d4
GoobBot <uristmchands@proton.me>
2025-04-09 18:34:03 +0000

    chore: Automatically update REUSE headers

--- Content.Shared/Entry/EntryPoint.cs
+++ Content.Shared/Entry/EntryPoint.cs
@@ -24,6 +24,7 @@
 // SPDX-FileCopyrightText: 2025 Conchelle <mary@thughunt.ing>
 // SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
 // SPDX-FileCopyrightText: 2025 Misandry <mary@thughunt.ing>
+// SPDX-FileCopyrightText: 2025 gluesniffler <159397573+gluesniffler@users.noreply.github.com>
 //
 // SPDX-License-Identifier: MIT
 
