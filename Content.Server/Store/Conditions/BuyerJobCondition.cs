2394b96efe549aec58a1399350b3fb5a69fa60b0
GoobBot <uristmchands@proton.me>
2025-04-09 19:53:51 +0000

    chore: Automatically update REUSE headers

--- Content.Server/Store/Conditions/BuyerJobCondition.cs
+++ Content.Server/Store/Conditions/BuyerJobCondition.cs
@@ -7,6 +7,7 @@
 // SPDX-FileCopyrightText: 2024 whateverusername0 <whateveremail>
 // SPDX-FileCopyrightText: 2025 ActiveMammmoth <140334666+ActiveMammmoth@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 Armok <155400926+ARMOKS@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Aviu00 <93730715+Aviu00@users.noreply.github.com>
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
