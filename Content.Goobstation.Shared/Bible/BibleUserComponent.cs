f17551189dfbbf0f98bfb7249d4408a572d5304d
SolsticeOfTheWinter <solsticeofthewinter@gmail.com>
2025-04-08 19:33:58 -1000

    whoops
    Signed-off-by: SolsticeOfTheWinter <solsticeofthewinter@gmail.com>

--- Content.Goobstation.Shared/Bible/BibleUserComponent.cs
+++ Content.Goobstation.Shared/Bible/BibleUserComponent.cs
@@ -5,7 +5,6 @@
 //
 // SPDX-License-Identifier: MIT
 
-namespace Content.Server.Bible.Components
 namespace Content.Goobstation.Shared.Bible
 {
     [RegisterComponent]
--- Content.Shared/Bible/Components/SummonableRespawningComponent.cs
+++ Content.Shared/Bible/Components/SummonableRespawningComponent.cs
@@ -4,7 +4,6 @@
 //
 // SPDX-License-Identifier: MIT
 
-namespace Content.Server.Bible.Components
 namespace Content.Shared.Bible.Components
 {
     /// <summary>
