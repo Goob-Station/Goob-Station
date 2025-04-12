ab725a332437a3911bdcad5a6fed565ae462cb8a
SolsticeOfTheWinter <solsticeofthewinter@gmail.com>
2025-04-11 19:51:55 -1000

    bweh
    Signed-off-by: SolsticeOfTheWinter <solsticeofthewinter@gmail.com>

--- Content.Goobstation.Server/Devil/Contract/DevilClausePrototype.cs
+++ Content.Goobstation.Server/Devil/Contract/DevilClausePrototype.cs
@@ -2,9 +2,6 @@
 // SPDX-FileCopyrightText: 2025 SolsticeOfTheWinter <solsticeofthewinter@gmail.com>
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
-
-using System.ComponentModel.DataAnnotations;
-using Content.Shared.Damage.Prototypes;
 using Robust.Shared.Prototypes;
 
 namespace Content.Goobstation.Server.Devil.Contract;
