fa9ef326add0b0929b352094978cbc2d1d99541f
GoobBot <uristmchands@proton.me>
2025-04-14 22:06:45 +0000

    chore: Automatically update REUSE headers

--- Content.Goobstation.Server/MisandryBox/GrassEnforce/GrassEnforcementSystem.cs
+++ Content.Goobstation.Server/MisandryBox/GrassEnforce/GrassEnforcementSystem.cs
@@ -1,4 +1,8 @@
-﻿using Content.Server.Afk;
+// SPDX-FileCopyrightText: 2025 Conchelle <mary@thughunt.ing>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+using Content.Server.Afk;
 using Content.Server.GameTicking.Events;
 using Content.Shared.GameTicking;
 
