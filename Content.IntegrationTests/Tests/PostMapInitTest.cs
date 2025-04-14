dc889eedcdccc42d6a70e08e9b251ace00933c8d
Unisol <1929445+Unisol@users.noreply.github.com>
2025-04-13 21:21:53 -0400

    New deathmatch map: DM01 Entryway (#31533)
    * added plastitanium walls and windows
    
    * Added DM01 Entryway to pool of deathmatch arenas
    
    * replaced non-occluding walls with occluding ones
    
    * added more details and fluff to empty rooms and hallways; added floor drains around the map
    
    * Fixed failing integration test
    
    * fixed YML syntax error causing test failures. What kind of retard thought that using whitespace as structural syntax is a good idea?
    
    * minor map changes
    
    ---------
    
    Co-authored-by: Unisol <pavelyakushevich@gmail.com>

--- Content.IntegrationTests/Tests/PostMapInitTest.cs
+++ Content.IntegrationTests/Tests/PostMapInitTest.cs
@@ -223,7 +223,8 @@ namespace Content.IntegrationTests.Tests
             "Gate", // Goobstation - goob changes
             "Lavatest", // Lavaland Change
             "Loop",
-            "Delta" // Goobstation - add Delta
+            "Delta", // Goobstation - add Delta
+            "dm01-entryway"
         };
 
         /// <summary>
