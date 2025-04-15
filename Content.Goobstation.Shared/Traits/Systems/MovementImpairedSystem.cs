58464af50351581beb7d0dd0b2e232c8aeffc26e
GoobBot <uristmchands@proton.me>
2025-04-15 04:26:58 +0000

    chore: Automatically update REUSE headers

--- Content.Goobstation.Shared/Traits/Systems/MovementImpairedSystem.cs
+++ Content.Goobstation.Shared/Traits/Systems/MovementImpairedSystem.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 SolsticeOfTheWinter <solsticeofthewinter@gmail.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using Content.Goobstation.Shared.Traits.Components;
 using Content.Shared.Examine;
 using Content.Shared.Hands;
