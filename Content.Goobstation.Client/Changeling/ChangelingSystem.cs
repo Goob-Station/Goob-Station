27de3d130852bf67290b03f30cff218233138e49
GoobBot <uristmchands@proton.me>
2025-04-15 22:17:26 +0000

    chore: Automatically update REUSE headers

--- Content.Goobstation.Client/Changeling/ChangelingSystem.cs
+++ Content.Goobstation.Client/Changeling/ChangelingSystem.cs
@@ -4,6 +4,7 @@
 // SPDX-FileCopyrightText: 2024 whateverusername0 <whateveremail>
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Aviu00 <93730715+Aviu00@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 Ilya246 <ilyukarno@gmail.com>
 // SPDX-FileCopyrightText: 2025 Misandry <mary@thughunt.ing>
 // SPDX-FileCopyrightText: 2025 Spatison <137375981+Spatison@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 gus <august.eymann@gmail.com>
