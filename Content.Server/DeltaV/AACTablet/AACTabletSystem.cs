77074358a8cadd43778cefa8f1971026ed56ef03
deltanedas <39013340+deltanedas@users.noreply.github.com>
2025-04-16 19:12:13 +0100

    aac tablet cleanup (#3483)
    * localized AAC tablet window
    
    * no writing novels
    
    * gleep
    
    ---------
    
    Co-authored-by: deltanedas <@deltanedas:kde.org>

--- Content.Server/DeltaV/AACTablet/AACTabletSystem.cs
+++ Content.Server/DeltaV/AACTablet/AACTabletSystem.cs
@@ -17,6 +17,8 @@ public sealed class AACTabletSystem : EntitySystem
 
     private readonly List<string> _localisedPhrases = [];
 
+    public const int MaxPhrases = 10; // no writing novels
+
     public override void Initialize()
     {
         base.Initialize();
@@ -25,7 +27,7 @@ public sealed class AACTabletSystem : EntitySystem
 
     private void OnSendPhrase(Entity<AACTabletComponent> ent, ref AACTabletSendPhraseMessage message)
     {
-        if (ent.Comp.NextPhrase > _timing.CurTime)
+        if (ent.Comp.NextPhrase > _timing.CurTime || message.PhraseIds.Count > MaxPhrases)
             return;
 
         var senderName = Identity.Entity(message.Actor, EntityManager);
