298a058dc78239a908c365e3a4994197fa597452
GoobBot <uristmchands@proton.me>
2025-04-16 07:15:50 +0000

    chore: Automatically update REUSE headers

--- Content.Server/Projectiles/ProjectileSystem.cs
+++ Content.Server/Projectiles/ProjectileSystem.cs
@@ -55,6 +55,7 @@
 // SPDX-FileCopyrightText: 2025 Aviu00 <93730715+Aviu00@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Aviu00 <aviu00@protonmail.com>
 // SPDX-FileCopyrightText: 2025 BombasterDS <deniskaporoshok@gmail.com>
+// SPDX-FileCopyrightText: 2025 BombasterDS2 <shvalovdenis.workmail@gmail.com>
 // SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
 // SPDX-FileCopyrightText: 2025 Ilya246 <57039557+Ilya246@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 SX_7 <sn1.test.preria.2002@gmail.com>
