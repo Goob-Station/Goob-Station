56f7af1d7df78f4808d72ac8eb15a9d37f93b88e
GoobBot <uristmchands@proton.me>
2025-04-17 00:55:44 +0000

    chore: Automatically update REUSE headers

--- Content.Goobstation.Shared/Chemistry/GunApplySolution/GunApplySolutionComponent.cs
+++ Content.Goobstation.Shared/Chemistry/GunApplySolution/GunApplySolutionComponent.cs
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: Contributors to the GoobStation14 project
+// SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
