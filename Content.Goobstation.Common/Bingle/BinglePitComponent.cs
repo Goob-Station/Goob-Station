cd28266cbc872ba57b30a19619ce8d8c379fece7
GoobBot <uristmchands@proton.me>
2025-04-17 19:59:53 +0000

    chore: Automatically update REUSE headers

--- Content.Goobstation.Common/Bingle/BinglePitComponent.cs
+++ Content.Goobstation.Common/Bingle/BinglePitComponent.cs
@@ -5,6 +5,7 @@
 // SPDX-FileCopyrightText: 2025 Misandry <mary@thughunt.ing>
 // SPDX-FileCopyrightText: 2025 fishbait <gnesse@gmail.com>
 // SPDX-FileCopyrightText: 2025 gus <august.eymann@gmail.com>
+// SPDX-FileCopyrightText: 2025 shibe <swiftshibe@gmail.com>
 // SPDX-FileCopyrightText: 2025 unknown <Administrator@DESKTOP-PMRIVVA.kommune.indresogn.no>
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
