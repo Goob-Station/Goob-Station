b528e028e7a866ba030d2deac13190fee68d2fe1
TheBorzoiMustConsume <197824988+TheBorzoiMustConsume@users.noreply.github.com>
2025-04-18 12:53:23 +1000

    bam

---
+++ Content.Goobstation.Shared/Enchanting/Components/CanEnchantComponent.cs
@@ -0,0 +1,10 @@
+// SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
+// SPDX-FileCopyrightText: 2025 TheBorzoiMustConsume <197824988+TheBorzoiMustConsume@users.noreply.github.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+using Robust.Shared.GameStates;
+
+namespace Content.Goobstation.Shared.Enchanting.Components;
+[RegisterComponent, NetworkedComponent]
+public sealed partial class CanEnchantComponent : Component;
