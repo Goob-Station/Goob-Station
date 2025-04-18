88dc6edfa6dd72b51b102245614ba38478c26a8a
GoobBot <uristmchands@proton.me>
2025-04-18 08:13:48 +0000

    chore: Automatically update REUSE headers

--- Content.Server/Weapons/Melee/MeleeWeaponSystem.cs
+++ Content.Server/Weapons/Melee/MeleeWeaponSystem.cs
@@ -28,6 +28,7 @@
 // SPDX-FileCopyrightText: 2024 Джексон Миссиссиппи <tripwiregamer@gmail.com>
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Eagle <lincoln.mcqueen@gmail.com>
+// SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
 // SPDX-FileCopyrightText: 2025 Misandry <mary@thughunt.ing>
 // SPDX-FileCopyrightText: 2025 SlamBamActionman <83650252+SlamBamActionman@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 VMSolidus <evilexecutive@gmail.com>
