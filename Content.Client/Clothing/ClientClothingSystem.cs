b6d51798d769d1d9f93749677f17ad426cf132f9
GoobBot <uristmchands@proton.me>
2025-04-20 13:44:42 +0000

    chore: Automatically update REUSE headers

--- Content.Client/Clothing/ClientClothingSystem.cs
+++ Content.Client/Clothing/ClientClothingSystem.cs
@@ -25,6 +25,7 @@
 // SPDX-FileCopyrightText: 2024 charlie <charlie.sc.wong@veiwsonic.com>
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Aviu00 <aviu00@protonmail.com>
+// SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
