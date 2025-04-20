90b8e9e2b1a22eda9585d60fc324d6d1a5a58e2e
Kyle Tyo <36606155+VerinSenpai@users.noreply.github.com>
2025-04-20 00:14:58 -0400

    replace obsolete IsGrid calls with HasComp<MapGridComponent> (#36729)
    * replace obsolete IsGrid calls with HasComp<MapGridComponent>
    
    * remove IMapManager dependency
    
    * well would you look at that
    
    * just look at it

--- Content.Server/Atmos/EntitySystems/GasTileOverlaySystem.cs
+++ Content.Server/Atmos/EntitySystems/GasTileOverlaySystem.cs
@@ -39,6 +39,7 @@ using Robust.Shared;
 using Robust.Shared.Configuration;
 using Robust.Shared.Enums;
 using Robust.Shared.Map;
+using Robust.Shared.Map.Components;
 using Robust.Shared.Player;
 using Robust.Shared.Threading;
 using Robust.Shared.Timing;
@@ -83,6 +84,7 @@ namespace Content.Server.Atmos.EntitySystems
         private float _updateInterval;
 
         private int _thresholds;
+        private EntityQuery<MapGridComponent> _gridQuery;
         private EntityQuery<GasTileOverlayComponent> _query;
 
         public override void Initialize()
@@ -99,6 +101,7 @@ namespace Content.Server.Atmos.EntitySystems
                 MapManager = _mapManager,
                 ChunkViewerPool = _chunkViewerPool,
                 LastSentChunks = _lastSentChunks,
+                GridQuery = _gridQuery,
             };
 
             _playerManager.PlayerStatusChanged += OnPlayerStatusChanged;
@@ -109,6 +112,7 @@ namespace Content.Server.Atmos.EntitySystems
             SubscribeLocalEvent<RoundRestartCleanupEvent>(Reset);
             SubscribeLocalEvent<GasTileOverlayComponent, ComponentStartup>(OnStartup);
             _query = GetEntityQuery<GasTileOverlayComponent>();
+            _gridQuery = GetEntityQuery<MapGridComponent>();
         }
 
         private void OnStartup(EntityUid uid, GasTileOverlayComponent component, ComponentStartup args)
@@ -398,6 +402,8 @@ namespace Content.Server.Atmos.EntitySystems
             public Dictionary<ICommonSession, Dictionary<NetEntity, HashSet<Vector2i>>> LastSentChunks;
             public List<ICommonSession> Sessions;
 
+            public EntityQuery<MapGridComponent> GridQuery;
+
             public void Execute(int index)
             {
                 var playerSession = Sessions[index];
@@ -414,7 +420,7 @@ namespace Content.Server.Atmos.EntitySystems
                         previouslySent.Remove(netGrid);
 
                         // If grid was deleted then don't worry about sending it to the client.
-                        if (!EntManager.TryGetEntity(netGrid, out var gridId) || !MapManager.IsGrid(gridId.Value))
+                        if (!EntManager.TryGetEntity(netGrid, out var gridId) || GridQuery.HasComp(gridId.Value))
                             ev.RemovedChunks[netGrid] = oldIndices;
                         else
                         {
