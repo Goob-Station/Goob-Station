13daa0fbeff8ec203b1a156564452f69af61e051
GoobBot <uristmchands@proton.me>
2025-04-21 14:10:24 +0000

    chore: Automatically update REUSE headers

--- Content.Goobstation.Server/Supermatter/Systems/SupermatterSystem.cs
+++ Content.Goobstation.Server/Supermatter/Systems/SupermatterSystem.cs
@@ -6,6 +6,7 @@
 // SPDX-FileCopyrightText: 2024 whateverusername0 <whateveremail>
 // SPDX-FileCopyrightText: 2024 yglop <95057024+yglop@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
 // SPDX-FileCopyrightText: 2025 Misandry <mary@thughunt.ing>
 // SPDX-FileCopyrightText: 2025 SX-7 <92227810+SX-7@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Steve <marlumpy@gmail.com>
