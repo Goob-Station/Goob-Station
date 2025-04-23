ce2c7198ff26c1f42ed8ca4d8e3dd32a2088796f
GoobBot <uristmchands@proton.me>
2025-04-23 15:10:18 +0000

    chore: Automatically update REUSE headers

--- Content.Goobstation.Shared/Temperature/TemperatureEvents.cs
+++ Content.Goobstation.Shared/Temperature/TemperatureEvents.cs
@@ -1,3 +1,4 @@
+// SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
 // SPDX-FileCopyrightText: 2025 Marcus F <199992874+thebiggestbruh@users.noreply.github.com>
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
