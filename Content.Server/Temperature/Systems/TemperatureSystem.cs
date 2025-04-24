e75fa0bc4ad96165be842a46faf9cf4aab2bc57c
GoobBot <uristmchands@proton.me>
2025-04-24 19:45:18 +0000

    chore: Automatically update REUSE headers

--- Content.Server/Temperature/Systems/TemperatureSystem.cs
+++ Content.Server/Temperature/Systems/TemperatureSystem.cs
@@ -88,6 +88,7 @@
 // SPDX-FileCopyrightText: 2025 Marcus F <199992874+thebiggestbruh@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Marcus F <marcus2008stoke@gmail.com>
 // SPDX-FileCopyrightText: 2025 deltanedas <39013340+deltanedas@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 thebiggestbruh <199992874+thebiggestbruh@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 thebiggestbruh <marcus2008stoke@gmail.com>
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
