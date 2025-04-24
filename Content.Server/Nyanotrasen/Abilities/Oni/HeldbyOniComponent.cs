b71378c7ee26db4050a2e7c03492165c3a8ef569
GoobBot <uristmchands@proton.me>
2025-04-24 11:47:03 +0000

    chore: Automatically update REUSE headers

--- Content.Server/Nyanotrasen/Abilities/Oni/HeldbyOniComponent.cs
+++ Content.Server/Nyanotrasen/Abilities/Oni/HeldbyOniComponent.cs
@@ -1,5 +1,7 @@
 // SPDX-FileCopyrightText: 2024 ScyronX <166930367+ScyronX@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 Rouge2t7 <81053047+Sarahon@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 Ted Lukin <66275205+pheenty@users.noreply.github.com>
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
