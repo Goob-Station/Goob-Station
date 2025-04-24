79d52e99514f76beeb8a641534826219db12b1a7
GoobBot <uristmchands@proton.me>
2025-04-24 17:14:57 +0000

    chore: Automatically update REUSE headers

--- Content.Server/Power/SMES/SmesSystem.cs
+++ Content.Server/Power/SMES/SmesSystem.cs
@@ -3,6 +3,7 @@
 // SPDX-FileCopyrightText: 2023 TemporalOroboros <TemporalOroboros@gmail.com>
 // SPDX-FileCopyrightText: 2023 metalgearsloth <comedian_vs_clown@hotmail.com>
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
 // SPDX-FileCopyrightText: 2025 whateverusername0 <whateveremail>
 //
 // SPDX-License-Identifier: MIT
