2480bc1917dc80f5104faec13401246adfc94473
beck-thompson <107373427+beck-thompson@users.noreply.github.com>
2025-04-23 20:00:50 -0700

    Put items inside cakes! (#31015)
    * First commit
    
    * I'm silly
    
    * Please be it
    
    * Some more fixes
    
    * Cleanup
    
    * fine!
    
    * removed = false
    
    * review
    
    ---------
    
    Co-authored-by: Milon <milonpl.git@proton.me>

--- Content.Shared/Storage/Components/SecretStashComponent.cs
+++ Content.Shared/Storage/Components/SecretStashComponent.cs
@@ -83,6 +83,7 @@ using Robust.Shared.Prototypes;
 using Robust.Shared.GameStates;
 using Robust.Shared.Audio;
 using Content.Shared.Whitelist;
+using Content.Shared.Damage;
 
 namespace Content.Shared.Storage.Components
 {
@@ -133,6 +134,12 @@ namespace Content.Shared.Storage.Components
         /// </summary>
         [DataField]
         public string? SecretStashName;
+
+        /// <summary>
+        /// How much damage is delt to something after eating a secret stash that contains an item.
+        /// </summary>
+        [DataField]
+        public DamageSpecifier? DamageEatenItemInside;
 
         /// <summary>
         ///     Container used to keep secret stash item.
