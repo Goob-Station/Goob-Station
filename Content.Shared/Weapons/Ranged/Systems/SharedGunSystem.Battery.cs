97c27523b4705be7025e29de74d8670e150abf29
GoobBot <uristmchands@proton.me>
2025-04-25 03:04:29 +0000

    chore: Automatically update REUSE headers

--- Content.Shared/Weapons/Ranged/Systems/SharedGunSystem.Battery.cs
+++ Content.Shared/Weapons/Ranged/Systems/SharedGunSystem.Battery.cs
@@ -9,6 +9,7 @@
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 deltanedas <39013340+deltanedas@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 deltanedas <@deltanedas:kde.org>
+// SPDX-FileCopyrightText: 2025 whateverusername0 <whateveremail>
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
