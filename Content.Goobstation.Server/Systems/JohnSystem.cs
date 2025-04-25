d2424e3e06b20d77c7a18f3fe782e7dce0a5f364
LuciferEOS <stepanteliatnik2022@gmail.com>
2025-04-25 20:59:28 +0200

    John

---
+++ Content.Goobstation.Server/Systems/JohnSystem.cs
@@ -0,0 +1,42 @@
+using Content.Shared.Mind;
+using Content.Shared.Mind.Components;
+using Content.Shared.Tag;
+using Robust.Shared.Prototypes;
+
+namespace Content.Goobstation.Server.Systems;
+
+public sealed class JohnSystem : EntitySystem
+{
+    [Dependency] private readonly TagSystem _tag = default!;
+    [Dependency] private readonly IPrototypeManager _proto = default!;
+
+    private const string TargetTag = "JohnMind";
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<MetaDataComponent, ComponentInit>(OnMetadataInit);
+        SubscribeLocalEvent<MetaDataComponent, EntityRenamedEvent>(OnRenamed);
+    }
+
+    private void OnMetadataInit(EntityUid uid, MetaDataComponent component, ComponentInit args)
+    {
+        CheckJohn(uid, component.EntityName);
+    }
+
+    private void OnRenamed(EntityUid uid, MetaDataComponent component, ref EntityRenamedEvent args)
+    {
+        CheckJohn(uid, args.NewName);
+    }
+
+    private void CheckJohn(EntityUid uid, string? name)
+    {
+        if (string.IsNullOrEmpty(name) || !name.Contains("John", System.StringComparison.OrdinalIgnoreCase))
+            return;
+
+        var tag = _proto.Index<TagPrototype>(TargetTag);
+        if (!_tag.HasTag(uid, tag))
+            _tag.AddTag(uid, tag);
+    }
+}
