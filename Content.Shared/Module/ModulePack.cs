8a0c01ff71bc992a88fdc7eb1be1ef963d0617af
GoobBot <uristmchands@proton.me>
2025-04-25 23:13:41 +0000

    chore: Automatically update REUSE headers

--- Content.Shared/Module/ModulePack.cs
+++ Content.Shared/Module/ModulePack.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 August Eymann <august.eymann@gmail.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 namespace Content.Shared.Module;
 
 public record struct RequiredAssembly(string AssemblyName, bool Server = true, bool Client = false)
