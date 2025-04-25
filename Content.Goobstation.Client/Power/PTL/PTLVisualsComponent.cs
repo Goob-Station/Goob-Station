b0621999897de827e29727a85526a0990d0f9247
whateverusername0 <whateveremail>
2025-04-25 21:37:37 +1000

    spritemaxxing

--- Content.Goobstation.Client/Power/PTL/PTLVisualsComponent.cs
+++ Content.Goobstation.Client/Power/PTL/PTLVisualsComponent.cs
@@ -8,6 +8,6 @@ namespace Content.Goobstation.Client.Power.PTL;
 [RegisterComponent]
 public sealed partial class PTLVisualsComponent : Component
 {
-    [DataField] public string ChargePrefix = "charge_";
+    [DataField] public string ChargePrefix = "charge";
     [DataField] public int MaxChargeStates = 6;
 }
