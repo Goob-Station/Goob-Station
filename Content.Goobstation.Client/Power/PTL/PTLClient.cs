614d26e1fa23872dddd6639e2bbdc64e6f46a6ef
GoobBot <uristmchands@proton.me>
2025-04-25 06:43:07 +0000

    chore: Automatically update REUSE headers

--- Content.Goobstation.Client/Power/PTL/PTLClient.cs
+++ Content.Goobstation.Client/Power/PTL/PTLClient.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 whateverusername0 <whateveremail>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
