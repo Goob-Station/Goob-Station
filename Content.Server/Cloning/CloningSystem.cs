e825736d0183617388a1dbdc56906238a7073ff0
GoobBot <uristmchands@proton.me>
2025-04-25 15:45:00 +0000

    chore: Automatically update REUSE headers

--- Content.Server/Cloning/CloningSystem.cs
+++ Content.Server/Cloning/CloningSystem.cs
@@ -43,6 +43,7 @@
 // SPDX-FileCopyrightText: 2024 username <113782077+whateverusername0@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2024 whateverusername0 <whateveremail>
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
 // SPDX-FileCopyrightText: 2025 ScarKy0 <106310278+ScarKy0@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 deltanedas <39013340+deltanedas@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 deltanedas <@deltanedas:kde.org>
