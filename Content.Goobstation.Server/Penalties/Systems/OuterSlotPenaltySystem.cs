a7917b564b64ac0b1ac848658702c5ae2e94acb7
GoobBot <uristmchands@proton.me>
2025-04-26 20:28:25 +0000

    chore: Automatically update REUSE headers

--- Content.Goobstation.Server/Penalties/Systems/OuterSlotPenaltySystem.cs
+++ Content.Goobstation.Server/Penalties/Systems/OuterSlotPenaltySystem.cs
@@ -1,5 +1,6 @@
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 August Eymann <august.eymann@gmail.com>
+// SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
 // SPDX-FileCopyrightText: 2025 Misandry <mary@thughunt.ing>
 // SPDX-FileCopyrightText: 2025 Piras314 <p1r4s@proton.me>
 // SPDX-FileCopyrightText: 2025 SX-7 <92227810+SX-7@users.noreply.github.com>
