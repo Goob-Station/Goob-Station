af618b920cf7809eead6a707f9a795b3be875b7a
GoobBot <uristmchands@proton.me>
2025-04-26 15:30:44 +0000

    chore: Automatically update REUSE headers

--- Content.Server/Shuttles/Systems/ShuttleSystem.cs
+++ Content.Server/Shuttles/Systems/ShuttleSystem.cs
@@ -100,6 +100,8 @@
 // SPDX-FileCopyrightText: 2024 to4no_fix <156101927+chavonadelal@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2024 voidnull000 <18663194+voidnull000@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 FaDeOkno <143940725+FaDeOkno@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 FaDeOkno <logkedr18@gmail.com>
 // SPDX-FileCopyrightText: 2025 Leon Friedrich <60421075+ElectroJr@users.noreply.github.com>
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
