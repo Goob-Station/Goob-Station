f8ab4e96c14206760d055276c05a1e9b978c0cbc
GoobBot <uristmchands@proton.me>
2025-04-27 14:23:47 +0000

    chore: Automatically update REUSE headers

--- Content.Client/Construction/ConstructionSystem.cs
+++ Content.Client/Construction/ConstructionSystem.cs
@@ -28,6 +28,7 @@
 // SPDX-FileCopyrightText: 2024 metalgearsloth <31366439+metalgearsloth@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2024 plykiya <plykiya@protonmail.com>
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
 // SPDX-FileCopyrightText: 2025 Ilya246 <ilyukarno@gmail.com>
 // SPDX-FileCopyrightText: 2025 Kyle Tyo <36606155+VerinSenpai@users.noreply.github.com>
 //
