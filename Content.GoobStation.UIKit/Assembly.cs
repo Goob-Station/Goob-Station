b7e2e4ff5e1ba65ebf6f35213ae2a93c72040ca6
GoobBot <uristmchands@proton.me>
2025-04-28 20:18:21 +0000

    chore: Automatically update REUSE headers

--- Content.GoobStation.UIKit/Assembly.cs
+++ Content.GoobStation.UIKit/Assembly.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 August Eymann <august.eymann@gmail.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using Avalonia.Metadata;
 
 [assembly: XmlnsDefinition("https://goobstation.com/", clrNamespace: "Content.GoobStation.UIKit.Widgets")]
