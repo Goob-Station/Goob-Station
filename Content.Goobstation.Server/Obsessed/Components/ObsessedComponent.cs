86266746d2e39785588ca80b983c12824f81e20e
FaDeOkno <logkedr18@gmail.com>
2025-04-30 19:01:28 +0400

    wip(src): New fields in component and wip updates

--- Content.Goobstation.Server/Obsessed/Components/ObsessedComponent.cs
+++ Content.Goobstation.Server/Obsessed/Components/ObsessedComponent.cs
@@ -3,7 +3,8 @@
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-using Robust.Shared.GameStates;
+using Content.Shared.Dataset;
+using Robust.Shared.Prototypes;
 
 namespace Content.Goobstation.Server.Obsessed;
 
@@ -16,11 +17,13 @@ public sealed partial class ObsessedComponent : Component
     /// Identifier of the obsession target
     /// </summary>
     /// <remarks>
-    /// We are using integer to for proper photos work
+    /// We are using int to for proper photos work
     /// </remarks>
-    [ViewVariables]
-    public int Target = 0;
+    [ViewVariables(VVAccess.ReadWrite)]
+    public int TargetId = 0;
 
+    [ViewVariables(VVAccess.ReadWrite)]
+    public EntityUid Target = EntityUid.Invalid;
     public float Sanity
     {
         get => _sanity;
@@ -33,21 +36,21 @@ public sealed partial class ObsessedComponent : Component
     /// <summary>
     /// Player sanity level. If it becomes too low, the obsession will become more hostile.
     /// </summary>
-    [ViewVariables]
+    [ViewVariables(VVAccess.ReadWrite)]
     private float _sanity = 100f;
 
-    [ViewVariables(VVAccess.ReadOnly)]
-    public TimeSpan NextUpdate = TimeSpan.Zero;
+    [DataField]
+    public float SanityLoss = -1.5f;
 
     /// <summary>
     /// How much sanity every interaction recovers.
     /// </summary>
-    [DataField]
+    [DataField, ViewVariables(VVAccess.ReadWrite)]
     public Dictionary<ObsessionInteraction, float> SanityRecovery = new()
     {
         { ObsessionInteraction.Hug, 2f },
         { ObsessionInteraction.Grab, 4f },
-        { ObsessionInteraction.Follow, 1f },
+        { ObsessionInteraction.Follow, 2f },
         { ObsessionInteraction.PhotoTake, 10f },
         { ObsessionInteraction.PhotoLook, 5f },
         { ObsessionInteraction.Hear, 2f },
@@ -57,7 +60,7 @@ public sealed partial class ObsessedComponent : Component
     /// <summary>
     /// How low sanity must be to trigger an effect.
     /// </summary>
-    [DataField]
+    [DataField, ViewVariables(VVAccess.ReadWrite)]
     public Dictionary<ObsessionEffect, float> SanityThresholds = new()
     {
         { ObsessionEffect.Popup, 75f },
@@ -66,4 +69,41 @@ public sealed partial class ObsessedComponent : Component
         { ObsessionEffect.Speech, 35f },
         { ObsessionEffect.Damage, 10f }
     };
+
+    /// <summary>
+    /// Pack for low sanity popups
+    /// </summary>
+    [DataField, ViewVariables(VVAccess.ReadWrite)]
+    public ProtoId<LocalizedDatasetPrototype> PopupDataset = "ObsessionPopups";
+
+    /// <summary>
+    /// Contains the last interactions with the target. Exsists to avoid hug/grab/other spam
+    /// Does not contains seeing and following
+    /// </summary>
+    [ViewVariables(VVAccess.ReadOnly)]
+    public List<ObsessionInteraction> LastInteractions = new();
+
+    /// <summary>
+    /// A counter for updates that we see the target
+    /// </summary>
+    [ViewVariables(VVAccess.ReadWrite)]
+    public int FollowUpdates = 0;
+
+    /// <summary>
+    /// How many updates we need to see the target to start recovering additional sanity
+    /// </summary>
+    [DataField]
+    public int FollowUpdatesToRecovery = 10;
+
+    [ViewVariables(VVAccess.ReadOnly)]
+    public TimeSpan SanityNextUpdate = TimeSpan.Zero;
+
+    [ViewVariables(VVAccess.ReadWrite)]
+    public float SanityUpdateInterval = 2f;
+
+    [ViewVariables(VVAccess.ReadOnly)]
+    public TimeSpan EffectNextUpdate = TimeSpan.Zero;
+
+    [ViewVariables(VVAccess.ReadWrite)]
+    public (float, float) EffectUpdateInterval = (7f, 15f);
 }
--- Content.Goobstation.Server/Obsessed/Systems/ObsessedSystem.cs
+++ Content.Goobstation.Server/Obsessed/Systems/ObsessedSystem.cs
@@ -1,10 +1,86 @@
+using System.Linq;
+using Content.Server.Interaction;
+using Content.Server.Jittering;
+using Robust.Shared.Random;
+using Robust.Shared.Timing;
+
 namespace Content.Goobstation.Server.Obsessed;
 
 public sealed class ObsessedSystem : EntitySystem
 {
+    [Dependency] private readonly IGameTiming _timing = default!;
+    [Dependency] private readonly IRobustRandom _random = default!;
+    [Dependency] private readonly EntityLookupSystem _lookup = default!;
+    [Dependency] private readonly InteractionSystem _interaction = default!;
+    [Dependency] private readonly JitteringSystem _jittering = default!;
+
     public override void Initialize()
     {
         base.Initialize();
 
+    }
+
+    public override void Update(float frameTime)
+    {
+        base.Update(frameTime);
+        var query = EntityQueryEnumerator<ObsessedComponent>();
+        while (query.MoveNext(out var uid, out var comp))
+        {
+            UpdateSanity(uid, comp);
+            UpdateEffects(uid, comp);
+        }
+    }
+
+    private void UpdateSanity(EntityUid uid, ObsessedComponent comp)
+    {
+        if (comp.SanityNextUpdate > _timing.CurTime)
+            return;
+
+        comp.SanityNextUpdate = _timing.CurTime + TimeSpan.FromSeconds(comp.SanityUpdateInterval);
+
+        comp.Sanity += comp.SanityLoss;
+
+        if (_interaction.InRangeUnobstructed(comp.Target, Transform(uid).Coordinates, 7f))
+        {
+            comp.Sanity += comp.SanityRecovery[comp.FollowUpdates >= comp.FollowUpdatesToRecovery ? ObsessionInteraction.Follow : ObsessionInteraction.See];
+            comp.FollowUpdates = Math.Clamp(comp.FollowUpdates + 1, 0, 12);
+        }
+        else
+            comp.FollowUpdates = Math.Clamp(comp.FollowUpdates - 1, 0, 12);
+    }
+
+    private void UpdateEffects(EntityUid uid, ObsessedComponent comp)
+    {
+        if (comp.EffectNextUpdate > _timing.CurTime)
+            return;
+
+        comp.EffectNextUpdate = _timing.CurTime + TimeSpan.FromSeconds(_random.NextFloat(comp.EffectUpdateInterval.Item1, comp.EffectUpdateInterval.Item2));
+
+        List<ObsessionEffect> effects = comp.SanityThresholds
+                                          .Where(x => x.Value >= comp.Sanity)
+                                          .Select(x => x.Key)
+                                          .ToList();
+
+        if (effects.Any())
+            DoEffect(uid, comp, _random.Pick(effects));
+    }
+
+    private void DoEffect(EntityUid uid, ObsessedComponent comp, ObsessionEffect effect)
+    {
+        // todo
+        switch (effect)
+        {
+            case ObsessionEffect.Popup:
+                break;
+            case ObsessionEffect.Shake:
+                _jittering.DoJitter(uid, TimeSpan.FromSeconds(1f), true, 6f, 6f);
+                break;
+            case ObsessionEffect.Sound:
+                break;
+            case ObsessionEffect.Speech:
+                break;
+            case ObsessionEffect.Damage:
+                break;
+        }
     }
 }
