5375b8d8f64dfb76042e9e131c77edfeb2771d77
GoobBot <uristmchands@proton.me>
2025-04-30 16:24:42 +0000

    chore: Automatically update REUSE headers

--- Content.Server/Power/Pow3r/PowerSolverShared.cs
+++ Content.Server/Power/Pow3r/PowerSolverShared.cs
@@ -2,6 +2,7 @@
 // SPDX-FileCopyrightText: 2021 Pieter-Jan Briers <pieterjan.briers+git@gmail.com>
 // SPDX-FileCopyrightText: 2022 wrexbe <81056464+wrexbe@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
 // SPDX-FileCopyrightText: 2025 Ilya246 <ilyukarno@gmail.com>
 //
 // SPDX-License-Identifier: MIT
--- Content.Server/Power/Pow3r/PowerState.cs
+++ Content.Server/Power/Pow3r/PowerState.cs
@@ -8,6 +8,7 @@
 // SPDX-FileCopyrightText: 2023 TemporalOroboros <TemporalOroboros@gmail.com>
 // SPDX-FileCopyrightText: 2024 Piras314 <p1r4s@proton.me>
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
 // SPDX-FileCopyrightText: 2025 Ilya246 <ilyukarno@gmail.com>
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
