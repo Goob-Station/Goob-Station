9d9e23cfe0e45d6c2808f533c85da6d743b83adc
GoobBot <uristmchands@proton.me>
2025-05-01 13:08:46 +0000

    chore: Automatically update REUSE headers

--- Content.Shared/Item/ItemToggle/ItemToggleSystem.cs
+++ Content.Shared/Item/ItemToggle/ItemToggleSystem.cs
@@ -42,6 +42,7 @@
 // SPDX-FileCopyrightText: 2024 themias <89101928+themias@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Aviu00 <aviu00@protonmail.com>
+// SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
 // SPDX-FileCopyrightText: 2025 SX_7 <sn1.test.preria.2002@gmail.com>
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
