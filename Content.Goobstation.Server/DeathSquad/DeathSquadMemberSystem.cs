2b38d52928fc2b1a536a860aeb35b664c586c37b
GoobBot <uristmchands@proton.me>
2025-05-01 05:06:47 +0000

    chore: Automatically update REUSE headers

--- Content.Goobstation.Server/DeathSquad/DeathSquadMemberSystem.cs
+++ Content.Goobstation.Server/DeathSquad/DeathSquadMemberSystem.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 SolsticeOfTheWinter <solsticeofthewinter@gmail.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using Content.Shared.Examine;
 using Content.Shared.IdentityManagement;
 using Content.Shared.Mobs;
