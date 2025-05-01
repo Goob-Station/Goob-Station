eb28efac89d8e4893ec822b9a83833f9945b48e4
GoobBot <uristmchands@proton.me>
2025-05-01 23:53:41 +0000

    chore: Automatically update REUSE headers

--- Content.Goobstation.Shared/Bible/BibleComponent.cs
+++ Content.Goobstation.Shared/Bible/BibleComponent.cs
@@ -5,6 +5,10 @@
 // SPDX-FileCopyrightText: 2022 wrexbe <81056464+wrexbe@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2023 DrSmugleaf <DrSmugleaf@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
+// SPDX-FileCopyrightText: 2025 Solstice <solsticeofthewinter@gmail.com>
+// SPDX-FileCopyrightText: 2025 SolsticeOfTheWinter <solsticeofthewinter@gmail.com>
+// SPDX-FileCopyrightText: 2025 TheBorzoiMustConsume <197824988+TheBorzoiMustConsume@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Winkarst <74284083+Winkarst-cpu@users.noreply.github.com>
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
