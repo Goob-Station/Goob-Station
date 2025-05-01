032e724558f031ec369ed279b0b712c4464fac6c
GoobBot <uristmchands@proton.me>
2025-05-01 12:05:33 +0000

    chore: Automatically update REUSE headers

--- Content.Goobstation.Shared/MartialArts/SharedMartialArtsSystem.CorporateJudo.cs
+++ Content.Goobstation.Shared/MartialArts/SharedMartialArtsSystem.CorporateJudo.cs
@@ -7,6 +7,7 @@
 // SPDX-FileCopyrightText: 2025 Lincoln McQueen <lincoln.mcqueen@gmail.com>
 // SPDX-FileCopyrightText: 2025 Misandry <mary@thughunt.ing>
 // SPDX-FileCopyrightText: 2025 gus <august.eymann@gmail.com>
+// SPDX-FileCopyrightText: 2025 pheenty <fedorlukin2006@gmail.com>
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
