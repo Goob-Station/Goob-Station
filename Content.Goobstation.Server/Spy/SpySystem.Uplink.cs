ec2c1ee0a207de81ab4f1d9841f8e195fb8737b3
SolsticeOfTheWinter <solsticeofthewinter@gmail.com>
2025-05-01 20:22:05 -1000

    moooore uplinks
    Signed-off-by: SolsticeOfTheWinter <solsticeofthewinter@gmail.com>

--- Content.Goobstation.Server/Spy/SpySystem.Uplink.cs
+++ Content.Goobstation.Server/Spy/SpySystem.Uplink.cs
@@ -4,14 +4,20 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
 using System.Linq;
+using Content.Goobstation.Server.Spy.Roles;
 using Content.Goobstation.Shared.Hologram;
 using Content.Goobstation.Shared.Spy;
 using Content.Server.Objectives.Components.Targets;
+using Content.Server.PDA.Ringer;
+using Content.Shared.Chat;
 using Content.Shared.DoAfter;
 using Content.Shared.Holopad;
 using Content.Shared.Interaction;
 using Content.Shared.Objectives;
+using Content.Shared.PDA;
+using Content.Shared.Verbs;
 using Robust.Shared.Audio;
+using Robust.Shared.Utility;
 
 namespace Content.Goobstation.Server.Spy;
 
@@ -20,17 +26,68 @@ namespace Content.Goobstation.Server.Spy;
 /// </summary>
 public sealed partial class SpySystem
 {
-    private static readonly SoundSpecifier StealSound =
-        new SoundPathSpecifier("/Audio/_Goobstation/Machines/wewewew.ogg");
-
-    private static readonly SoundSpecifier StealSuccessSound = new SoundPathSpecifier("/Audio/Effects/kaching.ogg");
-    private static readonly TimeSpan StealTime = TimeSpan.FromSeconds(5);
+    private static readonly SoundSpecifier CodeGeneratedSound = new SoundPathSpecifier("/Audio/Items/Medical/healthscanner.ogg");
+    private static readonly TimeSpan UplinkCreateDoAfterDuration = TimeSpan.FromSeconds(5);
 
     /// <inheritdoc/>
     private void InitializeUplink()
     {
+        SubscribeLocalEvent<PdaComponent, GetVerbsEvent<AlternativeVerb>>(OnGetVerbs);
+        SubscribeLocalEvent<PdaComponent, UplinkCreateDoAfterEvent>(OnCreateUplink);
         SubscribeLocalEvent<SpyUplinkComponent, AfterInteractEvent>(OnInteractEvent);
         SubscribeLocalEvent<SpyUplinkComponent, SpyStealDoAfterEvent>(OnSpyAttemptSteal);
+    }
+
+    private void OnGetVerbs(Entity<PdaComponent> ent, ref GetVerbsEvent<AlternativeVerb> args)
+    {
+        if (!args.CanAccess || !args.CanInteract || !args.CanComplexInteract)
+            return;
+
+        if (!_mind.TryGetMind(args.User, out var mindId, out _) // im sorry but this shit is disgusting in one if statement.
+        || !_role.MindHasRole<SpyRoleComponent>(mindId)
+        || HasComp<SpyUplinkComponent>(ent))
+            return;
+
+        var user = args.User;
+
+        AlternativeVerb createUplinkVerb = new()
+        {
+            Act = () => {StartCreateDoAfter(ent, user);},
+            Text = Loc.GetString("spy-create-uplink-verb"),
+            Icon = new SpriteSpecifier.Rsi(new("Objects/Devices/communication.rsi"), "radio"),
+            Priority = 1,
+        };
+
+        args.Verbs.Add(createUplinkVerb);
+    }
+
+    private void StartCreateDoAfter(Entity<PdaComponent> ent, EntityUid performer)
+    {
+        var doAfterArgs = new DoAfterArgs(
+            EntityManager,
+            performer,
+            UplinkCreateDoAfterDuration,
+            new UplinkCreateDoAfterEvent(),
+            ent)
+        {
+            BreakOnMove = true,
+            NeedHand = true,
+            BlockDuplicate = true,
+            BreakOnHandChange = true,
+        };
+
+        _doAfter.TryStartDoAfter(doAfterArgs);
+    }
+
+    private void OnCreateUplink(Entity<PdaComponent> ent, ref UplinkCreateDoAfterEvent args)
+    {
+        _uplink.AddUplink(args.User, 0, ent);
+
+        var code = EnsureComp<RingerUplinkComponent>(ent).Code;
+        EnsureComp<SpyUplinkComponent>(ent);
+
+        var codeMessage = Loc.GetString($"traitor-role-uplink-code-short", ("code", string.Join("-", code).Replace("sharp", "#")));
+        _antag.SendBriefing(args.User, codeMessage, Color.Crimson, CodeGeneratedSound);
     }
 
     private void OnInteractEvent(Entity<SpyUplinkComponent> ent, ref AfterInteractEvent args)
@@ -46,8 +103,7 @@ public sealed partial class SpySystem
 
         // Find matching bounty
         var matchingBounty = dbEnt.Comp.Bounties.FirstOrDefault(b =>
-            b.StealGroup == stealComp.StealGroup &&
-            !b.Claimed);
+            b.StealGroup == stealComp.StealGroup && !b.Claimed);
 
         if(!_protoMan.TryIndex<StealTargetGroupPrototype>(stealComp.StealGroup, out var prototype)) // store steal time here
             return;
@@ -55,15 +111,15 @@ public sealed partial class SpySystem
         if (matchingBounty == null)
             return;
 
-        var sound = _audio.PlayPvs(StealSound, ent, AudioParams.Default.WithLoop(true));
+        var sound = _audio.PlayPvs(ent.Comp.StealSound, ent, AudioParams.Default.WithLoop(true));
         if (sound is null)
             return;
 
-        var comp = EnsureComp<HologramComponent>(args.Target.Value);
+        EnsureComp<HologramComponent>(args.Target.Value);
 
         var doAfterArgs = new DoAfterArgs(_entityManager,
             args.User,
-            StealTime,
+            ent.Comp.StealTime,
             new SpyStealDoAfterEvent(GetNetEntity(sound.Value.Entity), stealComp.StealGroup), // Now passing steal group instead of entity
             ent,
             target: target);
@@ -75,10 +131,9 @@ public sealed partial class SpySystem
     {
         _audio.Stop(GetEntity(args.Sound));
 
-        if (args.Handled || args.Cancelled)
-            return;
-
-        if (!TrySetBountyClaimed(ent, args.Args.User, args.StealGroup, out var bountyData))
+        if (args.Handled
+        || args.Cancelled
+        || !TrySetBountyClaimed(ent, args.Args.User, args.StealGroup, out var bountyData))
             return;
 
         if (args.Args.Target != null)
@@ -86,6 +141,6 @@ public sealed partial class SpySystem
 
         var reward = Spawn(bountyData.RewardListing.ProductEntity, Transform(args.Args.User).Coordinates);
         _hands.PickupOrDrop(args.Args.User, reward);
-        _audio.PlayLocal(StealSuccessSound, ent, args.Args.User);
+        _audio.PlayLocal(ent.Comp.StealSuccessSound, ent, args.Args.User);
     }
 }
