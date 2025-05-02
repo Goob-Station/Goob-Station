ae454da4270f0f0093ec4cbca1ac27d348ed2f92
Ilya246 <ilyukarno@gmail.com>
2025-05-02 18:12:47 +0400

    fix test

--- Content.Server/GameTicking/Rules/NukeopsRuleSystem.cs
+++ Content.Server/GameTicking/Rules/NukeopsRuleSystem.cs
@@ -336,7 +336,7 @@ public sealed class NukeopsRuleSystem : GameRuleSystem<NukeopsRuleComponent>
 
     private void OnComponentRemove(EntityUid uid, NukeOperativeComponent component, ComponentRemove args)
     {
-        CheckRoundShouldEnd();
+        CheckRoundShouldEnd(false); // Goobstation
     }
 
     private void OnMobStateChanged(EntityUid uid, NukeOperativeComponent component, MobStateChangedEvent ev)
@@ -503,16 +503,18 @@ public sealed class NukeopsRuleSystem : GameRuleSystem<NukeopsRuleComponent>
             _roundEndSystem.EndRound();
     }
 
-    private void CheckRoundShouldEnd()
+    private void CheckRoundShouldEnd(bool announce = true) // Goobstation
     {
         var query = QueryActiveRules();
         while (query.MoveNext(out var uid, out _, out var nukeops, out _))
         {
-            CheckRoundShouldEnd((uid, nukeops));
+            CheckRoundShouldEnd((uid, nukeops),
+                                announce); // Goobstation
         }
     }
 
-    private void CheckRoundShouldEnd(Entity<NukeopsRuleComponent> ent)
+    private void CheckRoundShouldEnd(Entity<NukeopsRuleComponent> ent,
+                                     bool announce = true) // Goobstation
     {
         var nukeops = ent.Comp;
 
@@ -569,9 +571,10 @@ public sealed class NukeopsRuleSystem : GameRuleSystem<NukeopsRuleComponent>
         SetWinType(ent, WinType.CrewMajor, false);
 
         // goob edit - no more roundend behavior, just announcement
-        _chat.DispatchGlobalAnnouncement(
-            Loc.GetString(nukeops.RoundEndTextAnnouncement),
-            Loc.GetString(nukeops.RoundEndTextSender));
+        if (announce)
+            _chat.DispatchGlobalAnnouncement(
+                Loc.GetString(nukeops.RoundEndTextAnnouncement),
+                Loc.GetString(nukeops.RoundEndTextSender));
 
         // prevent it called multiple times
         nukeops.RoundEndBehavior = RoundEndBehavior.Nothing;
