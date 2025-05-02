3faf660c2d7601a2b26b996edf88f016fa864e5f
August Eymann <august.eymann@gmail.com>
2025-05-01 23:31:42 -0700

    action

---
+++ Content.Goobstation.Shared/ActionTargetMarkSystem/ActionTargetMarkComponent.cs
@@ -0,0 +1,10 @@
+namespace Content.Goobstation.Shared.ActionTargetMarkSystem;
+
+/// <summary>
+/// This is used for...
+/// </summary>
+[RegisterComponent]
+public sealed partial class ActionTargetMarkComponent : Component
+{
+    
+}
--- Content.Goobstation.Shared/ActionTargetMarkSystem/IActionTargetMarkSystem.cs
+++
@@ -1,9 +0,0 @@
-namespace Content.Goobstation.Shared.ActionTargetMarkSystem;
-
-public interface IActionTargetMarkSystem : IEntitySystem
-{
-    EntityUid? Target { get; }
-    EntityUid? Mark { get; }
-    void SetMark(EntityUid? uid);
-}
-
