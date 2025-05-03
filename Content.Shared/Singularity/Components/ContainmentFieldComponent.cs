c52e96a902245513a8b03303f2b21cbc4006a05a
GoobBot <uristmchands@proton.me>
2025-05-03 11:11:48 +0000

    chore: Automatically update REUSE headers

--- Content.Shared/Singularity/Components/ContainmentFieldComponent.cs
+++ Content.Shared/Singularity/Components/ContainmentFieldComponent.cs
@@ -76,6 +76,7 @@
 // SPDX-FileCopyrightText: 2024 slarticodefast <161409025+slarticodefast@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2024 Арт <123451459+JustArt1m@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
 // SPDX-FileCopyrightText: 2025 Ilya246 <ilyukarno@gmail.com>
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
