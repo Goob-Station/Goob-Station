6b3a250fb976280aaac94854833cec5839820f5a
GoobBot <uristmchands@proton.me>
2025-05-03 07:41:56 +0000

    chore: Automatically update REUSE headers

--- Content.Goobstation.Server/Bingle/BinglePitSystem.cs
+++ Content.Goobstation.Server/Bingle/BinglePitSystem.cs
@@ -3,6 +3,7 @@
 // SPDX-FileCopyrightText: 2025 Aidenkrz <aiden@djkraz.com>
 // SPDX-FileCopyrightText: 2025 Aviu00 <93730715+Aviu00@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Fishbait <Fishbait@git.ml>
+// SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
 // SPDX-FileCopyrightText: 2025 Ilya246 <57039557+Ilya246@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Ilya246 <ilyukarno@gmail.com>
 // SPDX-FileCopyrightText: 2025 Misandry <mary@thughunt.ing>
