27de059440477f5e5a339668ac08687f13cdaf86
Oxdeception <30361731+Oxdeception@users.noreply.github.com>
2025-05-03 18:36:13 -0400

    Adds a typing indicator to the AAC (#3595)
    * Adds a typing indicator to the AAC
    
    * Removes duplicate TypingChangedEvent constructor
    
    * Updates sprite attribution
    
    * Corrects fork addition comment style
    
    * Moves typing indicator override from Appearance to TypingIndicatorComponent
    
    * Reduces retrievals of TypingIndicatorSystem in the AAC BUI
    
    * Moves ClientAlternateTyping to _DV
    
    * Adds missing comment
    
    * Sets TypingIndicatorComponent Dirty
    
    * Caches ShowTypingIndicator in AlternateTypingIndicator
    
    * Removes unnecessary default assignment
    
    ---------
    
    Co-authored-by: Oxdeception <9624-oxdeception@users.noreply.gitgud.io>

--- Content.Shared/Chat/TypingIndicator/SharedTypingIndicatorSystem.cs
+++ Content.Shared/Chat/TypingIndicator/SharedTypingIndicatorSystem.cs
@@ -12,7 +12,7 @@ using Content.Shared.ActionBlocker;
 using Content.Shared.Clothing;
 using Content.Shared.Inventory;
 using Robust.Shared.Player;
-using Robust.Shared.Prototypes;
+using Robust.Shared.Prototypes; // DeltaV: TypingIndicator overrides
 using Robust.Shared.Timing;
 
 namespace Content.Shared.Chat.TypingIndicator;
@@ -30,6 +30,7 @@ public abstract class SharedTypingIndicatorSystem : EntitySystem
     ///     Default ID of <see cref="TypingIndicatorPrototype"/>
     /// </summary>
     public static readonly ProtoId<TypingIndicatorPrototype> InitialIndicatorId = "default";
+	public const string InitialIndicatorId = "default";
 
     public override void Initialize()
     {
@@ -99,5 +100,16 @@ public abstract class SharedTypingIndicatorSystem : EntitySystem
             return;
 
         _appearance.SetData(uid, TypingIndicatorVisuals.State, state, appearance);
+    }
+
+    /// <summary>
+    /// DeltaV: Adds an override to the TypingIndicator visuals
+    /// </summary>
+    /// <param name="protoId">The TypingIndicator to use in place of default or clothing indicators. Clears overrides when null.</param>
+    private void SetTypingOverride(EntityUid uid, ProtoId<TypingIndicatorPrototype>? protoId)
+    {
+        var comp = EnsureComp<TypingIndicatorComponent>(uid);
+        comp.TypingIndicatorOverridePrototype = protoId;
+        Dirty(uid, comp);
     }
 }
--- Content.Shared/Chat/TypingIndicator/TypingIndicatorEvents.cs
+++ Content.Shared/Chat/TypingIndicator/TypingIndicatorEvents.cs
@@ -18,10 +18,12 @@ namespace Content.Shared.Chat.TypingIndicator;
 public sealed class TypingChangedEvent : EntityEventArgs
 {
     public readonly TypingIndicatorState State;
+    public readonly ProtoId<TypingIndicatorPrototype>? OverrideIndicator; // DeltaV
 
     public TypingChangedEvent(TypingIndicatorState state)
     {
         State = state;
+        OverrideIndicator = proto; // DeltaV
     }
 }
 
