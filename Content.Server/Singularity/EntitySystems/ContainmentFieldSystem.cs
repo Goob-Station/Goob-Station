ca580af4db5c0135552a34b847ba9c58ddbb44a8
GoobBot <uristmchands@proton.me>
2025-05-03 10:37:37 +0000

    chore: Automatically update REUSE headers

--- Content.Server/Singularity/EntitySystems/ContainmentFieldSystem.cs
+++ Content.Server/Singularity/EntitySystems/ContainmentFieldSystem.cs
@@ -103,6 +103,7 @@
 // SPDX-FileCopyrightText: 2024 voidnull000 <18663194+voidnull000@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2024 Арт <123451459+JustArt1m@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 Ilya246 <ilyukarno@gmail.com>
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
