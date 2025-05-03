f66f877c0b47d6430bfc2260a70747ec541320b8
GoobBot <uristmchands@proton.me>
2025-05-03 11:58:37 +0000

    chore: Automatically update REUSE headers

--- Content.Server/Power/EntitySystems/ChargerSystem.cs
+++ Content.Server/Power/EntitySystems/ChargerSystem.cs
@@ -101,6 +101,7 @@
 // SPDX-FileCopyrightText: 2025 BombasterDS <deniskaporoshok@gmail.com>
 // SPDX-FileCopyrightText: 2025 BombasterDS2 <shvalovdenis.workmail@gmail.com>
 // SPDX-FileCopyrightText: 2025 Errant <35878406+Errant-4@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
 // SPDX-FileCopyrightText: 2025 Misandry <mary@thughunt.ing>
 // SPDX-FileCopyrightText: 2025 gus <august.eymann@gmail.com>
 // SPDX-FileCopyrightText: 2025 pacable <igor.mamaev1@gmail.com>
