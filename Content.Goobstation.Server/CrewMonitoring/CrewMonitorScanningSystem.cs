2b91cd960813ddd85873e01cbb89a72126ba08eb
GoobBot <uristmchands@proton.me>
2025-05-04 12:53:14 +0000

    chore: Automatically update REUSE headers

--- Content.Goobstation.Server/CrewMonitoring/CrewMonitorScanningSystem.cs
+++ Content.Goobstation.Server/CrewMonitoring/CrewMonitorScanningSystem.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 pheenty <fedorlukin2006@gmail.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using Content.Goobstation.Server.RelayedDeathrattle;
 using Content.Goobstation.Shared.CrewMonitoring;
 using Content.Server.Revolutionary.Components;
