13da2fcb6a868dafcb7243fb1c792624f91c6976
GoobBot <uristmchands@proton.me>
2025-05-04 22:31:20 +0000

    chore: Automatically update REUSE headers

--- Content.Goobstation.Shared/Factory/AutomationSystem.cs
+++ Content.Goobstation.Shared/Factory/AutomationSystem.cs
@@ -1,4 +1,5 @@
 // SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
+// SPDX-FileCopyrightText: 2025 deltanedas <39013340+deltanedas@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 deltanedas <@deltanedas:kde.org>
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
