1cf6a84b7ef6da4b232ae410015163402c6527f8
GoobBot <uristmchands@proton.me>
2025-05-07 18:35:39 +0000

    chore: Automatically update REUSE headers

--- Content.Server/Singularity/EntitySystems/SingularityGeneratorSystem.cs
+++ Content.Server/Singularity/EntitySystems/SingularityGeneratorSystem.cs
@@ -7,6 +7,7 @@
 // SPDX-FileCopyrightText: 2024 Saphire <lattice@saphi.re>
 // SPDX-FileCopyrightText: 2024 SlamBamActionman <slambamactionman@gmail.com>
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 Ilya246 <ilyukarno@gmail.com>
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
