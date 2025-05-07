5c3fdb8690bb35c522e6205912ea662c8da8181d
GoobBot <uristmchands@proton.me>
2025-05-07 11:41:16 +0000

    chore: Automatically update REUSE headers

--- Content.Goobstation.Shared/Waddle/WaddleClothingSystem.cs
+++ Content.Goobstation.Shared/Waddle/WaddleClothingSystem.cs
@@ -1,4 +1,5 @@
 // SPDX-FileCopyrightText: 2024 Hannah Giovanna Dawson <karakkaraz@gmail.com>
+// SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
 // SPDX-FileCopyrightText: 2025 deltanedas <@deltanedas:kde.org>
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
