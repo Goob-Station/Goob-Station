29db1b58a48c695a078d0bc3b30a2a1567787433
GoobBot <uristmchands@proton.me>
2025-05-07 16:32:14 +0000

    chore: Automatically update REUSE headers

--- Content.Goobstation.Server/Shuttle/ShuttleImpactSystem.cs
+++ Content.Goobstation.Server/Shuttle/ShuttleImpactSystem.cs
@@ -1,4 +1,5 @@
 // SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
+// SPDX-FileCopyrightText: 2025 Ilya246 <57039557+Ilya246@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Ilya246 <ilyukarno@gmail.com>
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
