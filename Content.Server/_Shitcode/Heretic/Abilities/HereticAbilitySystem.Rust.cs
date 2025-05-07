aa674e84ffb7ed3b574d0aa86fa17098f48aec8b
GoobBot <uristmchands@proton.me>
2025-05-07 05:21:10 +0000

    chore: Automatically update REUSE headers

--- Content.Server/_Shitcode/Heretic/Abilities/HereticAbilitySystem.Rust.cs
+++ Content.Server/_Shitcode/Heretic/Abilities/HereticAbilitySystem.Rust.cs
@@ -4,6 +4,7 @@
 // SPDX-FileCopyrightText: 2025 Aviu00 <aviu00@protonmail.com>
 // SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
 // SPDX-FileCopyrightText: 2025 Misandry <mary@thughunt.ing>
+// SPDX-FileCopyrightText: 2025 TheBorzoiMustConsume <197824988+TheBorzoiMustConsume@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 gus <august.eymann@gmail.com>
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
