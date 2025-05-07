4eae5645bec44e9ae875b1c43ea9c5a4f96224ea
GoobBot <uristmchands@proton.me>
2025-05-07 19:58:01 +0000

    chore: Automatically update REUSE headers

--- Content.Client/Overlays/EntityHealthBarOverlay.cs
+++ Content.Client/Overlays/EntityHealthBarOverlay.cs
@@ -74,6 +74,7 @@
 // SPDX-FileCopyrightText: 2024 to4no_fix <156101927+chavonadelal@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2024 voidnull000 <18663194+voidnull000@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 gus <august.eymann@gmail.com>
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
--- Content.Client/UserInterface/Systems/DamageOverlays/DamageOverlayUiController.cs
+++ Content.Client/UserInterface/Systems/DamageOverlays/DamageOverlayUiController.cs
@@ -8,6 +8,7 @@
 // SPDX-FileCopyrightText: 2024 Pieter-Jan Briers <pieterjan.briers+git@gmail.com>
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Coolsurf6 <coolsurf24@yahoo.com.au>
+// SPDX-FileCopyrightText: 2025 gus <august.eymann@gmail.com>
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
