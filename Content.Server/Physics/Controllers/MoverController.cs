56d195f2872f62ae672bf23bde3ef864b67e87bd
GoobBot <uristmchands@proton.me>
2025-05-07 16:33:23 +0000

    chore: Automatically update REUSE headers

--- Content.Server/Physics/Controllers/MoverController.cs
+++ Content.Server/Physics/Controllers/MoverController.cs
@@ -51,6 +51,7 @@
 // SPDX-FileCopyrightText: 2024 plykiya <plykiya@protonmail.com>
 // SPDX-FileCopyrightText: 2024 {Koks} <koks@blue-creature.com>
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
 // SPDX-FileCopyrightText: 2025 Ilya246 <ilyukarno@gmail.com>
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
