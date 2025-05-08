3294b23225f89107cbb0661b3c1a15ec192220b7
GoobBot <uristmchands@proton.me>
2025-05-08 01:03:08 +0000

    chore: Automatically update REUSE headers

--- Content.Shared/Magic/SharedMagicSystem.cs
+++ Content.Shared/Magic/SharedMagicSystem.cs
@@ -76,6 +76,7 @@
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Aviu00 <93730715+Aviu00@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Coenx-flex <coengmurray@gmail.com>
+// SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
 // SPDX-FileCopyrightText: 2025 Misandry <mary@thughunt.ing>
 // SPDX-FileCopyrightText: 2025 TheBorzoiMustConsume <197824988+TheBorzoiMustConsume@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 gus <august.eymann@gmail.com>
