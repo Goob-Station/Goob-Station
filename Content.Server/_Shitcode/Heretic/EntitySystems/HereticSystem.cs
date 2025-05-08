1a60db91bc84aeb92c19fedb77a1ea9d6ec730fe
GoobBot <uristmchands@proton.me>
2025-05-08 09:21:17 +0000

    chore: Automatically update REUSE headers

--- Content.Server/_Shitcode/Heretic/EntitySystems/HereticSystem.cs
+++ Content.Server/_Shitcode/Heretic/EntitySystems/HereticSystem.cs
@@ -9,6 +9,7 @@
 // SPDX-FileCopyrightText: 2025 Misandry <mary@thughunt.ing>
 // SPDX-FileCopyrightText: 2025 SolsticeOfTheWinter <solsticeofthewinter@gmail.com>
 // SPDX-FileCopyrightText: 2025 TheBorzoiMustConsume <197824988+TheBorzoiMustConsume@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 deltanedas <39013340+deltanedas@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 deltanedas <@deltanedas:kde.org>
 // SPDX-FileCopyrightText: 2025 gus <august.eymann@gmail.com>
 //
