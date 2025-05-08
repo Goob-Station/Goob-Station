e5720ecebc313e99d780e081b9419f834fbe767f
GoobBot <uristmchands@proton.me>
2025-05-08 19:46:06 +0000

    chore: Automatically update REUSE headers

--- Content.Goobstation.Server/Devil/Grip/DevilGripSystem.cs
+++ Content.Goobstation.Server/Devil/Grip/DevilGripSystem.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 SolsticeOfTheWinter <solsticeofthewinter@gmail.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using Content.Goobstation.Shared.Devil;
 using Content.Server.Chat.Systems;
 using Content.Server.Speech.EntitySystems;
