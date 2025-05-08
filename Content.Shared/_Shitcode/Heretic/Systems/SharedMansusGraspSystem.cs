6f6b0af01428b083ab0dd650d4c2d37cdf672d50
GoobBot <uristmchands@proton.me>
2025-05-08 10:07:43 +0000

    chore: Automatically update REUSE headers

--- Content.Shared/_Shitcode/Heretic/Systems/SharedMansusGraspSystem.cs
+++ Content.Shared/_Shitcode/Heretic/Systems/SharedMansusGraspSystem.cs
@@ -1,3 +1,8 @@
+// SPDX-FileCopyrightText: 2025 Aviu00 <93730715+Aviu00@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 Aviu00 <aviu00@protonmail.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using Content.Server.Heretic.Components.PathSpecific;
 using Content.Shared._EinsteinEngines.Silicon.Components;
 using Content.Shared._Goobstation.Heretic.Systems;
