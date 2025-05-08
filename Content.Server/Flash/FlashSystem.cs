043621c94bde0df6a25b2ce2ab7afbf2964675f2
GoobBot <uristmchands@proton.me>
2025-05-08 08:43:51 +0000

    chore: Automatically update REUSE headers

--- Content.Server/Flash/FlashSystem.cs
+++ Content.Server/Flash/FlashSystem.cs
@@ -46,6 +46,7 @@
 // SPDX-FileCopyrightText: 2024 whateverusername0 <whateveremail>
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Aviu00 <93730715+Aviu00@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
 // SPDX-FileCopyrightText: 2025 Misandry <mary@thughunt.ing>
 // SPDX-FileCopyrightText: 2025 Roudenn <romabond091@gmail.com>
 // SPDX-FileCopyrightText: 2025 Spatison <137375981+Spatison@users.noreply.github.com>
