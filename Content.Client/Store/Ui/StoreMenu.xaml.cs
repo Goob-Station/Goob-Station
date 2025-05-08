260d8568c2694ee6c860bb49962954a6ae011d8f
GoobBot <uristmchands@proton.me>
2025-05-08 07:18:56 +0000

    chore: Automatically update REUSE headers

--- Content.Client/Store/Ui/StoreMenu.xaml.cs
+++ Content.Client/Store/Ui/StoreMenu.xaml.cs
@@ -21,6 +21,7 @@
 // SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
 // SPDX-FileCopyrightText: 2025 LuciferMkshelter <154002422+LuciferEOS@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 LuciferMkshelter <stepanteliatnik2022@gmail.com>
+// SPDX-FileCopyrightText: 2025 gus <august.eymann@gmail.com>
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
