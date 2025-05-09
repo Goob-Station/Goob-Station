42b8cd25ac1ecf3356b81b2898029d025ef39dfe
SolsticeOfTheWinter <solsticeofthewinter@gmail.com>
2025-05-08 18:55:15 -1000

    hug yo lizards
    Signed-off-by: SolsticeOfTheWinter <solsticeofthewinter@gmail.com>

---
+++ Content.Goobstation.Server/Interaction/Components/TransferHeatOnInteractComponent.cs
@@ -0,0 +1,32 @@
+namespace Content.Goobstation.Server.Interaction.Components;
+
+/// <summary>
+/// Equalizes temperature between two entities on interact - Hug your friends!
+/// In the future, I want moths to be able to roast people in a swarm
+/// like how bees defend their hive.
+/// </summary>
+[RegisterComponent]
+public sealed partial class TransferHeatOnInteractComponent : Component
+{
+    /// <summary>
+    /// The amount of the host entity heat to transfer per interaction.
+    /// </summary>
+    [DataField]
+    public float TransferRatio = 0.45f;
+
+    /// <summary>
+    /// If set to true, activate interactions will also trigger the component.
+    /// </summary>
+    [DataField]
+    public bool OnActivate;
+
+    /// <summary>
+    /// Time delay between interactions to avoid spam.
+    /// </summary>
+    [DataField]
+    public TimeSpan InteractDelay = TimeSpan.FromSeconds(1.0);
+
+    [ViewVariables(VVAccess.ReadWrite)]
+    public TimeSpan LastInteractTime;
+
+}
---
+++ Content.Goobstation.Server/Interaction/Systems/TransferHeatOnInteractSystem.cs
@@ -0,0 +1,65 @@
+using Content.Goobstation.Server.Interaction.Components;
+using Content.Server.Temperature.Components;
+using Content.Server.Temperature.Systems;
+using Content.Shared.Bed.Sleep;
+using Content.Shared.Interaction;
+using Content.Shared.Mobs.Systems;
+using Robust.Shared.Timing;
+
+namespace Content.Goobstation.Server.Interaction.Systems;
+
+public sealed class TransferHeatOnInteract : EntitySystem
+{
+    [Dependency] private readonly IGameTiming _timing = default!;
+    [Dependency] private readonly MobStateSystem _mobStateSystem = default!;
+    [Dependency] private readonly TemperatureSystem _temperature = default!;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+        SubscribeLocalEvent<TransferHeatOnInteractComponent, InteractHandEvent>(OnInteractHand);
+        SubscribeLocalEvent<TransferHeatOnInteractComponent, ActivateInWorldEvent>(OnActivateInWorld);
+    }
+
+    private void OnActivateInWorld(Entity<TransferHeatOnInteractComponent> ent, ref ActivateInWorldEvent args)
+    {
+        if (!args.Complex || !ent.Comp.OnActivate)
+            return;
+
+        SharedInteract(ent, args.User, args);
+    }
+
+    private void OnInteractHand(Entity<TransferHeatOnInteractComponent> ent, ref InteractHandEvent args)
+    {
+        SharedInteract(ent, args.User, args);
+    }
+
+    private void SharedInteract(Entity<TransferHeatOnInteractComponent> ent, EntityUid user, HandledEntityEventArgs args )
+    {
+        if (args.Handled
+            || user == ent.Owner
+            || HasComp<SleepingComponent>(ent)
+            || !TryComp<TemperatureComponent>(user, out var userTemperature)
+            || !TryComp<TemperatureComponent>(ent, out var targetTemperature)
+            || !_mobStateSystem.IsAlive(ent))
+            return;
+
+        args.Handled = true;
+
+        if (_timing.CurTime < ent.Comp.LastInteractTime + ent.Comp.InteractDelay)
+            return;
+
+        ent.Comp.LastInteractTime = _timing.CurTime;
+
+        var targetSpecific = _temperature.GetHeatCapacity(ent);
+        var userSpecific = _temperature.GetHeatCapacity(user);
+
+        var δt = targetTemperature.CurrentTemperature - userTemperature.CurrentTemperature;
+        var energy = δt * ent.Comp.TransferRatio * (userSpecific * targetSpecific) / (userSpecific + targetSpecific);
+
+        _temperature.ChangeHeat(ent, -energy);
+        _temperature.ChangeHeat(user, energy);
+
+    }
+
+}
