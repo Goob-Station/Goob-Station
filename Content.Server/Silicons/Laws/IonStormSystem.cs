cfa84d8c0b28f00ea66c666f003b8bce1f1ce0e7
GoobBot <uristmchands@proton.me>
2025-05-09 11:04:56 +0000

    chore: Automatically update REUSE headers

--- Content.Server/Silicons/Laws/IonStormSystem.cs
+++ Content.Server/Silicons/Laws/IonStormSystem.cs
@@ -15,6 +15,7 @@
 // SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
 // SPDX-FileCopyrightText: 2025 Ilya246 <57039557+Ilya246@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Roudenn <romabond091@gmail.com>
+// SPDX-FileCopyrightText: 2025 gus <august.eymann@gmail.com>
 // SPDX-FileCopyrightText: 2025 lzk <124214523+lzk228@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 valquaint <57813693+valquaint@users.noreply.github.com>
 //
