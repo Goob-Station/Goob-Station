be8562340b730564b47ef3e42819835e4f961f0a
GoobBot <uristmchands@proton.me>
2025-05-09 17:01:49 +0000

    chore: Automatically update REUSE headers

--- Content.Goobstation.Shared/SpecialAnimation/SpecialAnimationPrototype.cs
+++ Content.Goobstation.Shared/SpecialAnimation/SpecialAnimationPrototype.cs
@@ -1,4 +1,8 @@
-﻿using Robust.Shared.Prototypes;
+// SPDX-FileCopyrightText: 2025 Roudenn <romabond091@gmail.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+using Robust.Shared.Prototypes;
 
 namespace Content.Goobstation.Shared.SpecialAnimation;
 
