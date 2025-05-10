28cea1ac2615a07eb70dacd4819a646d0af0f704
GoobBot <uristmchands@proton.me>
2025-05-10 01:05:37 +0000

    chore: Automatically update REUSE headers

--- Content.Goobstation.Shared/Religion/DivineInterventionSystem.cs
+++ Content.Goobstation.Shared/Religion/DivineInterventionSystem.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 TheBorzoiMustConsume <197824988+TheBorzoiMustConsume@users.noreply.github.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using Content.Goobstation.Common.Religion;
 using Content.Goobstation.Common.Religion.Events;
 using Content.Shared.Hands.Components;
