32d3ce456053c595742e8f3f44373a0f7dccbf59
GoobBot <uristmchands@proton.me>
2025-05-10 19:59:23 +0000

    chore: Automatically update REUSE headers

--- Content.Goobstation.Server/Changeling/Objectives/Components/AbsorbChangelingConditionComponent.cs
+++ Content.Goobstation.Server/Changeling/Objectives/Components/AbsorbChangelingConditionComponent.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 Marcus F <199992874+thebiggestbruh@users.noreply.github.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using Content.Goobstation.Server.Changeling.Objectives.Systems;
 
 namespace Content.Goobstation.Server.Changeling.Objectives.Components;
