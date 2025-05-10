7bab4b129eaef06bcce800f047421f890b176d1b
GoobBot <uristmchands@proton.me>
2025-05-10 01:07:11 +0000

    chore: Automatically update REUSE headers

--- Content.Shared/_Shitcode/Wizard/SharedSpellsSystem.cs
+++ Content.Shared/_Shitcode/Wizard/SharedSpellsSystem.cs
@@ -1,6 +1,7 @@
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Aidenkrz <aiden@djkraz.com>
 // SPDX-FileCopyrightText: 2025 Aviu00 <93730715+Aviu00@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
 // SPDX-FileCopyrightText: 2025 Misandry <mary@thughunt.ing>
 // SPDX-FileCopyrightText: 2025 Ted Lukin <66275205+pheenty@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 TheBorzoiMustConsume <197824988+TheBorzoiMustConsume@users.noreply.github.com>
