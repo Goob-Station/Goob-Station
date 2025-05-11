be84497737b7f42418a2db9eb4fea62ecea094a2
GoobBot <uristmchands@proton.me>
2025-05-11 14:16:54 +0000

    chore: Automatically update REUSE headers

--- Content.Goobstation.Server/Changeling/GameTicking/Rules/ChangelingRuleSystem.cs
+++ Content.Goobstation.Server/Changeling/GameTicking/Rules/ChangelingRuleSystem.cs
@@ -4,6 +4,7 @@
 // SPDX-FileCopyrightText: 2024 username <113782077+whateverusername0@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2024 whateverusername0 <whateveremail>
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
 // SPDX-FileCopyrightText: 2025 Marcus F <199992874+thebiggestbruh@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Misandry <mary@thughunt.ing>
 // SPDX-FileCopyrightText: 2025 gus <august.eymann@gmail.com>
