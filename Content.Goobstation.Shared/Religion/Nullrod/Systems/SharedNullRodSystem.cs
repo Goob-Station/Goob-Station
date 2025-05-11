3c4116fa571b59ed0811e2079a9715215279888f
TheBorzoiMustConsume <197824988+TheBorzoiMustConsume@users.noreply.github.com>
2025-05-11 21:10:43 +1000

    Revert "insulation still not working but this makes more sense to me?"
    This reverts commit dbe00ec98da96ca25d2f1f892d21e3147f4646e9.

--- Content.Goobstation.Shared/Religion/Nullrod/Systems/SharedNullRodSystem.cs
+++ Content.Goobstation.Shared/Religion/Nullrod/Systems/SharedNullRodSystem.cs
@@ -8,15 +8,20 @@
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
+using System.Linq;
 using Content.Goobstation.Shared.Bible;
 using Content.Goobstation.Shared.Religion.Nullrod.Components;
 using Content.Shared.Damage;
 using Content.Shared.Electrocution;
 using Content.Shared.Hands;
+using Content.Shared.Hands.Components;
+using Content.Shared.Heretic;
 using Content.Shared.Interaction.Events;
+using Content.Shared.Inventory;
 using Content.Shared.Popups;
 using Content.Shared.Weapons.Ranged.Events;
 using Robust.Shared.Audio.Systems;
+using Robust.Shared.Serialization.Manager;
 using Robust.Shared.Timing;
 
 namespace Content.Goobstation.Shared.Religion.Nullrod.Systems;
@@ -27,6 +32,9 @@ public abstract partial class SharedNullRodSystem : EntitySystem
     [Dependency] private readonly DamageableSystem _damageableSystem = default!;
     [Dependency] private readonly SharedPopupSystem _popupSystem = default!;
     [Dependency] private readonly IGameTiming _timing = default!;
+    [Dependency] private readonly IComponentFactory _componentFactory = default!;
+    [Dependency] private readonly ISerializationManager _serializationManager = default!;
+    [Dependency] private readonly InventorySystem _inventory = default!;
 
     public override void Initialize()
     {
@@ -35,8 +43,7 @@ public abstract partial class SharedNullRodSystem : EntitySystem
         SubscribeLocalEvent<NullrodComponent, AttackAttemptEvent>(OnAttackAttempt);
         SubscribeLocalEvent<NullrodComponent, ShotAttemptedEvent>(OnShootAttempt);
 
-        SubscribeLocalEvent<NullrodComponent, ElectrocutionAttemptEvent>(OnNullrodElectrocutionAttempt);
-        SubscribeLocalEvent<NullrodComponent, HeldRelayedEvent<ElectrocutionAttemptEvent>>((e, c, ev) => OnNullrodElectrocutionAttempt(e, c, ev.Args));
+        SubscribeLocalEvent<ElectrocutionAttemptEvent>(OnNullrodElectrocutionAttempt);
     }
 
     #region Attack Attempts
@@ -78,9 +85,16 @@ public abstract partial class SharedNullRodSystem : EntitySystem
 
     #region Variant Specific Events
     //Goidacode but there's always the possibility of more insulated Null Rods given the amount of YAMLMaxxers we have...
-    private void OnNullrodElectrocutionAttempt(EntityUid uid, NullrodComponent comp, ElectrocutionAttemptEvent args)
+    private void OnNullrodElectrocutionAttempt(ref ElectrocutionAttemptEvent args)
     {
-        args.SiemensCoefficient *= comp.SiemensCoefficient;
+        var contains = _inventory.GetHandOrInventoryEntities(args.TargetUid, SlotFlags.WITHOUT_POCKET);
+
+        foreach (var item in contains)
+        {
+            if (TryComp<NullrodComponent>(item, out var comp))
+                args.SiemensCoefficient = comp.SiemensCoefficient;
+        }
+
     }
     #endregion
 
