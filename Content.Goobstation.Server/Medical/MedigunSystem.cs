07f98c0666c71f7eecbfbff8d55d3971c5913992
GoobBot <uristmchands@proton.me>
2025-05-11 07:53:22 +0000

    chore: Automatically update REUSE headers

--- Content.Goobstation.Server/Medical/MedigunSystem.cs
+++ Content.Goobstation.Server/Medical/MedigunSystem.cs
@@ -1,5 +1,6 @@
 // SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
 // SPDX-FileCopyrightText: 2025 Roudenn <romabond091@gmail.com>
+// SPDX-FileCopyrightText: 2025 gluesniffler <linebarrelerenthusiast@gmail.com>
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
