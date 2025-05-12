4622c6b2849d9d6d363b8125e33023d85b63170e
GoobBot <uristmchands@proton.me>
2025-05-12 18:36:32 +0000

    chore: Automatically update REUSE headers

--- Content.Goobstation.Server/ItemMiner/Systems/ItemMinerSystem.cs
+++ Content.Goobstation.Server/ItemMiner/Systems/ItemMinerSystem.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 Ilya246 <ilyukarno@gmail.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using Content.Goobstation.Common.ItemMiner;
 using Content.Shared.Containers.ItemSlots;
 using Content.Shared.Stacks;
