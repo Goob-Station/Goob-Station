ca31656b727459f5c3c39ce4a59d3b3014b70c6c
GoobBot <uristmchands@proton.me>
2025-05-12 18:49:50 +0000

    chore: Automatically update REUSE headers

--- Content.Server/Devour/DevourSystem.cs
+++ Content.Server/Devour/DevourSystem.cs
@@ -5,7 +5,9 @@
 // SPDX-FileCopyrightText: 2024 metalgearsloth <31366439+metalgearsloth@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
+// SPDX-FileCopyrightText: 2025 Rouden <149893554+Roudenn@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Roudenn <romabond091@gmail.com>
+// SPDX-FileCopyrightText: 2025 coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
