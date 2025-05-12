3f537b4d774f3cc6dc15cc45760e08960e153459
GoobBot <uristmchands@proton.me>
2025-05-12 19:31:14 +0000

    chore: Automatically update REUSE headers

--- Content.Shared/Clothing/EntitySystems/FireProtectionSystem.cs
+++ Content.Shared/Clothing/EntitySystems/FireProtectionSystem.cs
@@ -2,6 +2,7 @@
 // SPDX-FileCopyrightText: 2024 deltanedas <@deltanedas:kde.org>
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 kosticia <kosticia46@gmail.com>
+// SPDX-FileCopyrightText: 2025 pheenty <fedorlukin2006@gmail.com>
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
