2482e1c0463dbf01ced1c5368f4bbe96a9bc1b59
GoobBot <uristmchands@proton.me>
2025-05-12 09:01:48 +0000

    chore: Automatically update REUSE headers

--- Content.Server/EntityEffects/Effects/HealthChange.cs
+++ Content.Server/EntityEffects/Effects/HealthChange.cs
@@ -43,6 +43,7 @@
 // SPDX-FileCopyrightText: 2024 metalgearsloth <31366439+metalgearsloth@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Aidenkrz <aiden@djkraz.com>
+// SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
 // SPDX-FileCopyrightText: 2025 SlamBamActionman <83650252+SlamBamActionman@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Solstice <solsticeofthewinter@gmail.com>
 // SPDX-FileCopyrightText: 2025 gluesniffler <159397573+gluesniffler@users.noreply.github.com>
