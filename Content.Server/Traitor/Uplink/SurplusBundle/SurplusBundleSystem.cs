ba3886a66090f250c5b17d261560550a169099da
GoobBot <uristmchands@proton.me>
2025-05-13 20:42:07 +0000

    chore: Automatically update REUSE headers

--- Content.Server/Traitor/Uplink/SurplusBundle/SurplusBundleSystem.cs
+++ Content.Server/Traitor/Uplink/SurplusBundle/SurplusBundleSystem.cs
@@ -3,6 +3,7 @@
 // SPDX-FileCopyrightText: 2024 Nemanja <98561806+EmoGarbage404@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Armok <155400926+ARMOKS@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
 // SPDX-FileCopyrightText: 2025 Ilya246 <57039557+Ilya246@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 gus <august.eymann@gmail.com>
 //
