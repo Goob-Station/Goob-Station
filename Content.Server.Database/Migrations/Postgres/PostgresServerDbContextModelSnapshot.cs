a54de35b04a4604d4689c946b29ea6c199c469d0
GoobBot <uristmchands@proton.me>
2025-05-14 13:05:12 +0000

    chore: Automatically update REUSE headers

--- Content.Server.Database/Migrations/Postgres/PostgresServerDbContextModelSnapshot.cs
+++ Content.Server.Database/Migrations/Postgres/PostgresServerDbContextModelSnapshot.cs
@@ -25,6 +25,7 @@
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Aiden <aiden@djkraz.com>
 // SPDX-FileCopyrightText: 2025 BombasterDS2 <shvalovdenis.workmail@gmail.com>
+// SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
 // SPDX-FileCopyrightText: 2025 Ilya246 <ilyukarno@gmail.com>
 // SPDX-FileCopyrightText: 2025 Myra <vasilis@pikachu.systems>
 // SPDX-FileCopyrightText: 2025 PJB3005 <pieterjan.briers+git@gmail.com>
--- Content.Server.Database/Migrations/Sqlite/20250514124828_AntagPity.cs
+++ Content.Server.Database/Migrations/Sqlite/20250514124828_AntagPity.cs
@@ -1,5 +1,6 @@
 // SPDX-FileCopyrightText: 2022 DrSmugleaf <DrSmugleaf@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
 // SPDX-FileCopyrightText: 2025 Ilya246 <ilyukarno@gmail.com>
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
