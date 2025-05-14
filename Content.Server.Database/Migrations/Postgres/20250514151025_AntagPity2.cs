0ad7bddfe491bdac0a6f8d3563dcaacc363b0589
Ilya246 <ilyukarno@gmail.com>
2025-05-14 19:10:27 +0400

    migration

---
+++ Content.Server.Database/Migrations/Postgres/20250514151025_AntagPity2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Content.Server.Database.Migrations.Postgres
+{
+    /// <inheritdoc />
+    public partial class AntagPity2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<TimeSpan>(
+                name: "last_rolled_antag",
+                table: "player",
+                type: "interval",
+                nullable: true,
+                oldClrType: typeof(TimeSpan),
+                oldType: "interval");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<TimeSpan>(
+                name: "last_rolled_antag",
+                table: "player",
+                type: "interval",
+                nullable: false,
+                defaultValue: new TimeSpan(0, 0, 0, 0, 0),
+                oldClrType: typeof(TimeSpan),
+                oldType: "interval",
+                oldNullable: true);
+        }
+    }
+}
---
+++ Content.Server.Database/Migrations/Sqlite/20250514151014_AntagPity2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Content.Server.Database.Migrations.Sqlite
+{
+    /// <inheritdoc />
+    public partial class AntagPity2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<TimeSpan>(
+                name: "last_rolled_antag",
+                table: "player",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(TimeSpan),
+                oldType: "TEXT");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<TimeSpan>(
+                name: "last_rolled_antag",
+                table: "player",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new TimeSpan(0, 0, 0, 0, 0),
+                oldClrType: typeof(TimeSpan),
+                oldType: "TEXT",
+                oldNullable: true);
+        }
+    }
+}
