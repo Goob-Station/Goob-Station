64a866ad3f7018b18ca257ffe7a408135431bda7
GoobBot <uristmchands@proton.me>
2025-05-14 04:32:11 +0000

    chore: Automatically update REUSE headers

--- Content.Goobstation.Shared/Held/HeldGrantComponentSystem.cs
+++ Content.Goobstation.Shared/Held/HeldGrantComponentSystem.cs
@@ -6,6 +6,7 @@
 // SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
 // SPDX-FileCopyrightText: 2025 Ilya246 <ilyukarno@gmail.com>
 // SPDX-FileCopyrightText: 2025 Misandry <mary@thughunt.ing>
+// SPDX-FileCopyrightText: 2025 TheBorzoiMustConsume <197824988+TheBorzoiMustConsume@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 gus <august.eymann@gmail.com>
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
