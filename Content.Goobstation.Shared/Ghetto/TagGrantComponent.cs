b5799573309bf9334cfc9c0bbc171ef3d29bdfa8
GoobBot <uristmchands@proton.me>
2025-05-14 11:19:48 +0000

    chore: Automatically update REUSE headers

--- Content.Goobstation.Shared/Ghetto/TagGrantComponent.cs
+++ Content.Goobstation.Shared/Ghetto/TagGrantComponent.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 LuciferEOS <stepanteliatnik2022@gmail.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using Content.Shared.Tag;
 using Robust.Shared.GameStates;
 using Robust.Shared.Prototypes;
