b567577999d4e6e3971f7432c981c1df22c75496
GoobBot <uristmchands@proton.me>
2025-05-14 18:13:26 +0000

    chore: Automatically update REUSE headers

--- Content.Goobstation.Shared/OverlaysAnimation/Components/OverlayAnimationComponent.cs
+++ Content.Goobstation.Shared/OverlaysAnimation/Components/OverlayAnimationComponent.cs
@@ -1,4 +1,8 @@
-﻿using System.Numerics;
+// SPDX-FileCopyrightText: 2025 Roudenn <romabond091@gmail.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+using System.Numerics;
 using Content.Goobstation.Shared.OverlaysAnimation.Animations;
 
 namespace Content.Goobstation.Shared.OverlaysAnimation.Components;
