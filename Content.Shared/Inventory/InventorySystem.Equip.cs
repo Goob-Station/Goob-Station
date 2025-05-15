c21a7e1b658d7f0c162099d1d2e6b9391f1db49d
GoobBot <uristmchands@proton.me>
2025-05-15 14:03:19 +0000

    chore: Automatically update REUSE headers

--- Content.Shared/Inventory/InventorySystem.Equip.cs
+++ Content.Shared/Inventory/InventorySystem.Equip.cs
@@ -93,6 +93,7 @@
 // SPDX-FileCopyrightText: 2024 voidnull000 <18663194+voidnull000@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2024 Джексон Миссиссиппи <tripwiregamer@gmail.com>
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
 // SPDX-FileCopyrightText: 2025 deltanedas <39013340+deltanedas@users.noreply.github.com>
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
