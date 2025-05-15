e0aac2632ce3ed5fecae6e74392c7eca71f8d0be
GoobBot <uristmchands@proton.me>
2025-05-15 00:21:48 +0000

    chore: Automatically update REUSE headers

--- Content.Shared/Body/Systems/SharedBodySystem.Parts.cs
+++ Content.Shared/Body/Systems/SharedBodySystem.Parts.cs
@@ -75,6 +75,7 @@
 // SPDX-FileCopyrightText: 2024 voidnull000 <18663194+voidnull000@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Aiden <aiden@djkraz.com>
+// SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
 // SPDX-FileCopyrightText: 2025 deltanedas <39013340+deltanedas@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 deltanedas <@deltanedas:kde.org>
 // SPDX-FileCopyrightText: 2025 gluesniffler <linebarrelerenthusiast@gmail.com>
