722ea4d35f0dbd69e05ec931ee8635d1ef10d224
GoobBot <uristmchands@proton.me>
2025-05-16 00:50:50 +0000

    chore: Automatically update REUSE headers

--- Content.Goobstation.Shared/Sandevistan/SandevistanUserComponent.cs
+++ Content.Goobstation.Shared/Sandevistan/SandevistanUserComponent.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 pheenty <fedorlukin2006@gmail.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using Content.Goobstation.Maths.FixedPoint;
 using Content.Shared.Actions;
 using Content.Shared.Damage;
--- Content.Server/_NF/Speech/EntitySystems/StreetpunkAccentSystem.cs
+++ Content.Server/_NF/Speech/EntitySystems/StreetpunkAccentSystem.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 pheenty <fedorlukin2006@gmail.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using Content.Server._NF.Speech.Components;
 using Content.Server.Speech;
 using Content.Server.Speech.EntitySystems;
