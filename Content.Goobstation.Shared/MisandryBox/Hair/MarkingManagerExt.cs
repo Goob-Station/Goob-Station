6f6f9ce26995bda61d293709dcff3c81cec7f2fc
Conchelle <mary@thughunt.ing>
2025-05-17 21:05:31 +0300

    uhuh

---
+++ Content.Goobstation.Shared/MisandryBox/Hair/MarkingManagerExt.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+using Content.Shared.Humanoid.Markings;
+using Content.Shared.Humanoid.Prototypes;
+using Content.Shared.Roles;
+using Robust.Shared.Prototypes;
+
+namespace Content.Goobstation.Shared.MisandryBox.Hair;
+
+public static class MarkingManagerExtensions
+{
+    public static bool HasHair(this MarkingManager mark, ProtoId<SpeciesPrototype> specie)
+    {
+        var hairstyles = mark.MarkingsByCategoryAndSpecies(MarkingCategories.Hair, specie).Keys.ToList();
+        return hairstyles.Count != 0;
+    }
+}
---
+++ Content.Goobstation.Shared/MisandryBox/Hair/StyleRequirement.cs
@@ -0,0 +1,64 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using Content.Shared.Humanoid;
+using Content.Shared.Humanoid.Markings;
+using Content.Shared.Preferences;
+using Content.Shared.Roles;
+using Robust.Shared.Prototypes;
+using Robust.Shared.Utility;
+
+namespace Content.Goobstation.Shared.MisandryBox.Hair;
+
+public sealed partial class StyleRequirement : JobRequirement
+{
+    [Dependency] private readonly MarkingManager _mark = default!;
+
+    [DataField(required: true)]
+    public HashSet<ProtoId<MarkingPrototype>> Styles;
+
+    public override bool Check(IEntityManager entManager,
+        IPrototypeManager protoManager,
+        HumanoidCharacterProfile? profile,
+        IReadOnlyDictionary<string, TimeSpan> playTimes,
+        [NotNullWhen(false)] out FormattedMessage? reason)
+    {
+        reason = new FormattedMessage();
+
+        if (profile is null)
+            return true;
+
+        IoCManager.InjectDependencies(this);
+
+        if (!_mark.HasHair(profile.Species))
+            return true;
+
+        var sb = new StringBuilder();
+
+        sb.Append("[color=orange]");
+
+        foreach (var s in Styles)
+        {
+            sb.Append("\n" + Loc.GetString($"marking-{s.Id}"));
+        }
+
+        var style = profile.Appearance.HairStyleId;
+
+        if (!Inverted)
+        {
+            reason = FormattedMessage.FromMarkupPermissive($"{Loc.GetString("role-timer-blacklisted-hair")}{sb}");
+
+            if (Styles.Any(e => e.Id == style))
+                return false;
+        }
+        else
+        {
+            reason = FormattedMessage.FromMarkupPermissive($"{Loc.GetString("role-timer-whitelisted-hair")}{sb}");
+
+            if (Styles.All(e => e.Id != style))
+                return false;
+        }
+
+        return true;
+    }
+}
