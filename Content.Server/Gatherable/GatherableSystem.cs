fef01502a99e77f48bef5151904d9d573636617f
GoobBot <uristmchands@proton.me>
2025-05-17 01:25:38 +0000

    chore: Automatically update REUSE headers

--- Content.Server/Gatherable/GatherableSystem.cs
+++ Content.Server/Gatherable/GatherableSystem.cs
@@ -16,6 +16,7 @@
 // SPDX-FileCopyrightText: 2024 metalgearsloth <31366439+metalgearsloth@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2024 plykiya <plykiya@protonmail.com>
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 deltanedas <@deltanedas:kde.org>
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
