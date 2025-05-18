ccdac04065da7cd56257884e0474de63ee16eb95
metalgearsloth <31366439+metalgearsloth@users.noreply.github.com>
2025-05-18 17:47:35 +1000

    Add ImplicitRoofComponent (#36112)
    * Add ImplicitRoofComponent
    
    So shuttles get marked as rooved.
    
    * Early-out
    
    So the new render doesn't suck up perf unnecessarily.

--- Content.Server/Shuttles/Systems/ShuttleSystem.cs
+++ Content.Server/Shuttles/Systems/ShuttleSystem.cs
@@ -117,6 +117,7 @@ using Content.Server.Stunnable;
 using Content.Shared.Buckle.Components;
 using Content.Shared.Damage;
 using Content.Shared.GameTicking;
+using Content.Shared.Light.Components;
 using Content.Shared.Inventory;
 using Content.Shared.Mobs.Systems;
 using Content.Shared.Movement.Events;
@@ -226,7 +227,8 @@ public sealed partial class ShuttleSystem : SharedShuttleSystem
         if (HasComp<MapComponent>(ev.EntityUid))
             return;
 
-        EntityManager.EnsureComponent<ShuttleComponent>(ev.EntityUid);
+        EnsureComp<ShuttleComponent>(ev.EntityUid);
+        EnsureComp<ImplicitRoofComponent>(ev.EntityUid);
     }
 
     private void OnShuttleStartup(EntityUid uid, ShuttleComponent component, ComponentStartup args)
