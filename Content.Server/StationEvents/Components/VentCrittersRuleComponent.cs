41a4a57f8757dad0ea1d01dde173c04c7e44a1d2
GoobBot <uristmchands@proton.me>
2025-05-18 06:52:50 +0000

    chore: Automatically update REUSE headers

--- Content.Server/StationEvents/Components/VentCrittersRuleComponent.cs
+++ Content.Server/StationEvents/Components/VentCrittersRuleComponent.cs
@@ -6,6 +6,7 @@
 // SPDX-FileCopyrightText: 2023 metalgearsloth <comedian_vs_clown@hotmail.com>
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Armok <155400926+ARMOKS@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
 // SPDX-FileCopyrightText: 2025 Piras314 <p1r4s@proton.me>
 // SPDX-FileCopyrightText: 2025 deltanedas <39013340+deltanedas@users.noreply.github.com>
 //
