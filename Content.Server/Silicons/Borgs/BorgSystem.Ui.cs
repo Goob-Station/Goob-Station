66623d7da25cbd45c4e74db3d9935ff45aedbb7a
GoobBot <uristmchands@proton.me>
2025-05-18 00:22:18 +0000

    chore: Automatically update REUSE headers

--- Content.Server/Silicons/Borgs/BorgSystem.Ui.cs
+++ Content.Server/Silicons/Borgs/BorgSystem.Ui.cs
@@ -6,6 +6,7 @@
 // SPDX-FileCopyrightText: 2024 metalgearsloth <31366439+metalgearsloth@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2024 metalgearsloth <comedian_vs_clown@hotmail.com>
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 Bandit <queenjess521@gmail.com>
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
