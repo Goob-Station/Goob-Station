340fee4c0ee9c22e77a1ede29accbf4fe2f2b727
GoobBot <uristmchands@proton.me>
2025-05-18 04:31:11 +0000

    chore: Automatically update REUSE headers

--- Content.Goobstation.Server/Hostname/DynamicHostnameSystem.cs
+++ Content.Goobstation.Server/Hostname/DynamicHostnameSystem.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using Content.Server.GameTicking;
 using Robust.Shared;
 using Robust.Shared.Configuration;
