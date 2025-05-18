bf0aaf9a9b0e2536b95f26d7bfbe70ba1610c94e
GoobBot <uristmchands@proton.me>
2025-05-18 20:26:37 +0000

    chore: Automatically update REUSE headers

--- Content.Server/Chat/Systems/ChatSystem.Emote.cs
+++ Content.Server/Chat/Systems/ChatSystem.Emote.cs
@@ -15,6 +15,7 @@
 // SPDX-FileCopyrightText: 2024 whateverusername0 <whateveremail>
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Conchelle <mary@thughunt.ing>
+// SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
 // SPDX-FileCopyrightText: 2025 Misandry <mary@thughunt.ing>
 // SPDX-FileCopyrightText: 2025 gus <august.eymann@gmail.com>
 // SPDX-FileCopyrightText: 2025 lzk <124214523+lzk228@users.noreply.github.com>
