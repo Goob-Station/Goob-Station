2e28f63fd0f0f574717af89e3f9232b76d8a7407
GoobBot <uristmchands@proton.me>
2025-05-21 19:39:35 +0000

    chore: Automatically update REUSE headers

--- Content.Shared/_NF/PlantAnalyzer/PlantAnalyzerScannedSeedPlantInformation.cs
+++ Content.Shared/_NF/PlantAnalyzer/PlantAnalyzerScannedSeedPlantInformation.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2024 Dvir <39403717+dvir001@users.noreply.github.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using Robust.Shared.Serialization;
 
 namespace Content.Shared._NF.PlantAnalyzer;
