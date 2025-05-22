2402cb96167f7c65496acb61d05fc72479d223f4
GoobBot <uristmchands@proton.me>
2025-05-22 07:03:01 +0000

    chore: Automatically update REUSE headers

--- Content.Server/Ghost/GhostSystem.cs
+++ Content.Server/Ghost/GhostSystem.cs
@@ -74,6 +74,7 @@
 // SPDX-FileCopyrightText: 2024 themias <89101928+themias@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Aviu00 <93730715+Aviu00@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
 // SPDX-FileCopyrightText: 2025 J <billsmith116@gmail.com>
 // SPDX-FileCopyrightText: 2025 SX_7 <sn1.test.preria.2002@gmail.com>
 // SPDX-FileCopyrightText: 2025 SpaceManiac <tad@platymuus.com>
