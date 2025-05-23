701852f175ff9e8c84042de01bb95ff77cf4cd48
GoobBot <uristmchands@proton.me>
2025-05-23 10:21:34 +0000

    chore: Automatically update REUSE headers

--- Content.Shared/Weapons/Melee/Components/MeleeThrowOnHitComponent.cs
+++ Content.Shared/Weapons/Melee/Components/MeleeThrowOnHitComponent.cs
@@ -3,6 +3,7 @@
 // SPDX-FileCopyrightText: 2025 ActiveMammmoth <140334666+ActiveMammmoth@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 ActiveMammmoth <kmcsmooth@gmail.com>
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
 // SPDX-FileCopyrightText: 2025 Misandry <mary@thughunt.ing>
 // SPDX-FileCopyrightText: 2025 Solstice <solsticeofthewinter@gmail.com>
 // SPDX-FileCopyrightText: 2025 gus <august.eymann@gmail.com>
