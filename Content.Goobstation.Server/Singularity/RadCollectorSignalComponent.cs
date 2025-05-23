ef86d9680b7101496617843185d2b9616b78f7e6
GoobBot <uristmchands@proton.me>
2025-05-23 19:17:14 +0000

    chore: Automatically update REUSE headers

--- Content.Goobstation.Server/Singularity/RadCollectorSignalComponent.cs
+++ Content.Goobstation.Server/Singularity/RadCollectorSignalComponent.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 deltanedas <@deltanedas:kde.org>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using Robust.Shared.Serialization;
 
 namespace Content.Goobstation.Server.Singularity;
