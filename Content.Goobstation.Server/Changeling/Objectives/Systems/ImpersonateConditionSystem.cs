c2f2f587d597bb4a28736eec80e35548955604c2
Weax <59857479+ImWeax@users.noreply.github.com>
2025-05-24 17:18:00 +1000

    i do not want to change this file RAAH

--- Content.Goobstation.Server/Changeling/Objectives/Systems/ImpersonateConditionSystem.cs
+++ Content.Goobstation.Server/Changeling/Objectives/Systems/ImpersonateConditionSystem.cs
@@ -2,8 +2,6 @@
 // SPDX-FileCopyrightText: 2024 username <113782077+whateverusername0@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2024 whateverusername0 <whateveremail>
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
-// SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
-// SPDX-FileCopyrightText: 2025 ImWeax <59857479+ImWeax@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Misandry <mary@thughunt.ing>
 // SPDX-FileCopyrightText: 2025 gus <august.eymann@gmail.com>
 //
