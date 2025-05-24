dd0402fb397670f8df20769a1c0cacd5257abca4
Armok <155400926+ARMOKS@users.noreply.github.com>
2025-05-24 17:10:39 -0600

    Fix cosmic cult (#2674)
    * fix
    
    * chore: Automatically update REUSE headers
    
    * chore: Automatically update REUSE headers
    
    * y
    
    * chore: Automatically update REUSE headers
    
    * Update Resources/Prototypes/_DV/CosmicCult/Objects/cosmicweapons.yml
    
    ---------
    
    Co-authored-by: GoobBot <uristmchands@proton.me>
    Co-authored-by: gluesniffler <159397573+gluesniffler@users.noreply.github.com>

--- Content.Shared/_DV/CosmicCult/Components/MonumentComponent.cs
+++ Content.Shared/_DV/CosmicCult/Components/MonumentComponent.cs
@@ -1,3 +1,9 @@
+// SPDX-FileCopyrightText: 2025 Armok <155400926+ARMOKS@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
+// SPDX-FileCopyrightText: 2025 Solstice <solsticeofthewinter@gmail.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using Content.Shared._DV.CosmicCult.Prototypes;
 using Content.Shared.Damage;
 using Robust.Shared.Audio;
@@ -82,7 +88,7 @@ public sealed partial class MonumentComponent : Component
     /// the amount of time between the above timer's ticks
     /// </summary>
     [DataField]
-    public TimeSpan CheckWait = TimeSpan.FromSeconds(5);
+    public TimeSpan CheckWait = TimeSpan.FromSeconds(1);
 
     /// <summary>
     /// Passive healing factor for cultists w/ the ability near the monument
@@ -92,15 +98,15 @@ public sealed partial class MonumentComponent : Component
     {
         DamageDict = new()
         {
-            { "Blunt", 2},
-            { "Slash", 2 },
-            { "Piercing", 2 },
-            { "Heat", 2},
-            { "Shock", 2},
-            { "Cold", 2},
-            { "Poison", 2},
-            { "Radiation", 2},
-            { "Asphyxiation", 2 }
+            { "Blunt", 1},
+            { "Slash", 1 },
+            { "Piercing", 1 },
+            { "Heat", 1},
+            { "Shock", 1},
+            { "Cold", 1},
+            { "Poison", 1},
+            { "Radiation", 1},
+            { "Asphyxiation", 1 }
         }
     };
 
