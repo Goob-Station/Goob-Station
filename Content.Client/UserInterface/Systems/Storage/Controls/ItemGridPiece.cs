b108c2e9a1f693385aac9491b247d4167c71294c
GoobBot <uristmchands@proton.me>
2025-05-24 22:07:45 +0000

    chore: Automatically update REUSE headers

--- Content.Client/UserInterface/Systems/Storage/Controls/ItemGridPiece.cs
+++ Content.Client/UserInterface/Systems/Storage/Controls/ItemGridPiece.cs
@@ -5,6 +5,7 @@
 // SPDX-FileCopyrightText: 2024 exincore <me@exin.xyz>
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Errant <35878406+Errant-4@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
 // SPDX-FileCopyrightText: 2025 Marty <martynashagriefer@gmail.com>
 // SPDX-FileCopyrightText: 2025 metalgearsloth <31366439+metalgearsloth@users.noreply.github.com>
 //
