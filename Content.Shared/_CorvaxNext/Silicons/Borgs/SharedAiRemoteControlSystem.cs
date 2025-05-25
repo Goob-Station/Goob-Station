f5fe05cc1fefcd1abb61ee729aef1bb5d115df8f
GoobBot <uristmchands@proton.me>
2025-05-25 05:00:59 +0000

    chore: Automatically update REUSE headers

--- Content.Shared/_CorvaxNext/Silicons/Borgs/SharedAiRemoteControlSystem.cs
+++ Content.Shared/_CorvaxNext/Silicons/Borgs/SharedAiRemoteControlSystem.cs
@@ -1,5 +1,7 @@
 // SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
+// SPDX-FileCopyrightText: 2025 ImHoks <142083149+ImHoks@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 KillanGenifer <killangenifer@gmail.com>
+// SPDX-FileCopyrightText: 2025 gluesniffler <159397573+gluesniffler@users.noreply.github.com>
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
