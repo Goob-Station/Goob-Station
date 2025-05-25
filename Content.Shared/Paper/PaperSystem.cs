d5f3ac59a1ff613dddb866e56dc9bdb46ae23c84
Midna <Midna@Midnight.Miami>
2025-05-25 19:54:26 -0400

    Syndicate recruiter & Cybersun briefcase.

--- Content.Shared/Paper/PaperSystem.cs
+++ Content.Shared/Paper/PaperSystem.cs
@@ -246,7 +246,8 @@ public sealed class PaperSystem : EntitySystem
         return new StampDisplayInfo
         {
             StampedName = stamp.StampedName,
-            StampedColor = stamp.StampedColor
+            StampedColor = stamp.StampedColor,
+            StampLargeIcon = stamp.StampLargeIcon // imp
         };
     }
 
---
+++ Resources/Locale/en-US/_DV/prototypes/roles/antags.ftl
@@ -0,0 +1,2 @@
+﻿roles-antag-syndicate-recruiter-name = Syndicate Recruiter
+roles-antag-syndicate-recruiter-objective = You are a Syndicate Recruiter. Conduct interviews, and have candidates sign a contract in blood.
