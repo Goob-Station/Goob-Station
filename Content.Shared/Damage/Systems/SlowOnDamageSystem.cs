bc51422c23a0552e393eb79730391ed0306c2abf
pheenty <fedorlukin2006@gmail.com>
2025-05-27 10:14:19 +0700

    goidashield winter expansion

--- Content.Shared/Damage/Systems/SlowOnDamageSystem.cs
+++ Content.Shared/Damage/Systems/SlowOnDamageSystem.cs
@@ -86,7 +86,7 @@ namespace Content.Shared.Damage
 
         private void OnExamined(Entity<ClothingSlowOnDamageModifierComponent> ent, ref ExaminedEvent args)
         {
-            var msg = Loc.GetString("slow-on-damage-modifier-examine", ("mod", (1 - ent.Comp.Modifier) * 100));
+            var msg = Loc.GetString("slow-on-damage-modifier-examine", ("mod", Math.Round(100 - ent.Comp.Modifier * 100))); // Goob edit
             args.PushMarkup(msg);
         }
 
