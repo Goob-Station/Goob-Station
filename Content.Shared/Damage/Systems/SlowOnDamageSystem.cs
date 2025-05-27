906dcb8332ab825602d30badca7b67998cc37b42
GoobBot <uristmchands@proton.me>
2025-05-27 03:17:51 +0000

    chore: Automatically update REUSE headers

--- Content.Shared/Damage/Systems/SlowOnDamageSystem.cs
+++ Content.Shared/Damage/Systems/SlowOnDamageSystem.cs
@@ -6,6 +6,8 @@
 // SPDX-FileCopyrightText: 2022 wrexbe <81056464+wrexbe@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2024 Nemanja <98561806+EmoGarbage404@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 gus <august.eymann@gmail.com>
+// SPDX-FileCopyrightText: 2025 pheenty <fedorlukin2006@gmail.com>
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
