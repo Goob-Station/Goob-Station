46086c4e26c4a2f2d27a21f4dfbbf028c7a59b2b
GoobBot <uristmchands@proton.me>
2025-05-27 07:09:07 +0000

    chore: Automatically update REUSE headers

--- Content.Client/Hands/Systems/HandsSystem.cs
+++ Content.Client/Hands/Systems/HandsSystem.cs
@@ -25,6 +25,7 @@
 // SPDX-FileCopyrightText: 2024 metalgearsloth <comedian_vs_clown@hotmail.com>
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Ed <96445749+TheShuEd@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
 // SPDX-FileCopyrightText: 2025 gluesniffler <linebarrelerenthusiast@gmail.com>
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
