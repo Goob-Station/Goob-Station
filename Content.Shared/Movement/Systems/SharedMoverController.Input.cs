9aded72c9aeedeca255af0a6fd8bef9879efa062
Tayrtahn <tayrtahn@gmail.com>
2025-05-27 20:14:49 -0400

    Fix walking in place animations when holding walk button (#37887)
    Fix SpriteMovement playing when holding walk button

--- Content.Shared/Movement/Systems/SharedMoverController.Input.cs
+++ Content.Shared/Movement/Systems/SharedMoverController.Input.cs
@@ -142,7 +142,7 @@ namespace Content.Shared.Movement.Systems
             RaiseLocalEvent(entity, ref moveEvent);
             Dirty(entity, entity.Comp);
 
-            var ev = new SpriteMoveEvent(entity.Comp.HeldMoveButtons != MoveButtons.None);
+            var ev = new SpriteMoveEvent(entity.Comp.HasDirectionalMovement);
             RaiseLocalEvent(entity, ref ev);
         }
 
@@ -173,7 +173,7 @@ namespace Content.Shared.Movement.Systems
                 entity.Comp.HeldMoveButtons = state.HeldMoveButtons;
                 RaiseLocalEvent(entity.Owner, ref moveEvent);
 
-                var ev = new SpriteMoveEvent(entity.Comp.HeldMoveButtons != MoveButtons.None);
+                var ev = new SpriteMoveEvent(entity.Comp.HasDirectionalMovement);
                 RaiseLocalEvent(entity, ref ev);
             }
         }
