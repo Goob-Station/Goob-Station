3ed76e2294649160062effd0f42f5b3974bcea31
GoobBot <uristmchands@proton.me>
2025-05-30 15:55:47 +0000

    chore: Automatically update REUSE headers

--- Content.Server/_Shitcode/Heretic/Abilities/HereticAbilitySystem.cs
+++ Content.Server/_Shitcode/Heretic/Abilities/HereticAbilitySystem.cs
@@ -6,6 +6,7 @@
 // SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
 // SPDX-FileCopyrightText: 2025 Ilya246 <57039557+Ilya246@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Ilya246 <ilyukarno@gmail.com>
+// SPDX-FileCopyrightText: 2025 Marcus F <199992874+thebiggestbruh@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Misandry <mary@thughunt.ing>
 // SPDX-FileCopyrightText: 2025 Piras314 <p1r4s@proton.me>
 // SPDX-FileCopyrightText: 2025 Rinary <72972221+Rinary1@users.noreply.github.com>
