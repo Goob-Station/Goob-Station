05b5f1888253607bde6f5f15da5711a9f56acb12
Nemanja <98561806+EmoGarbage404@users.noreply.github.com>
2025-05-31 09:40:25 -0400

    Add support for contextual information in EntityTables (#37737)
    * Add context support for entityTables
    
    * fix build fail
    
    * comments
    
    * Update Content.Shared/EntityTable/EntityTableSystem.cs
    
    ---------
    
    Co-authored-by: slarticodefast <161409025+slarticodefast@users.noreply.github.com>
    # Conflicts:
    #	Content.Shared/EntityTable/EntitySelectors/EntityTableSelector.cs
    #	Content.Shared/EntityTable/EntityTableSystem.cs

--- Content.Shared/EntityTable/EntitySelectors/NoneSelector.cs
+++ Content.Shared/EntityTable/EntitySelectors/NoneSelector.cs
@@ -84,7 +84,8 @@ public sealed partial class NoneSelector : EntityTableSelector
 {
     protected override IEnumerable<EntProtoId> GetSpawnsImplementation(System.Random rand,
         IEntityManager entMan,
-        IPrototypeManager proto)
+        IPrototypeManager proto,
+        EntityTableContext ctx)
     {
         yield break;
     }
