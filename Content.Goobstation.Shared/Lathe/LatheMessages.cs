447aa0813f22f905e41c3eee82d7f9efde55f0a9
GoobBot <uristmchands@proton.me>
2025-06-01 05:18:57 +0000

    chore: Automatically update REUSE headers

--- Content.Goobstation.Shared/Lathe/LatheMessages.cs
+++ Content.Goobstation.Shared/Lathe/LatheMessages.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 Soup-Byte07 <soupbyte30@gmail.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using Robust.Shared.Serialization;
 
 namespace Content.Goobstation.Shared.Lathe;
