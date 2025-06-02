4d0c8c2f7818dcd55d23008d04eb293270cc12b6
Soup-Byte07 <soupbyte30@gmail.com>
2025-06-02 08:45:02 +0200

    fixed merge conflict

--- Content.Server/Lathe/LatheSystem.cs
+++ Content.Server/Lathe/LatheSystem.cs
@@ -66,6 +66,7 @@
 // SPDX-FileCopyrightText: 2025 SX_7 <sn1.test.preria.2002@gmail.com>
 // SPDX-FileCopyrightText: 2025 ScarKy0 <106310278+ScarKy0@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Soup-Byte07 <soupbyte30@gmail.com>
+// SPDX-FileCopyrightText: 2025 Tim <timfalken@hotmail.com>
 // SPDX-FileCopyrightText: 2025 deltanedas <39013340+deltanedas@users.noreply.github.com>
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
