d77cf94765765c897098917f430dee0c436b5b46
GoobBot <uristmchands@proton.me>
2025-06-03 23:51:38 +0000

    chore: Automatically update REUSE headers

--- Content.Goobstation.Server/Xenobiology/SlimeGrinder/SlimeGrinderSystem.cs
+++ Content.Goobstation.Server/Xenobiology/SlimeGrinder/SlimeGrinderSystem.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 SolsticeOfTheWinter <solsticeofthewinter@gmail.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using System.Numerics;
 using Content.Goobstation.Shared.Xenobiology.Components;
 using Content.Server.Power.Components;
