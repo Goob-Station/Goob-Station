68c21f2ab4f1738e4a0751785c9a18c16d647372
GoobBot <uristmchands@proton.me>
2025-06-03 01:21:07 +0000

    chore: Automatically update REUSE headers

--- Content.Goobstation.Shared/Sandevistan/SandevistanUserComponent.cs
+++ Content.Goobstation.Shared/Sandevistan/SandevistanUserComponent.cs
@@ -1,4 +1,5 @@
 // SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
+// SPDX-FileCopyrightText: 2025 Ted Lukin <66275205+pheenty@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 pheenty <fedorlukin2006@gmail.com>
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
