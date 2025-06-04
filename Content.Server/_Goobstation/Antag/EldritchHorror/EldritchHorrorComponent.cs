e7f0870480756f26ba410526ee09046664c50b2c
GoobBot <uristmchands@proton.me>
2025-06-04 03:18:05 +0000

    chore: Automatically update REUSE headers

--- Content.Server/_Goobstation/Antag/EldritchHorror/EldritchHorrorComponent.cs
+++ Content.Server/_Goobstation/Antag/EldritchHorror/EldritchHorrorComponent.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 OnsenCapy <101037138+OnsenCapy@users.noreply.github.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using Robust.Shared.Prototypes;
 
 namespace Content.Server._Goobstation.Antag.EldritchHorror;
