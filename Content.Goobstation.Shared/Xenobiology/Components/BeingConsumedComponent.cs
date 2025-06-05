fa0179d5d9af1f4a928a6407be06682872f2852a
GoobBot <uristmchands@proton.me>
2025-06-05 09:42:59 +0000

    chore: Automatically update REUSE headers

--- Content.Goobstation.Shared/Xenobiology/Components/BeingConsumedComponent.cs
+++ Content.Goobstation.Shared/Xenobiology/Components/BeingConsumedComponent.cs
@@ -1,5 +1,6 @@
 // SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
 // SPDX-FileCopyrightText: 2025 SolsticeOfTheWinter <solsticeofthewinter@gmail.com>
+// SPDX-FileCopyrightText: 2025 TheBorzoiMustConsume <197824988+TheBorzoiMustConsume@users.noreply.github.com>
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
