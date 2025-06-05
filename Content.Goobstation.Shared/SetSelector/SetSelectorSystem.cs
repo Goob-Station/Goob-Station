e2867c4e8876a95ef4f4fed2a0c903c0ce7dc7bb
GoobBot <uristmchands@proton.me>
2025-06-05 19:37:25 +0000

    chore: Automatically update REUSE headers

--- Content.Goobstation.Shared/SetSelector/SetSelectorSystem.cs
+++ Content.Goobstation.Shared/SetSelector/SetSelectorSystem.cs
@@ -1,4 +1,5 @@
 // SPDX-FileCopyrightText: 2025 Conchelle <mary@thughunt.ing>
+// SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
 // SPDX-FileCopyrightText: 2025 Ted Lukin <66275205+pheenty@users.noreply.github.com>
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
