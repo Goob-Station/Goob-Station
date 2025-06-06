e3291c49a6cd85f33260a37768b41f38c6c91c71
GoobBot <uristmchands@proton.me>
2025-06-06 14:01:58 +0000

    chore: Automatically update REUSE headers

--- Content.Goobstation.Shared/NTR/NtrTaskData.cs
+++ Content.Goobstation.Shared/NTR/NtrTaskData.cs
@@ -3,6 +3,7 @@
 // SPDX-FileCopyrightText: 2023 metalgearsloth <31366439+metalgearsloth@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2024 Nemanja <98561806+EmoGarbage404@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 BeBright <98597725+be1bright@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
 // SPDX-FileCopyrightText: 2025 LuciferEOS <stepanteliatnik2022@gmail.com>
 // SPDX-FileCopyrightText: 2025 LuciferMkshelter <stepanteliatnik2022@gmail.com>
 // SPDX-FileCopyrightText: 2025 pheenty <fedorlukin2006@gmail.com>
