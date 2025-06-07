0c0019c7be3cd6a4e5f2215221319e54e92fbecf
GoobBot <uristmchands@proton.me>
2025-06-07 01:25:34 +0000

    chore: Automatically update REUSE headers

--- Content.Shared/Audio/SharedGlobalSoundSystem.cs
+++ Content.Shared/Audio/SharedGlobalSoundSystem.cs
@@ -2,6 +2,7 @@
 // SPDX-FileCopyrightText: 2022 ike709 <ike709@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2022 metalgearsloth <comedian_vs_clown@hotmail.com>
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
 // SPDX-FileCopyrightText: 2025 Marcus F <199992874+thebiggestbruh@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Solstice <solsticeofthewinter@gmail.com>
 // SPDX-FileCopyrightText: 2025 pathetic meowmeow <uhhadd@gmail.com>
