1a7137ad409a3c17ab4a2e6add4e29a4ff15f286
GoobBot <uristmchands@proton.me>
2025-06-09 21:00:02 +0000

    chore: Automatically update REUSE headers

--- Content.Shared/_Shitcode/Wizard/SpellEvents.cs
+++ Content.Shared/_Shitcode/Wizard/SpellEvents.cs
@@ -1,4 +1,5 @@
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 Armok <155400926+ARMOKS@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Aviu00 <93730715+Aviu00@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Misandry <mary@thughunt.ing>
 // SPDX-FileCopyrightText: 2025 Ted Lukin <66275205+pheenty@users.noreply.github.com>
