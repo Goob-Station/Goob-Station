c5eda3ca7fd16892d58c094790616efc422ded72
GoobBot <uristmchands@proton.me>
2025-06-10 18:48:04 +0000

    chore: Automatically update REUSE headers

--- Content.Goobstation.Shared/Mercenary/MercenaryRequesterComponent.cs
+++ Content.Goobstation.Shared/Mercenary/MercenaryRequesterComponent.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 IrisTheAmped <iristheamped@gmail.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using Robust.Shared.GameStates;
 using Content.Shared.Roles;
 using Robust.Shared.Audio;
