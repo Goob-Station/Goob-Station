d57ac4ea4f1498e4dae839a686bca3a439bc8fc5
GoobBot <uristmchands@proton.me>
2025-06-10 17:10:16 +0000

    chore: Automatically update REUSE headers

--- Content.IntegrationTests/Tests/_Goobstation/SecretPlus/SecretPlusTest.cs
+++ Content.IntegrationTests/Tests/_Goobstation/SecretPlus/SecretPlusTest.cs
@@ -1,6 +1,7 @@
 // SPDX-FileCopyrightText: 2024 TGRCDev <tgrc@tgrc.dev>
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Aviu00 <93730715+Aviu00@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 Ilya246 <ilyukarno@gmail.com>
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
