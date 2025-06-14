0672be49f0c05fe6bb74789cc6f10cd3faf8dc1a
ScarKy0 <106310278+ScarKy0@users.noreply.github.com>
2025-06-14 20:29:06 +0200

    Retractable items system + Arm Blade action (#38150)
    # Conflicts:
    #	Resources/Prototypes/Entities/Objects/Weapons/Melee/armblade.yml

--- Content.Shared/Containers/ItemSlot/ItemSlotsSystem.cs
+++ Content.Shared/Containers/ItemSlot/ItemSlotsSystem.cs
@@ -162,7 +162,7 @@ namespace Content.Shared.Containers.ItemSlots
                     continue;
 
                 var item = Spawn(slot.StartingItem, Transform(uid).Coordinates);
-                    
+
                 if (slot.ContainerSlot != null)
                     _containers.Insert(item, slot.ContainerSlot);
             }
