f08a0bf04b992b4d7a2cf5e62ff488b416f9c883
GoobBot <uristmchands@proton.me>
2025-06-14 14:25:22 +0000

    chore: Automatically update REUSE headers

--- Content.Goobstation.Shared/Overlays/ThermalVisionImmuneComponent.cs
+++ Content.Goobstation.Shared/Overlays/ThermalVisionImmuneComponent.cs
@@ -1,4 +1,5 @@
 // SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
+// SPDX-FileCopyrightText: 2025 Vrilly <tnjans@outlook.de>
 // SPDX-FileCopyrightText: 2025 widgetbeck <beckparrott@gmail.com>
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
