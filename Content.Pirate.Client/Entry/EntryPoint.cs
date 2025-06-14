9296481ef51f5074fdedbcadfb91eb4214208c0f
Dan <jamasty@protonmail.com>
2025-06-14 12:11:40 +0300

    custom ghost wip

---
+++ Content.Pirate.Client/Entry/EntryPoint.cs
@@ -0,0 +1,32 @@
+// SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 Misandry <mary@thughunt.ing>
+// SPDX-FileCopyrightText: 2025 gus <august.eymann@gmail.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+using Content.Pirate.Client.IoC;
+using Robust.Shared.ContentPack;
+using Robust.Shared.IoC;
+
+namespace Content.Pirate.Client.Entry;
+
+public sealed class EntryPoint : GameClient
+{
+    public override void PreInit()
+    {
+        base.PreInit();
+    }
+
+    public override void Init()
+    {
+        ContentGoobClientIoC.Register();
+
+        IoCManager.BuildGraph();
+        IoCManager.InjectDependencies(this);
+    }
+
+    public override void PostInit()
+    {
+        base.PostInit();
+    }
+}
