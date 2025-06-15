a809c61c7950d462123afc594c062456df152b9b
Aiden <28298836+Aidenkrz@users.noreply.github.com>
2025-06-15 16:51:41 -0500

    Suicide Test

--- Content.IntegrationTests/Tests/Commands/SuicideCommandTests.cs
+++ Content.IntegrationTests/Tests/Commands/SuicideCommandTests.cs
@@ -229,7 +229,7 @@ public sealed class SuicideCommandTests
                 Assert.That(mobStateSystem.IsDead(player, mobStateComp));
                 Assert.That(entManager.TryGetComponent<GhostComponent>(mindComponent.CurrentEntity, out var ghostComp) &&
                             !ghostComp.CanReturnToBody);
-                Assert.That(damageableComp.Damage.GetTotal(), Is.EqualTo(lethalDamageThreshold));
+                Assert.That(damageableComp.Damage.GetTotal(), Is.EqualTo(lethalDamageThreshold).Within(0.05));
             });
         });
 
