e5f9565c3ac126fea98c4331d5fc56dd4771e2be
Roudenn <romabond091@gmail.com>
2025-06-16 20:06:06 +0300

    it doisent work

---
+++ Content.Goobstation.Server/Emoting/RandomEmotesComponent.cs
@@ -0,0 +1,15 @@
+﻿using Content.Shared.Random;
+using Robust.Shared.Prototypes;
+
+namespace Content.Goobstation.Server.Emoting;
+
+/// <summary>
+/// Allows the entity to pick random emotes to say in the chat.
+/// Tied to work with
+/// </summary>
+[RegisterComponent]
+public sealed partial class RandomEmotesComponent : Component
+{
+    [DataField(required: true)]
+    public ProtoId<WeightedRandomPrototype> Weights;
+}
