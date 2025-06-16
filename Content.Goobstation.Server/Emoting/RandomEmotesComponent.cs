da6fe426e67437be82be6646d8f07485630dac97
GoobBot <uristmchands@proton.me>
2025-06-16 17:08:43 +0000

    chore: Automatically update REUSE headers

--- Content.Goobstation.Server/Emoting/RandomEmotesComponent.cs
+++ Content.Goobstation.Server/Emoting/RandomEmotesComponent.cs
@@ -1,4 +1,8 @@
-﻿using Content.Shared.Random;
+// SPDX-FileCopyrightText: 2025 Roudenn <romabond091@gmail.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+using Content.Shared.Random;
 using Robust.Shared.Prototypes;
 
 namespace Content.Goobstation.Server.Emoting;
--- Content.Goobstation.Server/NPC/HTN/PrimitiveTasks/Operators/EmoteKeyOperator.cs
+++ Content.Goobstation.Server/NPC/HTN/PrimitiveTasks/Operators/EmoteKeyOperator.cs
@@ -1,3 +1,14 @@
+// SPDX-FileCopyrightText: 2022 metalgearsloth <31366439+metalgearsloth@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2022 metalgearsloth <metalgearsloth@gmail.com>
+// SPDX-FileCopyrightText: 2023 DrSmugleaf <DrSmugleaf@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2023 deltanedas <39013340+deltanedas@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2024 Tornado Tech <54727692+Tornado-Technology@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 Roudenn <romabond091@gmail.com>
+// SPDX-FileCopyrightText: 2025 Timfa <timfalken@hotmail.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using Content.Server.Chat.Systems;
 using Content.Server.NPC;
 using Content.Server.NPC.HTN;
