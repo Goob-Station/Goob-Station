0e564cee300e4ae0a464187fb4284de211660880
GoobBot <uristmchands@proton.me>
2025-06-16 15:47:59 +0000

    chore: Automatically update REUSE headers

--- Content.Goobstation.Client/Physics/ComplexJointVisualsSystem.cs
+++ Content.Goobstation.Client/Physics/ComplexJointVisualsSystem.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 Roudenn <romabond091@gmail.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using Robust.Client.Graphics;
 using Robust.Shared.Prototypes;
 using Robust.Shared.Timing;
