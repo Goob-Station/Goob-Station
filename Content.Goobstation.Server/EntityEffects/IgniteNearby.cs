dd5c00abca456fdafb2b20f51b402c2d50fedbe6
TheBorzoiMustConsume <197824988+TheBorzoiMustConsume@users.noreply.github.com>
2025-06-16 14:25:03 +1000

    All extract jelly protos (missing functionality), tier 2 reactions

---
+++ Content.Goobstation.Server/EntityEffects/IgniteNearby.cs
@@ -0,0 +1,47 @@
+using System.Linq;
+using Content.Server.Atmos.Components;
+using Content.Server.Atmos.EntitySystems;
+using Content.Shared.Database;
+using Content.Shared.EntityEffects;
+using Robust.Shared.Prototypes;
+using SixLabors.ImageSharp.PixelFormats;
+
+namespace Content.Goobstation.Server.EntityEffects;
+
+/// <summary>
+///     Ignites mobs nearby.
+/// </summary>
+public sealed partial class IgniteNearby : EntityEffect
+{
+
+    [Dependency] private readonly EntityLookupSystem _lookup = default!;
+
+    [DataField]
+    public float Range = 7;
+
+    public override bool ShouldLog => true;
+
+    protected override string? ReagentEffectGuidebookText(IPrototypeManager prototype, IEntitySystemManager entSys)
+        => Loc.GetString("reagent-effect-guidebook-ignite", ("chance", Probability));
+
+    public override LogImpact LogImpact => LogImpact.Medium;
+
+    public override void Effect(EntityEffectBaseArgs args)
+    {
+        var flamSys = args.EntityManager.System<FlammableSystem>();
+        if (args is EntityEffectReagentArgs reagentArgs)
+        {
+            var lookup = _lookup.GetEntitiesInRange(args.TargetEntity, Range);
+            var flammableEntities = lookup
+                .Where(entity =>
+                    entity != null && args.EntityManager.TryGetComponent(entity, out FlammableComponent? flammable))
+                .Select(entity => (entity, args.EntityManager.GetComponent<FlammableComponent>(entity)))
+                .ToHashSet();
+
+            foreach (var (ent, flammable) in flammableEntities)
+            {
+                flamSys.Ignite(ent, args.TargetEntity, flammable: flammable);
+            }
+        }
+    }
+}
