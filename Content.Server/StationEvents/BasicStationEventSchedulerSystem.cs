6964808dcb2a4bbda3fa47fd4b970bf8f60f406e
GoobBot <uristmchands@proton.me>
2025-06-16 15:09:13 +0000

    chore: Automatically update REUSE headers

--- Content.Server/StationEvents/BasicStationEventSchedulerSystem.cs
+++ Content.Server/StationEvents/BasicStationEventSchedulerSystem.cs
@@ -7,6 +7,7 @@
 // SPDX-FileCopyrightText: 2024 deltanedas <39013340+deltanedas@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2024 deltanedas <@deltanedas:kde.org>
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
 // SPDX-FileCopyrightText: 2025 IProduceWidgets <107586145+IProduceWidgets@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Ilya246 <ilyukarno@gmail.com>
 //
