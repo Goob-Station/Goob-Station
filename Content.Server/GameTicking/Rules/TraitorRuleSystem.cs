b6f4746059354aee766c327380f980c34cdab625
GoobBot <uristmchands@proton.me>
2025-06-16 09:58:40 +0000

    chore: Automatically update REUSE headers

--- Content.Server/GameTicking/Rules/TraitorRuleSystem.cs
+++ Content.Server/GameTicking/Rules/TraitorRuleSystem.cs
@@ -104,6 +104,7 @@
 // SPDX-FileCopyrightText: 2024 voidnull000 <18663194+voidnull000@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2024 whateverusername0 <whateveremail>
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
 // SPDX-FileCopyrightText: 2025 MilenVolf <63782763+MilenVolf@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Milon <milonpl.git@proton.me>
 // SPDX-FileCopyrightText: 2025 SX-7 <sn1.test.preria.2002@gmail.com>
