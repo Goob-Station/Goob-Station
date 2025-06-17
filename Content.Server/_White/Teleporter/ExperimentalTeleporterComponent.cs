72dd6d472a503b5f5d2a2c4ef1e0d92776123c96
GoobBot <uristmchands@proton.me>
2025-06-17 19:07:18 +0000

    chore: Automatically update REUSE headers

--- Content.Server/_White/Teleporter/ExperimentalTeleporterComponent.cs
+++ Content.Server/_White/Teleporter/ExperimentalTeleporterComponent.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2024 Spatison <137375981+spatison@users.noreply.github.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using Robust.Shared.Audio;
 
 namespace Content.Server._White.Teleporter;
