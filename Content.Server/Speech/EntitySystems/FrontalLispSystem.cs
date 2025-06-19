3529dfd3cd5129617223bdc02632302762097254
Redrover1760 <39284090+Redrover1760@users.noreply.github.com>
2025-06-19 13:32:55 -0400

    makes the lisp accent slightly more comprehendable

--- Content.Server/Speech/EntitySystems/FrontalLispSystem.cs
+++ Content.Server/Speech/EntitySystems/FrontalLispSystem.cs
@@ -29,10 +29,10 @@ public sealed class FrontalLispSystem : EntitySystem
         var message = args.Message;
 
         // handles ts, sc(i|e|y), c(i|e|y), ps, st(io(u|n)), ch(i|e), z, s
-        message = RegexUpperTh.Replace(message, "TH");
+        message = RegexUpperTh.Replace(message, "Th"); // Goob Edit
         message = RegexLowerTh.Replace(message, "th");
         // handles ex(c), x
-        message = RegexUpperEcks.Replace(message, "EKTH");
+        message = RegexUpperEcks.Replace(message, "Ekth"); // Goob Edit
         message = RegexLowerEcks.Replace(message, "ekth");
 
         args.Message = message;
