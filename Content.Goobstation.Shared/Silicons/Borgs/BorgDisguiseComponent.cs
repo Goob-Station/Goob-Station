0faf1064358147b879900f1aa917dd9943e24926
GoobBot <uristmchands@proton.me>
2025-06-22 11:06:09 +0000

    chore: Automatically update REUSE headers

--- Content.Goobstation.Shared/Silicons/Borgs/BorgDisguiseComponent.cs
+++ Content.Goobstation.Shared/Silicons/Borgs/BorgDisguiseComponent.cs
@@ -1,3 +1,9 @@
+// SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
+// SPDX-FileCopyrightText: 2025 LuciferEOS <stepanteliatnik2022@gmail.com>
+// SPDX-FileCopyrightText: 2025 LuciferMkshelter <stepanteliatnik2022@gmail.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 
 using Robust.Shared.GameStates;
 using Robust.Shared.Prototypes;
