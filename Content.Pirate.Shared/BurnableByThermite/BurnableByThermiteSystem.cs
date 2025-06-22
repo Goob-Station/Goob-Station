59e7d6813fa128d86e796a0ba6bc7de8b507e289
CarryUa <mosuk81@gmail.com>
2025-06-22 13:17:14 +0200

    Organising code

--- Content.Pirate.Shared/BurnableByThermite/BurnableByThermiteSystem.cs
+++ Content.Pirate.Shared/BurnableByThermite/BurnableByThermiteSystem.cs
@@ -28,6 +28,8 @@ public sealed partial class SharedBurnableByThermiteSystem : EntitySystem
         base.Initialize();
         SubscribeLocalEvent<BurnableByThermiteComponent, InteractUsingEvent>(OnInteract);
     }
+
+    // Interaction Handling
     public void OnInteract(EntityUid uid, BurnableByThermiteComponent component, InteractUsingEvent args)
     {
         if (TryComp<SolutionContainerManagerComponent>(args.Used, out var beakerSolutionContainerComponent))
@@ -36,82 +38,12 @@ public sealed partial class SharedBurnableByThermiteSystem : EntitySystem
             OnInteractWithLighter(new(uid, component), args, ignitionSourceComponent);
         // TryComp<IgnitionSourceComponent>(args.Used, out var ignitionSourceComponent);
     }
-
-    public override void Update(float frameTime)
-    {
-        base.Update(frameTime);
-
-        var query = EntityQueryEnumerator<BurnableByThermiteComponent>();
-        while (query.MoveNext(out var uid, out var component))
-        {
-            if (!component.Ignited && !component.Burning) continue;
-            if (component.Ignited) OnUpdateIgnited(component);
-            if (component.Burning) OnUpdateBurning(uid, component, frameTime);
-        }
-
-    }
-    public void OnUpdateBurning(EntityUid uid, BurnableByThermiteComponent component, float deltaT)
-    {
-        SetSpriteData(uid, BurnableByThermiteVisuals.OnFireFull, true);
-        if (_gameTiming.CurTime.TotalSeconds - component.BurningStartTime >= component.BurnTime)
-        {
-            TryExtinguishStructure(new(uid, component));
-            return;
-        }
-        component.TotalDamageDealt += component.DPS * deltaT;
-        if (component.TotalDamageDealt >= component.TotalDamageUntilMelting)
-        {
-            OnEntityBurned(uid, component);
-            return;
-        }
-
-    }
-    public void OnEntityBurned(EntityUid uid, BurnableByThermiteComponent component)
-    {
-        component.BurningSoundStream = _audioSystem.Stop(component.BurningSoundStream);
-        _destructibleSystem.DestroyEntity(uid);
-    }
-    public void OnUpdateIgnited(BurnableByThermiteComponent component)
-    {
-        if (_gameTiming.CurTime.TotalSeconds - component.IgnitionStartTime >= component.IgnitionTime)
-        {
-            component.Ignited = false;
-            component.Burning = true;
-            component.BurningStartTime = (float) _gameTiming.CurTime.TotalSeconds;
-        }
-    }
     public void OnInteractWithLighter(Entity<BurnableByThermiteComponent> ent, InteractUsingEvent args, IgnitionSourceComponent ignitionSourceComponent)
     {
         if (!ignitionSourceComponent.Ignited) return;
         if (ent.Comp.Ignited || ent.Comp.Burning) return;
         TryIgniteStructure(ent, args);
     }
-    public bool TryExtinguishStructure(Entity<BurnableByThermiteComponent> ent)
-    {
-        if (!ent.Comp.Ignited && !ent.Comp.Burning) return false;
-        SetSpriteData(ent.Owner, BurnableByThermiteVisuals.CoveredInThermite, false, true);
-        ent.Comp.Ignited = false;
-        ent.Comp.Burning = false;
-        ent.Comp.BurningSoundStream = _audioSystem.Stop(ent.Comp.BurningSoundStream);
-        if (ent.Comp.ThermiteSolutionComponent is null) return true;
-        ent.Comp.ThermiteSolutionComponent.Solution.RemoveAllSolution();
-        return true;
-    }
-    public bool TryIgniteStructure(Entity<BurnableByThermiteComponent> ent, InteractUsingEvent args)
-    {
-        if (ent.Comp.ThermiteSolutionComponent is null) return false;
-        if (ent.Comp.ThermiteSolutionComponent.Solution.TryGetReagentQuantity(new("Thermite", null), out var thermiteQuantity) && thermiteQuantity == 0) return false;
-        SetSpriteData(ent.Owner, BurnableByThermiteVisuals.OnFireStart, true);
-        _pointLight.EnsureLight(ent.Owner);
-        _pointLight.SetColor(ent.Owner, Color.Yellow);
-        _popupSystem.PopupClient(Loc.GetString("thermite-on-structure-ignited"), args.User, args.User, PopupType.MediumCaution);
-        ent.Comp.Ignited = true;
-        TryPlayBurningSound(ent.Owner, ent.Comp);
-        ent.Comp.IgnitionStartTime = (float) _gameTiming.CurTime.TotalSeconds;
-        ent.Comp.ThermiteSolutionComponent.Solution.RemoveAllSolution();
-        return true;
-    }
-
     public void OnInteractWithBeaker(EntityUid structure, BurnableByThermiteComponent component, EntityUid beaker, InteractUsingEvent args, SolutionContainerManagerComponent beakerSolutionContainerComponent)
     {
         if (beakerSolutionContainerComponent.Containers is null) return;
@@ -139,6 +71,103 @@ public sealed partial class SharedBurnableByThermiteSystem : EntitySystem
         }
 
     }
+
+    // Update
+    public override void Update(float frameTime)
+    {
+        base.Update(frameTime);
+        var query = EntityQueryEnumerator<BurnableByThermiteComponent>();
+        while (query.MoveNext(out var uid, out var component))
+        {
+            if (!component.Ignited && !component.Burning) continue;
+            if (component.Ignited) OnUpdateIgnited(component);
+            if (component.Burning) OnUpdateBurning(uid, component, frameTime);
+        }
+    }
+    public void OnUpdateBurning(EntityUid uid, BurnableByThermiteComponent component, float deltaT)
+    {
+        SetSpriteData(uid, BurnableByThermiteVisuals.OnFireFull, true);
+        if (_gameTiming.CurTime.TotalSeconds - component.BurningStartTime >= component.BurnTime)
+        {
+            TryExtinguishStructure(new(uid, component));
+            return;
+        }
+        component.TotalDamageDealt += component.DPS * deltaT;
+        if (component.TotalDamageDealt >= component.TotalDamageUntilMelting)
+        {
+            OnEntityBurned(uid, component);
+            return;
+        }
+    }
+    public void OnUpdateIgnited(BurnableByThermiteComponent component)
+    {
+        if (_gameTiming.CurTime.TotalSeconds - component.IgnitionStartTime >= component.IgnitionTime)
+        {
+            component.Ignited = false;
+            component.Burning = true;
+            component.BurningStartTime = (float) _gameTiming.CurTime.TotalSeconds;
+        }
+    }
+
+    // Secondary Media
+    public void SetSpriteData(EntityUid uid, Enum visuals, bool state, bool disableOthers = false)
+    {
+        if (_appearanceSystem.TryGetData<bool>(uid, visuals, out var currentState))
+            if (currentState == state) return;
+
+        if (disableOthers)
+        {
+            _appearanceSystem.SetData(uid, BurnableByThermiteVisuals.CoveredInThermite, false);
+            _appearanceSystem.SetData(uid, BurnableByThermiteVisuals.OnFireStart, false);
+            _appearanceSystem.SetData(uid, BurnableByThermiteVisuals.OnFireFull, false);
+        }
+        _appearanceSystem.SetData(uid, visuals, state);
+    }
+    public bool TryPlayBurningSound(EntityUid uid, BurnableByThermiteComponent component)
+    {
+        if (_audioSystem.IsPlaying(component.BurningSoundStream))
+            _audioSystem.Stop(component.BurningSoundStream);
+        var resolvedSound = _audioSystem.ResolveSound(component.BurningSound);
+        component.BurningSoundStream = _audioSystem.PlayPvs(resolvedSound, uid)?.Entity;
+        return true;
+    }
+
+    // Secondary Structure
+    public void OnEntityBurned(EntityUid uid, BurnableByThermiteComponent component)
+    {
+        component.BurningSoundStream = _audioSystem.Stop(component.BurningSoundStream);
+        _destructibleSystem.DestroyEntity(uid);
+    }
+    public bool TryExtinguishStructure(Entity<BurnableByThermiteComponent> ent)
+
+
+    {
+        if (!ent.Comp.Ignited && !ent.Comp.Burning) return false;
+        SetSpriteData(ent.Owner, BurnableByThermiteVisuals.CoveredInThermite, false, true);
+        ent.Comp.Ignited = false;
+        ent.Comp.Burning = false;
+        ent.Comp.BurningSoundStream = _audioSystem.Stop(ent.Comp.BurningSoundStream);
+        if (ent.Comp.ThermiteSolutionComponent is null) return true;
+        ent.Comp.ThermiteSolutionComponent.Solution.RemoveAllSolution();
+        return true;
+    }
+    public bool TryIgniteStructure(Entity<BurnableByThermiteComponent> ent, InteractUsingEvent args)
+
+    {
+        if (ent.Comp.ThermiteSolutionComponent is null) return false;
+        if (ent.Comp.ThermiteSolutionComponent.Solution.TryGetReagentQuantity(new("Thermite", null), out var thermiteQuantity) && thermiteQuantity == 0) return false;
+        SetSpriteData(ent.Owner, BurnableByThermiteVisuals.OnFireStart, true);
+        _pointLight.EnsureLight(ent.Owner);
+        _pointLight.SetColor(ent.Owner, Color.Yellow);
+        _popupSystem.PopupClient(Loc.GetString("thermite-on-structure-ignited"), args.User, args.User, PopupType.MediumCaution);
+        ent.Comp.Ignited = true;
+        TryPlayBurningSound(ent.Owner, ent.Comp);
+        ent.Comp.IgnitionStartTime = (float) _gameTiming.CurTime.TotalSeconds;
+        ent.Comp.ThermiteSolutionComponent.Solution.RemoveAllSolution();
+        return true;
+    }
+
+    // Secondary Chemistry
     public void TransferReagent(Entity<SolutionComponent> to, ReagentQuantity reagent, Entity<SolutionComponent> from, EntityUid structure, EntityUid user)
     {
         _solutionSystem.TryAddReagent(to, reagent, out var transferedAmount);
@@ -154,28 +183,5 @@ public sealed partial class SharedBurnableByThermiteSystem : EntitySystem
             SetSpriteData(structure, BurnableByThermiteVisuals.CoveredInThermite, true);
             return;
         }
-    }
-    public void SetSpriteData(EntityUid uid, Enum visuals, bool state, bool disableOthers = false)
-    {
-        if (_appearanceSystem.TryGetData<bool>(uid, visuals, out var currentState))
-        {
-            if (currentState == state) return;
-        }
-        if (disableOthers)
-        {
-            _appearanceSystem.SetData(uid, BurnableByThermiteVisuals.CoveredInThermite, false);
-            _appearanceSystem.SetData(uid, BurnableByThermiteVisuals.OnFireStart, false);
-            _appearanceSystem.SetData(uid, BurnableByThermiteVisuals.OnFireFull, false);
-        }
-        _appearanceSystem.SetData(uid, visuals, state);
-    }
-
-    public bool TryPlayBurningSound(EntityUid uid, BurnableByThermiteComponent component)
-    {
-        if (_audioSystem.IsPlaying(component.BurningSoundStream))
-            _audioSystem.Stop(component.BurningSoundStream);
-        var resolvedSound = _audioSystem.ResolveSound(component.BurningSound);
-        component.BurningSoundStream = _audioSystem.PlayPvs(resolvedSound, uid)?.Entity;
-        return true;
     }
 }
