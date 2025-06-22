d459f552ca9018260d5d159d9c8bc46b173d813b
GoobBot <uristmchands@proton.me>
2025-06-22 10:32:07 +0000

    chore: Automatically update REUSE headers

--- Content.Client/_Goobstation/Silicons/Borgs/BorgDisguiseSystem.cs
+++ Content.Client/_Goobstation/Silicons/Borgs/BorgDisguiseSystem.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 LuciferMkshelter <stepanteliatnik2022@gmail.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using Content.Shared._Goobstation.Silicons.Borgs;
 using Content.Shared.Silicons.Borgs.Components;
 using Content.Client.Silicons.Borgs;
