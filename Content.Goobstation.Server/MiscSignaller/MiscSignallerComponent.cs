e403d15dfd19aafa76fee7764c6ab39194940a81
GoobBot <uristmchands@proton.me>
2025-06-24 08:34:17 +0000

    chore: Automatically update REUSE headers

--- Content.Goobstation.Server/MiscSignaller/MiscSignallerComponent.cs
+++ Content.Goobstation.Server/MiscSignaller/MiscSignallerComponent.cs
@@ -1,4 +1,5 @@
 // SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
+// SPDX-FileCopyrightText: 2025 SX-7 <92227810+SX-7@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 SomethingUnbearable <mewatcher102@gmail.com>
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
