3475466aa1a8b3d09722781e3c2542c60df85c10
GoobBot <uristmchands@proton.me>
2025-06-25 22:34:15 +0000

    chore: Automatically update REUSE headers

--- Content.Goobstation.Shared/TapeRecorder/Components/TapeRecorderComponent.cs
+++ Content.Goobstation.Shared/TapeRecorder/Components/TapeRecorderComponent.cs
@@ -1,6 +1,8 @@
 // SPDX-FileCopyrightText: 2024 deltanedas <39013340+deltanedas@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 BombasterDS <deniskaporoshok@gmail.com>
+// SPDX-FileCopyrightText: 2025 BombasterDS2 <shvalovdenis.workmail@gmail.com>
 // SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
+// SPDX-FileCopyrightText: 2025 deltanedas <@deltanedas:kde.org>
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
