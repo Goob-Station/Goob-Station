902516ade0302a147bf8bde644cca6326622298c
OnsenCapy <101037138+OnsenCapy@users.noreply.github.com>
2025-06-26 21:02:28 -0300

    Rotting

--- Content.Server/_DV/CosmicCult/Abilities/CosmicConversionSystem.cs
+++ Content.Server/_DV/CosmicCult/Abilities/CosmicConversionSystem.cs
@@ -16,6 +16,7 @@ using Content.Shared.Mindshield.Components;
 using Content.Shared.Mobs.Systems;
 using Content.Shared.Popups;
 using Content.Shared.Stunnable;
+using Content.Server.Atmos.Rotting;
 
 namespace Content.Server._DV.CosmicCult.Abilities;
 
@@ -28,6 +29,7 @@ public sealed class CosmicConversionSystem : EntitySystem
     [Dependency] private readonly PopupSystem _popup = default!;
     [Dependency] private readonly SharedCosmicCultSystem _cosmicCult = default!;
     [Dependency] private readonly SharedStunSystem _stun = default!;
+    [Dependency] private readonly RottingSystem _rotting = default!;
 
     public override void Initialize()
     {
@@ -63,7 +65,12 @@ public sealed class CosmicConversionSystem : EntitySystem
             {
                 _popup.PopupEntity(Loc.GetString("cult-glyph-target-dead"), uid, args.User);
                 args.Cancel();
-            }*/ 
+            }*/
+            if (_rotting.IsRotten(target))
+            {
+                _popup.PopupEntity(Loc.GetString("cult-glyph-target-rotting"), uid, args.User);
+                args.Cancel();
+            }
             if (HasComp<BibleUserComponent>(target))
             {
                 _popup.PopupEntity(Loc.GetString("cult-glyph-target-chaplain"), uid, args.User);
