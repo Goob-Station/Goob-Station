248e67460605db5b06b33c0ef487f194e8028ff1
GoobBot <uristmchands@proton.me>
2025-06-27 23:43:40 +0000

    chore: Automatically update REUSE headers

--- Content.Goobstation.Client/Changeling/FleshmendEffectSystem.cs
+++ Content.Goobstation.Client/Changeling/FleshmendEffectSystem.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 Marcus F <199992874+thebiggestbruh@users.noreply.github.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using Content.Goobstation.Shared.Changeling.Components;
 using Robust.Client.GameObjects;
 using Robust.Shared.Utility;
--- Content.Goobstation.Server/Changeling/FleshmendSystem.cs
+++ Content.Goobstation.Server/Changeling/FleshmendSystem.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 Marcus F <199992874+thebiggestbruh@users.noreply.github.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using System.Linq;
 using Content.Goobstation.Maths.FixedPoint;
 using Content.Goobstation.Shared.Changeling.Components;
