Simple-Station/Einstein-Engines

Shadowling Antagonist (SS13 Port and Remake) (#2207)
# Description

<!--
Explain this PR in as much detail as applicable

Some example prompts to consider:
How might this affect the game? The codebase?
What might be some alternatives to this?
How/Who does this benefit/hurt [the game/codebase]?
-->

Ports Shadowlings from SS13 to SS14 with a remake to make them fun to
play.

Minimal Design Doc (not up-to-date, read comments in this repo for
updates):

https://github.com/Lumminal/SS14-Design-Docs-Lumminal/blob/main/Shadowling.md

---

# TODO

- Abilities
  - [X] Hatch
  - [x] Glare
  - [X] Enthrall
  - [x] Veil
  - [x] Shadow Walk
  - [x] Icy Veins
  - [x] Collective Mind
  - [x] Rapid Re-Hatch
  - [x] Destroy Engines
  - [x] Sonic Screech
  - [x] Blindness Smoke
  - [x] Null Charge
  - [x] Black Recuperation
  - [x] Empowered Enthrall
  - [x] Nox Imperii
  - [x] Ascension
  - [x] Annihilate
  - [x] Hypnosis
  - [x] Plane-Shift
  - [x] Lighting Storm
  - [x] Ascendant Broadcast
- Antags
  - [X] Thrall
      - [x] Guise
      - [x] Thrall Darksight 
  - [x] Lesser Shadowling
- Passive
  - [x] Light Resistance Scaling
  - [x] Shadowmind
  - [x] Damage on Light
- Other
  - [x] Sounds
  - [x] Sprites
  - [x] Psionic Interactions
  - [x] Handle Edge Cases
---

<details><summary><h1>Media</h1></summary>
<p>

https://www.youtube.com/watch?v=H-Ee5wuRINc

</p>
</details>

---

# Changelog

:cl:
- add: The shadows have awakened, and their ascendance is soon to
follow. Do not enter maints.

---------

Signed-off-by: Lumminal <81829924+Lumminal@users.noreply.github.com>

Content.Server/GameTicking/Rules/ShadowlingRuleSystem.cs
@@ -1,0 +1,133 @@
+using Content.Server.Antag;
+using Content.Server.GameTicking.Rules.Components;
+using Content.Server.Mind;
+using Content.Server.Roles;
+using Content.Server.Zombies;
+using Content.Shared._EE.Shadowling;
+using Content.Shared.GameTicking.Components;
+using Content.Shared.Mobs.Systems;
+using Content.Shared.NPC.Prototypes;
+using Content.Shared.NPC.Systems;
+using Content.Shared.Roles;
+using Robust.Shared.Audio;
+using Robust.Shared.Prototypes;
+
+namespace Content.Server.GameTicking.Rules;
+
+public sealed class ShadowlingRuleSystem : GameRuleSystem<ShadowlingRuleComponent>
+{
+    [Dependency] private readonly SharedRoleSystem _role = default!;
+    [Dependency] private readonly AntagSelectionSystem _antag = default!;
+    [Dependency] private readonly MindSystem _mind = default!;
+    [Dependency] private readonly MobStateSystem _mob = default!;
+    [Dependency] private readonly NpcFactionSystem _npc = default!;
+
+    public readonly SoundSpecifier BriefingSound = new SoundPathSpecifier("/Audio/_EE/Shadowling/shadowling.ogg");
+
+    [ValidatePrototypeId<EntityPrototype>] EntProtoId _mindRole = "MindRoleShadowling";
+
+    public readonly ProtoId<NpcFactionPrototype> ShadowlingFactionId = "Shadowling";
+
+    public readonly ProtoId<NpcFactionPrototype> NanotrasenFactionId = "NanoTrasen";
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<ShadowlingRuleComponent, AfterAntagEntitySelectedEvent>(OnSelectAntag);
+        SubscribeLocalEvent<ShadowlingRoleComponent, GetBriefingEvent>(OnGetBriefing);
+
+        SubscribeLocalEvent<ShadowlingAscendEvent>(OnAscend);
+        SubscribeLocalEvent<ShadowlingDeathEvent>(OnDeath);
+    }
+
+    private void OnDeath(ShadowlingDeathEvent args)
+    {
+        var rulesQuery = QueryActiveRules();
+        while (rulesQuery.MoveNext(out _, out var shadowling, out _))
+        {
+            var shadowlingCount = 0;
+            var shadowlingDead = 0;
+            var query = EntityQueryEnumerator<ShadowlingComponent>();
+
+            while (query.MoveNext(out var uid, out _))
+            {
+                shadowlingCount++;
+                if (_mob.IsDead(uid) || _mob.IsInvalidState(uid))
+                    shadowlingDead++;
+            }
+
+            if (shadowlingCount == shadowlingDead)
+                shadowling.WinCondition = ShadowlingWinCondition.Failure;
+        }
+    }
+
+    private void OnAscend(ShadowlingAscendEvent args)
+    {
+        var rulesQuery = QueryActiveRules();
+        while (rulesQuery.MoveNext(out _, out var shadowling, out _))
+        {
+            shadowling.WinCondition = ShadowlingWinCondition.Win;
+            return;
+        }
+    }
+
+    private void OnGetBriefing(EntityUid uid, ShadowlingRoleComponent component, ref GetBriefingEvent args)
+    {
+        var ent = args.Mind.Comp.OwnedEntity;
+        var sling = HasComp<ShadowlingComponent>(ent);
+        args.Briefing = Loc.GetString(sling ? "shadowling-briefing" : "thrall-briefing");
+    }
+
+    private void OnSelectAntag(EntityUid uid, ShadowlingRuleComponent comp, ref AfterAntagEntitySelectedEvent args)
+    {
+        MakeShadowling(args.EntityUid, comp);
+    }
+
+    public bool MakeShadowling(EntityUid target, ShadowlingRuleComponent rule)
+    {
+        EnsureComp<ZombieImmuneComponent>(target);
+
+        if (!_mind.TryGetMind(target, out var mindId, out var mind))
+            return false;
+
+        _role.MindAddRole(mindId, _mindRole.Id, mind, true);
+
+        _npc.RemoveFaction(target, NanotrasenFactionId, false);
+        _npc.AddFaction(target, ShadowlingFactionId);
+
+        TryComp<MetaDataComponent>(target, out var metaData);
+        if (metaData == null)
+            return false;
+
+        var briefing = Loc.GetString("shadowling-role-greeting");
+
+        _antag.SendBriefing(target, briefing, Color.MediumPurple, BriefingSound);
+
+        EnsureComp<ShadowlingComponent>(target);
+
+        rule.ShadowlingMinds.Add(mindId);
+        return true;
+    }
+
+    protected override void AppendRoundEndText(
+        EntityUid uid,
+        ShadowlingRuleComponent component,
+        GameRuleComponent gamerule,
+        ref RoundEndTextAppendEvent args
+    )
+    {
+        base.AppendRoundEndText(uid, component, gamerule, ref args);
+        var winText = Loc.GetString($"shadowling-condition-{component.WinCondition.ToString().ToLower()}");
+        args.AddLine(winText);
+
+        args.AddLine(Loc.GetString("shadowling-list-start"));
+
+        var sessionData = _antag.GetAntagIdentifiers(uid);
+        foreach (var (_, data, name) in sessionData)
+        {
+            var listing = Loc.GetString("shadowling-list-name", ("name", name), ("user", data.UserName));
+            args.AddLine(listing);
+        }
+    }
+}

Content.Shared/_EE/Shadowling/Components/Abilities/CollectiveMind/ShadowlingSonicScreechComponent.cs
@@ -1,0 +1,45 @@
+using System.Numerics;
+using Content.Shared.Damage;
+using Robust.Shared.Audio;
+using Robust.Shared.GameStates;
+
+
+namespace Content.Shared._EE.Shadowling;
+
+
+/// <summary>
+/// This is used for the Sonic Screech ability.
+/// </summary>
+[RegisterComponent, NetworkedComponent]
+public sealed partial class ShadowlingSonicScreechComponent : Component
+{
+    [DataField]
+    public float Range = 5f;
+
+    [DataField]
+    public TimeSpan SiliconStunTime = TimeSpan.FromSeconds(5);
+
+    [DataField]
+    public string WindowTag = "Window";
+
+    [DataField]
+    public DamageSpecifier WindowDamage = new()
+    {
+        DamageDict = new()
+        {
+            { "Structural", 50 }
+        }
+    };
+
+    [DataField]
+    public float ScreechKick = 80f;
+
+    [DataField]
+    public string ProtoFlash = "EffectScreech";
+
+    [DataField]
+    public SoundSpecifier? ScreechSound = new SoundPathSpecifier("/Audio/_EE/Shadowling/screech.ogg");
+
+    [DataField]
+    public string? SonicScreechEffect = "ShadowlingSonicScreechEffect";
+}

Content.Shared/_EE/Shadowling/Components/Abilities/PreAscension/ShadowlingRapidRehatchComponent.cs
@@ -1,0 +1,26 @@
+using Robust.Shared.Audio;
+using Robust.Shared.GameStates;
+
+
+namespace Content.Shared._EE.Shadowling;
+
+
+/// <summary>
+/// This is used for the Rapid Re-Hatch ability
+/// </summary>
+[RegisterComponent, NetworkedComponent]
+public sealed partial class ShadowlingRapidRehatchComponent : Component
+{
+    public string? ActionRapidRehatch = "ActionRapidRehatch";
+
+    public EntityUid? ActionRapidRehatchEntity { get; set; }
+
+    [DataField]
+    public float DoAfterTime = 4f;
+
+    [DataField]
+    public string? RapidRehatchEffect = "ShadowlingRapidRehatchEffect";
+
+    [DataField]
+    public SoundSpecifier? RapidRehatchSound = new SoundPathSpecifier("/Audio/_EE/Shadowling/rapid_rehatch.ogg");
+}

Content.Shared/_EE/Shadowling/Components/HatchingEggComponent.cs
@@ -1,0 +1,27 @@
+using Robust.Shared.Audio;
+using Robust.Shared.GameStates;
+
+namespace Content.Shared._EE.Shadowling;
+
+[RegisterComponent, NetworkedComponent]
+public sealed partial class HatchingEggComponent : Component
+{
+    [DataField]
+    public float CooldownTimer = 15.0f;
+
+    [DataField]
+    public EntityUid? ShadowlingInside;
+
+    public bool HasBeenHatched;
+
+    // I know this looks dumb but it looks more readable and less error-prone than when I tried with an array
+    // maybe there's a better way but its only 3 messages that need to appear
+    // Update: I found a way but im too lazy to do it so just deal with this
+    public bool HasFirstMessageAppeared;
+    public bool HasSecondMessageAppeared;
+    public bool HasThirdMessageAppeared;
+
+    public SoundSpecifier? CrackFirst = new SoundPathSpecifier("/Audio/_EE/Shadowling/egg/crack01.ogg");
+    public SoundSpecifier? CrackSecond = new SoundPathSpecifier("/Audio/_EE/Shadowling/egg/crack02.ogg");
+    public SoundSpecifier? CrackThird = new SoundPathSpecifier("/Audio/_EE/Shadowling/egg/crack03.ogg");
+}

Content.Shared/_EE/Shadowling/Components/ShadowlingAscensionEggComponent.cs
@@ -1,0 +1,51 @@
+using Content.Shared.Announcements.Prototypes;
+using Robust.Shared.Audio;
+using Robust.Shared.GameStates;
+using Robust.Shared.Prototypes;
+
+
+namespace Content.Shared._EE.Shadowling.Components;
+
+
+/// <summary>
+/// This is used for the Ascension Egg
+/// </summary>
+[RegisterComponent, NetworkedComponent]
+public sealed partial class ShadowlingAscensionEggComponent : Component
+{
+    [DataField]
+    public TimeSpan NextUpdateTime = TimeSpan.Zero;
+
+    [DataField]
+    public TimeSpan UpdateInterval = TimeSpan.FromSeconds(300);
+
+    [DataField]
+    public TimeSpan AscendingEffectInterval = TimeSpan.FromSeconds(8.02);
+
+    [DataField]
+    public EntityUid? Creator;
+
+    [DataField]
+    public bool StartTimer;
+
+    [DataField]
+    public string VerbName = "Start Ascension";
+
+    [DataField]
+    public EntityUid? ShadowlingInsideEntity;
+
+    [DataField]
+    public string? ShadowlingInside = "AscensionEggShadowlingInside";
+
+    [DataField]
+    public string? AscendingEffect = "ShadowlingAscendingEffect";
+
+    [DataField]
+    public bool AscendingEffectAdded;
+
+    [DataField]
+    public EntityUid? AscendingEffectEntity;
+
+    [DataField]
+    public SoundSpecifier? AscensionEnterSound = new SoundPathSpecifier("/Audio/_EE/Shadowling/egg/ascension_enter.ogg");
+}

Content.Shared/_EE/Shadowling/Components/ThrallComponent.cs
@@ -1,0 +1,50 @@
+using Content.Shared.Language;
+using Content.Shared.StatusIcon;
+using Robust.Shared.Audio;
+using Robust.Shared.GameStates;
+using Robust.Shared.Prototypes;
+
+
+namespace Content.Shared._EE.Shadowling;
+
+
+/// <summary>
+/// This is used for marking Thralls and storing their icons
+/// </summary>
+[RegisterComponent, NetworkedComponent]
+public sealed partial class ThrallComponent : Component
+{
+    [DataField]
+    public float EnthrallDurationEffect = 1.5f;
+
+    [DataField, ViewVariables(VVAccess.ReadWrite)]
+    public ProtoId<FactionIconPrototype> StatusIcon { get; set; } = "ThrallFaction";
+
+    public readonly List<ProtoId<EntityPrototype>> BaseThrallActions = new()
+    {
+        "ActionThrallDarksight",
+        "ActionGuise"
+    };
+
+    public string? ActionThrallDarksight = "ActionThrallDarksight";
+    public string? ActionGuise = "ActionGuise";
+
+    public EntityUid? ActionThrallDarksightEntity;
+    public EntityUid? ActionGuiseEntity;
+
+    [DataField]
+    public bool NightVisionMode;
+
+    [DataField]
+    public SoundSpecifier? ThrallConverted = new SoundPathSpecifier("/Audio/_EE/Shadowling/thrall.ogg");
+
+    /// <summary>
+    /// The shadowling that converted the Thrall
+    /// </summary>
+    [DataField]
+    public EntityUid? Converter;
+
+    [DataField]
+    public ProtoId<LanguagePrototype> SlingLanguageId { get; set; } = "Shadowmind";
+
+}

Goob-Station/Goob-Station

      - [x] Thrall Darksight

Content.Goobstation.Server/Shadowling/Rules/ShadowlingRuleSystem.cs
@@ -1,0 +1,133 @@
+using Content.Server.Antag;
+using Content.Server.GameTicking.Rules.Components;
+using Content.Server.Mind;
+using Content.Server.Roles;
+using Content.Server.Zombies;
+using Content.Shared._EE.Shadowling;
+using Content.Shared.GameTicking.Components;
+using Content.Shared.Mobs.Systems;
+using Content.Shared.NPC.Prototypes;
+using Content.Shared.NPC.Systems;
+using Content.Shared.Roles;
+using Robust.Shared.Audio;
+using Robust.Shared.Prototypes;
+
+namespace Content.Server.GameTicking.Rules;
+
+public sealed class ShadowlingRuleSystem : GameRuleSystem<ShadowlingRuleComponent>
+{
+    [Dependency] private readonly SharedRoleSystem _role = default!;
+    [Dependency] private readonly AntagSelectionSystem _antag = default!;
+    [Dependency] private readonly MindSystem _mind = default!;
+    [Dependency] private readonly MobStateSystem _mob = default!;
+    [Dependency] private readonly NpcFactionSystem _npc = default!;
+
+    public readonly SoundSpecifier BriefingSound = new SoundPathSpecifier("/Audio/_EinsteinEngines/Shadowling/shadowling.ogg");
+
+    [ValidatePrototypeId<EntityPrototype>] EntProtoId _mindRole = "MindRoleShadowling";
+
+    public readonly ProtoId<NpcFactionPrototype> ShadowlingFactionId = "Shadowling";
+
+    public readonly ProtoId<NpcFactionPrototype> NanotrasenFactionId = "NanoTrasen";
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<ShadowlingRuleComponent, AfterAntagEntitySelectedEvent>(OnSelectAntag);
+        SubscribeLocalEvent<ShadowlingRoleComponent, GetBriefingEvent>(OnGetBriefing);
+
+        SubscribeLocalEvent<ShadowlingAscendEvent>(OnAscend);
+        SubscribeLocalEvent<ShadowlingDeathEvent>(OnDeath);
+    }
+
+    private void OnDeath(ShadowlingDeathEvent args)
+    {
+        var rulesQuery = QueryActiveRules();
+        while (rulesQuery.MoveNext(out _, out var shadowling, out _))
+        {
+            var shadowlingCount = 0;
+            var shadowlingDead = 0;
+            var query = EntityQueryEnumerator<ShadowlingComponent>();
+
+            while (query.MoveNext(out var uid, out _))
+            {
+                shadowlingCount++;
+                if (_mob.IsDead(uid) || _mob.IsInvalidState(uid))
+                    shadowlingDead++;
+            }
+
+            if (shadowlingCount == shadowlingDead)
+                shadowling.WinCondition = ShadowlingWinCondition.Failure;
+        }
+    }
+
+    private void OnAscend(ShadowlingAscendEvent args)
+    {
+        var rulesQuery = QueryActiveRules();
+        while (rulesQuery.MoveNext(out _, out var shadowling, out _))
+        {
+            shadowling.WinCondition = ShadowlingWinCondition.Win;
+            return;
+        }
+    }
+
+    private void OnGetBriefing(EntityUid uid, ShadowlingRoleComponent component, ref GetBriefingEvent args)
+    {
+        var ent = args.Mind.Comp.OwnedEntity;
+        var sling = HasComp<ShadowlingComponent>(ent);
+        args.Briefing = Loc.GetString(sling ? "shadowling-briefing" : "thrall-briefing");
+    }
+
+    private void OnSelectAntag(EntityUid uid, ShadowlingRuleComponent comp, ref AfterAntagEntitySelectedEvent args)
+    {
+        MakeShadowling(args.EntityUid, comp);
+    }
+
+    public bool MakeShadowling(EntityUid target, ShadowlingRuleComponent rule)
+    {
+        EnsureComp<ZombieImmuneComponent>(target);
+
+        if (!_mind.TryGetMind(target, out var mindId, out var mind))
+            return false;
+
+        _role.MindAddRole(mindId, _mindRole.Id, mind, true);
+
+        _npc.RemoveFaction(target, NanotrasenFactionId, false);
+        _npc.AddFaction(target, ShadowlingFactionId);
+
+        TryComp<MetaDataComponent>(target, out var metaData);
+        if (metaData == null)
+            return false;
+
+        var briefing = Loc.GetString("shadowling-role-greeting");
+
+        _antag.SendBriefing(target, briefing, Color.MediumPurple, BriefingSound);
+
+        EnsureComp<ShadowlingComponent>(target);
+
+        rule.ShadowlingMinds.Add(mindId);
+        return true;
+    }
+
+    protected override void AppendRoundEndText(
+        EntityUid uid,
+        ShadowlingRuleComponent component,
+        GameRuleComponent gamerule,
+        ref RoundEndTextAppendEvent args
+    )
+    {
+        base.AppendRoundEndText(uid, component, gamerule, ref args);
+        var winText = Loc.GetString($"shadowling-condition-{component.WinCondition.ToString().ToLower()}");
+        args.AddLine(winText);
+
+        args.AddLine(Loc.GetString("shadowling-list-start"));
+
+        var sessionData = _antag.GetAntagIdentifiers(uid);
+        foreach (var (_, data, name) in sessionData)
+        {
+            var listing = Loc.GetString("shadowling-list-name", ("name", name), ("user", data.UserName));
+            args.AddLine(listing);
+        }
+    }
+}

Content.Goobstation.Shared/Shadowling/Components/Abilities/CollectiveMind/ShadowlingSonicScreechComponent.cs
@@ -1,0 +1,45 @@
+using System.Numerics;
+using Content.Shared.Damage;
+using Robust.Shared.Audio;
+using Robust.Shared.GameStates;
+
+
+namespace Content.Shared._EE.Shadowling;
+
+
+/// <summary>
+/// This is used for the Sonic Screech ability.
+/// </summary>
+[RegisterComponent, NetworkedComponent]
+public sealed partial class ShadowlingSonicScreechComponent : Component
+{
+    [DataField]
+    public float Range = 5f;
+
+    [DataField]
+    public TimeSpan SiliconStunTime = TimeSpan.FromSeconds(5);
+
+    [DataField]
+    public string WindowTag = "Window";
+
+    [DataField]
+    public DamageSpecifier WindowDamage = new()
+    {
+        DamageDict = new()
+        {
+            { "Structural", 50 }
+        }
+    };
+
+    [DataField]
+    public float ScreechKick = 80f;
+
+    [DataField]
+    public string ProtoFlash = "EffectScreech";
+
+    [DataField]
+    public SoundSpecifier? ScreechSound = new SoundPathSpecifier("/Audio/_EinsteinEngines/Shadowling/screech.ogg");
+
+    [DataField]
+    public string? SonicScreechEffect = "ShadowlingSonicScreechEffect";
+}

Content.Goobstation.Shared/Shadowling/Components/Abilities/PreAscension/ShadowlingRapidRehatchComponent.cs
@@ -1,0 +1,26 @@
+using Robust.Shared.Audio;
+using Robust.Shared.GameStates;
+
+
+namespace Content.Shared._EE.Shadowling;
+
+
+/// <summary>
+/// This is used for the Rapid Re-Hatch ability
+/// </summary>
+[RegisterComponent, NetworkedComponent]
+public sealed partial class ShadowlingRapidRehatchComponent : Component
+{
+    public string? ActionRapidRehatch = "ActionRapidRehatch";
+
+    public EntityUid? ActionRapidRehatchEntity { get; set; }
+
+    [DataField]
+    public float DoAfterTime = 4f;
+
+    [DataField]
+    public string? RapidRehatchEffect = "ShadowlingRapidRehatchEffect";
+
+    [DataField]
+    public SoundSpecifier? RapidRehatchSound = new SoundPathSpecifier("/Audio/_EinsteinEngines/Shadowling/rapid_rehatch.ogg");
+}

Content.Goobstation.Shared/Shadowling/Components/HatchingEggComponent.cs
@@ -1,0 +1,27 @@
+using Robust.Shared.Audio;
+using Robust.Shared.GameStates;
+
+namespace Content.Shared._EE.Shadowling;
+
+[RegisterComponent, NetworkedComponent]
+public sealed partial class HatchingEggComponent : Component
+{
+    [DataField]
+    public float CooldownTimer = 15.0f;
+
+    [DataField]
+    public EntityUid? ShadowlingInside;
+
+    public bool HasBeenHatched;
+
+    // I know this looks dumb but it looks more readable and less error-prone than when I tried with an array
+    // maybe there's a better way but its only 3 messages that need to appear
+    // Update: I found a way but im too lazy to do it so just deal with this
+    public bool HasFirstMessageAppeared;
+    public bool HasSecondMessageAppeared;
+    public bool HasThirdMessageAppeared;
+
+    public SoundSpecifier? CrackFirst = new SoundPathSpecifier("/Audio/_EinsteinEngines/Shadowling/egg/crack01.ogg");
+    public SoundSpecifier? CrackSecond = new SoundPathSpecifier("/Audio/_EinsteinEngines/Shadowling/egg/crack02.ogg");
+    public SoundSpecifier? CrackThird = new SoundPathSpecifier("/Audio/_EinsteinEngines/Shadowling/egg/crack03.ogg");
+}

Content.Goobstation.Shared/Shadowling/Components/ShadowlingAscensionEggComponent.cs
@@ -1,0 +1,51 @@
+using Content.Shared.Announcements.Prototypes;
+using Robust.Shared.Audio;
+using Robust.Shared.GameStates;
+using Robust.Shared.Prototypes;
+
+
+namespace Content.Shared._EE.Shadowling.Components;
+
+
+/// <summary>
+/// This is used for the Ascension Egg
+/// </summary>
+[RegisterComponent, NetworkedComponent]
+public sealed partial class ShadowlingAscensionEggComponent : Component
+{
+    [DataField]
+    public TimeSpan NextUpdateTime = TimeSpan.Zero;
+
+    [DataField]
+    public TimeSpan UpdateInterval = TimeSpan.FromSeconds(300);
+
+    [DataField]
+    public TimeSpan AscendingEffectInterval = TimeSpan.FromSeconds(8.02);
+
+    [DataField]
+    public EntityUid? Creator;
+
+    [DataField]
+    public bool StartTimer;
+
+    [DataField]
+    public string VerbName = "Start Ascension";
+
+    [DataField]
+    public EntityUid? ShadowlingInsideEntity;
+
+    [DataField]
+    public string? ShadowlingInside = "AscensionEggShadowlingInside";
+
+    [DataField]
+    public string? AscendingEffect = "ShadowlingAscendingEffect";
+
+    [DataField]
+    public bool AscendingEffectAdded;
+
+    [DataField]
+    public EntityUid? AscendingEffectEntity;
+
+    [DataField]
+    public SoundSpecifier? AscensionEnterSound = new SoundPathSpecifier("/Audio/_EinsteinEngines/Shadowling/egg/ascension_enter.ogg");
+}

Content.Goobstation.Shared/Shadowling/Components/ThrallComponent.cs
@@ -1,0 +1,50 @@
+using Content.Shared.Language;
+using Content.Shared.StatusIcon;
+using Robust.Shared.Audio;
+using Robust.Shared.GameStates;
+using Robust.Shared.Prototypes;
+
+
+namespace Content.Shared._EE.Shadowling;
+
+
+/// <summary>
+/// This is used for marking Thralls and storing their icons
+/// </summary>
+[RegisterComponent, NetworkedComponent]
+public sealed partial class ThrallComponent : Component
+{
+    [DataField]
+    public float EnthrallDurationEffect = 1.5f;
+
+    [DataField, ViewVariables(VVAccess.ReadWrite)]
+    public ProtoId<FactionIconPrototype> StatusIcon { get; set; } = "ThrallFaction";
+
+    public readonly List<ProtoId<EntityPrototype>> BaseThrallActions = new()
+    {
+        "ActionThrallDarksight",
+        "ActionGuise"
+    };
+
+    public string? ActionThrallDarksight = "ActionThrallDarksight";
+    public string? ActionGuise = "ActionGuise";
+
+    public EntityUid? ActionThrallDarksightEntity;
+    public EntityUid? ActionGuiseEntity;
+
+    [DataField]
+    public bool NightVisionMode;
+
+    [DataField]
+    public SoundSpecifier? ThrallConverted = new SoundPathSpecifier("/Audio/_EinsteinEngines/Shadowling/thrall.ogg");
+
+    /// <summary>
+    /// The shadowling that converted the Thrall
+    /// </summary>
+    [DataField]
+    public EntityUid? Converter;
+
+    [DataField]
+    public ProtoId<LanguagePrototype> SlingLanguageId { get; set; } = "Shadowmind";
+
+}