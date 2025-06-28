8a8ad7dd29e74ff1c4ecc693eb65729fe5bca425
Conchelle <mary@thughunt.ing>
2025-06-29 02:13:35 +0300

    Huh

--- Content.Goobstation.Shared/MisandryBox/PlaytimeDegrade/PlaytimeDegrade.cs
+++ Content.Goobstation.Shared/MisandryBox/PlaytimeDegrade/PlaytimeDegrade.cs
@@ -1,17 +1,5 @@
-﻿using System.Diagnostics;
-using System.Linq;
-using Content.Goobstation.Maths.FixedPoint;
-using Content.Shared.CombatMode;
-using Content.Shared.Mind;
-using Content.Shared.Mobs;
-using Content.Shared.Mobs.Systems;
-using Content.Shared.Movement.Components;
-using Content.Shared.Movement.Systems;
-using Content.Shared.Players.PlayTimeTracking;
+﻿using Content.Shared.CombatMode;
 using Content.Shared.Roles;
-using Content.Shared.Roles.Jobs;
-using Robust.Shared.Player;
-using Robust.Shared.Utility;
 
 namespace Content.Goobstation.Shared.MisandryBox.PlaytimeDegrade;
 
@@ -20,17 +8,6 @@ namespace Content.Goobstation.Shared.MisandryBox.PlaytimeDegrade;
 /// </summary>
 public sealed partial class PlaytimeDegrade : JobSpecial
 {
-    [Dependency] private readonly ISharedPlaytimeManager _playtime = default!;
-    [Dependency] private readonly ISharedPlayerManager _player = default!;
-    [Dependency] private readonly IEntityManager _ent = default!;
-    [Dependency] private readonly IEntitySystemManager _sysMan = default!;
-
-    // You can't IoC these y'kno?
-    private MobThresholdSystem _mob = default!;
-    private MovementSpeedModifierSystem _movement = default!;
-    private SharedJobSystem _job = default!;
-    private SharedMindSystem _mind = default!;
-
     /// <summary>
     /// Start decaying stats after playing this amount of minutes
     /// </summary>
@@ -59,78 +36,17 @@ public sealed partial class PlaytimeDegrade : JobSpecial
     /// Apply <see cref="DisarmMalusComponent"/> at halfway point?
     /// </summary>
     [DataField]
-    public bool? DisarmMalus { get; set; }
+    public bool DisarmMalus { get; set; } = false;
 
     public override void AfterEquip(EntityUid mob)
     {
-        IoCManager.InjectDependencies(this);
-        _sysMan.Resolve(ref _mob, ref _movement, ref _job, ref _mind);
+        var entManager = IoCManager.Resolve<IEntityManager>();
 
-        DebugTools.Assert(Until <= Since, "PlaytimeDegrade Until must be greater than Since");
-        if (Until <= Since)
-            return;
-
-        if (!_player.TryGetSessionByEntity(mob, out var session) ||
-            !_mind.TryGetMind(mob, out var mind, out var _) ||
-            !_job.MindTryGetJob(mind, out var jobPrototype))
-            return;
-
-        var playtimes = _playtime.GetPlayTimes(session);
-        var jobPlaytime = playtimes.FirstOrNull(e => e.Key == jobPrototype.ID);
-
-        if (jobPlaytime is null)
-            return;
-
-        var totalMinutes = (int)jobPlaytime.Value.Value.TotalMinutes;
-
-        if (totalMinutes < Since)
-            return;
-
-        if (!_mob.TryGetThresholdForState(mob, MobState.Critical, out var criticalThreshold) ||
-            !_mob.TryGetThresholdForState(mob, MobState.Dead, out var deadThreshold))
-            return;
-
-        var effectiveMinutes = Math.Min(totalMinutes, Until) - Since;
-        var totalDecayMinutes = Until - Since;
-        var decayRatio = (float)effectiveMinutes / totalDecayMinutes;
-
-        var critFloor = criticalThreshold.Value * Floor;
-        var deadFloor = deadThreshold.Value * Floor;
-
-        FixedPoint2 newCritThreshold, newDeadThreshold;
-
-        if (By.HasValue)
-        {
-            var totalDecay = By.Value * effectiveMinutes;
-            newCritThreshold = FixedPoint2.Max(critFloor, criticalThreshold.Value - totalDecay);
-            newDeadThreshold = FixedPoint2.Max(deadFloor, deadThreshold.Value - totalDecay);
-        }
-        else
-        {
-            var critDecay = (criticalThreshold.Value - critFloor) * decayRatio;
-            var deadDecay = (deadThreshold.Value - deadFloor) * decayRatio;
-
-            newCritThreshold = criticalThreshold.Value - critDecay;
-            newDeadThreshold = deadThreshold.Value - deadDecay;
-        }
-
-        _mob.SetMobStateThreshold(mob, newCritThreshold, MobState.Critical);
-        _mob.SetMobStateThreshold(mob, newDeadThreshold, MobState.Dead);
-
-        if (DisarmMalus.HasValue && DisarmMalus.Value && decayRatio >= 0.5f)
-        {
-            _ent.EnsureComponent<DisarmMalusComponent>(mob, out var malus);
-            malus.Malus = decayRatio;
-        }
-
-        if (_ent.TryGetComponent<MovementSpeedModifierComponent>(mob, out var moveComp))
-        {
-            var speedRatio = 1.0f - (decayRatio * (1.0f - Floor));
-            var newWalkSpeed = moveComp.BaseWalkSpeed * speedRatio;
-            var newSprintSpeed = moveComp.BaseSprintSpeed * speedRatio;
-
-            _movement.ChangeBaseSpeed(mob, newWalkSpeed, newSprintSpeed, moveComp.BaseAcceleration, moveComp);
-            _movement.RefreshMovementSpeedModifiers(mob);
-        }
+        entManager.EnsureComponent<PlaytimeDegradeComponent>(mob, out var comp);
+        comp.Since = Since;
+        comp.By = By;
+        comp.Until = Until;
+        comp.Floor = Floor;
+        comp.DisarmMalus = DisarmMalus;
     }
 }
