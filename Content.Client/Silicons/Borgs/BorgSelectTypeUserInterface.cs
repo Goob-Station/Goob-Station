a9d17ce7667b529d3bff55179b2c91465d55f960
GoobBot <uristmchands@proton.me>
2025-06-28 19:11:46 +0000

    chore: Automatically update REUSE headers

--- Content.Client/Silicons/Borgs/BorgSelectTypeUserInterface.cs
+++ Content.Client/Silicons/Borgs/BorgSelectTypeUserInterface.cs
@@ -1,5 +1,8 @@
 // SPDX-FileCopyrightText: 2024 Pieter-Jan Briers <pieterjan.briers+git@gmail.com>
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 BeBright <98597725+be1bright@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
+// SPDX-FileCopyrightText: 2025 lunarcomets <140772713+lunarcomets@users.noreply.github.com>
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
