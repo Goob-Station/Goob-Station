cbd23519df29a041a1204a8d189d05e293e353a8
GoobBot <uristmchands@proton.me>
2025-06-29 22:44:32 +0000

    chore: Automatically update REUSE headers

--- Content.Server/Silicons/Borgs/BorgSwitchableTypeSystem.cs
+++ Content.Server/Silicons/Borgs/BorgSwitchableTypeSystem.cs
@@ -1,7 +1,9 @@
 // SPDX-FileCopyrightText: 2024 Pieter-Jan Briers <pieterjan.briers+git@gmail.com>
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
+// SPDX-FileCopyrightText: 2025 ImHoks <142083149+ImHoks@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 KillanGenifer <killangenifer@gmail.com>
+// SPDX-FileCopyrightText: 2025 gluesniffler <linebarrelerenthusiast@gmail.com>
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
--- Content.Shared/Silicons/Borgs/BorgTypePrototype.cs
+++ Content.Shared/Silicons/Borgs/BorgTypePrototype.cs
@@ -1,6 +1,7 @@
 // SPDX-FileCopyrightText: 2024 Pieter-Jan Briers <pieterjan.briers+git@gmail.com>
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Tayrtahn <tayrtahn@gmail.com>
+// SPDX-FileCopyrightText: 2025 gluesniffler <linebarrelerenthusiast@gmail.com>
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
