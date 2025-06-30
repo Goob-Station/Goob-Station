8d2d02871ca7af343d085d43634cfc53fecf8a92
GoobBot <uristmchands@proton.me>
2025-06-30 22:26:32 +0000

    chore: Automatically update REUSE headers

--- Content.Shared/Silicons/StationAi/SharedStationAiSystem.Customization.cs
+++ Content.Shared/Silicons/StationAi/SharedStationAiSystem.Customization.cs
@@ -1,5 +1,7 @@
 // SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
+// SPDX-FileCopyrightText: 2025 Nemanja <98561806+EmoGarbage404@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 OnsenCapy <101037138+OnsenCapy@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 chromiumboy <50505512+chromiumboy@users.noreply.github.com>
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
