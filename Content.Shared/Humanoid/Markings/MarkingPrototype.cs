232d13f32fbe21c1668215af4be10a712bb20556
GoobBot <uristmchands@proton.me>
2025-07-01 20:48:55 +0000

    chore: Automatically update REUSE headers

--- Content.Shared/Humanoid/Markings/MarkingPrototype.cs
+++ Content.Shared/Humanoid/Markings/MarkingPrototype.cs
@@ -10,6 +10,7 @@
 // SPDX-FileCopyrightText: 2025 Ed <96445749+TheShuEd@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
 // SPDX-FileCopyrightText: 2025 SX-7 <92227810+SX-7@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 SX-7 <sn1.test.preria.2002@gmail.com>
 // SPDX-FileCopyrightText: 2025 Tayrtahn <tayrtahn@gmail.com>
 //
 // SPDX-License-Identifier: MIT
