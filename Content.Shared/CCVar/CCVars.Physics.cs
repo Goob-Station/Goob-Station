5b748f7217422edf4c71ceded368263e531c3cf5
Ilya246 <57039557+Ilya246@users.noreply.github.com>
2025-07-02 13:54:57 +0000

    frictionless space

--- Content.Shared/CCVar/CCVars.Physics.cs
+++ Content.Shared/CCVar/CCVars.Physics.cs
@@ -22,7 +22,7 @@ public sealed partial class CCVars
         CVarDef.Create("physics.air_friction", 0.2f, CVar.ARCHIVE | CVar.REPLICATED | CVar.SERVER);
 
     public static readonly CVarDef<float> OffgridFriction =
-        CVarDef.Create("physics.offgrid_friction", 0.05f, CVar.ARCHIVE | CVar.REPLICATED | CVar.SERVER);
+        CVarDef.Create("physics.offgrid_friction", 0.0f, CVar.ARCHIVE | CVar.REPLICATED | CVar.SERVER); // Goobstation    
 
     public static readonly CVarDef<float> TileFrictionModifier =
         CVarDef.Create("physics.tile_friction", 8.0f, CVar.ARCHIVE | CVar.REPLICATED | CVar.SERVER);
