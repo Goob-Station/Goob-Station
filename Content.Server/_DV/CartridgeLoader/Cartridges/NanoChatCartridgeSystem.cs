59aed8dd470988b3a54082a9b0bb1c82df8fa3d0
GoobBot <uristmchands@proton.me>
2025-07-02 18:17:31 +0000

    chore: Automatically update REUSE headers

--- Content.Server/_DV/CartridgeLoader/Cartridges/NanoChatCartridgeSystem.cs
+++ Content.Server/_DV/CartridgeLoader/Cartridges/NanoChatCartridgeSystem.cs
@@ -2,6 +2,7 @@
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Aiden <aiden@djkraz.com>
 // SPDX-FileCopyrightText: 2025 Piras314 <p1r4s@proton.me>
+// SPDX-FileCopyrightText: 2025 SX-7 <sn1.test.preria.2002@gmail.com>
 // SPDX-FileCopyrightText: 2025 Skubman <ba.fallaria@gmail.com>
 // SPDX-FileCopyrightText: 2025 Tobias Berger <toby@tobot.dev>
 // SPDX-FileCopyrightText: 2025 deltanedas <39013340+deltanedas@users.noreply.github.com>
