19bc5468337a13614f61a2a1221b19713071574b
GoobBot <uristmchands@proton.me>
2025-07-03 04:40:32 +0000

    chore: Automatically update REUSE headers

--- Content.Server/Crayon/CrayonSystem.cs
+++ Content.Server/Crayon/CrayonSystem.cs
@@ -12,7 +12,9 @@
 // SPDX-FileCopyrightText: 2024 Piras314 <p1r4s@proton.me>
 // SPDX-FileCopyrightText: 2024 Saphire Lattice <lattice@saphi.re>
 // SPDX-FileCopyrightText: 2024 metalgearsloth <31366439+metalgearsloth@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 AffrayedHawk <144155004+AffrayedHawk@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 SX-7 <sn1.test.preria.2002@gmail.com>
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
