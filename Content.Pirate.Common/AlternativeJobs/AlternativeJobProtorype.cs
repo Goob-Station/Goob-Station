e106e1d35f8258a09f230b50137e55a6f66ebfc7
CarryUa <mosuk81@gmail.com>
2025-07-04 13:47:05 +0200

    Adding alternative jobs prototype

---
+++ Content.Pirate.Common/AlternativeJobs/AlternativeJobProtorype.cs
@@ -0,0 +1,30 @@
+using Robust.Shared.Prototypes;
+
+
+namespace Content.Pirate.Common.AlternativeJobs;
+
+[Prototype]
+public sealed partial class AlternativeJobPrototype : IPrototype
+{
+    [IdDataField]
+    public string ID { get; set; } = default!;
+
+
+    /// <summary>
+    /// The name of the job used in ID cards on <see cref="RulePlayerJobsAssignedEvent"/>
+    /// </summary>
+    [DataField(required: true)]
+    public string JobName { get; set; } = default!;
+
+    [ViewVariables(VVAccess.ReadOnly)]
+    public string LocalizedJobName => Loc.GetString(JobName);
+
+    [DataField(required: true)]
+    public string JobDescription { get; set; } = default!;
+
+    [ViewVariables(VVAccess.ReadOnly)]
+    public string LocalizedJob => Loc.GetString(JobName);
+
+    [DataField(required: true)]
+    public string ParentJobId = default!;
+}
