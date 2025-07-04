d27c0aa52f0f5b98a23add768f9adbaf2431ca4b
Tayrtahn <tayrtahn@gmail.com>
2025-07-04 16:48:55 -0400

    Validate `ProtoId`s in tests (#38745)
    * Convert string literals to protoids in Content.Tests
    
    * Convert string literals to protoids or consts in Content.IntegrationTests
    
    * Fix linter failures
    Tricksy static using misled me

--- Content.IntegrationTests/Tests/PostMapInitTest.cs
+++ Content.IntegrationTests/Tests/PostMapInitTest.cs
@@ -237,6 +237,8 @@ namespace Content.IntegrationTests.Tests
             "Serpentcrest", // Goobstation
         };
 
+        private static readonly ProtoId<EntityCategoryPrototype> DoNotMapCategory = "DoNotMap";
+
         /// <summary>
         /// Asserts that specific files have been saved as grids and not maps.
         /// </summary>
@@ -430,7 +432,7 @@ namespace Content.IntegrationTests.Tests
                 return;
 
             var yamlEntities = node["entities"];
-            if (!protoManager.TryIndex<EntityCategoryPrototype>("DoNotMap", out var dnmCategory))
+            if (!protoManager.TryIndex(DoNotMapCategory, out var dnmCategory))
                 return;
 
             Assert.Multiple(() =>
