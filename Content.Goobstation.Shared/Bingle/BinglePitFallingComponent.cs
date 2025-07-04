109c97e5d9954c99c373402ace2ab58e51a01c61
GoobBot <uristmchands@proton.me>
2025-07-04 15:34:23 +0000

    chore: Automatically update REUSE headers

--- Content.Goobstation.Shared/Bingle/BinglePitFallingComponent.cs
+++ Content.Goobstation.Shared/Bingle/BinglePitFallingComponent.cs
@@ -4,6 +4,7 @@
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Aiden <aiden@djkraz.com>
 // SPDX-FileCopyrightText: 2025 Fishbait <Fishbait@git.ml>
+// SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
 // SPDX-FileCopyrightText: 2025 Misandry <mary@thughunt.ing>
 // SPDX-FileCopyrightText: 2025 SX-7 <sn1.test.preria.2002@gmail.com>
 // SPDX-FileCopyrightText: 2025 deltanedas <@deltanedas:kde.org>
