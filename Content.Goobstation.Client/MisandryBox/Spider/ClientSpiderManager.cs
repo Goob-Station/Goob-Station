ce04b58fb911cc8331b1ab623e494b743afb31c8
GoobBot <uristmchands@proton.me>
2025-07-05 15:17:39 +0000

    chore: Automatically update REUSE headers

--- Content.Goobstation.Client/MisandryBox/Spider/ClientSpiderManager.cs
+++ Content.Goobstation.Client/MisandryBox/Spider/ClientSpiderManager.cs
@@ -1,3 +1,8 @@
+// SPDX-FileCopyrightText: 2025 Conchelle <mary@thughunt.ing>
+// SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
+//
+// SPDX-License-Identifier: MPL-2.0
+
 using Content.Client.Lobby;
 using Content.Goobstation.Common.CCVar;
 using Content.Goobstation.Common.MisandryBox;
