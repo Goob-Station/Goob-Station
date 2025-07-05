3417d352cfeb0d8a84dd2c2e283ca509e9d992c0
Conchelle <mary@thughunt.ing>
2025-07-05 23:36:54 +0300

    Goobmod-specific launch config

---
+++ Content.Goobstation.Client/Program.cs
@@ -0,0 +1,12 @@
+﻿using Robust.Client;
+
+namespace Content.Goobstation.Client;
+
+internal sealed class Program
+{
+    [STAThread]
+    public static void Main(string[] args)
+    {
+        ContentStart.Start(args);
+    }
+}
---
+++ Content.Goobstation.Server/Program.cs
@@ -0,0 +1,11 @@
+﻿using Robust.Server;
+
+namespace Content.Goobstation.Server;
+
+internal static class Program
+{
+    public static void Main(string[] args)
+    {
+        ContentStart.Start(args);
+    }
+}
