d4ddb51efada8f9c36de3c19302e2cc1bc94c50f
GoobBot <uristmchands@proton.me>
2025-07-05 22:24:17 +0000

    chore: Automatically update REUSE headers

--- Content.Server/Objectives/Systems/NotJobRequirementSystem.cs
+++ Content.Server/Objectives/Systems/NotJobRequirementSystem.cs
@@ -5,6 +5,7 @@
 // SPDX-FileCopyrightText: 2024 username <113782077+whateverusername0@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2024 whateverusername0 <whateveremail>
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
 // SPDX-FileCopyrightText: 2025 Misandry <mary@thughunt.ing>
 // SPDX-FileCopyrightText: 2025 loltart <lo1tartyt@gmail.com>
 //
