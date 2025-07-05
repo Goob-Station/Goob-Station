54ef1c2f20cc3309f6b83888754f94cf44ec5ab9
GoobBot <uristmchands@proton.me>
2025-07-05 07:54:24 +0000

    chore: Automatically update REUSE headers

--- Content.Client/Entry/EntryPoint.cs
+++ Content.Client/Entry/EntryPoint.cs
@@ -110,9 +110,11 @@
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Aiden <aiden@djkraz.com>
 // SPDX-FileCopyrightText: 2025 Aidenkrz <aiden@djkraz.com>
+// SPDX-FileCopyrightText: 2025 Eris <eris@erisws.com>
 // SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
 // SPDX-FileCopyrightText: 2025 Misandry <mary@thughunt.ing>
 // SPDX-FileCopyrightText: 2025 Pieter-Jan Briers <pieterjan.briers+git@gmail.com>
+// SPDX-FileCopyrightText: 2025 SX-7 <sn1.test.preria.2002@gmail.com>
 // SPDX-FileCopyrightText: 2025 gus <august.eymann@gmail.com>
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
--- Content.Client/_White/ItemSlotRenderer/ItemSlotRendererSystem.cs
+++ Content.Client/_White/ItemSlotRenderer/ItemSlotRendererSystem.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 Eris <eris@erisws.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using Content.Client.Hands;
 using Content.Shared.Containers.ItemSlots;
 using Robust.Client.GameObjects;
