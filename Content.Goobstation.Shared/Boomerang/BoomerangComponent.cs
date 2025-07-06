6ccdd0058e4321370d7973a225029aafaee0efbf
Ilya246 <ilyukarno@gmail.com>
2025-07-06 21:09:01 +0400

    it is real

--- Content.Goobstation.Shared/Boomerang/BoomerangComponent.cs
+++ Content.Goobstation.Shared/Boomerang/BoomerangComponent.cs
@@ -11,19 +11,39 @@ using Robust.Shared.GameStates;
 
 namespace Content.Goobstation.Shared.Boomerang;
 
+/// <summary>
+/// Component for entities that should boomerang back to their thrower once thrown
+/// </summary>
 [NetworkedComponent, RegisterComponent, AutoGenerateComponentState]
 public sealed partial class BoomerangComponent : Component
 {
     /// <summary>
-    /// The entity that threw this boomerang
+    /// Entity we should return to after landing
     /// </summary>
     [DataField, AutoNetworkedField]
     public EntityUid? Thrower;
 
-    [DataField, AutoNetworkedField]
-    public TimeSpan? TimeToReturn = TimeSpan.Zero;
+    /// <summary>
+    /// Distance to thrower we should try get picked up at or fail
+    /// </summary>
+    [DataField]
+    public float PickupDistance = 1.5f;
 
+    /// <summary>
+    /// Speed we should return at
+    /// </summary>
+    [DataField]
+    public float ReturnSpeed = 10f;
 
+    /// <summary>
+    /// Maximum return hops we can make
+    /// </summary>
+    [DataField]
+    public int MaxHops = 6;
+
+    /// <summary>
+    /// Return hops we've made so far
+    /// </summary>
     [DataField, AutoNetworkedField]
-    public bool SendBack = false;
+    public int CurrentHops = 0;
 }
