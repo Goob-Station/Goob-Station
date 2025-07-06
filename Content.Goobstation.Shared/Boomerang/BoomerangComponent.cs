b09ad14ce088ebec92c82b2b6af3e0e03e4e5b74
GoobBot <uristmchands@proton.me>
2025-07-06 17:13:24 +0000

    chore: Automatically update REUSE headers

--- Content.Goobstation.Shared/Boomerang/BoomerangComponent.cs
+++ Content.Goobstation.Shared/Boomerang/BoomerangComponent.cs
@@ -1,6 +1,7 @@
 // SPDX-FileCopyrightText: 2025 ActiveMammmoth <140334666+ActiveMammmoth@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 ActiveMammmoth <kmcsmooth@gmail.com>
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
 // SPDX-FileCopyrightText: 2025 Ilya246 <ilyukarno@gmail.com>
 // SPDX-FileCopyrightText: 2025 Misandry <mary@thughunt.ing>
 // SPDX-FileCopyrightText: 2025 SX-7 <sn1.test.preria.2002@gmail.com>
