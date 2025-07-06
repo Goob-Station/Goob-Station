ce9dc223adb5739d7a1359617539f6d3398ef7b0
GoobBot <uristmchands@proton.me>
2025-07-06 12:12:04 +0000

    chore: Automatically update REUSE headers

--- Content.Server/Atmos/EntitySystems/HeatExchangerSystem.cs
+++ Content.Server/Atmos/EntitySystems/HeatExchangerSystem.cs
@@ -8,6 +8,7 @@
 // SPDX-FileCopyrightText: 2024 Pieter-Jan Briers <pieterjan.briers+git@gmail.com>
 // SPDX-FileCopyrightText: 2024 Piras314 <p1r4s@proton.me>
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
 // SPDX-FileCopyrightText: 2025 Ilya246 <ilyukarno@gmail.com>
 // SPDX-FileCopyrightText: 2025 SX-7 <sn1.test.preria.2002@gmail.com>
 //
