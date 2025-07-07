1475760638b5757168b5511cd7f9ac915ed5f4a5
gluesniffler <linebarrelerenthusiast@gmail.com>
2025-07-07 03:48:02 -0400

    sloppa

--- Content.Server/_DV/Silicons/Borgs/BorgSwitchableTypeSystem.Lawset.cs
+++ Content.Server/_DV/Silicons/Borgs/BorgSwitchableTypeSystem.Lawset.cs
@@ -1,6 +1,6 @@
+using Content.Server._DV.Silicons.Laws;
 using Content.Server.Silicons.Laws;
-using Content.Shared.Emag.Components;
-using Content.Shared.Emag.Systems;
+using Content.Shared._DV.Silicons.Laws;
 using Content.Shared.Silicons.Laws;
 using Content.Shared.Silicons.Laws.Components;
 using Robust.Shared.Prototypes;
@@ -13,18 +13,23 @@ namespace Content.Server.Silicons.Borgs;
 /// </summary>
 public sealed partial class BorgSwitchableTypeSystem
 {
+    [Dependency] private readonly SlavedBorgSystem _slavedBorg = default!;
     [Dependency] private readonly SiliconLawSystem _law = default!;
 
     private void ConfigureLawset(EntityUid uid, ProtoId<SiliconLawsetPrototype> id)
     {
         var laws = _law.GetLawset(id);
+
+        if (TryComp<SlavedBorgComponent>(uid, out var slaved))
+            _slavedBorg.AddLaw(laws, slaved.Law);
+
         _law.SetLaws(laws.Laws, uid);
 
         // re-add law 0 and final law based on new lawset
         if (CompOrNull<EmagSiliconLawComponent>(uid)?.OwnerName != null)
         {
             // raising the event manually to bypass re-emagging checks
-            var ev = new GotEmaggedEvent(uid, EmagType.Interaction); // user wont be used since OwnerName isnt null, safe to pass itself
+            var ev = new SiliconEmaggedEvent(uid);
             RaiseLocalEvent(uid, ref ev);
         }
 
