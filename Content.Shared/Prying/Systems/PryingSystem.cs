21a5af9e60a844fdf927a4998ec118f0dd31f4cc
GoobBot <uristmchands@proton.me>
2025-07-07 00:58:47 +0000

    chore: Automatically update REUSE headers

--- Content.Shared/Prying/Systems/PryingSystem.cs
+++ Content.Shared/Prying/Systems/PryingSystem.cs
@@ -70,6 +70,7 @@
 // SPDX-FileCopyrightText: 2024 Арт <123451459+JustArt1m@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Avalon <jfbentley1@gmail.com>
+// SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
 // SPDX-FileCopyrightText: 2025 SX-7 <sn1.test.preria.2002@gmail.com>
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
--- Content.Shared/_DV/Prying/Systems/PlayerToolModifierSystem.cs
+++ Content.Shared/_DV/Prying/Systems/PlayerToolModifierSystem.cs
@@ -1,4 +1,5 @@
 // SPDX-FileCopyrightText: 2025 Avalon <jfbentley1@gmail.com>
+// SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
