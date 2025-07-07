d7203e7939850413c73a0e6d0a5a38d38e8679c3
GoobBot <uristmchands@proton.me>
2025-07-07 19:46:04 +0000

    chore: Automatically update REUSE headers

--- Content.Shared/_DV/Carrying/CarryingSystem.cs
+++ Content.Shared/_DV/Carrying/CarryingSystem.cs
@@ -2,6 +2,8 @@
 // SPDX-FileCopyrightText: 2025 Aiden <aiden@djkraz.com>
 // SPDX-FileCopyrightText: 2025 Aviu00 <93730715+Aviu00@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Piras314 <p1r4s@proton.me>
+// SPDX-FileCopyrightText: 2025 SX-7 <sn1.test.preria.2002@gmail.com>
+// SPDX-FileCopyrightText: 2025 SolsticeOfTheWinter <solsticeofthewinter@gmail.com>
 // SPDX-FileCopyrightText: 2025 deltanedas <39013340+deltanedas@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 deltanedas <@deltanedas:kde.org>
 //
