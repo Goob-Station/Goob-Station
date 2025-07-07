2ecb475ff2df6fbe51aa0cca754338c84566f1a9
Smugman <85798843+Smugman@users.noreply.github.com>
2025-07-07 20:53:17 +0200

    Branded lighters addition (#35514)
    * newlighters
    
    * port complete
    
    * donk pocket box addition
    
    * should fix test fail, proper credit
    
    * captalisation issue
    
    * fixed
    
    * typo corrected
    
    * removed accidental change to combat bakery
    
    * fixed lock message, made NT flippo start fueled.
    
    * Requested changes
    
    * fixed discount dan
    
    * moved matchbox
    
    * requested changes, discount dan more broken than ever (somehow)
    
    * unbungle
    
    * uhhhhh
    
    * maybe fixes?
    
    * changed inheritance
    
    * fixed enum?
    
    * nevermind
    
    * finally fixed
    
    * breaking changes
    
    * inheritance unbungled

--- Content.Shared/Lock/LockSystem.cs
+++ Content.Shared/Lock/LockSystem.cs
@@ -99,6 +99,7 @@ using Content.Shared.Storage.Components;
 using Content.Shared.UserInterface;
 using Content.Shared.Verbs;
 using Content.Shared.Wires;
+using Content.Shared.Item.ItemToggle.Components;
 using JetBrains.Annotations;
 using Robust.Shared.Audio.Systems;
 using Robust.Shared.Utility;
@@ -141,6 +142,8 @@ public sealed class LockSystem : EntitySystem
 
         SubscribeLocalEvent<ActivatableUIRequiresLockComponent, ActivatableUIOpenAttemptEvent>(OnUIOpenAttempt);
         SubscribeLocalEvent<ActivatableUIRequiresLockComponent, LockToggledEvent>(LockToggled);
+
+        SubscribeLocalEvent<ItemToggleRequiresLockComponent, ItemToggleActivateAttemptEvent>(OnActivateAttempt);
     }
 
     private void OnStartup(EntityUid uid, LockComponent lockComp, ComponentStartup args)
@@ -498,5 +501,21 @@ public sealed class LockSystem : EntitySystem
             return;
 
         _activatableUI.CloseAll(uid);
+    }
+    
+    private void OnActivateAttempt(EntityUid uid, ItemToggleRequiresLockComponent component, ref ItemToggleActivateAttemptEvent args)
+    {
+        if (args.Cancelled)
+            return;
+
+        if (TryComp<LockComponent>(uid, out var lockComp) && lockComp.Locked != component.RequireLocked)
+        {
+            args.Cancelled = true;
+            if (lockComp.Locked)
+                _sharedPopupSystem.PopupClient(Loc.GetString("lock-comp-generic-fail",
+                ("target", Identity.Entity(uid, EntityManager))),
+                uid,
+                args.User);
+        }
     }
 }
