60e23ec4f06313081286f7df7c2b6e75952bb355
GoobBot <uristmchands@proton.me>
2025-07-07 05:06:29 +0000

    chore: Automatically update REUSE headers

--- Content.Shared/_Shitcode/Heretic/Components/DisgustComponent.cs
+++ Content.Shared/_Shitcode/Heretic/Components/DisgustComponent.cs
@@ -1,5 +1,6 @@
 // SPDX-FileCopyrightText: 2025 Armok <155400926+ARMOKS@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Aviu00 <93730715+Aviu00@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
