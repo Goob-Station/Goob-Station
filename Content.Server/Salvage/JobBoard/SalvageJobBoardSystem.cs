024f315da5ed230f896e12f41261729d8babf0b1
GoobBot <uristmchands@proton.me>
2025-07-07 01:36:48 +0000

    chore: Automatically update REUSE headers

--- Content.Server/Salvage/JobBoard/SalvageJobBoardSystem.cs
+++ Content.Server/Salvage/JobBoard/SalvageJobBoardSystem.cs
@@ -1,4 +1,5 @@
 // SPDX-FileCopyrightText: 2025 CerberusWolfie <wb.johnb.willis@gmail.com>
+// SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
 // SPDX-FileCopyrightText: 2025 Nemanja <98561806+EmoGarbage404@users.noreply.github.com>
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
