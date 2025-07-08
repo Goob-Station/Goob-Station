48dcb41200920f37e4f29731c7dddddb70bdc701
SolsticeOfTheWinter <solsticeofthewinter@gmail.com>
2025-07-07 15:59:46 -1000

    just hold this
    Signed-off-by: SolsticeOfTheWinter <solsticeofthewinter@gmail.com>

---
+++ Content.Goobstation.Server/Chemistry/SexChange.cs
@@ -0,0 +1,37 @@
+using Content.Shared.Humanoid;
+using Content.Shared.Chemistry.Reagent;
+using Content.Shared.EntityEffects;
+using Robust.Shared.Prototypes;
+
+namespace Content.Goobstation.Server.Chemistry;
+
+public sealed partial class SexChange : EntityEffect
+{
+    /// <summary>
+    ///     What sex is the consumer changed to? If not set then swap between male/female.
+    /// </summary>
+    [DataField("sex")]
+    public Sex? NewSex;
+
+    protected override string? ReagentEffectGuidebookText(IPrototypeManager prototype, IEntitySystemManager entSys)
+        => Loc.GetString("reagent-effect-guidebook-sex-change", ("chance", Probability));
+
+    public override void Effect(EntityEffectBaseArgs args)
+    {
+        if (!args.EntityManager.TryGetComponent<HumanoidAppearanceComponent>(args.TargetEntity, out var appearance))
+            return;
+
+        var uid = args.TargetEntity;
+        var newSex = NewSex;
+        var humanoidAppearanceSystem = args.EntityManager.System<SharedHumanoidAppearanceSystem>();
+
+        if (newSex.HasValue)
+        {
+            humanoidAppearanceSystem.SetSex(uid, newSex.Value);
+            return;
+        }
+
+        if (appearance.Sex != Sex.Unsexed)
+            humanoidAppearanceSystem.SwapSex(uid);
+    }
+}
