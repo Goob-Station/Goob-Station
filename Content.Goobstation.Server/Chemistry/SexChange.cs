84700b0a3eaafc8557fb8f6059592bcd40b7ba79
GoobBot <uristmchands@proton.me>
2025-07-09 21:23:51 +0000

    chore: Automatically update REUSE headers

--- Content.Goobstation.Server/Chemistry/SexChange.cs
+++ Content.Goobstation.Server/Chemistry/SexChange.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 SolsticeOfTheWinter <solsticeofthewinter@gmail.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using Content.Shared.Humanoid;
 using Content.Shared.EntityEffects;
 using JetBrains.Annotations;
