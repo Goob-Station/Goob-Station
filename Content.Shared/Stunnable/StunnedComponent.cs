f1110adfa8f31270e822873d33466fc4ebd0871d
Princess Cheeseballs <66055347+Princess-Cheeseballs@users.noreply.github.com>
2025-07-11 05:13:11 -0700

    Stun and Stamina Visuals (#37196)
    * Stun animation
    
    * Commit 2
    
    * Almost working commit
    
    * Best commit
    
    * Minor cleanup and value adjustments
    
    * Fix animation data getting wasted and cleaned up some stuff
    
    * Don't animate if dead
    
    * AppearanceSystem is for chumps
    
    * Cleanup
    
    * More cleanup
    
    * More cleanup
    
    * Half working commit
    
    * Documentation
    
    * Works
    
    * ComponentHandleState my beloved
    
    * AppearanceComp compatibility
    
    * Address review
    
    * Borgar
    
    * AND NOW THE END IS NEAR
    
    * AppearanceSystem compliance (Real)
    
    * Don't need to log missing there
    
    * I actually hate mob prototypes so much you don't even know
    
    ---------
    
    Co-authored-by: Princess Cheeseballs <66055347+Pronana@users.noreply.github.com>

--- Content.Shared/Stunnable/StunnedComponent.cs
+++ Content.Shared/Stunnable/StunnedComponent.cs
@@ -14,6 +14,4 @@ using Robust.Shared.GameStates;
 namespace Content.Shared.Stunnable;
 
 [RegisterComponent, NetworkedComponent, Access(typeof(SharedStunSystem))]
-public sealed partial class StunnedComponent : Component
-{
-}
+public sealed partial class StunnedComponent : Component;
