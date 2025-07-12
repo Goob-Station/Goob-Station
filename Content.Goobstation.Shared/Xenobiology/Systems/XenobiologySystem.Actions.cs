e9c6226f0ec0921c020ed1f6ecd3aa92e9d8a7c1
TheBorzoiMustConsume <197824988+TheBorzoiMustConsume@users.noreply.github.com>
2025-07-12 22:19:14 +1000

    Formatting Suggestion
    Co-authored-by: gluesniffler <159397573+gluesniffler@users.noreply.github.com>

--- Content.Goobstation.Shared/Xenobiology/Systems/XenobiologySystem.Actions.cs
+++ Content.Goobstation.Shared/Xenobiology/Systems/XenobiologySystem.Actions.cs
@@ -182,7 +182,8 @@ public partial class XenobiologySystem
 
     private void OnStartLatch(Entity<SlimeComponent> slime, ref SlimeLatchDoAfterEvent args)
     {
-        if (args.Target is not { } target || _net.IsClient)
+        if (args.Target is not { } target
+            || _net.IsClient)
             return;
 
         if (args.Handled || args.Cancelled)
