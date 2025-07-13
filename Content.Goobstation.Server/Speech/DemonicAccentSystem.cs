9edd18e4be0804b7eea4484cbb587cd9008c0219
GoobBot <uristmchands@proton.me>
2025-07-13 18:16:51 +0000

    chore: Automatically update REUSE headers

--- Content.Goobstation.Server/Speech/DemonicAccentSystem.cs
+++ Content.Goobstation.Server/Speech/DemonicAccentSystem.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 Lumminal <81829924+Lumminal@users.noreply.github.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using Content.Goobstation.Common.Speech;
 using Content.Server.Speech;
 using Robust.Shared.Random;
