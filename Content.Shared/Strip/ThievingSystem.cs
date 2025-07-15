db502dbb1ad5d6a644314fed14d3f091f880f8b1
GoobBot <uristmchands@proton.me>
2025-07-15 22:10:22 +0000

    chore: Automatically update REUSE headers

--- Content.Shared/Strip/ThievingSystem.cs
+++ Content.Shared/Strip/ThievingSystem.cs
@@ -4,6 +4,9 @@
 // SPDX-FileCopyrightText: 2024 Krunklehorn <42424291+Krunklehorn@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2024 Piras314 <p1r4s@proton.me>
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 ArtisticRoomba <145879011+artisticroomba@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 SX-7 <sn1.test.preria.2002@gmail.com>
+// SPDX-FileCopyrightText: 2025 thetuerk <46725294+ThanosDeGraf@users.noreply.github.com>
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
