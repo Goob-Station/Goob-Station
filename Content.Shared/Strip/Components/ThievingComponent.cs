794b9e84ddd4b48314a6d137e8984085cc5d13d3
GoobBot <uristmchands@proton.me>
2025-07-16 07:28:31 +0000

    chore: Automatically update REUSE headers

--- Content.Shared/Strip/Components/ThievingComponent.cs
+++ Content.Shared/Strip/Components/ThievingComponent.cs
@@ -6,6 +6,7 @@
 // SPDX-FileCopyrightText: 2025 ArtisticRoomba <145879011+artisticroomba@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
 // SPDX-FileCopyrightText: 2025 SX-7 <sn1.test.preria.2002@gmail.com>
+// SPDX-FileCopyrightText: 2025 thetuerk <46725294+ThanosDeGraf@users.noreply.github.com>
 //
 // SPDX-License-Identifier: MIT
 
