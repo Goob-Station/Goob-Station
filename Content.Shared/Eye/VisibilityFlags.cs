d7112e882a9f1983fdb3972d96eb9c162b4c39a5
GoobBot <uristmchands@proton.me>
2025-07-16 09:18:10 +0000

    chore: Automatically update REUSE headers

--- Content.Shared/Eye/VisibilityFlags.cs
+++ Content.Shared/Eye/VisibilityFlags.cs
@@ -1,5 +1,6 @@
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 GMWQ <garethquaile@gmail.com>
+// SPDX-FileCopyrightText: 2025 Gareth Quaile <garethquaile@gmail.com>
 // SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
 // SPDX-FileCopyrightText: 2025 Piras314 <p1r4s@proton.me>
 // SPDX-FileCopyrightText: 2025 Roudenn <romabond091@gmail.com>
