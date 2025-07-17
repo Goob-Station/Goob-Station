f4fd8f9b9ae858347c8dba09e47405cf86914e4f
GoobBot <uristmchands@proton.me>
2025-07-17 23:27:48 +0000

    chore: Automatically update REUSE headers

--- Content.Goobstation.Client/Entry/EntryPoint.cs
+++ Content.Goobstation.Client/Entry/EntryPoint.cs
@@ -3,6 +3,7 @@
 // SPDX-FileCopyrightText: 2025 Conchelle <mary@thughunt.ing>
 // SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
 // SPDX-FileCopyrightText: 2025 Misandry <mary@thughunt.ing>
+// SPDX-FileCopyrightText: 2025 Sara Aldrete's Top Guy <mary@thughunt.ing>
 // SPDX-FileCopyrightText: 2025 gus <august.eymann@gmail.com>
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
