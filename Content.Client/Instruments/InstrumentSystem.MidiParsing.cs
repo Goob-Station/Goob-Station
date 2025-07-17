953e29b7545d54a8cabc249f7603a419e12da15f
krusti <krusti@fluffytech.xyz>
2025-07-17 11:32:08 +0200

    Revert "chore: Automatically update REUSE headers"
    This reverts commit bd57c2f02f745341bbd82d1f18e2c8a287c0e43a.

--- Content.Client/Instruments/InstrumentSystem.MidiParsing.cs
+++ Content.Client/Instruments/InstrumentSystem.MidiParsing.cs
@@ -1,10 +1,4 @@
-// SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
-// SPDX-FileCopyrightText: 2025 Simon <63975668+Simyon264@users.noreply.github.com>
-// SPDX-FileCopyrightText: 2025 krusti <krusti@fluffytech.xyz>
-//
-// SPDX-License-Identifier: AGPL-3.0-or-later
-
-using System.Linq;
+﻿using System.Linq;
 using Content.Shared.Instruments;
 using Robust.Shared.Audio.Midi;
 
