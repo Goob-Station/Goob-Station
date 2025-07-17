9014564ca3455a42b8784a2f24e5259abf0b39f2
GoobBot <uristmchands@proton.me>
2025-07-17 23:06:22 +0000

    chore: Automatically update REUSE headers

--- Content.Server/Objectives/ObjectivesSystem.cs
+++ Content.Server/Objectives/ObjectivesSystem.cs
@@ -18,6 +18,7 @@
 // SPDX-FileCopyrightText: 2024 username <113782077+whateverusername0@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2024 whateverusername0 <whateveremail>
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 Conchelle <mary@thughunt.ing>
 // SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
 // SPDX-FileCopyrightText: 2025 Killerqu00 <47712032+Killerqu00@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Misandry <mary@thughunt.ing>
