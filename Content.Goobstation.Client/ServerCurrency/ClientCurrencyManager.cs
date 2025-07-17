36f16e01292ac1524993e118fcf42fb717e85822
GoobBot <uristmchands@proton.me>
2025-07-17 23:01:25 +0000

    chore: Automatically update REUSE headers

--- Content.Goobstation.Client/ServerCurrency/ClientCurrencyManager.cs
+++ Content.Goobstation.Client/ServerCurrency/ClientCurrencyManager.cs
@@ -1,4 +1,8 @@
-﻿using Content.Goobstation.Common.ServerCurrency;
+// SPDX-FileCopyrightText: 2025 Conchelle <mary@thughunt.ing>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+using Content.Goobstation.Common.ServerCurrency;
 using Robust.Client.Player;
 using Robust.Shared.Enums;
 using Robust.Shared.Network;
--- Content.Goobstation.Common/ServerCurrency/ICommonCurrencyManager.cs
+++ Content.Goobstation.Common/ServerCurrency/ICommonCurrencyManager.cs
@@ -1,4 +1,8 @@
-﻿using System.Threading.Tasks;
+// SPDX-FileCopyrightText: 2025 Conchelle <mary@thughunt.ing>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+using System.Threading.Tasks;
 using Robust.Shared.Asynchronous;
 using Robust.Shared.Network;
 
