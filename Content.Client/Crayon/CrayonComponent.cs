44e4c84902bfc4b5ae4ceec82f2e4e7300dbd39f
GoobBot <uristmchands@proton.me>
2025-07-18 03:29:38 +0000

    chore: Automatically update REUSE headers

--- Content.Client/Crayon/CrayonComponent.cs
+++ Content.Client/Crayon/CrayonComponent.cs
@@ -10,7 +10,9 @@
 // SPDX-FileCopyrightText: 2022 metalgearsloth <31366439+metalgearsloth@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2023 DrSmugleaf <DrSmugleaf@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2024 Piras314 <p1r4s@proton.me>
+// SPDX-FileCopyrightText: 2025 AffrayedHawk <144155004+AffrayedHawk@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 SX-7 <sn1.test.preria.2002@gmail.com>
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
