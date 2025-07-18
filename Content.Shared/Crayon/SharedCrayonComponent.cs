34838fdc5dfa7f6a7e3c4565c7721a78969f09ab
GoobBot <uristmchands@proton.me>
2025-07-18 03:55:16 +0000

    chore: Automatically update REUSE headers

--- Content.Shared/Crayon/SharedCrayonComponent.cs
+++ Content.Shared/Crayon/SharedCrayonComponent.cs
@@ -16,6 +16,7 @@
 // SPDX-FileCopyrightText: 2024 Saphire Lattice <lattice@saphi.re>
 // SPDX-FileCopyrightText: 2025 AffrayedHawk <144155004+AffrayedHawk@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
 // SPDX-FileCopyrightText: 2025 SX-7 <sn1.test.preria.2002@gmail.com>
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
