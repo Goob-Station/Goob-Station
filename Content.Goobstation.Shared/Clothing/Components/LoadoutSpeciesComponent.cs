4553ce76533b8c663d2ad842629ccd13e561b4a1
Marty <martynashagriefer@gmail.com>
2025-07-20 00:25:45 +0300

    add loadout species override

---
+++ Content.Goobstation.Shared/Clothing/Components/LoadoutSpeciesComponent.cs
@@ -0,0 +1,26 @@
+using Content.Shared.Roles;
+using Robust.Shared.GameStates;
+using Robust.Shared.Prototypes;
+namespace Content.Goobstation.Shared.Clothing.Components;
+/// <summary>
+/// Requires LoadoutComponent, replaces a role loadout or specific slots with different ones depending on species.
+/// </summary>
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
+public sealed partial class LoadoutSpeciesComponent : Component
+{
+    /// <summary>
+    /// Key is species prototype ID, value is the StartingGearPrototype to override with.
+    /// </summary>
+    [DataField, AutoNetworkedField]
+    public Dictionary<string, ProtoId<StartingGearPrototype>> SpeciesStartingGearOverride = new();
+
+    /// <summary>
+    /// Slot-specific prototype overrides. Runs after full loadout override.
+    /// </summary>
+    [DataField, AutoNetworkedField]
+    public Dictionary<string, Dictionary<string, EntProtoId>> SpeciesSlotOverride = new();
+
+    [DataField, AutoNetworkedField]
+    public bool Overridden = false;
+}
+
