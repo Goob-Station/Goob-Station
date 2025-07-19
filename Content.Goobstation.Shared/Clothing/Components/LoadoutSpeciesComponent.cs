943f3bfd893bd19fc388a1fb0549f6131636b288
GoobBot <uristmchands@proton.me>
2025-07-19 22:36:39 +0000

    chore: Automatically update REUSE headers

--- Content.Goobstation.Shared/Clothing/Components/LoadoutSpeciesComponent.cs
+++ Content.Goobstation.Shared/Clothing/Components/LoadoutSpeciesComponent.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 Marty <martynashagriefer@gmail.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using Content.Shared.Roles;
 using Robust.Shared.GameStates;
 using Robust.Shared.Prototypes;
