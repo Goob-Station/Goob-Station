86f1cf5f89895a003b44dfc6cf1c75aec95cd5b3
IgorAnt028 <118114530+IgorAnt028@users.noreply.github.com>
2025-07-20 01:10:38 +0300

    Fix bug with pipe color (#30645)
    Co-authored-by: ArtisticRoomba <145879011+ArtisticRoomba@users.noreply.github.com>

--- Content.Shared/Atmos/Components/AtmosPipeColorComponent.cs
+++ Content.Shared/Atmos/Components/AtmosPipeColorComponent.cs
@@ -9,22 +9,25 @@
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-using Content.Server.Atmos.Piping.EntitySystems;
+using Content.Shared.Atmos.EntitySystems;
+using Robust.Shared.GameStates;
 using JetBrains.Annotations;
 
-namespace Content.Server.Atmos.Piping.Components;
+namespace Content.Shared.Atmos.Components;
 
-[RegisterComponent]
+[RegisterComponent, NetworkedComponent]
+[AutoGenerateComponentState]
 public sealed partial class AtmosPipeColorComponent : Component
 {
     [DataField]
+    [AutoNetworkedField]
     public Color Color { get; set; } = Color.White;
 
     [ViewVariables(VVAccess.ReadWrite), UsedImplicitly]
     public Color ColorVV
     {
         get => Color;
-        set => IoCManager.Resolve<IEntityManager>().System<AtmosPipeColorSystem>().SetColor(Owner, this, value);
+        set => IoCManager.Resolve<IEntityManager>().System<AtmosPipeColorSystem>().SetColor((Owner, this), value);
     }
 }
 
