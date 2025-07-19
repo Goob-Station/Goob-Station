8e87edca31b6de6d22fecff713d1226b4f5c1e16
GoobBot <uristmchands@proton.me>
2025-07-19 09:38:29 +0000

    chore: Automatically update REUSE headers

--- Content.Goobstation.Server/Heatlamp/HeatlampComponent.cs
+++ Content.Goobstation.Server/Heatlamp/HeatlampComponent.cs
@@ -1,5 +1,6 @@
 // SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
 // SPDX-FileCopyrightText: 2025 SolsticeOfTheWinter <solsticeofthewinter@gmail.com>
+// SPDX-FileCopyrightText: 2025 loltart <159829224+loltart@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 loltart <lo1tartyt@gmail.com>
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
