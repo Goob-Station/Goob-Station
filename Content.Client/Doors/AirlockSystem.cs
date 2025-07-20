41a397334203bce828c7564b2cad9e7e332c4cf7
KillanGenifer <killangenifer@gmail.com>
2025-07-20 09:19:59 +0700

    PR : Resprite fixes

--- Content.Client/Doors/AirlockSystem.cs
+++ Content.Client/Doors/AirlockSystem.cs
@@ -111,7 +111,7 @@ public sealed class AirlockSystem : SharedAirlockSystem
                 ||  state == DoorState.Opening
                 ||  state == DoorState.Denying
                 || (state == DoorState.Open && comp.OpenUnlitVisible)
-                || (state == DoorState.Closed && comp.OpenUnlitVisible)) // Corvax-Resprite-Airlocks-Edit
+                || (state == DoorState.Closed && comp.OpenUnlitVisible)) // CorvaxGoob-Resprite-Airlocks-Edit
                     && !boltedVisible && !emergencyLightsVisible;
         }
 
--- Content.Server/Administration/Commands/AdminWhoCommand.cs
+++ Content.Server/Administration/Commands/AdminWhoCommand.cs
@@ -21,7 +21,7 @@ using Robust.Shared.Utility;
 
 namespace Content.Server.Administration.Commands;
 
-[AnyCommand]
+[AnyCommand] // CorvaxGoob-Anyone-can-adminwho
 public sealed class AdminWhoCommand : IConsoleCommand
 {
     public string Command => "adminwho";
