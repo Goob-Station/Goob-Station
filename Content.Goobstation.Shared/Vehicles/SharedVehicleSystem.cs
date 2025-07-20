35c648451ff253d264396824ee20afe88c26b7ab
GoobBot <uristmchands@proton.me>
2025-07-20 03:09:59 +0000

    chore: Automatically update REUSE headers

--- Content.Goobstation.Shared/Vehicles/SharedVehicleSystem.cs
+++ Content.Goobstation.Shared/Vehicles/SharedVehicleSystem.cs
@@ -2,6 +2,7 @@
 // SPDX-FileCopyrightText: 2024 Scruq445 <storchdamien@gmail.com>
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Fishbait <Fishbait@git.ml>
+// SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
 // SPDX-FileCopyrightText: 2025 Misandry <mary@thughunt.ing>
 // SPDX-FileCopyrightText: 2025 fishbait <gnesse@gmail.com>
 // SPDX-FileCopyrightText: 2025 gluesniffler <linebarrelerenthusiast@gmail.com>
