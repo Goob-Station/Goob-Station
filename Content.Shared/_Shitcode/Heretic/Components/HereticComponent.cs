69d1b9a987dd1b956c780004ccedb9e1c7f7162a
GoobBot <uristmchands@proton.me>
2025-07-20 19:55:54 +0000

    chore: Automatically update REUSE headers

--- Content.Shared/_Shitcode/Heretic/Components/HereticComponent.cs
+++ Content.Shared/_Shitcode/Heretic/Components/HereticComponent.cs
@@ -4,6 +4,7 @@
 // SPDX-FileCopyrightText: 2025 Aviu00 <93730715+Aviu00@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Misandry <mary@thughunt.ing>
 // SPDX-FileCopyrightText: 2025 Piras314 <p1r4s@proton.me>
+// SPDX-FileCopyrightText: 2025 SolsticeOfTheWinter <solsticeofthewinter@gmail.com>
 // SPDX-FileCopyrightText: 2025 gus <august.eymann@gmail.com>
 // SPDX-FileCopyrightText: 2025 yglop <95057024+yglop@users.noreply.github.com>
 //
