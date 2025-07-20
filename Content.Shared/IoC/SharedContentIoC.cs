43d09d1b1fba6d5df7b369ad5bea21352bbf7b36
GoobBot <uristmchands@proton.me>
2025-07-20 02:29:51 +0000

    chore: Automatically update REUSE headers

--- Content.Shared/IoC/SharedContentIoC.cs
+++ Content.Shared/IoC/SharedContentIoC.cs
@@ -6,6 +6,8 @@
 // SPDX-FileCopyrightText: 2022 metalgearsloth <comedian_vs_clown@hotmail.com>
 // SPDX-FileCopyrightText: 2022 wrexbe <81056464+wrexbe@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 RadsammyT <radsammyt@gmail.com>
+// SPDX-FileCopyrightText: 2025 SX-7 <sn1.test.preria.2002@gmail.com>
 //
 // SPDX-License-Identifier: MIT
 
