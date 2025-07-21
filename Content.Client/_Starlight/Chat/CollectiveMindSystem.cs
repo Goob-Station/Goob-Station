110f98ad349d88eed43d29ed3be3fdfd1bae257c
GoobBot <uristmchands@proton.me>
2025-07-21 10:24:01 +0000

    chore: Automatically update REUSE headers

--- Content.Client/_Starlight/Chat/CollectiveMindSystem.cs
+++ Content.Client/_Starlight/Chat/CollectiveMindSystem.cs
@@ -1,4 +1,5 @@
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 BombasterDS <deniskaporoshok@gmail.com>
 // SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
 // SPDX-FileCopyrightText: 2025 Ilya246 <57039557+Ilya246@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Ilya246 <ilyukarno@gmail.com>
