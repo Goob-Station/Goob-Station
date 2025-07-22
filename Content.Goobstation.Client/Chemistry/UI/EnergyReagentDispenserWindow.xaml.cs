c51445876f6126da6f64480fc41fbb09fe3fea16
GoobBot <uristmchands@proton.me>
2025-07-22 01:27:15 +0000

    chore: Automatically update REUSE headers

--- Content.Goobstation.Client/Chemistry/UI/EnergyReagentDispenserWindow.xaml.cs
+++ Content.Goobstation.Client/Chemistry/UI/EnergyReagentDispenserWindow.xaml.cs
@@ -29,6 +29,7 @@
 // SPDX-FileCopyrightText: 2024 Nemanja <98561806+EmoGarbage404@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2024 Tayrtahn <tayrtahn@gmail.com>
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
 // SPDX-FileCopyrightText: 2025 IrisTheAmped <iristheamped@gmail.com>
 // SPDX-FileCopyrightText: 2025 SX-7 <sn1.test.preria.2002@gmail.com>
 // SPDX-FileCopyrightText: 2025 SolsticeOfTheWinter <solsticeofthewinter@gmail.com>
--- Content.Goobstation.Server/Chemistry/EntitySystems/EnergyReagentDispenserSystem.cs
+++ Content.Goobstation.Server/Chemistry/EntitySystems/EnergyReagentDispenserSystem.cs
@@ -30,6 +30,7 @@
 // SPDX-FileCopyrightText: 2024 metalgearsloth <31366439+metalgearsloth@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2024 metalgearsloth <comedian_vs_clown@hotmail.com>
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
 // SPDX-FileCopyrightText: 2025 IrisTheAmped <iristheamped@gmail.com>
 // SPDX-FileCopyrightText: 2025 SolsticeOfTheWinter <solsticeofthewinter@gmail.com>
 //
