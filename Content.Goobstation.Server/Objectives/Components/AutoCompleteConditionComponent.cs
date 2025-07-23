cb9aca7daf0069b2fded838af57f6dca7c055cf3
GoobBot <uristmchands@proton.me>
2025-07-23 04:33:26 +0000

    chore: Automatically update REUSE headers

--- Content.Goobstation.Server/Objectives/Components/AutoCompleteConditionComponent.cs
+++ Content.Goobstation.Server/Objectives/Components/AutoCompleteConditionComponent.cs
@@ -1,5 +1,6 @@
 // SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
 // SPDX-FileCopyrightText: 2025 SaffronFennec <firefoxwolf2020@protonmail.com>
+// SPDX-FileCopyrightText: 2025 Solstice <solsticeofthewinter@gmail.com>
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
