1a76d324b5e6971ff6de2524c572d610c511622e
GoobBot <uristmchands@proton.me>
2025-07-23 02:57:51 +0000

    chore: Automatically update REUSE headers

--- Content.Server/GameTicking/Rules/RuleGridsSystem.cs
+++ Content.Server/GameTicking/Rules/RuleGridsSystem.cs
@@ -2,6 +2,7 @@
 // SPDX-FileCopyrightText: 2024 deltanedas <39013340+deltanedas@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 BombasterDS <deniskaporoshok@gmail.com>
+// SPDX-FileCopyrightText: 2025 BombasterDS2 <shvalovdenis.workmail@gmail.com>
 // SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
 // SPDX-FileCopyrightText: 2025 SX-7 <sn1.test.preria.2002@gmail.com>
 //
