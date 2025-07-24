68b27026fabdeaac9bf5cba91af8faa2ff8d6f3f
GoobBot <uristmchands@proton.me>
2025-07-24 11:00:06 +0000

    chore: Automatically update REUSE headers

--- Content.Goobstation.Server/MobCaller/MobCallerSystem.cs
+++ Content.Goobstation.Server/MobCaller/MobCallerSystem.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 Ilya246 <ilyukarno@gmail.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using Content.Server.NPC;
 using Content.Server.NPC.Systems;
 using Content.Server.Power.EntitySystems;
