ed438d90d16ef89bf42cd07f9dae2a68a1e57ad8
GoobBot <uristmchands@proton.me>
2025-07-25 10:04:04 +0000

    chore: Automatically update REUSE headers

--- Content.Shared/Buckle/Components/BuckleComponent.cs
+++ Content.Shared/Buckle/Components/BuckleComponent.cs
@@ -92,6 +92,7 @@
 // SPDX-FileCopyrightText: 2024 voidnull000 <18663194+voidnull000@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2024 Арт <123451459+JustArt1m@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
 // SPDX-FileCopyrightText: 2025 Ilya246 <ilyukarno@gmail.com>
 // SPDX-FileCopyrightText: 2025 SX-7 <sn1.test.preria.2002@gmail.com>
 //
