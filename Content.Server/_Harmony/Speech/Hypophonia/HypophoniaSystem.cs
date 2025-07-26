b87b45b90d5c0bb5e8422c09d98441147cefd611
GoobBot <uristmchands@proton.me>
2025-07-26 17:55:44 +0000

    chore: Automatically update REUSE headers

--- Content.Server/_Harmony/Speech/Hypophonia/HypophoniaSystem.cs
+++ Content.Server/_Harmony/Speech/Hypophonia/HypophoniaSystem.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 MarkerWicker <markerWicker@proton.me>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using Content.Server.Chat.Systems;
 using Content.Server.Popups;
 using Content.Server.Speech.EntitySystems;
--- Content.Shared/Speech/SpeakAttemptEvent.cs
+++ Content.Shared/Speech/SpeakAttemptEvent.cs
@@ -3,6 +3,8 @@
 // SPDX-FileCopyrightText: 2022 mirrorcult <lunarautomaton6@gmail.com>
 // SPDX-FileCopyrightText: 2022 wrexbe <81056464+wrexbe@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 MarkerWicker <markerWicker@proton.me>
+// SPDX-FileCopyrightText: 2025 SX-7 <sn1.test.preria.2002@gmail.com>
 //
 // SPDX-License-Identifier: MIT
 
