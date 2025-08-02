358dd66ad1cb3cf0e728e2893fe97e82b458d0ea
GoobBot <uristmchands@proton.me>
2025-08-02 00:49:10 +0000

    chore: Automatically update REUSE headers

--- Content.Goobstation.Shared/Clothing/Components/SealableClothingComponent.cs
+++ Content.Goobstation.Shared/Clothing/Components/SealableClothingComponent.cs
@@ -3,6 +3,7 @@
 // SPDX-FileCopyrightText: 2025 BombasterDS <115770678+BombasterDS@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 BombasterDS <deniskaporoshok@gmail.com>
 // SPDX-FileCopyrightText: 2025 BombasterDS2 <shvalovdenis.workmail@gmail.com>
+// SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
 // SPDX-FileCopyrightText: 2025 Martynas6ha4 <martynashagriefer@gmail.com>
 // SPDX-FileCopyrightText: 2025 Misandry <mary@thughunt.ing>
 // SPDX-FileCopyrightText: 2025 SX-7 <sn1.test.preria.2002@gmail.com>
