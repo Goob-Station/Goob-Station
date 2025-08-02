d4d78ed932af89cd5ef01280263c736680aa7f2e
DrSmugleaf <10968691+DrSmugleaf@users.noreply.github.com>
2025-08-02 15:59:48 -0700

    Fix ActionsSystem.IsCooldownActive always returning false if curTime is null (#39329)

--- Content.Shared/Actions/SharedActionsSystem.cs
+++ Content.Shared/Actions/SharedActionsSystem.cs
@@ -1238,6 +1238,7 @@ public abstract class SharedActionsSystem : EntitySystem
     public bool IsCooldownActive(ActionComponent action, TimeSpan? curTime = null)
     {
         // TODO: Check for charge recovery timer
+        curTime ??= GameTiming.CurTime;
         return action.Cooldown.HasValue && action.Cooldown.Value.End > curTime;
     }
 
