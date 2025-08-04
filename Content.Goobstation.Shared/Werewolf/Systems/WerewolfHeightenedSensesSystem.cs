fe49a70d8d7ca71c76d5018b6e02dc757cc38237
GoobBot <uristmchands@proton.me>
2025-08-04 16:24:04 +0000

    chore: Automatically update REUSE headers

--- Content.Goobstation.Shared/Werewolf/Systems/WerewolfHeightenedSensesSystem.cs
+++ Content.Goobstation.Shared/Werewolf/Systems/WerewolfHeightenedSensesSystem.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 Lumminal <81829924+Lumminal@users.noreply.github.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using Content.Goobstation.Shared.Overlays;
 using Content.Goobstation.Shared.Werewolf.Components;
 using Content.Goobstation.Shared.Werewolf.Events;
