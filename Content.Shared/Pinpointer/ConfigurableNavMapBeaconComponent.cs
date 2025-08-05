dcca30b4f612c7d33837ca43c50fb81af270ff2e
Kyle Tyo <36606155+VerinSenpai@users.noreply.github.com>
2025-08-05 17:20:43 -0400

    Predict Nav Beacon Examine (#39408)
    * commit
    
    * Update Content.Shared/Pinpointer/SharedNavMapSystem.cs
    
    Co-authored-by: slarticodefast <161409025+slarticodefast@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: slarticodefast <161409025+slarticodefast@users.noreply.github.com>

--- Content.Shared/Pinpointer/ConfigurableNavMapBeaconComponent.cs
+++ Content.Shared/Pinpointer/ConfigurableNavMapBeaconComponent.cs
@@ -13,10 +13,7 @@ namespace Content.Shared.Pinpointer;
 /// </summary>
 [RegisterComponent, NetworkedComponent]
 [Access(typeof(SharedNavMapSystem))]
-public sealed partial class ConfigurableNavMapBeaconComponent : Component
-{
-
-}
+public sealed partial class ConfigurableNavMapBeaconComponent : Component;
 
 [Serializable, NetSerializable]
 public sealed class NavMapBeaconConfigureBuiMessage : BoundUserInterfaceMessage
@@ -36,11 +33,11 @@ public sealed class NavMapBeaconConfigureBuiMessage : BoundUserInterfaceMessage
 [Serializable, NetSerializable]
 public enum NavMapBeaconUiKey : byte
 {
-    Key
+    Key,
 }
 
 [Serializable, NetSerializable]
 public enum NavMapBeaconVisuals : byte
 {
-    Enabled
+    Enabled,
 }
