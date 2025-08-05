71ae1767338b2aa231ebf7133f33ed922ea4ebfc
GoobBot <uristmchands@proton.me>
2025-08-05 10:38:27 +0000

    chore: Automatically update REUSE headers

--- Content.Shared/Movement/Pulling/Components/PullableComponent.cs
+++ Content.Shared/Movement/Pulling/Components/PullableComponent.cs
@@ -72,6 +72,7 @@
 // SPDX-FileCopyrightText: 2024 voidnull000 <18663194+voidnull000@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Aidenkrz <aiden@djkraz.com>
+// SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
 // SPDX-FileCopyrightText: 2025 Hagvan <22118902+Hagvan@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Misandry <mary@thughunt.ing>
 // SPDX-FileCopyrightText: 2025 SX-7 <sn1.test.preria.2002@gmail.com>
