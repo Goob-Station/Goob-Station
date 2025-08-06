321975f4f161a0e4dc799328d787c5841d0928e8
GoobBot <uristmchands@proton.me>
2025-08-06 15:09:56 +0000

    chore: Automatically update REUSE headers

--- Content.Shared/Mech/Components/MechComponent.cs
+++ Content.Shared/Mech/Components/MechComponent.cs
@@ -9,6 +9,8 @@
 // SPDX-FileCopyrightText: 2024 ScyronX <166930367+ScyronX@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2024 gluesniffler <159397573+gluesniffler@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 gluesniffler <linebarrelerenthusiast@gmail.com>
+// SPDX-FileCopyrightText: 2025 gus <august.eymann@gmail.com>
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
