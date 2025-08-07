1697608a70f5e44a572b77036c453096a100e723
lolman360 <22850904+lolman360@users.noreply.github.com>
2025-08-08 02:01:18 +1000

    fixes items with complex shapes failing to insert sometimes (#38896)
    * fixes item insertion bug
    
    fixes bug where items with complex shapes would fail to insert if the item's StoredRotation wasn't a right angle
    
    * independence from StoredRotation

--- Content.Shared/Storage/EntitySystems/SharedStorageSystem.cs
+++ Content.Shared/Storage/EntitySystems/SharedStorageSystem.cs
@@ -1399,7 +1399,7 @@ public abstract class SharedStorageSystem : EntitySystem
         Angle startAngle;
         if (storageEnt.Comp.DefaultStorageOrientation == null)
         {
-            startAngle = Angle.FromDegrees(-itemEnt.Comp.StoredRotation);
+            startAngle = Angle.Zero;
         }
         else
         {
