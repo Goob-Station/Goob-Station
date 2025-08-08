96efd32d65e32377416007c00b5e19ee79866096
Lumminal <81829924+Lumminal@users.noreply.github.com>
2025-08-08 23:06:05 +0300

    Lycan Base Abilities Part 2

---
+++ Content.Goobstation.Server/Werewolf/Systems/WerewolfAmbushSystem.cs
@@ -0,0 +1,13 @@
+namespace Content.Goobstation.Server.Werewolf.Systems;
+
+/// <summary>
+/// This handles...
+/// </summary>
+public sealed class WerewolfAmbushSystem : EntitySystem
+{
+    /// <inheritdoc/>
+    public override void Initialize()
+    {
+        
+    }
+}
---
+++ Content.Goobstation.Shared/Werewolf/Systems/GrantsFurySystem.cs
@@ -0,0 +1,13 @@
+namespace Content.Goobstation.Shared.Werewolf.Systems;
+
+/// <summary>
+/// This handles...
+/// </summary>
+public sealed class GrantsFurySystem : EntitySystem
+{
+    /// <inheritdoc/>
+    public override void Initialize()
+    {
+        
+    }
+}
