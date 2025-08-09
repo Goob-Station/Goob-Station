eda97a8f2517494fbe7ba9ebf837eb492bc98785
GoobBot <uristmchands@proton.me>
2025-08-09 23:47:37 +0000

    chore: Automatically update REUSE headers

--- Content.Server/_DV/Abilities/ItemCougherSystem.cs
+++ Content.Server/_DV/Abilities/ItemCougherSystem.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 Aidenkrz <28298836+Aidenkrz@users.noreply.github.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using Content.Shared.Actions;
 using Content.Shared.Actions.Events;
 using Content.Shared.Clothing.Components;
