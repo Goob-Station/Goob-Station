11a37537a664ae631b57b0eafda5157d0726acf8
GoobBot <uristmchands@proton.me>
2025-08-09 20:07:05 +0000

    chore: Automatically update REUSE headers

--- Content.Goobstation.Server/Flashbang/FlashbangSystem.cs
+++ Content.Goobstation.Server/Flashbang/FlashbangSystem.cs
@@ -6,6 +6,7 @@
 // SPDX-FileCopyrightText: 2025 Misandry <mary@thughunt.ing>
 // SPDX-FileCopyrightText: 2025 Rouden <149893554+Roudenn@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Roudenn <romabond091@gmail.com>
+// SPDX-FileCopyrightText: 2025 gluesniffler <159397573+gluesniffler@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 gus <august.eymann@gmail.com>
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
