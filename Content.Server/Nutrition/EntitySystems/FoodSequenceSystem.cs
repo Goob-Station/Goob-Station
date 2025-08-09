0f7829d5617c88d868ba7de297969690f5fd590c
GoobBot <uristmchands@proton.me>
2025-08-09 20:22:53 +0000

    chore: Automatically update REUSE headers

--- Content.Server/Nutrition/EntitySystems/FoodSequenceSystem.cs
+++ Content.Server/Nutrition/EntitySystems/FoodSequenceSystem.cs
@@ -71,7 +71,10 @@
 // SPDX-FileCopyrightText: 2024 to4no_fix <156101927+chavonadelal@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2024 voidnull000 <18663194+voidnull000@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 CerberusWolfie <wb.johnb.willis@gmail.com>
 // SPDX-FileCopyrightText: 2025 MossyGreySlope <mossygreyslope@gmail.com>
+// SPDX-FileCopyrightText: 2025 SX-7 <sn1.test.preria.2002@gmail.com>
+// SPDX-FileCopyrightText: 2025 Wolfkey-SomeoneElseTookMyUsername <wolfkey75@gmail.com>
 // SPDX-FileCopyrightText: 2025 slarticodefast <161409025+slarticodefast@users.noreply.github.com>
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
