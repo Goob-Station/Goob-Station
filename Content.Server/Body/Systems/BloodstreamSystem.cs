9b282c28a22d9e4a765c77a29ba390200e06f712
GoobBot <uristmchands@proton.me>
2025-08-09 18:53:49 +0000

    chore: Automatically update REUSE headers

--- Content.Server/Body/Systems/BloodstreamSystem.cs
+++ Content.Server/Body/Systems/BloodstreamSystem.cs
@@ -110,6 +110,7 @@
 // SPDX-FileCopyrightText: 2025 Aviu00 <aviu00@protonmail.com>
 // SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
 // SPDX-FileCopyrightText: 2025 Kayzel <43700376+KayzelW@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 MarkerWicker <markerWicker@proton.me>
 // SPDX-FileCopyrightText: 2025 Roudenn <romabond091@gmail.com>
 // SPDX-FileCopyrightText: 2025 Spatison <137375981+Spatison@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Trest <144359854+trest100@users.noreply.github.com>
