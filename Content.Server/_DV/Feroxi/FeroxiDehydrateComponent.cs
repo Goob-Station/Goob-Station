33c8af0352c627717fe65c39f5000cd0cf721e5e
GoobBot <uristmchands@proton.me>
2025-08-10 19:47:24 +0000

    chore: Automatically update REUSE headers

--- Content.Server/_DV/Feroxi/FeroxiDehydrateComponent.cs
+++ Content.Server/_DV/Feroxi/FeroxiDehydrateComponent.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 AstroDogeDX <48888500+AstroDogeDX@users.noreply.github.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using Content.Shared.Body.Prototypes;
 using Robust.Shared.Prototypes;
 
