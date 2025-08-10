28b97c4a2db0f0701566370431a39494caf01a02
Dogbone10 <falcolom1@outlook.com>
2025-08-10 13:29:49 -0400

    Readded all changes made to new branch.

--- Content.Shared/_DV/Implant/BibleUserImplantComponent.cs
+++ Content.Shared/_DV/Implant/BibleUserImplantComponent.cs
@@ -3,7 +3,7 @@ using Robust.Shared.GameStates;
 namespace Content.Shared.Implants.Components;
 
 /// <summary>
-/// Implant to get BibleUser status (to pray, summon familiars, bless with bibles)
+/// Implant to get BibleUser status (to pray, summon familiars, bless with bibles) For characters who are canonically religious.
 /// </summary>
 [RegisterComponent, NetworkedComponent]
 public sealed partial class BibleUserImplantComponent : Component
