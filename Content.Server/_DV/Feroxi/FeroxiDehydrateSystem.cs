8ba6e1c4bf87c9f2fea2a4f2ff778c2dca0742e3
Aidenkrz <28298836+Aidenkrz@users.noreply.github.com>
2025-08-10 15:14:42 -0500

    cleanup of shitstem

--- Content.Server/_DV/Feroxi/FeroxiDehydrateSystem.cs
+++ Content.Server/_DV/Feroxi/FeroxiDehydrateSystem.cs
@@ -18,15 +18,7 @@ public sealed class FeroxiDehydrateSystem : EntitySystem
         var query = EntityQueryEnumerator<FeroxiDehydrateComponent, ThirstComponent>();
 
         while (query.MoveNext(out var uid, out var feroxiDehydrate, out var thirst))
-        {
-            var currentThirst = thirst.CurrentThirst;
-            var shouldBeDehydrated = currentThirst <= feroxiDehydrate.DehydrationThreshold;
-
-            if (feroxiDehydrate.Dehydrated != shouldBeDehydrated)
-            {
-                UpdateDehydrationStatus((uid, feroxiDehydrate), shouldBeDehydrated);
-            }
-        }
+            UpdateDehydrationStatus((uid, feroxiDehydrate), thirst.CurrentThirst <= feroxiDehydrate.DehydrationThreshold);
     }
 
     /// <summary>
@@ -36,14 +28,16 @@ public sealed class FeroxiDehydrateSystem : EntitySystem
     /// <param name="shouldBeDehydrated"></param>
     private void UpdateDehydrationStatus(Entity<FeroxiDehydrateComponent> ent, bool shouldBeDehydrated)
     {
+        if (ent.Comp.Dehydrated == shouldBeDehydrated)
+            return;
+
         ent.Comp.Dehydrated = shouldBeDehydrated;
 
         foreach (var entity in _body.GetBodyOrganEntityComps<LungComponent>(ent.Owner))
         {
             if (!TryComp<MetabolizerComponent>(entity, out var metabolizer) || metabolizer.MetabolizerTypes == null)
-            {
                 continue;
-            }
+
             //Changing the metabolizer to the appropriate value based
             var newMetabolizer = shouldBeDehydrated ? ent.Comp.DehydratedMetabolizer : ent.Comp.HydratedMetabolizer;
             metabolizer.MetabolizerTypes!.Clear();
