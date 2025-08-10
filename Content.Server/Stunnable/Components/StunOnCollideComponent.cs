b72301df5b39f15d82e8ffd5bbdba817f934cf1f
GoobBot <uristmchands@proton.me>
2025-08-10 15:45:13 +0000

    chore: Automatically update REUSE headers

--- Content.Server/Stunnable/Components/StunOnCollideComponent.cs
+++ Content.Server/Stunnable/Components/StunOnCollideComponent.cs
@@ -5,6 +5,8 @@
 // SPDX-FileCopyrightText: 2022 wrexbe <81056464+wrexbe@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2023 DrSmugleaf <DrSmugleaf@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 Lumminal <81829924+Lumminal@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 SX-7 <sn1.test.preria.2002@gmail.com>
 //
 // SPDX-License-Identifier: MIT
 
