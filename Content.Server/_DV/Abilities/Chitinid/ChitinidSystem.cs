15a4e9953ec3e54d7ddabc40cacfc4fbe1bf0bae
GoobBot <uristmchands@proton.me>
2025-08-10 01:31:51 +0000

    chore: Automatically update REUSE headers

--- Content.Server/_DV/Abilities/Chitinid/ChitinidSystem.cs
+++ Content.Server/_DV/Abilities/Chitinid/ChitinidSystem.cs
@@ -1,6 +1,8 @@
+// SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Aidenkrz <28298836+Aidenkrz@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Ark <189933909+ark1368@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
+// SPDX-FileCopyrightText: 2025 gluesniffler <159397573+gluesniffler@users.noreply.github.com>
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
