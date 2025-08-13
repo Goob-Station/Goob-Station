c9da6e5d0c412f6a2f054f9fd4d8a5f02f303caf
Winkarst-cpu <74284083+Winkarst-cpu@users.noreply.github.com>
2025-08-13 13:52:08 +0300

    Disable the lock/unlock verb if we can't do that (#39605)
    * Fix
    
    * Update

--- Content.Shared/Lock/LockSystem.cs
+++ Content.Shared/Lock/LockSystem.cs
@@ -395,6 +395,7 @@ public sealed class LockSystem : EntitySystem
 
         AlternativeVerb verb = new()
         {
+            Disabled = !CanToggleLock(uid, args.User),
             Act = component.Locked
                 ? () => TryUnlock(uid, args.User, component)
                 : () => TryLock(uid, args.User, component),
