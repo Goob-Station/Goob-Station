01b60db4c5c8fcfc8521d43ffc99eb547bf6cb22
beck-thompson <107373427+beck-thompson@users.noreply.github.com>
2025-08-15 09:10:38 -0700

    Added button and manager for in game bug reports (Part 1) (#35350)
    * Added the button and manager
    
    * Minor cleanup
    
    * Reigstered to the wrong thing!
    
    * Unload UI
    
    * Address the review
    
    * First commit :)
    
    * Some cleanup
    
    * Added some comments and now the placehoder text goes away once you start typing
    
    * Some cleanup and better test command
    
    * Basic rate limiter class (Not finished)
    
    * Cleanup
    
    * Removed forgotten comment xD
    
    * Whitespace removal
    
    * Minor cleanup, cvar hours -> minutes
    
    * More minor tweaks
    
    * Don't cache timer and add examples to fields
    
    * Added CCvar for time between bug reports
    
    * Minor crash when restarting rounds fixed
    
    * It compiled on my computer!
    
    * Fix comment indents
    
    * Remove unecessary async, removed magic strings, simplfied sawmill to not use post inject
    
    * Make struct private
    
    * Simplfiy TryGetLongHeader
    
    * Changed list to enumerable
    
    * URI cleanup
    
    * Got rid of the queue, used a much better way!
    
    * Made the comments a little better and fix some issues with them
    
    * Added header consts
    
    * Maximum reports per round is now an error message
    
    * Time between reports is now in seconds
    
    * Change ordering
    
    * Change hotkey to O
    
    * only update window when its open
    
    * Split up validation
    
    * address review
    
    * Address a few issues
    
    * inheritance fix
    
    * API now doesn't keep track of requests, just uses the rate limited response from github
    
    * Rough idea of how channels would work
    
    * refactor: reorganized code, placed rate limiter into http-client-handler AND manager (usually only manager-one should work)
    
    * cleanup
    
    * Add user agent so api doesn't get mad
    
    * Better error logs
    
    * Cleanup
    
    * It now throws!
    
    * refactor: renaming, moved some methods, xml-doc cleanups
    
    * refactor: BugReportWindow formatted to convention, enforced 1 updates only 1 per sec
    
    * Add very basic licence info
    
    * Fixed the issues!
    
    * Set ccvar default to false
    
    * make the button better
    
    * fix test fail silly me
    
    * Adress the review!
    
    * refactor: cleanup of entry point code, binding server-side code with client-facing manager
    
    * Resolve the other issues and cleanup and stuff smile :)
    
    * not entity
    
    * fixes
    
    * Cleanup
    
    * Cleanup
    
    * forgor region
    
    * fixes
    
    * Split up function and more stuff
    
    * Better unsubs yaygit add -A
    
    * I pray...
    
    * Revert "I pray..."
    
    This reverts commit 9629fb4f1289c9009a03e4e4facd9ae975e6303e.
    
    * I think I have to add it in the pr
    
    * Revert "I think I have to add it in the pr"
    
    This reverts commit e185b42f570fe5f0f51e0e44761d7938e22e67f7.
    
    * Tweaks
    
    * Minor tweak to permissions
    
    ---------
    
    Co-authored-by: pa.pecherskij <pa.pecherskij@interfax.ru>

--- Content.Server/IoC/ServerContentIoC.cs
+++ Content.Server/IoC/ServerContentIoC.cs
@@ -170,6 +170,7 @@ using Content.Server.Administration.Logs;
 using Content.Server.Administration.Managers;
 using Content.Server.Administration.Notes;
 using Content.Server.Afk;
+using Content.Server.BugReports;
 using Content.Server.Chat.Managers;
 using Content.Server.Connection;
 using Content.Server.Database;
@@ -177,6 +178,7 @@ using Content.Server.Discord;
 using Content.Server.Discord.WebhookMessages;
 using Content.Server.EUI;
 using Content.Server.GhostKick;
+using Content.Server.Github;
 using Content.Server.Info;
 using Content.Server.Mapping;
 using Content.Server.Maps;
@@ -227,6 +229,7 @@ namespace Content.Server.IoC
             IoCManager.Register<GhostKickManager>();
             IoCManager.Register<ISharedAdminLogManager, AdminLogManager>();
             IoCManager.Register<IAdminLogManager, AdminLogManager>();
+            IoCManager.Register<IBugReportManager, BugReportManager>();
             IoCManager.Register<PlayTimeTrackingManager>();
             IoCManager.Register<UserDbDataManager>();
             IoCManager.Register<ServerInfoManager>();
@@ -244,6 +247,11 @@ namespace Content.Server.IoC
             IoCManager.Register<ConnectionManager>();
             IoCManager.Register<MultiServerKickManager>();
             IoCManager.Register<CVarControlManager>();
+            IoCManager.Register<DiscordLink>();
+            IoCManager.Register<DiscordChatLink>();
+            IoCManager.Register<GithubApiManager>();
+            IoCManager.Register<GithubBackgroundWorker>();
+            IoCManager.Register<GithubClient>();
             IoCManager.Register<LastRolledAntagManager>(); // Goobstation - antag pity
             IoCManager.Register<LinkAccountManager>(); // RMC - Patreon
         }
