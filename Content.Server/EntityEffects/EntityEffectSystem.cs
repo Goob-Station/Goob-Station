256af82aefc4754d1ac713f9f986f1216af7fe79
Zeneganto <fantik8800@gmail.com>
2025-08-17 07:01:04 +1000

    Swap ExudeGasses and ConsumeGasses (#39688)
    Uh, um

--- Content.Server/EntityEffects/EntityEffectSystem.cs
+++ Content.Server/EntityEffects/EntityEffectSystem.cs
@@ -950,7 +950,7 @@ public sealed class EntityEffectSystem : EntitySystem
         if (plantholder.Seed == null)
             return;
 
-        var gasses = plantholder.Seed.ExudeGasses;
+        var gasses = plantholder.Seed.ConsumeGasses;
 
         // Add a random amount of a random gas to this gas dictionary
         float amount = _random.NextFloat(args.Effect.MinValue, args.Effect.MaxValue);
@@ -972,7 +972,7 @@ public sealed class EntityEffectSystem : EntitySystem
         if (plantholder.Seed == null)
             return;
 
-        var gasses = plantholder.Seed.ConsumeGasses;
+        var gasses = plantholder.Seed.ExudeGasses;
 
         // Add a random amount of a random gas to this gas dictionary
         float amount = _random.NextFloat(args.Effect.MinValue, args.Effect.MaxValue);
