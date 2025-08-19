7490110dea8b4cff4405f617b8166c5d568829ad
Winkarst-cpu <74284083+Winkarst-cpu@users.noreply.github.com>
2025-08-19 17:40:21 +0300

    Cleanup: Un-hardcode reagents standout (#39752)

--- Content.Shared/Chemistry/Reagent/ReagentPrototype.cs
+++ Content.Shared/Chemistry/Reagent/ReagentPrototype.cs
@@ -178,6 +178,12 @@ namespace Content.Shared.Chemistry.Reagent
         [DataField]
         public bool Recognizable;
 
+        /// <summary>
+        /// Whether this reagent stands out (blood, slime).
+        /// </summary>
+        [DataField]
+        public bool Standsout;
+
         [DataField]
         public ProtoId<FlavorPrototype>? Flavor;
 
