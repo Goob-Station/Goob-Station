170d6524f7c9ab6702009dbf09d43dc67719349f
Lumminal <81829924+Lumminal@users.noreply.github.com>
2025-08-22 20:41:55 +0300

    hunger healing + yamlmaxxing

--- Content.Goobstation.Shared/Werewolf/Systems/SharedWerewolfSystem.cs
+++ Content.Goobstation.Shared/Werewolf/Systems/SharedWerewolfSystem.cs
@@ -4,7 +4,6 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
 using Content.Goobstation.Shared.Werewolf.Components;
-using Content.Shared.Nutrition;
 using Content.Shared.Polymorph;
 
 namespace Content.Goobstation.Shared.Werewolf.Systems;
