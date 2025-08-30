38bd50db6c7e6ae3c00707947a1c8b4ea4866972
Princess Cheeseballs <66055347+Princess-Cheeseballs@users.noreply.github.com>
2025-08-29 17:05:47 -0700

    Staging into Master (#39977)

--- Content.Shared/Zombies/ZombieImmuneComponent.cs
+++ Content.Shared/Zombies/ZombieImmuneComponent.cs
@@ -13,7 +13,7 @@ namespace Content.Shared.Zombies;
 /// <summary>
 /// Entities with this component cannot be zombified.
 /// </summary>
-[RegisterComponent, NetworkedComponent]
+[RegisterComponent]
 public sealed partial class ZombieImmuneComponent : Component
 {
     //still no
