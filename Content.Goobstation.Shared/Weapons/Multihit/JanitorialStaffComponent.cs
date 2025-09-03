759a863cbf10d2a4133afaa622dc6841315956cb
loltart <lo1tartyt@gmail.com>
2025-09-03 03:34:02 -0700

    stuff

---
+++ Content.Goobstation.Shared/Weapons/Multihit/JanitorialStaffComponent.cs
@@ -0,0 +1,8 @@
+using Robust.Shared.GameStates;
+
+namespace Content.Goobstation.Shared.Weapons.Multihit;
+
+[RegisterComponent, NetworkedComponent]
+public sealed partial class JanitorialStaffComponent : Component
+{
+}
