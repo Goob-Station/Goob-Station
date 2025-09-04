cd204ff6dbcc5bfff9bf6f566f2c321aac8d4c36
Quantum-cross <7065792+Quantum-cross@users.noreply.github.com>
2025-09-04 15:11:03 -0400

    Dynamic anomaly scanner texture (#37585)

--- Content.Shared/Anomaly/Components/AnomalyScannerComponent.cs
+++ Content.Shared/Anomaly/Components/AnomalyScannerComponent.cs
@@ -8,14 +8,16 @@
 
 using Content.Shared.Anomaly;
 using Robust.Shared.Audio;
+using Robust.Shared.GameStates;
 
-namespace Content.Server.Anomaly.Components;
+namespace Content.Shared.Anomaly.Components;
 
 /// <summary>
 /// This is used for scanning anomalies and
 /// displaying information about them in the ui
 /// </summary>
-[RegisterComponent, Access(typeof(SharedAnomalySystem))]
+[RegisterComponent, Access(typeof(SharedAnomalyScannerSystem))]
+[NetworkedComponent]
 public sealed partial class AnomalyScannerComponent : Component
 {
     /// <summary>
@@ -27,12 +29,12 @@ public sealed partial class AnomalyScannerComponent : Component
     /// <summary>
     /// How long the scan takes
     /// </summary>
-    [DataField("scanDoAfterDuration")]
+    [DataField]
     public float ScanDoAfterDuration = 5;
 
     /// <summary>
     /// The sound plays when the scan finished
     /// </summary>
-    [DataField("completeSound")]
+    [DataField]
     public SoundSpecifier? CompleteSound = new SoundPathSpecifier("/Audio/Items/beep.ogg");
 }
