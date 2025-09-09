67839c5e036bf0d54cdd2be171b05be9ca34c20d
Evaisa <evagiacosa1@gmail.com>
2025-09-09 22:19:26 +0200

    Fix check

--- Content.Client/Lobby/UI/HumanoidProfileEditor.xaml.cs
+++ Content.Client/Lobby/UI/HumanoidProfileEditor.xaml.cs
@@ -698,7 +698,8 @@ namespace Content.Client.Lobby.UI
             foreach (var trait in traits)
             {
                 // Begin Goobstation: ported from DeltaV - Species trait exclusion
-                if (Profile?.Species is { } selectedSpecies && trait.IncludedSpecies.Count > 0 && !trait.IncludedSpecies.Contains(selectedSpecies))
+                if (Profile?.Species is { } selectedSpecies && (trait.ExcludedSpecies.Contains(selectedSpecies) ||
+                    trait.IncludedSpecies.Count > 0 && !trait.IncludedSpecies.Contains(selectedSpecies)))
                 {
                     Profile = Profile?.WithoutTraitPreference(trait.ID, _prototypeManager);
                     continue;
