ee17119aa926ae29fdb5def2b4db6cec5eeffa84
Baptr0b0t <152836416+Baptr0b0t@users.noreply.github.com>
2025-09-09 16:57:00 +0200

    Update Content.Shared/_White/Xenomorphs/Infection/XenomorphPreventSuicideComponent.cs
    Co-authored-by: Rouden <149893554+Roudenn@users.noreply.github.com>

--- Content.Shared/_White/Xenomorphs/Infection/XenomorphPreventSuicideComponent.cs
+++ Content.Shared/_White/Xenomorphs/Infection/XenomorphPreventSuicideComponent.cs
@@ -3,7 +3,4 @@ using Robust.Shared.GameStates;
 namespace Content.Shared._White.Xenomorphs.Infection;
 
 [RegisterComponent, NetworkedComponent]
-public sealed partial class XenomorphPreventSuicideComponent : Component
-{
-
-}
+public sealed partial class XenomorphPreventSuicideComponent : Component;
