db8645deb8d1dd2f1a6bf83522ca7af577aa83db
OnsenCapy <101037138+OnsenCapy@users.noreply.github.com>
2025-09-10 13:22:57 -0300

    Hold on, I forgot robust toolbox

--- Content.Goobstation.Shared/Wraith/Events/Wraith.Actions.cs
+++ Content.Goobstation.Shared/Wraith/Events/Wraith.Actions.cs
@@ -1,3 +1,25 @@
+using Content.Shared.Actions;
+using Content.Shared.DoAfter;
+using Robust.Shared.Serialization;
+
 namespace Content.Goobstation.Shared.Wraith.Events;
 
 // Here belong all action events for the base wraith
+public sealed partial class RevenantHauntActionEvent : InstantActionEvent
+{
+}
+
+[Serializable, NetSerializable]
+public sealed partial class RevenantHauntWitnessEvent : EntityEventArgs
+{
+    public HashSet<NetEntity> Witnesses = new();
+
+    public RevenantHauntWitnessEvent(HashSet<NetEntity> witnesses)
+    {
+        Witnesses = witnesses;
+    }
+
+    public RevenantHauntWitnessEvent() : this(new())
+    {
+    }
+}
