7fd759d78f91e4f1203f34bb8e89c3f82a8d24c6
Roudenn <romabond091@gmail.com>
2025-09-11 21:36:42 +0300

    Update NightmareComponent.cs

--- Content.Goobstation.Shared/Nightmare/Components/NightmareComponent.cs
+++ Content.Goobstation.Shared/Nightmare/Components/NightmareComponent.cs
@@ -1,15 +1,6 @@
-using Content.Shared.Polymorph;
 using Robust.Shared.GameStates;
-using Robust.Shared.Prototypes;
 
 namespace Content.Goobstation.Shared.Nightmare.Components;
 
-/// <summary>
-/// This is used for nightmares
-/// </summary>
 [RegisterComponent, NetworkedComponent]
-public sealed partial class NightmareComponent : Component
-{
-    [DataField]
-    public ProtoId<PolymorphPrototype> ShadowSpeciesProto = "ShadowNightmarePolymorph";
-}
+public sealed partial class NightmareComponent : Component;
