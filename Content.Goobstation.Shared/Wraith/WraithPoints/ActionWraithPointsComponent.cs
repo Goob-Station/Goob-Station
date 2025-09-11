734a987c1ebe0a539e9dacb6b548762202c6ee73
Lumminal <81829924+Lumminal@users.noreply.github.com>
2025-09-11 22:34:48 +0300

    WP + trying to rewrite blood writing

---
+++ Content.Goobstation.Shared/Wraith/WraithPoints/ActionWraithPointsComponent.cs
@@ -0,0 +1,10 @@
+namespace Content.Goobstation.Shared.Wraith.WraithPoints;
+
+/// <summary>
+/// This is used for...
+/// </summary>
+[RegisterComponent]
+public sealed partial class ActionWraithPointsComponent : Component
+{
+    
+}
---
+++ Content.Goobstation.Shared/Wraith/WraithPoints/WraithPointsSystem.cs
@@ -0,0 +1,13 @@
+namespace Content.Goobstation.Shared.Wraith.WraithPoints;
+
+/// <summary>
+/// This handles...
+/// </summary>
+public sealed class WraithPointsSystem : EntitySystem
+{
+    /// <inheritdoc/>
+    public override void Initialize()
+    {
+        
+    }
+}
