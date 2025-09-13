9b135946f6a8fd60805f20d3e0fd944f82d6aa82
Richard Blonski <48651647+RichardBlonski@users.noreply.github.com>
2025-09-14 01:18:14 +0300

    Added copyright to woundmed pain decay files

--- Content.Shared/_Shitmed/Surgery/Pain/Components/PainDecayComponent.cs
+++ Content.Shared/_Shitmed/Surgery/Pain/Components/PainDecayComponent.cs
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
+// SPDX-FileCopyrightText: 2025 RichardBlonski <48651647+RichardBlonski@users.noreply.github.com>
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
 using Content.Goobstation.Maths.FixedPoint;
