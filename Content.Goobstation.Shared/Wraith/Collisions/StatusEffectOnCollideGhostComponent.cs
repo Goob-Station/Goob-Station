40624abc5bb84d529a956b79ecb5ed412a5f9522
Lumminal <81829924+Lumminal@users.noreply.github.com>
2025-09-14 20:51:08 +0300

    weak to salt done

---
+++ Content.Goobstation.Shared/Wraith/Collisions/StatusEffectOnCollideGhostComponent.cs
@@ -0,0 +1,25 @@
+using Content.Shared.StatusEffect;
+using Content.Shared.Whitelist;
+using Robust.Shared.GameStates;
+using Robust.Shared.Prototypes;
+
+namespace Content.Goobstation.Shared.Wraith.Collisions;
+
+[RegisterComponent, NetworkedComponent]
+public sealed partial class StatusEffectOnCollideGhostComponent : Component
+{
+    [DataField]
+    public ProtoId<StatusEffectPrototype> StatusEffect = "Corporeal";
+
+    [DataField]
+    public EntityWhitelist? Whitelist;
+
+    [DataField]
+    public TimeSpan Duration;
+
+    [DataField]
+    public bool Refresh = true;
+
+    [ViewVariables]
+    public string FixtureId = "statusEffectCollision";
+}
