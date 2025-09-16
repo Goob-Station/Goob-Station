317a5aa81498ee0e5859cb127414916b4696def3
TurboTracker <130304754+TurboTrackerss14@users.noreply.github.com>
2025-09-16 23:06:02 +0100

    secret classic on server restart is hell.

--- Content.Shared/CCVar/CCVars.Game.cs
+++ Content.Shared/CCVar/CCVars.Game.cs
@@ -42,7 +42,7 @@ public sealed partial class CCVars
     ///     Controls the default game preset.
     /// </summary>
     public static readonly CVarDef<string>
-        GameLobbyDefaultPreset = CVarDef.Create("game.defaultpreset", "secret", CVar.ARCHIVE);
+        GameLobbyDefaultPreset = CVarDef.Create("game.defaultpreset", "secretplusmid", CVar.ARCHIVE); // Goob edit - Change default gamemode to secret+ medium
 
     /// <summary>
     ///     Controls if the game can force a different preset if the current preset's criteria are not met.
