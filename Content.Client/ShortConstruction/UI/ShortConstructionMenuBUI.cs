Simple-Station/Einstein-Engines

More Quick Construction Recipes And Upgrades (#945)
# Description

This PR adds a few new recipes to Quick Construction (accessed by
pressing Z on commonly used construction materials/items, such as rods,
glass, steel, plasma, etc etc). Along with simple nesting for grouping
items up under categories, akin to how the RCD works. Which allows
engineers to massively speed up pipe laying work, disposal rebuilding,
setting up the supermatter, etc etc.

First time doing UI code, apologies for the shitcode in advance :godo:

---

<details><summary><h1>Media</h1></summary>
<p>


https://github.com/user-attachments/assets/9d540d7f-7e4f-4a43-874d-5ea069011a37

</p>
</details>

---

# Considerations

Is this powergamey? Probably. Is it fucking awesome? Hell yeah. Some
servers might question the fact that everyone can build stuff this fast
now that they dont have to deal too much with the construction menu for
the most common things... And might want to lock it behind traits or
given as a job freebie, like CPR for doctors.

Other servers might appreciate it if they are looking for more action to
happen over the round, and giving everyone the ability to deal with
breaches and repairs quicker is probably a step into that direction.
I'll be observing for feedback and adjust accordingly.

It also shouldn't invalidate any eventual RPDs, or other devices of the
kind since this is just a shortcut for construction, you still require
materials and a doafter for most of them.

---

# Changelog

:cl: Mocho
- add: Added a lot of recipes to the quick construction menus. Give it a
shot by pressing Z with different construction materials in your hand!

---------

Signed-off-by: gluesniffler <159397573+gluesniffler@users.noreply.github.com>
Co-authored-by: VMSolidus <evilexecutive@gmail.com>

Content.Client/ShortConstruction/UI/ShortConstructionMenuBUI.cs
@@ -12,6 +12,7 @@ using Robust.Client.UserInterface;
 using Robust.Client.UserInterface.Controls;
 using Robust.Shared.Enums;
 using Robust.Shared.Prototypes;
+using Robust.Shared.Utility;
 
 // ReSharper disable InconsistentNaming
 
@@ -30,7 +31,6 @@ public sealed class ShortConstructionMenuBUI : BoundUserInterface
     private readonly SpriteSystem _spriteSystem;
 
     private RadialMenu? _menu;
-
     public ShortConstructionMenuBUI(EntityUid owner, Enum uiKey) : base(owner, uiKey)
     {
         _construction = _entManager.System<ConstructionSystem>();
@@ -39,8 +39,17 @@ public sealed class ShortConstructionMenuBUI : BoundUserInterface
 
     protected override void Open()
     {
-        _menu = FormMenu();
-        _menu.OnClose += Close;
+        _menu = new RadialMenu
+        {
+            HorizontalExpand = true,
+            VerticalExpand = true,
+            BackButtonStyleClass = "RadialMenuBackButton",
+            CloseButtonStyleClass = "RadialMenuCloseButton"
+        };
+
+        if (_entManager.TryGetComponent<ShortConstructionComponent>(Owner, out var crafting))
+            CreateMenu(crafting.Entries);
+
         _menu.OpenCenteredAt(_inputManager.MouseScreenPosition.Position / _displayManager.ScreenSize);
     }
 
@@ -51,56 +60,58 @@ public sealed class ShortConstructionMenuBUI : BoundUserInterface
             _menu?.Dispose();
     }
 
-    private RadialMenu FormMenu()
+    private void CreateMenu(List<ShortConstructionEntry> entries, string? parentCategory = null)
     {
-        var menu = new RadialMenu
+        if (_menu == null)
+            return;
+
+        var container = new RadialContainer
         {
-            HorizontalExpand = true,
-            VerticalExpand = true,
-            BackButtonStyleClass = "RadialMenuBackButton",
-            CloseButtonStyleClass = "RadialMenuCloseButton"
+            Name = parentCategory ?? "Main",
+            Radius = 48f + 24f * MathF.Log(entries.Count),
         };
 
-        if (!_entManager.TryGetComponent<ShortConstructionComponent>(Owner, out var crafting))
-            return menu;
+        _menu.AddChild(container);
 
-        var mainContainer = new RadialContainer
+        foreach (var entry in entries)
         {
-            Radius = 36f / MathF.Sin(MathF.PI / 2f / crafting.Prototypes.Count)
-        };
-
-        foreach (var protoId in crafting.Prototypes)
-        {
-            if (!_protoManager.TryIndex(protoId, out var proto))
-                continue;
-
-            var button = new RadialMenuTextureButton
+            if (entry.Category != null)
             {
-                ToolTip = Loc.GetString(proto.Name),
-                StyleClasses = { "RadialMenuButton" },
-                SetSize = new Vector2(48f, 48f)
-            };
-
-            var texture = new TextureRect
+                var button = CreateButton(entry.Category.Name, entry.Category.Icon);
+                button.TargetLayer = entry.Category.Name;
+                CreateMenu(entry.Category.Entries, entry.Category.Name);
+                container.AddChild(button);
+            }
+            else if (entry.Prototype != null
+                && _protoManager.TryIndex(entry.Prototype, out var proto))
             {
-                VerticalAlignment = Control.VAlignment.Center,
-                HorizontalAlignment = Control.HAlignment.Center,
-                Texture = _spriteSystem.Frame0(proto.Icon),
-                TextureScale = new Vector2(1.5f, 1.5f)
-            };
-
-            button.AddChild(texture);
-
-            button.OnButtonUp += _ =>
-            {
-                ConstructItem(proto);
-            };
-
-            mainContainer.AddChild(button);
+                var button = CreateButton(proto.Name, proto.Icon);
+                button.OnButtonUp += _ => ConstructItem(proto);
+                container.AddChild(button);
+            }
         }
 
-        menu.AddChild(mainContainer);
-        return menu;
+    }
+
+    private RadialMenuTextureButton CreateButton(string name, SpriteSpecifier icon)
+    {
+        var button = new RadialMenuTextureButton
+        {
+            ToolTip = Loc.GetString(name),
+            StyleClasses = { "RadialMenuButton" },
+            SetSize = new Vector2(64f, 64f),
+        };
+
+        var texture = new TextureRect
+        {
+            VerticalAlignment = Control.VAlignment.Center,
+            HorizontalAlignment = Control.HAlignment.Center,
+            Texture = _spriteSystem.Frame0(icon),
+            TextureScale = new Vector2(2f, 2f)
+        };
+
+        button.AddChild(texture);
+        return button;
     }
 
     /// <summary>
@@ -121,6 +132,6 @@ public sealed class ShortConstructionMenuBUI : BoundUserInterface
         }, new ConstructionPlacementHijack(_construction, prototype));
 
         // Should only close the menu if we're placing a construction hijack.
-        _menu!.Close();
+        // Theres not much point to closing it though. _menu!.Close();
     }
 }

Goob-Station/Goob-Station

Content.Client/ShortConstruction/UI/ShortConstructionMenuBUI.cs
@@ -12,6 +12,7 @@ using Robust.Client.UserInterface;
 using Robust.Client.UserInterface.Controls;
 using Robust.Shared.Enums;
 using Robust.Shared.Prototypes;
+using Robust.Shared.Utility;
 
 // ReSharper disable InconsistentNaming
 
@@ -30,7 +31,6 @@ public sealed class ShortConstructionMenuBUI : BoundUserInterface
     private readonly SpriteSystem _spriteSystem;
 
     private RadialMenu? _menu;
-
     public ShortConstructionMenuBUI(EntityUid owner, Enum uiKey) : base(owner, uiKey)
     {
         _construction = _entManager.System<ConstructionSystem>();
@@ -39,8 +39,17 @@ public sealed class ShortConstructionMenuBUI : BoundUserInterface
 
     protected override void Open()
     {
-        _menu = FormMenu();
-        _menu.OnClose += Close;
+        _menu = new RadialMenu
+        {
+            HorizontalExpand = true,
+            VerticalExpand = true,
+            BackButtonStyleClass = "RadialMenuBackButton",
+            CloseButtonStyleClass = "RadialMenuCloseButton"
+        };
+
+        if (_entManager.TryGetComponent<ShortConstructionComponent>(Owner, out var crafting))
+            CreateMenu(crafting.Entries);
+
         _menu.OpenCenteredAt(_inputManager.MouseScreenPosition.Position / _displayManager.ScreenSize);
     }
 
@@ -51,56 +60,58 @@ public sealed class ShortConstructionMenuBUI : BoundUserInterface
             _menu?.Dispose();
     }
 
-    private RadialMenu FormMenu()
+    private void CreateMenu(List<ShortConstructionEntry> entries, string? parentCategory = null)
     {
-        var menu = new RadialMenu
+        if (_menu == null)
+            return;
+
+        var container = new RadialContainer
         {
-            HorizontalExpand = true,
-            VerticalExpand = true,
-            BackButtonStyleClass = "RadialMenuBackButton",
-            CloseButtonStyleClass = "RadialMenuCloseButton"
+            Name = parentCategory ?? "Main",
+            InitialRadius = 48f + 24f * MathF.Log(entries.Count),
         };
 
-        if (!_entManager.TryGetComponent<ShortConstructionComponent>(Owner, out var crafting))
-            return menu;
+        _menu.AddChild(container);
 
-        var mainContainer = new RadialContainer
+        foreach (var entry in entries)
         {
-            Radius = 36f / MathF.Sin(MathF.PI / crafting.Prototypes.Count)
-        };
-
-        foreach (var protoId in crafting.Prototypes)
-        {
-            if (!_protoManager.TryIndex(protoId, out var proto))
-                continue;
-
-            var button = new RadialMenuTextureButton
+            if (entry.Category != null)
             {
-                ToolTip = Loc.GetString(proto.Name),
-                StyleClasses = { "RadialMenuButton" },
-                SetSize = new Vector2(48f, 48f)
-            };
-
-            var texture = new TextureRect
+                var button = CreateButton(entry.Category.Name, entry.Category.Icon);
+                button.TargetLayer = entry.Category.Name;
+                CreateMenu(entry.Category.Entries, entry.Category.Name);
+                container.AddChild(button);
+            }
+            else if (entry.Prototype != null
+                && _protoManager.TryIndex(entry.Prototype, out var proto))
             {
-                VerticalAlignment = Control.VAlignment.Center,
-                HorizontalAlignment = Control.HAlignment.Center,
-                Texture = _spriteSystem.Frame0(proto.Icon),
-                TextureScale = new Vector2(1.5f, 1.5f)
-            };
-
-            button.AddChild(texture);
-
-            button.OnButtonUp += _ =>
-            {
-                ConstructItem(proto);
-            };
-
-            mainContainer.AddChild(button);
+                var button = CreateButton(proto.Name, proto.Icon);
+                button.OnButtonUp += _ => ConstructItem(proto);
+                container.AddChild(button);
+            }
         }
 
-        menu.AddChild(mainContainer);
-        return menu;
+    }
+
+    private RadialMenuTextureButton CreateButton(string name, SpriteSpecifier icon)
+    {
+        var button = new RadialMenuTextureButton
+        {
+            ToolTip = Loc.GetString(name),
+            StyleClasses = { "RadialMenuButton" },
+            SetSize = new Vector2(64f, 64f),
+        };
+
+        var texture = new TextureRect
+        {
+            VerticalAlignment = Control.VAlignment.Center,
+            HorizontalAlignment = Control.HAlignment.Center,
+            Texture = _spriteSystem.Frame0(icon),
+            TextureScale = new Vector2(2f, 2f)
+        };
+
+        button.AddChild(texture);
+        return button;
     }
 
     /// <summary>
@@ -121,6 +132,6 @@ public sealed class ShortConstructionMenuBUI : BoundUserInterface
         }, new ConstructionPlacementHijack(_construction, prototype));
 
         // Should only close the menu if we're placing a construction hijack.
-        _menu!.Close();
+        // Theres not much point to closing it though. _menu!.Close();
     }
 }