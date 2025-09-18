9a265d3435d2223542b79a9f74bcbe162ebf846a
OnsenCapy <101037138+OnsenCapy@users.noreply.github.com>
2025-09-18 11:47:41 -0300

    Possess object part 1

---
+++ Content.Goobstation.Shared/Wraith/Components/PossessObjectComponent.cs
@@ -0,0 +1,12 @@
+using Robust.Shared.Audio;
+using Robust.Shared.GameStates;
+using Robust.Shared.Prototypes;
+
+namespace Content.Goobstation.Shared.Wraith.Components;
+
+[RegisterComponent, NetworkedComponent]
+public sealed partial class PossessObjectComponent : Component
+{
+    [DataField]
+    public SoundSpecifier? Sound;
+}
