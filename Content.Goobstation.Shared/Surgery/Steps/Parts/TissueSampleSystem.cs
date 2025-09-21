e6e0ecbb03eccd1e5f63199d8b8ab860d9e67757
deltanedas <@deltanedas:kde.org>
2025-09-21 23:03:52 +0100

    g

--- Content.Goobstation.Shared/Surgery/Steps/Parts/TissueSampleSystem.cs
+++ Content.Goobstation.Shared/Surgery/Steps/Parts/TissueSampleSystem.cs
@@ -5,7 +5,7 @@ namespace Content.Goobstation.Shared.Surgery.Steps.Parts;
 
 public sealed class TissueSampleSystem : EntitySystem
 {
-    [Dependency] private readonly SurgeryToolExamine _toolExamine = default!;
+    [Dependency] private readonly SurgeryToolExamineSystem _toolExamine = default!;
 
     public override void Initialize()
     {
