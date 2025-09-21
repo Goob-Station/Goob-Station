a4ae88d323b3b9f5374d1418a4801d2ffe088baa
Leon Friedrich <60421075+ElectroJr@users.noreply.github.com>
2025-09-21 14:52:23 +1200

    Fix explosion grid alignment for static grids (#40193)

--- Content.Server/Explosion/EntitySystems/ExplosionSystem.TileFill.cs
+++ Content.Server/Explosion/EntitySystems/ExplosionSystem.TileFill.cs
@@ -360,7 +360,7 @@ public sealed partial class ExplosionSystem
         radius = Math.Min(radius, MaxIterations / 4);
 
         EntityUid? referenceGrid = null;
-        float mass = 0;
+        var mass = float.MinValue;
 
         // First attempt to find a grid that is relatively close to the explosion's center. Instead of looking in a
         // diameter x diameter sized box, use a smaller box with radius sized sides:
@@ -370,7 +370,7 @@ public sealed partial class ExplosionSystem
         _mapManager.FindGridsIntersecting(epicenter.MapId, box, ref _grids);
         foreach (var grid in _grids)
         {
-            if (TryComp(grid.Owner, out PhysicsComponent? physics) && physics.Mass > mass)
+            if (TryComp(grid.Owner, out PhysicsComponent? physics) && physics.FixturesMass > mass)
             {
                 mass = physics.Mass;
                 referenceGrid = grid.Owner;
@@ -400,7 +400,7 @@ public sealed partial class ExplosionSystem
         {
             if (TryComp(grid.Owner, out PhysicsComponent? physics) && physics.Mass > mass)
             {
-                mass = physics.Mass;
+                mass = physics.FixturesMass;
                 referenceGrid = grid.Owner;
             }
         }
