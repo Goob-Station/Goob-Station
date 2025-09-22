de653247c839520495b7e15800041e3e0393e1cc
deltanedas <@deltanedas:kde.org>
2025-09-22 08:20:10 +0100

    goida

--- Content.Goobstation.Shared/Body/OrganActionsSystem.cs
+++ Content.Goobstation.Shared/Body/OrganActionsSystem.cs
@@ -1,6 +1,8 @@
 using Content.Shared._Shitmed.Body.Organ;
 using Content.Shared.Actions;
 using Content.Shared.Actions.Components;
+using Content.Shared.Body.Events;
+using Content.Shared.Body.Organ;
 
 namespace Content.Goobstation.Shared.Body;
 
@@ -9,13 +11,19 @@ public sealed class OrganActionsSystem : EntitySystem
     [Dependency] private readonly ActionContainerSystem _actionContainer = default!;
     [Dependency] private readonly SharedActionsSystem _actions = default!;
 
+    private EntityQuery<OrganComponent> _organQuery;
+
     public override void Initialize()
     {
         base.Initialize();
 
+        _organQuery = GetEntityQuery<OrganComponent>();
+
         SubscribeLocalEvent<OrganActionsComponent, MapInitEvent>(OnMapInit);
+        SubscribeLocalEvent<OrganActionsComponent, OrganAddedEvent>(OnAdded);
         SubscribeLocalEvent<OrganActionsComponent, OrganEnabledEvent>(OnEnabled);
         SubscribeLocalEvent<OrganActionsComponent, OrganDisabledEvent>(OnDisabled);
+        SubscribeLocalEvent<OrganActionsComponent, OrganRemovedEvent>(OnRemoved);
     }
 
     private void OnMapInit(Entity<OrganActionsComponent> ent, ref MapInitEvent args)
@@ -27,6 +35,14 @@ public sealed class OrganActionsSystem : EntitySystem
         }
     }
 
+    private void OnAdded(Entity<OrganActionsComponent> ent, ref OrganAddedEvent args)
+    {
+        if (_organQuery.CompOrNull(ent)?.Enabled != true)
+            return;
+
+        _actions.GrantContainedActions(args.Body, ent.Owner);
+    }
+
     private void OnEnabled(Entity<OrganActionsComponent> ent, ref OrganEnabledEvent args)
     {
         if (args.Organ.Comp.Body is {} body)
@@ -36,6 +52,12 @@ public sealed class OrganActionsSystem : EntitySystem
     private void OnDisabled(Entity<OrganActionsComponent> ent, ref OrganDisabledEvent args)
     {
         if (args.Organ.Comp.Body is {} body)
+            _actions.RemoveProvidedActions(body, ent.Owner);
+    }
+
+    private void OnRemoved(Entity<OrganActionsComponent> ent, ref OrganRemovedEvent args)
+    {
+        if (args.OldBody is {} body)
             _actions.RemoveProvidedActions(body, ent.Owner);
     }
 }
