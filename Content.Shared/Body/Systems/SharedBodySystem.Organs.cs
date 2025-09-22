fbaefc5a1d501b01555e3383f74501e61602b4b6
deltanedas <@deltanedas:kde.org>
2025-09-22 07:32:41 +0100

    g

--- Content.Shared/Body/Systems/SharedBodySystem.Organs.cs
+++ Content.Shared/Body/Systems/SharedBodySystem.Organs.cs
@@ -121,7 +121,7 @@ public partial class SharedBodySystem
         EntityUid parentPartUid)
     {
         organEnt.Comp.Body = bodyUid;
-        var addedEv = new OrganAddedEvent(parentPartUid, body); // Shitmed - add body
+        var addedEv = new OrganAddedEvent(parentPartUid, bodyUid); // Shitmed - add body
         RaiseLocalEvent(organEnt, ref addedEv);
 
         if (organEnt.Comp.Body is not null)
