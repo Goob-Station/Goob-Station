fabba3426c445015707123ab4d16208b081520fb
Richard Blonski <RichardBlonski@users.noreplay.github.com>
2025-09-23 10:59:47 +0300

    shit cursed code solution for bleeding on resin tiles

--- Content.Server/_White/Xenomorphs/Xenomorph/XenomorphSystem.cs
+++ Content.Server/_White/Xenomorphs/Xenomorph/XenomorphSystem.cs
@@ -1,11 +1,17 @@
 using System.Linq;
 using Content.Server._EinsteinEngines.Language;
 using Content.Server.Administration.Managers;
+using Content.Server.Body.Components;
+using Content.Server.Body.Systems;
 using Content.Server.Chat.Managers;
 using Content.Server.Chat.Systems;
 using Content.Shared._EinsteinEngines.Language;
+using Content.Shared._Shitmed.Medical.Surgery.Traumas.Components;
+using Content.Shared._Shitmed.Medical.Surgery.Wounds.Systems;
 using Content.Shared._White.Xenomorphs.Xenomorph;
+using Content.Shared.Body.Components;
 using Content.Shared.Chat;
+using Content.Shared.Chemistry.EntitySystems;
 using Content.Shared.Damage;
 using Robust.Shared.Network;
 using Robust.Shared.Player;
@@ -22,6 +28,9 @@ public sealed class XenomorphSystem : SharedXenomorphSystem
 
     [Dependency] private readonly DamageableSystem _damageable = default!;
     [Dependency] private readonly LanguageSystem _language = default!;
+    [Dependency] private readonly WoundSystem _wounds = default!;
+    [Dependency] private readonly SharedSolutionContainerSystem _solutionContainer = default!;
+    [Dependency] private readonly BodySystem _body = default!;
 
     public override void Initialize()
     {
@@ -35,16 +44,51 @@ public sealed class XenomorphSystem : SharedXenomorphSystem
         base.Update(frameTime);
 
         var time = _timing.CurTime;
+        var query = EntityQueryEnumerator<XenomorphComponent, BloodstreamComponent>();
 
-        var query = EntityQueryEnumerator<XenomorphComponent>();
-        while (query.MoveNext(out var uid, out var xenomorph))
+        while (query.MoveNext(out var uid, out var xenomorph, out var bloodstream))
         {
-            if (xenomorph.WeedHeal == null || !xenomorph.OnWeed || time < xenomorph.NextPointsAt)
+            if (xenomorph.WeedHeal == null || time < xenomorph.NextPointsAt)
                 continue;
 
+            // Update next heal time
             xenomorph.NextPointsAt = time + xenomorph.WeedHealRate;
 
+            if (!xenomorph.OnWeed)
+                continue;
+
+            // Apply regular weed healing if on weeds
             _damageable.TryChangeDamage(uid, xenomorph.WeedHeal);
+
+            // Then ensure all wounds are marked as not bleeding
+            if (TryComp<BodyComponent>(uid, out var body))
+            {
+                // Get all body parts
+                foreach (var part in _body.GetBodyChildren(uid, body))
+                {
+                    // Get all wounds on this body part
+                    var wounds = _wounds.GetWoundableWounds(part.Id);
+                    foreach (var wound in wounds)
+                    {
+                        //  Remove & Add the bleeding component to get rid of bleeding & allow it to work
+                        if (!HasComp<BleedInflicterComponent>(wound))
+                            continue;
+                        RemComp<BleedInflicterComponent>(wound);
+                        AddComp<BleedInflicterComponent>(wound);
+                        Log.Debug($"Stopped bleeding on wound {ToPrettyString(wound)}");
+                    }
+                }
+            }
+
+            // Heal existing blood loss damage if not at max blood
+            if (_solutionContainer.ResolveSolution(uid, bloodstream.BloodSolutionName, ref bloodstream.BloodSolution, out var bloodSolution)
+                && bloodSolution.Volume < bloodstream.BloodMaxVolume)
+            {
+                var bloodloss = new DamageSpecifier();
+                bloodloss.DamageDict.Add("Bloodloss", -0.2); // Heal blood per tick.
+                _damageable.TryChangeDamage(uid, bloodloss);
+                Log.Debug("Healing blood loss");
+            }
         }
     }
 
