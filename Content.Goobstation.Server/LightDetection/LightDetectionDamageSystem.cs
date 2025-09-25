652094160224bd423e8609606a0759cef987ad75
BombasterDS2 <bombasterds.github@mail.ru>
2025-09-26 06:12:35 +1000

    Shadowling - Patch of First day (#4383)
    * Thrall fixes
    
    * Light Detection System & Shadowling shadow regen and damage changes
    
    * TimeSpan
    
    ---------
    
    Co-authored-by: BombasterDS <deniskaporoshok@gmail.com>

--- Content.Goobstation.Server/LightDetection/LightDetectionDamageSystem.cs
+++ Content.Goobstation.Server/LightDetection/LightDetectionDamageSystem.cs
@@ -1,9 +1,14 @@
 using Content.Goobstation.Shared.LightDetection.Components;
 using Content.Goobstation.Shared.LightDetection.Systems;
+using Content.Shared._Shitmed.Damage;
+using Content.Shared._Shitmed.Medical.Surgery.Pain.Systems;
+using Content.Shared._Shitmed.Medical.Surgery.Wounds.Systems;
+using Content.Shared._Shitmed.Targeting;
 using Content.Shared.Damage;
 using Content.Shared.Mobs.Systems;
 using Robust.Shared.Audio;
 using Robust.Shared.Audio.Systems;
+using Robust.Shared.Timing;
 
 namespace Content.Goobstation.Server.LightDetection;
 
@@ -12,7 +17,9 @@ namespace Content.Goobstation.Server.LightDetection;
 /// </summary>
 public sealed class LightDetectionDamageSystem : SharedLightDetectionDamageSystem
 {
+    [Dependency] private readonly IGameTiming _timing = default!;
     [Dependency] private readonly DamageableSystem _damageable = default!;
+    [Dependency] private readonly WoundSystem _woundSystem = default!;
     [Dependency] private readonly MobStateSystem _mobState = default!;
     [Dependency] private readonly SharedAudioSystem _audio = default!;
 
@@ -23,57 +30,34 @@ public sealed class LightDetectionDamageSystem : SharedLightDetectionDamageSyste
         var query = EntityQueryEnumerator<LightDetectionDamageComponent, LightDetectionComponent>();
         while (query.MoveNext(out var uid, out var comp, out var lightDet))
         {
-            comp.Accumulator -= frameTime;
+            if (comp.NextUpdate > _timing.CurTime)
+                return;
 
-            if (comp.TakeDamageOnLight)
-                comp.DamageAccumulator -= frameTime;
-            if (comp.HealOnShadows)
-                comp.HealAccumulator -= frameTime;
+            comp.NextUpdate = _timing.CurTime + comp.UpdateInterval;
 
-            if (comp.Accumulator <= 0)
+            UpdateDetectionValues(comp, lightDet.CurrentLightLevel);
+            DirtyField(uid, comp, nameof(LightDetectionDamageComponent.DetectionValue));
+
+            if (comp.DetectionValue <= 0 && comp.TakeDamageOnLight && !_mobState.IsDead(uid))
             {
-                UpdateDetectionValues(comp, lightDet.IsOnLight);
-                DirtyField(uid, comp, nameof(LightDetectionDamageComponent.DetectionValue));
-                DirtyField(uid, lightDet, nameof(LightDetectionComponent.IsOnLight));
-                comp.Accumulator = comp.UpdateInterval;
-            }
-
-            if (comp.DamageAccumulator <= 0
-                && comp.DetectionValue <= 0
-                && comp.TakeDamageOnLight
-                && !_mobState.IsCritical(uid))
-            {
-                // Take Damage
-                _damageable.TryChangeDamage(uid, comp.DamageToDeal * comp.ResistanceModifier);
+                _damageable.TryChangeDamage(uid, comp.DamageToDeal * comp.ResistanceModifier, splitDamage: SplitDamageBehavior.SplitEnsureAll);
                 _audio.PlayPvs(comp.SoundOnDamage, uid, AudioParams.Default.WithVolume(-2f));
-                comp.DamageAccumulator = comp.DamageInterval;
+                return;
             }
 
-            if (comp.HealAccumulator <= 0
-                && comp.DetectionValue >= comp.DetectionValueMax
-                && comp.HealOnShadows)
+            if (comp.DetectionValue > 0 && comp.HealOnShadows && !_mobState.IsDead(uid))
             {
-                _damageable.TryChangeDamage(uid, comp.DamageToHeal);
-                comp.HealAccumulator = comp.HealInterval;
+                _woundSystem.TryHealWoundsOnOwner(uid, comp.DamageToHeal, true);
+                _damageable.TryChangeDamage(uid, comp.DamageToHeal, true, false, targetPart: TargetBodyPart.All, splitDamage: SplitDamageBehavior.SplitEnsureAllOrganic, canMiss: false);
+                return;
             }
         }
     }
 
-    private void UpdateDetectionValues(LightDetectionDamageComponent comp, bool onLight)
+    private void UpdateDetectionValues(LightDetectionDamageComponent comp, float detectionDamage)
     {
-        if (onLight)
-        {
-            comp.DetectionValue -= comp.DetectionValueFactor;
-
-            if (comp.DetectionValue <= 0)
-                comp.DetectionValue = 0;
-        }
-        else
-        {
-            comp.DetectionValue += comp.DetectionValueFactor;
-
-            if (comp.DetectionValue >= comp.DetectionValueMax)
-                comp.DetectionValue = comp.DetectionValueMax;
-        }
+        var detectionDelta = comp.DetectionValueRegeneration - detectionDamage;
+        comp.DetectionValue += detectionDelta;
+        comp.DetectionValue = Math.Clamp(comp.DetectionValue, 0f, comp.DetectionValueMax);
     }
 }
