fa522b31de1f95579b4ef064179504848a10670a
FaDeOkno <logkedr18@gmail.com>
2025-09-30 18:23:49 +0400

    add(src, ui): Book approval UI

--- Content.Goobstation.Client/Books/CustomBooksSystem.cs
+++ Content.Goobstation.Client/Books/CustomBooksSystem.cs
@@ -1,7 +1,7 @@
+using Content.Goobstation.Client.Books.Ui;
 using Content.Goobstation.Shared.Books;
 using Robust.Client.GameObjects;
-using Robust.Client.Utility;
-using Robust.Shared.Prototypes;
+using Robust.Client.UserInterface;
 using Robust.Shared.Utility;
 
 namespace Content.Goobstation.Client.Books;
@@ -9,14 +9,17 @@ namespace Content.Goobstation.Client.Books;
 public sealed partial class CustomBooksSystem : SharedCustomBooksSystem
 {
     [Dependency] private readonly AppearanceSystem _appearance = default!;
-    [Dependency] private readonly IPrototypeManager _proto = default!;
     [Dependency] private readonly SpriteSystem _sprite = default!;
+    [Dependency] private readonly IUserInterfaceManager _ui = default!;
 
     public override void Initialize()
     {
         base.Initialize();
 
         SubscribeLocalEvent<CustomBookComponent, AppearanceChangeEvent>(OnBookAppearanceChange);
+
+        SubscribeNetworkEvent<OpenPendingBooksListMessage>(OnOpenPending);
+        SubscribeNetworkEvent<PopulatePendingBooksMenuMessage>(OnPopulatePending);
     }
 
     private void OnBookAppearanceChange(Entity<CustomBookComponent> ent, ref AppearanceChangeEvent args)
@@ -31,5 +34,18 @@ public sealed partial class CustomBooksSystem : SharedCustomBooksSystem
 
             _sprite.LayerSetRsi(ent.Owner, layer, newLayer.Path, newLayer.State);
         }
+    }
+
+    private void OnOpenPending(OpenPendingBooksListMessage args)
+    {
+        var controller = _ui.GetUIController<AdminBookVerificationUiController>();
+        controller.ToggleMenu();
+        controller.Populate(args.Books);
+    }
+
+    private void OnPopulatePending(PopulatePendingBooksMenuMessage args)
+    {
+        var controller = _ui.GetUIController<AdminBookVerificationUiController>();
+        controller.Populate(args.Books);
     }
 }
