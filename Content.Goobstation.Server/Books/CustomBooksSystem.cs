eacb97991c5fbeb850b888de0ed41a2d6b733d65
FaDeOkno <logkedr18@gmail.com>
2025-09-30 18:22:52 +0400

    add(src): Saving books and a bit of polishing

--- Content.Goobstation.Server/Books/CustomBooksSystem.cs
+++ Content.Goobstation.Server/Books/CustomBooksSystem.cs
@@ -23,8 +23,10 @@ public sealed partial class CustomBooksSystem : SharedCustomBooksSystem
     [Dependency] private readonly AudioSystem _audio = default!;
     [Dependency] private readonly IGameTiming _timing = default!;
     [Dependency] private readonly IChatManager _chat = default!;
+    [Dependency] private readonly IServerDbManager _db = default!;
 
-    private List<BookData> _pendingBooks = new();
+    private Dictionary<int, BookData> _pendingBooks = new();
+    private int _nextPendingBook = 0;
 
     public override void Initialize()
     {
@@ -120,8 +122,15 @@ public sealed partial class CustomBooksSystem : SharedCustomBooksSystem
 
     private void SaveBook(Entity<CustomBookComponent> ent)
     {
-        var data = new BookData(ent.Comp.Title, ent.Comp.Genre, ent.Comp.Author, ent.Comp.Desc, ent.Comp.Pages);
-        _pendingBooks.Add(data);
+        if (ent.Comp.Binding == null)
+            return;
+
+        var data = new BookData(ent.Comp.Title, ent.Comp.Genre, ent.Comp.Author, ent.Comp.Desc, ent.Comp.Pages, ent.Comp.Binding);
+        _pendingBooks.Add(_nextPendingBook, data);
+        _nextPendingBook++;
+
+        var ev = new PopulatePendingBooksMenuMessage(_pendingBooks);
+        RaiseNetworkEvent(ev, Filter.Empty().AddWhere(x => _adminManager.IsAdmin(x)));
 
         _chat.SendAdminAnnouncement(Loc.GetString("pending-book-chat-notify", ("title", ent.Comp.Title)));
         _audio.PlayGlobal(new SoundPathSpecifier("/Audio/Machines/high_tech_confirm.ogg"), Filter.Empty().AddPlayers(_adminManager.ActiveAdmins), false, AudioParams.Default.WithVolume(-8f));
@@ -129,6 +138,69 @@ public sealed partial class CustomBooksSystem : SharedCustomBooksSystem
 
     private void UpdateScannerUi(Entity<BookScannerComponent> ent)
     {
-        UI.SetUiState(ent.Owner, BookScannerUiKey.Key, new BookScannerUiState("", "", "", "", ent.Comp.IsScanning || ent.Comp.NextScan > _timing.CurTime));
+        if (!TryComp<CustomBookComponent>(ent.Comp.Book, out var comp))
+            UI.SetUiState(ent.Owner, BookScannerUiKey.Key, new BookScannerUiState("", "", "", "", ent.Comp.IsScanning || ent.Comp.NextScan > _timing.CurTime));
+        else
+            UI.SetUiState(ent.Owner, BookScannerUiKey.Key, new BookScannerUiState(comp.Author, comp.Genre, comp.Title, comp.Desc, ent.Comp.IsScanning || ent.Comp.NextScan > _timing.CurTime));
+    }
+
+    public void OpenPendingBooks(ICommonSession session)
+    {
+        var ev = new OpenPendingBooksListMessage(_pendingBooks);
+        RaiseNetworkEvent(ev, session);
+    }
+
+    public async void SaveBookToDb(int id, BookData data)
+    {
+        BookEntry entry = new()
+        {
+            Title = data.Title,
+            Genre = data.Genre,
+            Author = data.Author,
+            Content = data.Pages,
+            Description = data.Desc
+        };
+
+        entry.BindingMaps = new();
+        entry.BindingPaths = new();
+        entry.BindingStates = new();
+
+        foreach (var item in data.Binding)
+        {
+            entry.BindingMaps.Add(item.Key);
+            entry.BindingPaths.Add(item.Value.Path.CanonPath);
+            entry.BindingStates.Add(item.Value.State);
+        }
+
+        await _db.UploadBookPrinterEntryAsync(entry);
+
+        _pendingBooks.Remove(id);
+        var ev = new PopulatePendingBooksMenuMessage(_pendingBooks);
+        RaiseNetworkEvent(ev, Filter.Empty().AddWhere(x => _adminManager.IsAdmin(x)));
+
+        foreach (var printer in EntityManager.AllEntities<BookPrinterComponent>())
+        {
+            UpdatePrinterUi(printer);
+        }
+    }
+
+    private async void UpdatePrinterUi(Entity<BookPrinterComponent> ent)
+    {
+        var result = new Dictionary<int, BookData>();
+        var entries = await _db.GetBookPrinterEntriesAsync();
+
+        foreach (var entry in entries)
+        {
+            var data = new BookData(entry.Title, entry.Genre, entry.Author, entry.Description, entry.Content, new());
+            for (var i = 0; i < entry.BindingMaps.Count; i++)
+            {
+                KeyValuePair<string, (ResPath Path, string State)> bindingLayer = new(entry.BindingMaps[i], (new(entry.BindingPaths[i]), entry.BindingStates[i]));
+                data.Binding.Add(bindingLayer.Key, bindingLayer.Value);
+            }
+
+            result.Add(entry.Id, data);
+        }
+
+        UI.SetUiState(ent.Owner, BookPrinterUiKey.Key, new BookPrinterUiState(result, ent.Comp.AllowDeleting, ent.Comp.NextPrint > _timing.CurTime));
     }
 }
