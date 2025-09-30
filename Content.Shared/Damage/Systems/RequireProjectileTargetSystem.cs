8bed61e100e2f90845f3d25c439f1c297948084d
Hagvan <22118902+Hagvan@users.noreply.github.com>
2025-09-30 12:45:53 +0300

    Added a method to be able to lead shots against moving crawing targets.

--- Content.Shared/Damage/Systems/RequireProjectileTargetSystem.cs
+++ Content.Shared/Damage/Systems/RequireProjectileTargetSystem.cs
@@ -9,6 +9,7 @@
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
+using Content.Goobstation.Common.CCVar; //Goobstation - Crawling
 using Content.Goobstation.Common.Projectiles;
 using Content.Shared._DV.Abilities;
 using Content.Shared.Projectiles;
@@ -17,18 +18,24 @@ using Content.Shared.Standing;
 using Robust.Shared.Physics.Events;
 using Robust.Shared.Containers;
 using Robust.Shared.Physics.Components;
+using Robust.Shared.Configuration; //Goobstation - Crawling
 
 namespace Content.Shared.Damage.Components;
 
 public sealed class RequireProjectileTargetSystem : EntitySystem
 {
     [Dependency] private readonly SharedContainerSystem _container = default!;
+    [Dependency] private readonly SharedTransformSystem _transform = default!; //Goobstation
+    [Dependency] private readonly IConfigurationManager _cfg = default!; //Goobstation
+
+    private float _crawlHitzoneSize; //Goobstation
 
     public override void Initialize()
     {
         SubscribeLocalEvent<RequireProjectileTargetComponent, PreventCollideEvent>(PreventCollide);
         SubscribeLocalEvent<RequireProjectileTargetComponent, StoodEvent>(StandingBulletHit);
         SubscribeLocalEvent<RequireProjectileTargetComponent, DownedEvent>(LayingBulletPass);
+        _cfg.OnValueChanged(GoobCVars.CrawlHitzoneSize, value => _crawlHitzoneSize = value, true); //Goobstation - Crawling
     }
 
     private void PreventCollide(Entity<RequireProjectileTargetComponent> ent, ref PreventCollideEvent args)
@@ -71,6 +78,9 @@ public sealed class RequireProjectileTargetSystem : EntitySystem
             // ProjectileGrenades delete the entity that's shooting the projectile,
             // so it's impossible to check if the entity is in a container
             if (TerminatingOrDeleted(shooter.Value))
+                return;
+
+            if ((_transform.GetMapCoordinates(ent).Position - projectile.TargetCoordinates).Length() <= _crawlHitzoneSize) //Goobstation
                 return;
 
             if (!_container.IsEntityOrParentInContainer(shooter.Value))
