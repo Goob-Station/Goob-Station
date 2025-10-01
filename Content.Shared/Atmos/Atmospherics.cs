85f12834b5fc8ac376d274991627c3e312332a04
Armok <155400926+ARMOKS@users.noreply.github.com>
2025-09-30 21:56:25 -0600

    s

--- Content.Shared/Atmos/Atmospherics.cs
+++ Content.Shared/Atmos/Atmospherics.cs
@@ -460,7 +460,7 @@ namespace Content.Shared.Atmos
         ///     (The pressure threshold is so low that it doesn't make sense to do any calculations,
         ///     so it just applies this flat value).
         /// </summary>
-        public const int LowPressureDamage = 4;
+        public const int LowPressureDamage = 2;
 
         public const float WindowHeatTransferCoefficient = 0.1f;
 
