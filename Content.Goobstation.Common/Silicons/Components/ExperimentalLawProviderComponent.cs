7858067e70ad3b2c0165e298eaffca076a3bc245
Armok <155400926+ARMOKS@users.noreply.github.com>
2025-10-02 11:21:04 -0600

    borg ion changes

--- Content.Goobstation.Common/Silicons/Components/ExperimentalLawProviderComponent.cs
+++ Content.Goobstation.Common/Silicons/Components/ExperimentalLawProviderComponent.cs
@@ -17,5 +17,5 @@ public sealed partial class ExperimentalLawProviderComponent : Component
     // Numbers are equivalent to 83 points per second, so it's like running a dangerous anomaly for 2 minutes.
     [DataField] public float RewardTime = 120.0f;
 
-    [DataField] public int RewardPoints = 10000;
+    [DataField] public int RewardPoints = 100000; //830 Points per second
 }
