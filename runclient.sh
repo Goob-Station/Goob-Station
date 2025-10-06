519a64ce57b1afb38df394a93317fc464812c218
deltanedas <@deltanedas:kde.org>
2025-10-06 21:55:45 +0100

    trauma mod

--- runclient.sh
+++ runclient.sh
@@ -4,5 +4,5 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
 #!/bin/sh
-dotnet run --project Content.Goobstation.Client
+dotnet run --project Content.Trauma.Client
 read -p "Press enter to continue"
