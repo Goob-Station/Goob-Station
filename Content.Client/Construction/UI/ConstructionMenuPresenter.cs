107a99c0a0a8677e83bc35e013e2370741b666ac
David <david.owen.dev@gmail.com>
2025-10-07 05:45:01 -0600

    (Cleanup) Fix logger obsolete warnings (#40553)
    * Switched obsolete logger usages to use Sawmill
    
    Fix the majority of obsolete logger usages outside the engine code.
    
    * Fix injection in ChatManager and revert BuildMech changes
    
    * Removed extra manual injection
    
    * Reduced extra static injection and reverted changes to CommandButton as it needs engine changes.
    
    * Removed two more cases of double injection and an extra using
    
    * Reverted changes for Inventory Display
    
    * Moved sawmill setup outside constructor in Table to resolve test failure

--- Content.Client/Construction/UI/ConstructionMenuPresenter.cs
+++ Content.Client/Construction/UI/ConstructionMenuPresenter.cs
@@ -69,8 +69,10 @@ namespace Content.Client.Construction.UI
         [Dependency] private readonly IUserInterfaceManager _uiManager = default!;
         [Dependency] private readonly IPlayerManager _playerManager = default!;
         [Dependency] private readonly IClientPreferencesManager _preferencesManager = default!;
-        private readonly SpriteSystem _spriteSystem;
+        [Dependency] private readonly ILogManager _logManager = default!;
 
+        private readonly ISawmill _sawmill = default!;
+        private readonly SpriteSystem _spriteSystem;
         private readonly IConstructionMenuView _constructionView;
         private readonly EntityWhitelistSystem _whitelistSystem;
         private readonly IConfigurationManager _cfg; // Goobstation
@@ -127,6 +129,8 @@ namespace Content.Client.Construction.UI
         {
             // This is a lot easier than a factory
             IoCManager.InjectDependencies(this);
+            _sawmill = _logManager.GetSawmill("construction.menu");
+
             _constructionView = new ConstructionMenu();
             _whitelistSystem = _entManager.System<EntityWhitelistSystem>();
             _spriteSystem = _entManager.System<SpriteSystem>();
@@ -332,7 +336,7 @@ namespace Content.Client.Construction.UI
 
                 if (!_constructionSystem!.TryGetRecipePrototype(recipe.ID, out var targetProtoId))
                 {
-                    Logger.Error("Cannot find the target prototype in the recipe cache with the id \"{0}\" of {1}.",
+                    _sawmill.Error("Cannot find the target prototype in the recipe cache with the id \"{0}\" of {1}.",
                         recipe.ID,
                         nameof(ConstructionPrototype));
                     continue;
--- Content.Client/Guidebook/Richtext/Table.cs
+++ Content.Client/Guidebook/Richtext/Table.cs
@@ -13,6 +13,11 @@ namespace Content.Client.Guidebook.Richtext;
 [UsedImplicitly]
 public sealed class Table : TableContainer, IDocumentTag
 {
+    [Dependency] private readonly ILogManager _logManager = default!;
+
+    private ISawmill Sawmill => _sawmill ??= _logManager.GetSawmill("table");
+    private ISawmill? _sawmill;
+
     public bool TryParseTag(Dictionary<string, string> args, [NotNullWhen(true)] out Control? control)
     {
         HorizontalExpand = true;
@@ -20,7 +25,7 @@ public sealed class Table : TableContainer, IDocumentTag
 
         if (!args.TryGetValue("Columns", out var columns) || !int.TryParse(columns, out var columnsCount))
         {
-            Logger.Error("Guidebook tag \"Table\" does not specify required property \"Columns.\"");
+            Sawmill.Error("Guidebook tag \"Table\" does not specify required property \"Columns.\"");
             control = null;
             return false;
         }
