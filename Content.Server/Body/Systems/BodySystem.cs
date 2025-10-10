359ea931a650e6864a9dcf2ad0eefdbb57ffd891
Murphy Ibbs <Murphyibbs@gmail.com>
2025-10-10 23:51:13 +0100

    #37949 cherry pick 4

--- Content.Server/Body/Systems/BodySystem.cs
+++ Content.Server/Body/Systems/BodySystem.cs
@@ -77,11 +77,15 @@
 using Content.Server.Body.Components;
 using System.Numerics;
 using Content.Server.Ghost;
+using Content.Server.GhostTypes;
+using Content.Server.Body.Components;
 using Content.Server.Humanoid;
 using Content.Shared._Shitmed.Body.Part;
 using Content.Shared.Body.Components;
+using Content.Shared.Body.Events;
 using Content.Shared.Body.Part;
 using Content.Shared.Body.Systems;
+using Content.Shared.Damage.Components;
 using Content.Shared.Humanoid;
 using Content.Shared.Mind;
 using Content.Shared.Mobs.Systems;
