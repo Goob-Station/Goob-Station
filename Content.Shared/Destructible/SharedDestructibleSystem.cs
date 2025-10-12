b904497f373c2bf9253da016c72fb07e8ef4be0e
Fildrance <fildrance@gmail.com>
2025-10-12 20:54:08 +0300

    Use PredictedQueueDel in SharedDestructibleSystem.DestroyEntity (#40856)
    feat: predict queuedel in SharedDestructibleSystem.DestroyEntity
    
    Co-authored-by: pa.pecherskij <pa.pecherskij@interfax.ru>

--- Content.Shared/Destructible/SharedDestructibleSystem.cs
+++ Content.Shared/Destructible/SharedDestructibleSystem.cs
@@ -10,7 +10,7 @@ namespace Content.Shared.Destructible;
 public abstract class SharedDestructibleSystem : EntitySystem
 {
     /// <summary>
-    ///     Force entity to be destroyed and deleted.
+    /// Force entity to be destroyed and deleted.
     /// </summary>
     public bool DestroyEntity(EntityUid owner)
     {
@@ -22,12 +22,12 @@ public abstract class SharedDestructibleSystem : EntitySystem
         var eventArgs = new DestructionEventArgs();
         RaiseLocalEvent(owner, eventArgs);
 
-        QueueDel(owner);
+        PredictedQueueDel(owner);
         return true;
     }
 
     /// <summary>
-    ///     Force entity to break.
+    /// Force entity to break.
     /// </summary>
     public void BreakEntity(EntityUid owner)
     {
@@ -37,7 +37,7 @@ public abstract class SharedDestructibleSystem : EntitySystem
 }
 
 /// <summary>
-///     Raised before an entity is about to be destroyed and deleted
+/// Raised before an entity is about to be destroyed and deleted
 /// </summary>
 public sealed class DestructionAttemptEvent : CancellableEntityEventArgs
 {
@@ -45,7 +45,7 @@ public sealed class DestructionAttemptEvent : CancellableEntityEventArgs
 }
 
 /// <summary>
-///     Raised when entity is destroyed and about to be deleted.
+/// Raised when entity is destroyed and about to be deleted.
 /// </summary>
 public sealed class DestructionEventArgs : EntityEventArgs
 {
@@ -53,7 +53,7 @@ public sealed class DestructionEventArgs : EntityEventArgs
 }
 
 /// <summary>
-///     Raised when entity was heavy damage and about to break.
+/// Raised when entity was heavy damage and about to break.
 /// </summary>
 public sealed class BreakageEventArgs : EntityEventArgs
 {
