ccbeb2cf25aa8b870a00e6a405e7c99250acc979
X <70487315+XWasHere@users.noreply.github.com>
2025-10-13 07:17:31 -0400

    its over

--- Content.Server/Speech/EntitySystems/DamagedSiliconAccentSystem.cs
+++ Content.Server/Speech/EntitySystems/DamagedSiliconAccentSystem.cs
@@ -4,7 +4,7 @@ using Content.Server.PowerCell;
 using Content.Shared.Speech.Components;
 using Content.Shared.Damage;
 using Content.Goobstation.Maths.FixedPoint;
-using Content.Server.Power.Components;
+using Content.Server.Power.Components; // Goobstation
 using Content.Shared.Speech;
 using Robust.Shared.Random;
 
