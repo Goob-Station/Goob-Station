03bde864b5ba9e403633d20730470806e6c8fdc9
XWasHere <xwashere0@protonmail.com>
2025-10-13 17:18:40 -0400

    fix: make plasmamen and skeletons immune to facehuggers

---
+++ Content.Shared/_White/Xenomorphs/FaceHugger/FacehuggerImmuneComponent.cs
@@ -0,0 +1,8 @@
+namespace Content.Shared._White.Xenomorphs.FaceHugger;
+
+// Goobstation
+/// <summary>
+///     Makes the attached entity immune to facehuggers.
+/// </summary>
+[RegisterComponent]
+public sealed partial class FacehuggerImmuneComponent : Component;
