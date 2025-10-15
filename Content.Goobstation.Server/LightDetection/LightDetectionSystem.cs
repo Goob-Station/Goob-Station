020d1ba1ada3fec185842651c5eb021e67772e6a
korczoczek <danielkorczok@gmail.com>
2025-10-15 21:44:25 +0200

    after all, why can't it be the first thing it checks?

--- Content.Goobstation.Server/LightDetection/LightDetectionSystem.cs
+++ Content.Goobstation.Server/LightDetection/LightDetectionSystem.cs
@@ -82,16 +82,16 @@ public sealed class LightDetectionSystem : SharedLightDetectionSystem
         {
             var (uid, comp, xform) = UpdateEnts[index];
 
-            var worldPos = XformSys.GetWorldPosition(xform);
-
-            var totalLightLevel = 0f;
-
             //ignore lights while travelling through disposals
             if (LightSys.HasComp<BeingDisposedComponent>(uid))
             {
                 comp.CurrentLightLevel = 0f;
                 return;
             }
+
+            var worldPos = XformSys.GetWorldPosition(xform);
+
+            var totalLightLevel = 0f;
 
             var lookup = LookupSys.GetEntitiesInRange<PointLightComponent>(xform.Coordinates, LightSys.LookupRange);
             foreach (var ent in lookup)
