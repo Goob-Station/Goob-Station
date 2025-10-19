7328fe050f7035beb9f564de04118d265f05e5ec
Murphy Ibbs <Murphyibbs@gmail.com>
2025-10-19 18:50:43 +0100

    No more event

--- Content.Goobstation.Server/HellGoose/HellGooseStatueSystem.cs
+++ Content.Goobstation.Server/HellGoose/HellGooseStatueSystem.cs
@@ -1,22 +1,27 @@
 using Content.Goobstation.Shared.HellGoose.Components;
 using Content.Goobstation.Shared.HellGoose.Systems;
+using Content.Goobstation.Shared.Emoting;
 using Content.Server.Pointing.Components;
-using Content.Server.Polymorph.Systems;
+using Content.Shared.Body.Systems;
 using Content.Shared.Interaction.Events;
 using Content.Shared.Verbs;
 using Robust.Shared.Utility;
+using Robust.Shared.Audio.Systems;
+using Robust.Shared.Audio;
 
 namespace Content.Goobstation.Server.HellGoose;
 
 
 public sealed class HellGooseStatueSystem : HellGooseStatueSharedSystem
 {
-    [Dependency] private readonly PolymorphSystem _polymorphSystem = default!;
+    [Dependency] private readonly SharedAudioSystem _audio = default!;
+    [Dependency] private readonly SharedBodySystem _bodySystem = default!;
 
-    protected override void PolymorphTheGoose(EntityUid uid)
+    protected override void GibTheGoose(EntityUid uid, EntityUid statue)
     {
-        base.PolymorphTheGoose(uid);
-        _polymorphSystem.PolymorphEntity(uid, "HellGooseMorph");
+        base.GibTheGoose(uid, statue);
+        _bodySystem.GibBody(uid, splatModifier: 40);
+        _audio.PlayPvs(new SoundPathSpecifier("/Audio/_Goobstation/Misc/odetojoy.ogg"), statue);
     }
 
 }
--- Content.Goobstation.Shared/HellGoose/Systems/HellGooseStatueSharedSystem.cs
+++ Content.Goobstation.Shared/HellGoose/Systems/HellGooseStatueSharedSystem.cs
@@ -1,11 +1,18 @@
 using Content.Goobstation.Shared.HellGoose.Components;
 using Content.Shared.Verbs;
+using Content.Shared.Body.Systems;
+using Content.Shared.Popups;
+using Robust.Shared.Audio.Systems;
 using Robust.Shared.Utility;
 
 namespace Content.Goobstation.Shared.HellGoose.Systems;
 
 public abstract class HellGooseStatueSharedSystem : EntitySystem
 {
+    [Dependency] private readonly SharedPopupSystem _popupSystem = default!;
+    [Dependency] private readonly SharedBodySystem _bodySystem = default!;
+    [Dependency] private readonly SharedAudioSystem _audio = default!;
+
     public override void Initialize()
     {
         base.Initialize();
@@ -23,11 +30,12 @@ public abstract class HellGooseStatueSharedSystem : EntitySystem
             Icon = new SpriteSpecifier.Texture(new ResPath("/Textures/Interface/pray.svg.png")),
             Act = () =>
             {
-                PolymorphTheGoose(args.User);
+                GibTheGoose(args.User, uid);
+                _popupSystem.PopupClient(Loc.GetString("hell-goose-statue-not-worthy"), args.User, args.User);
             }
         };
 
         args.Verbs.Add(verb);
     }
-    protected virtual void PolymorphTheGoose(EntityUid uid) {}
+    protected virtual void GibTheGoose(EntityUid uid, EntityUid statue) {}
 }
