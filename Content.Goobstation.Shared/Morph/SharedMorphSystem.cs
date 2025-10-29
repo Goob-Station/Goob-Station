5ac07da695408dfd78b862352d80e90b2f12eba2
Fishbait <Fishbait@git.ml>
2025-10-29 18:30:30 +0100

    remove unsude parts

--- Content.Goobstation.Shared/Morph/SharedMorphSystem.cs
+++ Content.Goobstation.Shared/Morph/SharedMorphSystem.cs
@@ -1,20 +1,8 @@
-using Content.Shared.Actions;
-using Content.Shared.Clothing.Components;
-using Content.Shared.Clothing.EntitySystems;
-using Content.Shared.Damage;
 using Content.Shared.Humanoid;
-using Content.Shared.Interaction.Components;
-using Content.Shared.Inventory;
-using Content.Shared.Inventory.VirtualItem;
 using Content.Shared.Polymorph.Components;
 using Content.Shared.Polymorph.Systems;
-using Content.Shared.Popups;
 using Content.Shared.Weapons.Melee.Events;
-using Content.Shared.Whitelist;
-using Robust.Shared.Containers;
 using Robust.Shared.Network;
-using Robust.Shared.Prototypes;
-using Robust.Shared.Serialization.Manager;
 
 namespace Content.Goobstation.Shared.Morph;
 
@@ -25,8 +13,6 @@ public abstract class SharedMorphSystem : EntitySystem
 {
     [Dependency] private readonly SharedChameleonProjectorSystem _chamleon = default!;
     [Dependency] private readonly INetManager _net = default!;
-    [Dependency] private readonly SharedContainerSystem _container = default!;
-    [Dependency] private readonly InventorySystem _inventory = default!;
     [Dependency] private readonly SharedHumanoidAppearanceSystem _humanoidAppearance = default!;
 
     public override void Initialize()
