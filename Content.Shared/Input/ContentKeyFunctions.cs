179f77b788c338514c314519e73d7fc2972eb97b
notactuallymarty <27968892+NotActuallyMarty@users.noreply.github.com>
2025-10-29 10:52:47 +0200

    Nuke WD

--- Content.Shared/Input/ContentKeyFunctions.cs
+++ Content.Shared/Input/ContentKeyFunctions.cs
@@ -187,7 +187,6 @@ namespace Content.Shared.Input
         public static readonly BoundKeyFunction ZoomOut = "ZoomOut";
         public static readonly BoundKeyFunction ZoomIn = "ZoomIn";
         public static readonly BoundKeyFunction ResetZoom = "ResetZoom";
-        public static readonly BoundKeyFunction ToggleStanding = "ToggleStanding"; // WD EDIT
         public static readonly BoundKeyFunction ResistGrab = "ResistGrab"; // Goobstation - Grab Intent
 
         // Shitmed Change Start
