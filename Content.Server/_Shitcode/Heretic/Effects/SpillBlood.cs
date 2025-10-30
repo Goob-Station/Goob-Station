4a78e7aadc6d2dda4b2f1ebcd06062478afa9f02
Aviu00 <93730715+Aviu00@users.noreply.github.com>
2025-10-30 14:26:57 +0000

    Added cool message on heretic influence gain + more (#4840)
    * Influence gain and fracture examine message
    
    * Fixes.
    
    * Fix gib exception ritual fail?

--- Content.Server/_Shitcode/Heretic/Effects/SpillBlood.cs
+++ Content.Server/_Shitcode/Heretic/Effects/SpillBlood.cs
@@ -7,23 +7,39 @@
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
+using Content.Goobstation.Maths.FixedPoint;
 using Content.Server._Goobstation.Heretic.EntitySystems.PathSpecific;
+using Content.Server.Body.Systems;
+using Content.Server.Fluids.EntitySystems;
 using Content.Shared._Goobstation.Heretic.Components;
+using Content.Shared.Body.Components;
+using Content.Shared.Chemistry.EntitySystems;
 using Content.Shared.EntityEffects;
 using Robust.Shared.Prototypes;
 
 namespace Content.Server._Goobstation.Heretic.Effects;
 
-public sealed partial class VoidCurse : EntityEffect
+public sealed partial class SpillBlood : EntityEffect
 {
-    [DataField]
-    public int Stacks = 1;
+    [DataField(required: true)]
+    public FixedPoint2 Amount;
 
     protected override string? ReagentEffectGuidebookText(IPrototypeManager prototype, IEntitySystemManager entSys)
-        => "Inflicts void curse.";
+        => "Spills target blood.";
 
     public override void Effect(EntityEffectBaseArgs args)
     {
-        args.EntityManager.System<VoidCurseSystem>().DoCurse(args.TargetEntity, Stacks);
+        if (!args.EntityManager.TryGetComponent(args.TargetEntity, out BloodstreamComponent? bloodStream))
+            return;
+
+        if (!args.EntityManager.System<SharedSolutionContainerSystem>()
+                .ResolveSolution(args.TargetEntity,
+                    bloodStream.BloodSolutionName,
+                    ref bloodStream.BloodSolution,
+                    out var bloodSolution))
+            return;
+
+        args.EntityManager.System<PuddleSystem>()
+            .TrySpillAt(args.TargetEntity, bloodSolution.SplitSolution(Amount), out _);
     }
 }
