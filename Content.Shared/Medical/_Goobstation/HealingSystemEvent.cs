756f6a721918dce3e5ae9203cf3a2957d2974b9c
RichardBlonski <48651647+RichardBlonski@users.noreply.github.com>
2025-11-01 14:53:39 +0200

    [WOUNDMED] Bleeding Refactor Part 1 - HealingSystem.cs

---
+++ Content.Shared/Medical/_Goobstation/HealingSystemEvent.cs
@@ -0,0 +1,14 @@
+using Content.Shared.Damage;
+using Content.Shared.DoAfter;
+using Content.Shared.Medical.Healing;
+
+namespace Content.Shared.Medical._Goobstation;
+
+/// <summary>
+/// Event raised when a wound is being medicated.
+/// </summary>
+[ByRefEvent]
+public record struct HealingSystemEvent(EntityUid Entity)
+{
+    public bool AnythingToDo = false;
+}
