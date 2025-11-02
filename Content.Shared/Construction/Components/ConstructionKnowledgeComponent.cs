39162be758ef932e5a0ce0fedbe3d520a07814d8
Roudenn <romabond091@gmail.com>
2025-11-02 20:34:23 +0300

    Add categories to all constructions

---
+++ Content.Shared/Construction/Components/ConstructionKnowledgeComponent.cs
@@ -0,0 +1,11 @@
+﻿namespace Content.Shared.Construction.Components;
+
+/// <summary>
+/// Goobstation
+/// Knowledge component that allows for
+/// </summary>
+[RegisterComponent]
+public sealed partial class ConstructionKnowledgeComponent : Component
+{
+
+}
