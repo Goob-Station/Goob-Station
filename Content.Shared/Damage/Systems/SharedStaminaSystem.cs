bf79bb84c781c7db84197736bbe1f1b16f453c68
Roudenn <romabond091@gmail.com>
2025-11-04 13:02:54 +0300

    !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- Content.Shared/Damage/Systems/SharedStaminaSystem.cs
+++ Content.Shared/Damage/Systems/SharedStaminaSystem.cs
@@ -170,7 +170,7 @@ public abstract partial class SharedStaminaSystem : EntitySystem
     private void OnStartup(Entity<StaminaComponent> entity, ref ComponentStartup args)
     {
         // Goobstation edit start - prevent a server crash from YAMLmaxxing
-        if (entity.Comp.CritThreshold > entity.Comp.AnimationThreshold)
+        if (entity.Comp.CritThreshold <= entity.Comp.AnimationThreshold)
             Log.Error($"Entity {ToPrettyString(entity)} failed to initialize StaminaComponent properly: {nameof(StaminaComponent.CritThreshold)} is lower or equal to {nameof(StaminaComponent.AnimationThreshold)}. Please fix its YAML prototype!");
         // Goobstation edit end
         UpdateStaminaVisuals(entity);
