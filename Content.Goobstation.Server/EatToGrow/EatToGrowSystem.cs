b2ac61e51395f98e649bb57cda19603942fc6228
Arraydeess <arraygitbash@gmail.com>
2025-11-04 19:48:06 -0600

    Scales Fixtures Reasonably

--- Content.Goobstation.Server/EatToGrow/EatToGrowSystem.cs
+++ Content.Goobstation.Server/EatToGrow/EatToGrowSystem.cs
@@ -1,11 +1,18 @@
 using Content.Goobstation.Shared.EatToGrow;
 using Content.Server.Nutrition.Components;
 using Content.Shared.Nutrition;
+using Robust.Shared.GameObjects;
 using Robust.Shared.GameStates;
+using Robust.Shared.Physics;
+using Robust.Shared.Physics.Collision.Shapes;
+using Robust.Shared.Physics.Systems;
+using System.Numerics;
+using static Robust.Server.Console.Commands.ScaleCommand;
 namespace Content.Goobstation.Server.EatToGrow;
 
 public sealed class EatToGrowSystem : EntitySystem
 {
+    [Dependency] private readonly IEntityManager _entityManager = default!;
     public override void Initialize()
     {
         
@@ -29,6 +36,29 @@ public sealed class EatToGrowSystem : EntitySystem
         comp.CurrentScale = MathF.Min(comp.CurrentScale, comp.MaxGrowth);
 
         Dirty(eater, comp); // Sync updated growth to client.
+
+        var physics = _entityManager.System<SharedPhysicsSystem>();
+
+
+
+
+        if (_entityManager.TryGetComponent(eater, out FixturesComponent? manager))
+        {
+            foreach (var (id, fixture) in manager.Fixtures)
+            {
+                if (fixture.Shape is PhysShapeCircle circle)
+                {
+                    physics.SetPositionRadius(
+                        eater, id, fixture, circle,
+                        circle.Position, circle.Radius + comp.Growth/4, manager);
+                }
+                else
+                {
+                    throw new NotImplementedException("Only circle fixtures supported for scaling.");
+                }
+            }
+        }
+
     }
 
     private void OnGetState(EntityUid uid, EatToGrowComponent comp, ref ComponentGetState args)
