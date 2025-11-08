635c4377d4b65fb97d56ee68401e6af37aa89589
JrInventor05Next <205915704+JrInventor05Next@users.noreply.github.com>
2025-11-09 00:30:36 +0800

    Comments

--- Content.Shared/Chemistry/EntitySystems/Hypospray/SolutionCartridgeSystem.cs
+++ Content.Shared/Chemistry/EntitySystems/Hypospray/SolutionCartridgeSystem.cs
@@ -7,7 +7,7 @@ using Content.Shared.Chemistry.Components;
 using Content.Shared.Chemistry.Components.SolutionManager;
 using Robust.Shared.Containers;
 using Robust.Shared.Timing;
-using Robust.Shared.Network;
+using Robust.Shared.Network; //Goobstation
 
 namespace Content.Shared.Chemistry.EntitySystems.Hypospray;
 
@@ -16,7 +16,7 @@ public sealed class SolutionCartridgeSystem : EntitySystem
     [Dependency] private readonly SharedSolutionContainerSystem _solution = default!;
     [Dependency] private readonly SharedContainerSystem _container = default!;
     [Dependency] private readonly IGameTiming _timing = default!;
-    [Dependency] private readonly INetManager _net = default!;
+    [Dependency] private readonly INetManager _net = default!; // Goobstation
 
     public override void Initialize()
     {
@@ -58,7 +58,7 @@ public sealed class SolutionCartridgeSystem : EntitySystem
         if (!_container.TryGetContainer(ent, "item", out var container))
             return;
 
-        if (_net.IsClient)
+        if (_net.IsClient) // Goobstation
             return;
 
         _container.CleanContainer(container);
