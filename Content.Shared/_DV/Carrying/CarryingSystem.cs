ad9a6f8733b8f2c4b14ff506b98216d3203683ff
Fishbait <Fishbait@git.ml>
2025-11-08 11:04:08 +0100

    init

--- Content.Shared/_DV/Carrying/CarryingSystem.cs
+++ Content.Shared/_DV/Carrying/CarryingSystem.cs
@@ -37,6 +37,7 @@ using Robust.Shared.Map.Components;
 using Robust.Shared.Network;
 using Robust.Shared.Physics.Components;
 using System.Numerics;
+using Content.Shared.Body.Events;
 using Content.Shared.Hands.EntitySystems;
 using Content.Shared.Mind.Components;
 
@@ -84,6 +85,7 @@ public sealed class CarryingSystem : EntitySystem
         SubscribeLocalEvent<BeingCarriedComponent, UnstrappedEvent>(OnDrop);
         SubscribeLocalEvent<BeingCarriedComponent, EscapeInventoryEvent>(OnDrop);
         SubscribeLocalEvent<CarriableComponent, CarryDoAfterEvent>(OnDoAfter);
+        SubscribeLocalEvent<BeingCarriedComponent, BeingGibbedEvent>(OnGib);
     }
 
     private void AddCarryVerb(Entity<CarriableComponent> ent, ref GetVerbsEvent<AlternativeVerb> args)
@@ -379,6 +381,9 @@ public sealed class CarryingSystem : EntitySystem
 
         return length;
     }
+
+    private void OnGib(Entity<BeingCarriedComponent> ent, ref  BeingGibbedEvent args)
+        => DropCarried(ent.Comp.Carrier, ent.Owner);
 
     public override void Update(float frameTime)
     {
