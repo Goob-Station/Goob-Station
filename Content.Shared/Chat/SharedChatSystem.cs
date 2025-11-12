7c758763f49ad083e27dd2e6d1b0818ca71d5122
Aiden <28298836+Aidenkrz@users.noreply.github.com>
2025-11-12 11:50:46 -0600

    woof woof bark woof (#4893)
    * Init
    
    * bark system but its not dogshit
    
    * we dont need these tbh
    
    * oops
    
    * spcies whitelists
    
    * fix tests?
    
    * Clean up + Address reviews
    
    * Goob Comments
    
    * fix tests lmao
    
    * volume slightly better
    
    ---------
    
    Co-authored-by: RichardBlonski <48651647+RichardBlonski@users.noreply.github.com>

--- Content.Shared/Chat/SharedChatSystem.cs
+++ Content.Shared/Chat/SharedChatSystem.cs
@@ -22,6 +22,9 @@
 // SPDX-FileCopyrightText: 2025 Piras314 <p1r4s@proton.me>
 // SPDX-FileCopyrightText: 2025 Rinary <72972221+Rinary1@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Timfa <timfalken@hotmail.com>
+// SPDX-FileCopyrightText: 2025 Ted Lukin <66275205+pheenty@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 Timfa <timfalken@hotmail.com>
+// SPDX-FileCopyrightText: 2025 Zekins <zekins3366@gmail.com>
 // SPDX-FileCopyrightText: 2025 gluesniffler <159397573+gluesniffler@users.noreply.github.com>
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
