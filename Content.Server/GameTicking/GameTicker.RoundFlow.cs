eafeb5cb7a75b9429b9d2b6ba7296cc13690ae58
RichardBlonski <48651647+RichardBlonski@users.noreply.github.com>
2025-11-15 15:01:36 +0200

    Fixed `MindComponent.cs` returning a null LastMob which caused build errors - moved `LastMob` into it's own component in GoobMod

--- Content.Server/GameTicking/GameTicker.RoundFlow.cs
+++ Content.Server/GameTicking/GameTicker.RoundFlow.cs
@@ -74,6 +74,7 @@
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Leon Friedrich <60421075+ElectroJr@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 pathetic meowmeow <uhhadd@gmail.com>
+// SPDX-FileCopyrightText: 2025 RichardBlonski <48651647+RichardBlonski@users.noreply.github.com>
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
@@ -103,12 +104,14 @@ using Robust.Shared.Network;
 using Robust.Shared.Player;
 using Robust.Shared.Random;
 using Robust.Shared.Utility;
+
 // Goob Station - End of Round Screen
 using Content.Goobstation.Common.LastWords;
 using Content.Shared.Damage;
 using Content.Shared.Mobs;
 using Content.Shared.Mobs.Components;
 using Content.Goobstation.Maths.FixedPoint;
+using Content.Goobstation.Shared.Mind.Components;
 
 namespace Content.Server.GameTicking
 {
@@ -647,12 +650,15 @@ namespace Content.Server.GameTicking
 
                 var roles = _roles.MindGetAllRoleInfo(mindId);
 
-                #region Goob Station
+                // Goobstation - End of round last words
+                #region Goob Station - End of round last words
 
                 var lastWords = "";
                 var mobState = MobState.Invalid;
                 var damagePerGroup = new Dictionary<string, FixedPoint2>();
-                var lastMob = mind.LastMob;
+                var lastMob = TryComp<MindLastMobComponent>(mindId, out var lastMobComponent)
+                    ? lastMobComponent.LastMob
+                    : null;
                 if (TryComp<LastWordsComponent>(mindId, out var lastWordsComponent)
                     && !TerminatingOrDeleted(lastMob))
                 {
@@ -666,6 +672,7 @@ namespace Content.Server.GameTicking
                 }
 
                 #endregion
+                // END
 
                 var playerEndRoundInfo = new RoundEndMessageEvent.RoundEndPlayerInfo()
                 {
