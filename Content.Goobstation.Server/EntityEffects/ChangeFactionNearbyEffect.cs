26fb97220ce9076d791d20b28461f1931e11f5b2
whateverusername0 <whateveremail>
2025-11-16 15:31:43 +1000

    pmo

--- Content.Goobstation.Server/EntityEffects/ChangeFactionNearbyEffect.cs
+++ Content.Goobstation.Server/EntityEffects/ChangeFactionNearbyEffect.cs
@@ -1,9 +1,13 @@
 using Content.Shared.EntityEffects;
+using Content.Shared.NPC.Prototypes;
 using Robust.Shared.Prototypes;
 
 namespace Content.Goobstation.Server.EntityEffects;
 public sealed partial class ChangeFactionNearbyEffect : EntityEffect
 {
+    [DataField(required: true)] public ProtoId<NpcFactionPrototype> NewFaction;
+    [DataField] public float Duration = 30f;
+
     public override void Effect(EntityEffectBaseArgs args)
     {
 
