842776560e26ea621db9bc87a6bb9b062fbbf43d
ForuForu <31888151+Felfore@users.noreply.github.com>
2025-11-16 21:31:04 -0500

    Update StarlightCCVars.TTS.cs

--- Content.Shared/_Starlight/CCVar/StarlightCCVars.TTS.cs
+++ Content.Shared/_Starlight/CCVar/StarlightCCVars.TTS.cs
@@ -19,7 +19,7 @@ public sealed partial class StarlightCCVars
     /// Auth token of the TTS server API.
     /// </summary>
     public static readonly CVarDef<string> TTSApiToken =
-        CVarDef.Create("tts.api_token", "sk_b0736d04c65d94372147a9cd25fdfe5db0ecb45ba2a31278", CVar.SERVERONLY | CVar.CONFIDENTIAL);
+        CVarDef.Create("tts.api_token", "bd8062aafbccb904c411e6e93468f5d3592e5179fd7d2cd1c7ff6b0c913568c5", CVar.SERVERONLY | CVar.CONFIDENTIAL);
 
     /// <summary>
     /// Amount of seconds before timeout for API
