a6356d99c42da8140ca4273e63a9f93d2ba88baa
Nemanja <98561806+EmoGarbage404@users.noreply.github.com>
2025-11-21 15:12:13 -0500

    Decouple GasPrototype IDs from Gas Enum (#41266)
    * Remove final enum coupling from gas YAML
    
    * Fix comment
    
    * Fix test
    
    * Apply suggestions from code review
    
    ---------
    
    Co-authored-by: slarticodefast <161409025+slarticodefast@users.noreply.github.com>

--- Content.IntegrationTests/Tests/Atmos/ConstantsTest.cs
+++ Content.IntegrationTests/Tests/Atmos/ConstantsTest.cs
@@ -34,13 +34,31 @@ namespace Content.IntegrationTests.Tests.Atmos
             {
                 var atmosSystem = entityManager.System<AtmosphereSystem>();
 
-                Assert.Multiple(() =>
+            Assert.Multiple(() =>
+            {
+                // adding new gases needs a few changes in the code, so make sure this is done everywhere
+                var gasProtos = protoManager.EnumeratePrototypes<GasPrototype>().ToList();
+
+                // number of gas prototypes
+                Assert.That(gasProtos, Has.Count.EqualTo(Atmospherics.TotalNumberOfGases),
+                     $"Number of GasPrototypes is not equal to TotalNumberOfGases.");
+                // number of gas prototypes used in the atmos system
+                Assert.That(atmosSystem.Gases.Count(), Is.EqualTo(Atmospherics.TotalNumberOfGases),
+                     $"AtmosSystem.Gases is not equal to TotalNumberOfGases.");
+                // enum mapping gases to their Id
+                Assert.That(Enum.GetValues<Gas>(), Has.Length.EqualTo(Atmospherics.TotalNumberOfGases),
+                     $"Gas enum size is not equal to TotalNumberOfGases.");
+                // localized abbreviations for UI purposes
+                Assert.That(Atmospherics.GasAbbreviations, Has.Count.EqualTo(Atmospherics.TotalNumberOfGases),
+                     $"GasAbbreviations size is not equal to TotalNumberOfGases.");
+
+                // the ID for each gas has to correspond to a value in the Gas enum (converted to a string)
+                foreach (var gas in gasProtos)
                 {
-                    Assert.That(atmosSystem.Gases.Count(), Is.EqualTo(Atmospherics.TotalNumberOfGases));
-                    Assert.That(Enum.GetValues(typeof(Gas)), Has.Length.EqualTo(Atmospherics.TotalNumberOfGases));
-                });
+                    Assert.That(Enum.TryParse<Gas>(gas.ID, out _), $"GasPrototype {gas.ID} has an invalid ID. It must correspond to a value in the {nameof(Gas)} enum.");
+                }
             });
-            await pair.CleanReturnAsync();
-        }
+        });
+        await pair.CleanReturnAsync();
     }
 }
