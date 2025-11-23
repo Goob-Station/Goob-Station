62ee22c62e4cc158238a2e4e5cb968bb01b85819
John Willis <143434770+CerberusWolfie@users.noreply.github.com>
2025-11-23 17:16:45 -0500

    Update Content.Shared/_Shitmed/Surgery/SharedSurgerySystem.Steps.cs

--- Content.Shared/_Shitmed/Surgery/SharedSurgerySystem.Steps.cs
+++ Content.Shared/_Shitmed/Surgery/SharedSurgerySystem.Steps.cs
@@ -924,12 +924,8 @@ public abstract partial class SharedSurgerySystem
 
         var speed = toolSpeed;
         if(TryComp<BuckleComponent>(target, out var buckleComp)) // Get buckle component from target.
-        {
             if(TryComp<OperatingTableComponent>(buckleComp.BuckledTo, out var operatingTableComponent))  // If they are buckled to entity with operating table component
-            {
                 speed *= operatingTableComponent.SpeedModifier; // apply surgery speed modifier
-            }
-        }
         if (TryComp(user, out SurgerySpeedModifierComponent? surgerySpeedMod))
             speed *= surgerySpeedMod.SpeedModifier;
 
