ce63f9836931ee06c28d218f2e49d995218236b3
ArtisticRoomba <145879011+ArtisticRoomba@users.noreply.github.com>
2025-11-26 00:30:29 -0800

    cherrypick 95d0f1dd6e269dec3c8f9caf8574a15773a10b65 from WizDen PR 41585

--- Content.Server/Atmos/EntitySystems/AtmosphereSystem.cs
+++ Content.Server/Atmos/EntitySystems/AtmosphereSystem.cs
@@ -52,6 +52,8 @@ using Robust.Shared.Map;
 using Robust.Shared.Physics.Systems;
 using Robust.Shared.Prototypes;
 using System.Linq;
+using Content.Server.Atmos.Piping.EntitySystems;
+using Robust.Shared.Threading;
 
 namespace Content.Server.Atmos.EntitySystems;
 
@@ -75,6 +77,7 @@ public sealed partial class AtmosphereSystem : SharedAtmosphereSystem
     [Dependency] private readonly MapSystem _map = default!;
     [Dependency] public readonly PuddleSystem Puddle = default!;
     [Dependency] private readonly ThrowingSystem _throwing = default!;
+    [Dependency] private readonly AtmosDeviceSystem _atmosDeviceSys = default!;
 
     private const float ExposedUpdateDelay = 1f;
     private float _exposedTimer = 0f;
