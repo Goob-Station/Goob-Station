a3852dafad5470386d6fcc25761367705d446fe4
slarticodefast <161409025+slarticodefast@users.noreply.github.com>
2025-11-28 22:41:44 +0100

    Fix looking at verbs causing sounds or popups (#41609)
    * fix verb popups
    
    * spelling

--- Content.Server/DeviceNetwork/Systems/NetworkConfiguratorSystem.cs
+++ Content.Server/DeviceNetwork/Systems/NetworkConfiguratorSystem.cs
@@ -921,11 +921,5 @@ public sealed class NetworkConfiguratorSystem : SharedNetworkConfiguratorSystem
 
         UpdateListUiState(conf, conf.Comp);
     }
-
-    private void OnUiOpenAttempt(EntityUid uid, NetworkConfiguratorComponent configurator, ActivatableUIOpenAttemptEvent args)
-    {
-        if (configurator.LinkModeActive)
-            args.Cancel();
-    }
     #endregion
 }
