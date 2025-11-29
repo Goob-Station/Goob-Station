33efd7dcb3d1cb25efe01fc4f795b976b69bc539
Androidonator <33227516+Androidonator@users.noreply.github.com>
2025-11-29 14:35:28 +0100

    goida god

---
+++ Content.Goobstation.Client/Security/Systems/ContrabandIconsSystem.cs
@@ -0,0 +1,5 @@
+using Content.Shared._Goobstation.Security.ContrabandIcons;
+
+namespace Content.Goobstation.Client.Security.Systems;
+
+public sealed class ContrabandIconsSystem : SharedContrabandIconsSystem;
---
+++ Content.Goobstation.Server/Security/ContrabandIcons/ContrabandIconsSystem.cs
@@ -0,0 +1,5 @@
+using Content.Shared._Goobstation.Security.ContrabandIcons;
+
+namespace Content.Goobstation.Server.Security.ContrabandIcons;
+
+public sealed class ContrabandIconsSystem : SharedContrabandIconsSystem;
