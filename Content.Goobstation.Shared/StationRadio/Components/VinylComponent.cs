c5c274b11dfbe18d5820062dd8ba64a877ecc019
Murphy Ibbs <Murphyibbs@gmail.com>
2025-12-01 21:19:43 +0000

    Vinyls now play music!

--- Content.Goobstation.Shared/StationRadio/Components/VinylComponent.cs
+++ Content.Goobstation.Shared/StationRadio/Components/VinylComponent.cs
@@ -1,12 +1,13 @@
 using Robust.Shared.Audio;
+using Robust.Shared.GameStates;
 
 namespace Content.Goobstation.Shared.StationRadio.Components;
 
-[RegisterComponent]
+[RegisterComponent, NetworkedComponent]
 public sealed partial class VinylComponent : Component
 {
     /// <summary>
     /// What song should be played when the vinyl is played
     /// </summary>
-    [DataField] public SoundPathSpecifier? Song;
+    [DataField] public SoundPathSpecifier? Song = new SoundPathSpecifier("/Audio/_Goobstation/RadioStation/music/peakasshole.ogg");
 }
---
+++ Content.Goobstation.Shared/StationRadio/Systems/VinylPlayerSystem.cs
@@ -0,0 +1,34 @@
+using Content.Goobstation.Shared.StationRadio.Components;
+using Robust.Shared.Audio.Systems;
+using Robust.Shared.Containers;
+using Robust.Shared.Network;
+
+namespace Content.Goobstation.Shared.StationRadio.Systems;
+
+public sealed class VinylPlayerSystem : EntitySystem
+{
+
+    [Dependency] private readonly INetManager _net = default!;
+    [Dependency] private readonly SharedAudioSystem _audio = default!;
+    public override void Initialize()
+    {
+        SubscribeLocalEvent<VinylPlayerComponent, EntInsertedIntoContainerMessage>(OnVinylInserted);
+        SubscribeLocalEvent<VinylPlayerComponent, EntRemovedFromContainerMessage>(OnVinylRemove);
+    }
+
+    private void OnVinylRemove(EntityUid uid, VinylPlayerComponent comp, EntRemovedFromContainerMessage args)
+    {
+        if(comp.SoundEntity != null)
+            _audio.Stop(comp.SoundEntity);
+    }
+
+    private void OnVinylInserted(EntityUid uid, VinylPlayerComponent comp, EntInsertedIntoContainerMessage args)
+    {
+        if (!TryComp(args.Entity, out VinylComponent? vinylcomp) || _net.IsClient)
+            return;
+
+        var audio = _audio.PlayPredicted(vinylcomp.Song, uid, args.Entity);
+        if (audio != null)
+            comp.SoundEntity = audio.Value.Entity;
+    }
+}
