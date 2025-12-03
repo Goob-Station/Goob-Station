e48982304c6c26a0d518c3a17cecaa27e2ec4061
Androidonator <33227516+Androidonator@users.noreply.github.com>
2025-12-03 18:52:18 +0100

    idk

--- Content.Goobstation.Shared/Security/ContrabandIcons/SharedContrabandIconsSystem.cs
+++ Content.Goobstation.Shared/Security/ContrabandIcons/SharedContrabandIconsSystem.cs
@@ -1,21 +1,6 @@
-using Content.Goobstation.Shared.Contraband;
-using Content.Goobstation.Shared.Security.ContrabandIcons.Components;
-using Robust.Shared.Configuration;
-
 namespace Content.Goobstation.Shared.Security.ContrabandIcons;
 
 /// <summary>
 /// This is responsible for updating the contraband status icon
 /// </summary>
-public abstract class SharedContrabandIconsSystem : EntitySystem
-{
-    private string StatusToIcon(ContrabandStatus status)
-    {
-        return status switch
-        {
-            ContrabandStatus.None => "ContrabandIconNone",
-            ContrabandStatus.Contraband => "ContrabandIconContraband",
-            _ => "ContrabandIconNone"
-        };
-    }
-}
+public abstract class SharedContrabandIconsSystem : EntitySystem;
