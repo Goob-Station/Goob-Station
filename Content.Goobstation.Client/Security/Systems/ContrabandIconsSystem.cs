7545e41c8eb6701de1390a551a8a9df13a4dd7cd
Androidonator <33227516+Androidonator@users.noreply.github.com>
2025-12-05 18:11:39 +0100

    made it less shitcode items in hands dont get checked

--- Content.Goobstation.Client/Security/Systems/ContrabandIconsSystem.cs
+++ Content.Goobstation.Client/Security/Systems/ContrabandIconsSystem.cs
@@ -1,5 +1,38 @@
 using Content.Goobstation.Shared.Security.ContrabandIcons;
+using Content.Goobstation.Shared.Security.ContrabandIcons.Components;
 
 namespace Content.Goobstation.Client.Security.Systems;
 
-public sealed class ContrabandIconsSystem : SharedContrabandIconsSystem;
+public sealed class ContrabandIconsSystem : SharedContrabandIconsSystem
+{
+    public override void Initialize()
+    {
+        base.Initialize();
+        
+        SubscribeLocalEvent<VisibleContrabandComponent, ComponentStartup>(OnComponentStartup);
+    }
+    
+    private void OnComponentStartup(EntityUid uid, VisibleContrabandComponent component, ComponentStartup args)
+    {
+        // Force an initial update of the icon on startup
+        var newStatus = component.VisibleItems.Count > 0 ? StatusToIcon(ContrabandStatus.Contraband) : StatusToIcon(ContrabandStatus.None);
+        if (component.StatusIcon != newStatus)
+        {
+            component.StatusIcon = newStatus;
+            Dirty(uid, component);
+        }
+    }
+
+    /// <summary>
+    ///     returns the icon string based on status enum
+    /// </summary>
+    private string StatusToIcon(ContrabandStatus status)
+    {
+        return status switch
+        {
+            ContrabandStatus.None => "ContrabandIconNone",
+            ContrabandStatus.Contraband => "ContrabandIconContraband",
+            _ => "ContrabandIconNone"
+        };
+    }
+}
--- Content.Goobstation.Shared/Security/ContrabandIcons/SharedContrabandIconsSystem.cs
+++ Content.Goobstation.Shared/Security/ContrabandIcons/SharedContrabandIconsSystem.cs
@@ -1,6 +1,106 @@
+using Content.Goobstation.Common.CCVar;
+using Content.Goobstation.Common.Security.ContrabandIcons.Events;
+using Content.Goobstation.Shared.Contraband;
+using Content.Goobstation.Shared.Security.ContrabandIcons.Components;
+using Content.Shared.Contraband;
+using Content.Shared.Inventory;
+using Content.Shared.Inventory.Events;
+using Robust.Shared.Configuration;
+using Robust.Shared.Timing;
+
 namespace Content.Goobstation.Shared.Security.ContrabandIcons;
 
-/// <summary>
-/// This is responsible for updating the contraband status icon
-/// </summary>
-public abstract class SharedContrabandIconsSystem : EntitySystem;
+public abstract class SharedContrabandIconsSystem : EntitySystem
+{
+    [Dependency] private readonly IConfigurationManager _configuration = default!;
+    [Dependency] private readonly IGameTiming _timing = default!;
+    [Dependency] private readonly SharedContrabandDetectorSystem _detectorSystem = default!;
+    [Dependency] private readonly InventorySystem _inventory = default!;
+
+    private bool _isEnabled = true;
+    private EntityQuery<ContrabandComponent> _contrabandQuery;
+    private EntityQuery<VisibleContrabandComponent> _visibleContrabandQuery;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+        _contrabandQuery = GetEntityQuery<ContrabandComponent>();
+        _visibleContrabandQuery = GetEntityQuery<VisibleContrabandComponent>();
+
+        Subs.CVar(_configuration, GoobCVars.ContrabandIconsEnabled, value => _isEnabled = value);
+
+        SubscribeLocalEvent<VisibleContrabandComponent, DidEquipEvent>(OnEquip);
+        SubscribeLocalEvent<VisibleContrabandComponent, DidUnequipEvent>(OnUnequip);
+
+        SubscribeLocalEvent<IdCardInsertedEvent>(OnIdCardInserted);
+        SubscribeLocalEvent<IdCardRemovedEvent>(OnIdCardRemoved);
+    }
+
+    private void OnEquip(EntityUid uid, VisibleContrabandComponent comp, DidEquipEvent args)
+    {
+        if (!_isEnabled ||
+            !_contrabandQuery.TryComp(args.Equipment, out var contra) 
+            || (contra.Severity == "Restricted" && _detectorSystem.CheckContrabandPermission(args.Equipment, args.Equipee, contra)) 
+            || contra.Severity == "Minor" 
+            || contra.Severity == "GrandTheft" 
+            || args.SlotFlags == SlotFlags.POCKET)
+            return;
+
+        comp.VisibleItems.Add(args.Equipment);
+        UpdateStatusIcon(comp, args.Equipee, ContrabandStatus.Contraband);
+    }
+
+    private void OnUnequip(EntityUid uid, VisibleContrabandComponent comp, DidUnequipEvent args)
+    {
+        comp.VisibleItems.Remove(args.Equipment);
+        if (comp.VisibleItems.Count == 0)
+            UpdateStatusIcon(comp, args.Equipee, ContrabandStatus.None);
+    }
+
+    private void OnIdCardInserted(IdCardInsertedEvent args)
+    {
+        CheckAllContra(args.TargetUid);
+    }
+
+    private void OnIdCardRemoved(IdCardRemovedEvent args)
+    {
+        CheckAllContra(args.TargetUid);
+    }
+
+    private string StatusToIcon(ContrabandStatus status)
+    {
+        return status == ContrabandStatus.Contraband ? "ContrabandIconContraband" : "ContrabandIconNone";
+    }
+
+    private void CheckAllContra(EntityUid uid)
+    {
+        uid = GetHighestContainerOwner(uid);
+
+        if (!_visibleContrabandQuery.TryComp(uid, out var visible))
+            return;
+
+        var contralist = _detectorSystem.FindContraband(uid, false, SlotFlags.WITHOUT_POCKET);
+        var status = contralist.Count > 0 ? ContrabandStatus.Contraband : ContrabandStatus.None;
+        UpdateStatusIcon(visible, uid, status);
+    }
+
+    private void UpdateStatusIcon(VisibleContrabandComponent comp, EntityUid uid, ContrabandStatus status)
+    {
+        var newStatus = StatusToIcon(status);
+        if (comp.StatusIcon == newStatus)
+            return;
+
+        comp.StatusIcon = newStatus;
+        Dirty(uid, comp);
+    }
+
+    public EntityUid GetHighestContainerOwner(EntityUid uid)
+    {
+        while (_inventory.TryGetContainingEntity(uid, out var containerEntity))
+        {
+            uid = containerEntity.Value;
+        }
+
+        return uid;
+    }
+}
