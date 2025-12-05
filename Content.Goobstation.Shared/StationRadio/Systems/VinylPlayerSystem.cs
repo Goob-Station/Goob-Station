50e69781e8033ac421a63557aeec922500c3015e
Murphy Ibbs <Murphyibbs@gmail.com>
2025-12-05 17:19:30 +0000

    Power check

--- Content.Goobstation.Shared/StationRadio/Systems/VinylPlayerSystem.cs
+++ Content.Goobstation.Shared/StationRadio/Systems/VinylPlayerSystem.cs
@@ -1,6 +1,8 @@
 using Content.Goobstation.Shared.StationRadio.Components;
 using Content.Goobstation.Shared.StationRadio.Events;
 using Content.Shared.Destructible;
+using Content.Shared.Power;
+using Content.Shared.Power.EntitySystems;
 using Robust.Shared.Audio;
 using Robust.Shared.Audio.Systems;
 using Robust.Shared.Containers;
@@ -13,12 +15,29 @@ public sealed class VinylPlayerSystem : EntitySystem
 
     [Dependency] private readonly INetManager _net = default!;
     [Dependency] private readonly SharedAudioSystem _audio = default!;
+    [Dependency] private readonly SharedPowerReceiverSystem _power = default!;
     public override void Initialize()
     {
         base.Initialize();
         SubscribeLocalEvent<VinylPlayerComponent, EntInsertedIntoContainerMessage>(OnVinylInserted);
         SubscribeLocalEvent<VinylPlayerComponent, EntRemovedFromContainerMessage>(OnVinylRemove);
         SubscribeLocalEvent<VinylPlayerComponent, DestructionEventArgs>(OnDestruction);
+        SubscribeLocalEvent<VinylPlayerComponent, PowerChangedEvent>(OnPowerChanged);
+    }
+
+    private void OnPowerChanged(EntityUid uid, VinylPlayerComponent comp, PowerChangedEvent args)
+    {
+        if(comp.SoundEntity != null && !args.Powered)
+            _audio.Stop(comp.SoundEntity);
+
+        if(!comp.RelayToRadios)
+            return;
+
+        var query = EntityQueryEnumerator<StationRadioReceiverComponent>();
+        while (query.MoveNext(out var receiver, out var _))
+        {
+            RaiseLocalEvent(receiver, new StationRadioMediaStoppedEvent());
+        }
     }
 
     private void OnDestruction(EntityUid uid, VinylPlayerComponent comp, DestructionEventArgs args)
@@ -35,7 +54,7 @@ public sealed class VinylPlayerSystem : EntitySystem
 
     private void OnVinylInserted(EntityUid uid, VinylPlayerComponent comp, EntInsertedIntoContainerMessage args)
     {
-        if (!TryComp(args.Entity, out VinylComponent? vinylcomp) || _net.IsClient || vinylcomp.Song == null)
+        if (!TryComp(args.Entity, out VinylComponent? vinylcomp) || _net.IsClient || vinylcomp.Song == null || !_power.IsPowered(uid))
             return;
 
         var audio = _audio.PlayPredicted(vinylcomp.Song, uid, uid, AudioParams.Default.WithVolume(3f).WithMaxDistance(4.5f));
