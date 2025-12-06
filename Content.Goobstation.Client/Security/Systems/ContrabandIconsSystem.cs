2a47f1402a2af2b7d4fd237909ac23b267b3aa7f
Androidonator <33227516+Androidonator@users.noreply.github.com>
2025-12-06 21:09:20 +0100

    bro why am i such a retard

--- Content.Goobstation.Client/Security/Systems/ContrabandIconsSystem.cs
+++ Content.Goobstation.Client/Security/Systems/ContrabandIconsSystem.cs
@@ -80,18 +80,18 @@ public sealed class ContrabandIconsSystem : SharedContrabandIconsSystem
     
     private void OnUnequipHands(EntityUid uid, VisibleContrabandComponent comp, DidUnequipHandEvent args)
     {
-        if (comp.VisibleItems.Count == 0)
+        if (comp.VisibleItems.Count == 0 || _isEnabled)
             UpdateStatusIcon(comp, args.User, ContrabandStatus.None);
     }
     private void OnIdCardInserted(IdCardInsertedEvent args)
     {
-        if(!_isEnabled)
+        if(_isEnabled)
             CheckAllContra(args.TargetUid);
     }
 
     private void OnIdCardRemoved(IdCardRemovedEvent args)
     {
-        if (!_isEnabled)
+        if (_isEnabled)
             CheckAllContra(args.TargetUid);
     }
     private void OnComponentStartup(EntityUid uid, VisibleContrabandComponent component, ComponentStartup args)
