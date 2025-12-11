85c7c535734d7dec30a47d225cf9b82a686bc464
SabreML <57483089+SabreML@users.noreply.github.com>
2025-12-11 14:33:39 +0000

    big ol' refactor
    This doesn't actually work yet because `Content.Server` can't access `Content.GoobStation.Server` stuff.

--- Content.Goobstation.Server/Radio/RadioSystem.cs
+++ Content.Goobstation.Server/Radio/RadioSystem.cs
@@ -1,7 +1,6 @@
 using Content.Server._Imp.Drone;
-using Content.Shared.Access.Components;
+using Content.Shared.Access.Systems;
 using Content.Shared.PAI;
-using Content.Shared.PDA;
 using Content.Shared.Silicons.Borgs.Components;
 using Content.Shared.Silicons.StationAi;
 using Content.Shared.StatusIcon;
@@ -9,14 +8,18 @@ using Content.Shared.StatusIcon.Components;
 using Robust.Shared.Prototypes;
 using System.Diagnostics.CodeAnalysis;
 
-namespace Content.Server.Radio.EntitySystems;
+namespace Content.Goobstation.Server.Radio;
 
-public sealed partial class RadioSystem
+public sealed class GoobRadioSystem : EntitySystem
 {
-    // These are static vars rather than inlined in `TryGetJobIcon()` so that the YAML linter can verify that they actually exist.
+    [Dependency] private readonly AccessReaderSystem _accessReader = default!;
+    [Dependency] private readonly SharedIdCardSystem _idCardSystem = default!;
+
+    // These are static vars rather than being inlined so that the YAML linter can verify that they actually exist.
     private static readonly ProtoId<JobIconPrototype> JobIconAI = new("JobIconStationAi");
     private static readonly ProtoId<JobIconPrototype> JobIconBorg = new("JobIconBorg");
     private static readonly ProtoId<JobIconPrototype> JobIconNoID = new("JobIconNoId");
+
 
     /// <summary>
     /// This handles getting the radio job icons that are displayed next to a players name when sending a message over radio.
@@ -28,11 +31,27 @@ public sealed partial class RadioSystem
     /// </param>
     /// <param name="jobName">The name of <paramref name="ent"/>'s job. If they don't <i>have</i> a job, (either none at all or "Unknown") then this will be <see langword="null"/>.</param>
     /// <returns>If <paramref name="ent"/> has a valid job icon, returns <see langword="true"/> and sets the out parameters. Otherwise returns <see langword="false"/>.</returns>
-    private bool TryGetJobIcon(EntityUid ent, [NotNullWhen(true)] out ProtoId<JobIconPrototype>? jobIcon, out string? jobName)
+    public bool TryGetJobIcon(EntityUid ent, [NotNullWhen(true)] out ProtoId<JobIconPrototype>? jobIcon, out string? jobName)
     {
-        jobIcon = jobName = null;
+        // If they're an AI/borg/other silicon, they get to return early and skip the `StatusIconComponent` check.
+        if (TryGetSiliconIcon(ent, out jobIcon, out jobName))
+            return true;
 
-        // First things first, check if they're an AI or a borg. (They skip the `StatusIconComponent` check)
+        // If they don't have special silicon privileges, then only show a job icon in chat for entities who normally have one in-game.
+        if (!HasComp<StatusIconComponent>(ent))
+            return false;
+
+        // Try to get the icon stuff from their ID card, if they have one.
+        if (TryGetEquippedIDJob(ent, out jobIcon, out jobName))
+            return true;
+
+        // If none of the above methods found anything, set `jobIcon` to the 'Unknown'/'No ID' sprite.
+        jobIcon = JobIconNoID;
+        return true;
+    }
+
+    private bool TryGetSiliconIcon(EntityUid ent, [NotNullWhen(true)] out ProtoId<JobIconPrototype>? jobIcon, out string? jobName)
+    {
         if (HasComp<StationAiHeldComponent>(ent))
         {
             jobIcon = JobIconAI;
@@ -40,52 +59,41 @@ public sealed partial class RadioSystem
         }
         else if (HasComp<BorgChassisComponent>(ent)
             || HasComp<BorgBrainComponent>(ent)
-            || HasComp<PAIComponent>(ent) // pAIs and Drones don't have radio access, but they can still talk near an intercom.
+            || HasComp<PAIComponent>(ent) // pAIs and Drones don't have radio access, but they can get picked up by an intercom.
             || HasComp<DroneComponent>(ent))
         {
             jobIcon = JobIconBorg;
             jobName = Loc.GetString("job-name-borg");
         }
+        else // Not a silicon
+        {
+            jobIcon = jobName = null;
+            return false;
+        }
+        return true;
+    }
 
-        // If they don't have special silicon privileges, then only show a job icon in chat for entities who normally have one in-game.
-        else if (!HasComp<StatusIconComponent>(ent))
+    private bool TryGetEquippedIDJob(EntityUid ent, [NotNullWhen(true)] out ProtoId<JobIconPrototype>? jobIcon, out string? jobName)
+    {
+        jobIcon = jobName = null;
+        // Ideally this would only use `SharedIdCardSystem.TryFindIdCard()` rather than needing accessReader, but currently that doesn't check the offhand.
+        if (!_accessReader.FindAccessItemsInventory(ent, out var items))
         {
             return false;
         }
 
-        // Try to get an ID card.
-        if (jobIcon is null && _accessReader.FindAccessItemsInventory(ent, out var items))
+        foreach (var item in items)
         {
-            IdCardComponent? idCard = null;
-            foreach (var item in items)
+            // Check if each item is an ID card, or if it's a PDA with an ID inside it.
+            if (_idCardSystem.TryGetIdCard(item, out var idCard))
             {
-                // ID Card
-                if (TryComp<IdCardComponent>(item, out var id))
-                {
-                    idCard = id;
-                    break;
-                }
-
-                // PDA
-                if (TryComp<PdaComponent>(item, out var pda)
-                    && pda.ContainedId != null
-                    && TryComp(pda.ContainedId, out id))
-                {
-                    idCard = id;
-                    break;
-                }
-            }
-
-            if (idCard != null)
-            {
-                jobIcon = idCard.JobIcon;
-                jobName = idCard.LocalizedJobTitle;
+                jobIcon = idCard.Comp.JobIcon;
+                jobName = idCard.Comp.LocalizedJobTitle;
+                return true;
             }
         }
 
-        // If `jobIcon` is still null, set it to an 'Unknown' icon.
-        jobIcon ??= JobIconNoID;
-
-        return true;
+        // Couldn't find an ID card
+        return false;
     }
 }
