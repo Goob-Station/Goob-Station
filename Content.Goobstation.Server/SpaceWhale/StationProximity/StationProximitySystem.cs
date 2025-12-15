3cf6c61ab1282f98b3e09d0d6ee1586f87f36cd7
LuciferEOS <stepanteliatnik2022@gmail.com>
2025-12-15 17:23:50 +0100

    stuff

--- Content.Goobstation.Server/SpaceWhale/StationProximity/StationProximitySystem.cs
+++ Content.Goobstation.Server/SpaceWhale/StationProximity/StationProximitySystem.cs
@@ -5,6 +5,8 @@ using Content.Shared.Humanoid;
 using Content.Shared.Mobs;
 using Content.Shared.Mobs.Components;
 using Content.Shared.Popups;
+using Robust.Server.Audio;
+using Robust.Shared.Audio;
 using Robust.Shared.Map.Components;
 using Robust.Shared.Timing;
 
@@ -17,6 +19,7 @@ public sealed class StationProximitySystem : EntitySystem
     [Dependency] private readonly IGameTiming _timing = default!;
     [Dependency] private readonly PopupSystem _popup = default!;
     [Dependency] private readonly SharedTransformSystem _transform = default!;
+    [Dependency] private readonly AudioSystem _audio = default!;
 
     private static readonly TimeSpan CheckInterval = TimeSpan.FromSeconds(60); // le hardcode major
     private const float MaxDistance = 200; // todo 1000 or so
@@ -42,35 +45,49 @@ public sealed class StationProximitySystem : EntitySystem
     private void CheckStationProximity()
     {
         var stationQuery = EntityQueryEnumerator<BecomesStationComponent, MapGridComponent>();
-        var stations = new List<(EntityUid Uid, MapGridComponent Grid)>();
+        var stations = new List<(EntityUid Uid, MapGridComponent Grid, TransformComponent Xform)>();
 
         while (stationQuery.MoveNext(out var uid, out _, out var grid))
         {
-            stations.Add((uid, grid));
+            var xform = Transform(uid);
+            stations.Add((uid, grid, xform));
         }
 
         if (stations.Count == 0)
             return;
 
-        var humanoidQuery = EntityQueryEnumerator<HumanoidAppearanceComponent, MobStateComponent>();
-        while (humanoidQuery.MoveNext(out var uid, out _, out var mobState))
+        var humanoidQuery = EntityQueryEnumerator<HumanoidAppearanceComponent, MobStateComponent, TransformComponent>();
+        while (humanoidQuery.MoveNext(out var uid, out _, out var mobState, out var humanoidXform))
         {
             if (mobState.CurrentState != MobState.Alive)
                 continue;
 
-            CheckHumanoidProximity(uid, stations);
+            var sameMap = false;
+            foreach (var (_, _, stationXform) in stations)
+            {
+                if (stationXform.MapUid == humanoidXform.MapUid)
+                {
+                    sameMap = true;
+                    break;
+                }
+            }
+            if (!sameMap)
+                continue;
+
+            CheckHumanoidProximity(uid, stations, humanoidXform);
         }
     }
 
-    private void CheckHumanoidProximity(EntityUid humanoid, List<(EntityUid Uid, MapGridComponent Grid)> stations)
+    private void CheckHumanoidProximity(EntityUid humanoid,
+        List<(EntityUid Uid, MapGridComponent Grid, TransformComponent Xform)> stations,
+        TransformComponent humanoidTransform)
     {
-        var humanoidTransform = Transform(humanoid);
         var isNearStation = false;
 
         if (humanoidTransform.GridUid != null)
         {
-            foreach (var (stationUid, _) in stations)
-            { // check if the guy is on the station grid
+            foreach (var (stationUid, _, _) in stations)
+            {
                 if (stationUid == humanoidTransform.GridUid)
                 {
                     isNearStation = true;
@@ -81,12 +98,15 @@ public sealed class StationProximitySystem : EntitySystem
 
         if (!isNearStation) // if not, check the distance #30436
         {
-            var humanoidWorldPos = _transform.GetWorldPosition(humanoid);
+            var humanoidWorldPos = _transform.GetWorldPosition(humanoidTransform);
             var closestDistance = float.MaxValue;
 
-            foreach (var (stationUid, grid) in stations)
+            foreach (var (stationUid, grid, stationXform) in stations)
             {
-                var stationWorldPos = _transform.GetWorldPosition(stationUid);
+                if (stationXform.MapUid != humanoidTransform.MapUid)
+                    continue;
+
+                var stationWorldPos = _transform.GetWorldPosition(stationXform);
                 var distance = (humanoidWorldPos - stationWorldPos).Length();
 
                 if (grid.LocalAABB.Size.Length() > 0)
@@ -123,13 +143,18 @@ public sealed class StationProximitySystem : EntitySystem
         }
     }
 
-    private void HandleFarFromStation(EntityUid entity)
+    private void HandleFarFromStation(EntityUid entity) // basically handles space whale spawnings
     {
         _popup.PopupEntity(
             Loc.GetString("station-proximity-far-from-station"),
             entity,
             entity,
             PopupType.LargeCaution);
+
+        _audio.PlayEntity(new SoundPathSpecifier("/Audio/_Goobstation/Ambience/SpaceWhale/leviathan-appear.ogg"),
+            entity,
+            entity,
+            AudioParams.Default.WithVolume(1f));
 
         if (HasComp<MobCallerComponent>(entity))
             return;
@@ -141,6 +166,7 @@ public sealed class StationProximitySystem : EntitySystem
         mobCaller.MinDistance = 100f; // should be far away
         mobCaller.NeedAnchored = false;
         mobCaller.NeedPower = false;
+        mobCaller.SpawnSpacing = TimeSpan.FromSeconds(65); // to give the guy some time to get back to the station + prevent him from like, QSI-ing to the station to summon the worm in the station lmao, also bru these 5 seconds are really important
         mobCaller.SpawnedEntities = new List<EntityUid>(); // crashes without this
     }
 }
