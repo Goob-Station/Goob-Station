5b045decab4a3cbc516957398175a42fa49dcb2c
SabreML <57483089+SabreML@users.noreply.github.com>
2025-12-15 18:30:27 +0000

    Make it look a little fancier.

--- Content.Goobstation.Server/Tools/WeldingSparksSystem.cs
+++ Content.Goobstation.Server/Tools/WeldingSparksSystem.cs
@@ -17,7 +17,9 @@ public sealed class WeldingSparksSystem : EntitySystem
 
     private void OnUseTool(Entity<WeldingSparksComponent> ent, ref UseToolEvent args)
     {
-        if (args.Target is not { } target)
+        // `target` can be ent in a few cases like welding a tile.
+        // Spawning the sparks on top of the welder looks silly so just return instead.
+        if (args.Target is not { } target || target == ent.Owner)
             return;
 
         _sparksEffect = Spawn(ent.Comp.Effect, Transform(target).Coordinates);
