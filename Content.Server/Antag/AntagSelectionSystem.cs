6b094b395d895b00e1693d3d868605bb19bc0798
10KE <151458027+10kee@users.noreply.github.com>
2025-12-16 11:29:11 +0000

    Revert "change antag pity system (#5068)"
    This reverts commit 0fa27c54e82e6211a6556a469c852726bf47d54a.

--- Content.Server/Antag/AntagSelectionSystem.cs
+++ Content.Server/Antag/AntagSelectionSystem.cs
@@ -355,11 +355,7 @@ public sealed partial class AntagSelectionSystem : GameRuleSystem<AntagSelection
 
         // weight by playtime since last rolled
         foreach (var se in pool)
-        {
-            var lastRoll = (float)(_playTime.GetOverallPlaytime(se) - _lastRolled.GetLastRolled(se.UserId)).TotalSeconds;
-            //weight clamped between 5 hours and 20 hours
-            weights[se] = float.Clamp(lastRoll, 18000.0f, 72000.0f);
-        }
+            weights[se] = (float)(_playTime.GetOverallPlaytime(se) - _lastRolled.GetLastRolled(se.UserId)).TotalSeconds;
 
         return weights;
     }
