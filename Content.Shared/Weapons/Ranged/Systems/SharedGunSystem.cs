1353f8e250b72c13a3413c0956028ce69bf4fd65
Centronias <me@centronias.com>
2025-12-17 14:52:32 -0800

    Adds BallisticAmmoSelfRefillerComponent (#38537)
    * Adds BallisticAmmoSelfRefillerComponent
    
    And uses it to replace battery-based refilling of the Syndicate L6 and Viper modules.
    
    - Add `BallisticAmmoSelfRefillerComponent`
    - Handle `EmpPulseEvent` to pause refilling behavior for EMP's duration
    
    - Change `Content.Server.Weapons.Ranged.Systems.Update` override in `GunSystem.AutoFire.cs` to `UpdateAutoFire`
    - Add `Content.Server.Weapons.Ranged.Systems.Update` to `GunSystem.cs` so that it can call `UpdateAutoFire` and `UpdateBallistic`
    - Add public methods to GunSystem for use by refilling implementation
      - PauseSelfRefill
      - IsFullBallistic (same as #299)
      - CanInsertBallistic (same as #299)
      - TryBallisticInsert (same as #299)
    
    * _timing -> Timing
    
    * unspawned count stuff
    
    * imagine building the code before pushing
    
    * - apply to c20r ROW
    - make predicted/shared
    
    * revert server system import only changes
    
    * oop
    
    * o great and wise Slarti
    
    * Scar comments
    
    * field deltas + correct serializer
    
    * review
    
    ---------
    
    Co-authored-by: ScarKy0 <scarky0@onet.eu>

--- Content.Shared/Weapons/Ranged/Systems/SharedGunSystem.cs
+++ Content.Shared/Weapons/Ranged/Systems/SharedGunSystem.cs
@@ -853,6 +853,7 @@ public abstract partial class SharedGunSystem : EntitySystem
     public override void Update(float frameTime)
     {
         UpdateBattery(frameTime);
+        UpdateBallistic(frameTime);
     }
 }
 
