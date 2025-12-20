a3d61c1e862ea659362db322589d838d1d4645f8
SabreML <57483089+SabreML@users.noreply.github.com>
2025-12-20 20:30:04 +0000

    Actually I do want a failure message

--- Content.Server/SprayPainter/SprayPainterSystem.cs
+++ Content.Server/SprayPainter/SprayPainterSystem.cs
@@ -286,7 +286,10 @@ public sealed class SprayPainterSystem : SharedSprayPainterSystem
 
         var decals = _decals.GetDecalsInRange(grid, args.ClickLocation.Position);
         if (decals.Count == 0)
+        {
+            _popup.PopupEntity(Loc.GetString("spray-painter-interact-no-color"), args.User, args.User);
             return;
+        }
 
         // Get the decal at the click location with the highest `ZIndex`. (If there's multiple stacked it'll be the visible one)
         var topmostDecal = decals.Aggregate((d1, d2) => d1.Decal.ZIndex > d2.Decal.ZIndex ? d1 : d2).Decal;
--- Resources/Locale/en-US/_Goobstation/spray-painter/spray-painter.ftl
+++ Resources/Locale/en-US/_Goobstation/spray-painter/spray-painter.ftl
@@ -1,1 +1,2 @@
+spray-painter-interact-no-color = Can't find a color to pick!
 spray-painter-interact-color-picked = Picked color from '{$id}'.
