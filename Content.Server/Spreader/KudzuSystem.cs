6b29e42ae59539d1e0eddf9fa40aeb381ea3239b
OnsenCapy <101037138+OnsenCapy@users.noreply.github.com>
2025-12-30 17:48:54 -0300

    Update KudzuSystem.cs

--- Content.Server/Spreader/KudzuSystem.cs
+++ Content.Server/Spreader/KudzuSystem.cs
@@ -114,7 +114,7 @@ public sealed class KudzuSystem : EntitySystem
             if (grow.NextTick > curTime)
                 continue;
 
-            grow.NextTick = curTime + TimeSpan.FromSeconds(0.5);
+            grow.NextTick = curTime + TimeSpan.FromSeconds(0.1667);
 
             if (!kudzuQuery.TryGetComponent(uid, out var kudzu))
             {
