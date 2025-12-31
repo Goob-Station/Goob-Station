1f63ab42a7940c21cca9ab44ab4178098ba2825a
Connor Huffine <chuffine@gmail.com>
2025-12-30 20:27:59 -0500

    Fix broken FTL references (#42181)
    Add broken FTL links

--- Content.Server/Implants/ImplanterSystem.cs
+++ Content.Server/Implants/ImplanterSystem.cs
@@ -114,7 +114,7 @@ public sealed partial class ImplanterSystem : SharedImplanterSystem
         if (!_doAfter.TryStartDoAfter(args))
             return;
 
-        _popup.PopupEntity(Loc.GetString("injector-component-injecting-user"), target, user);
+        _popup.PopupEntity(Loc.GetString("injector-component-needle-injecting-user"), target, user);
 
         var userName = Identity.Entity(user, EntityManager);
         _popup.PopupEntity(Loc.GetString("implanter-component-implanting-target", ("user", userName)), user, target, PopupType.LargeCaution);
@@ -138,7 +138,7 @@ public sealed partial class ImplanterSystem : SharedImplanterSystem
         };
 
         if (_doAfter.TryStartDoAfter(args))
-            _popup.PopupEntity(Loc.GetString("injector-component-injecting-user"), target, user);
+            _popup.PopupEntity(Loc.GetString("injector-component-needle-injecting-user"), target, user);
 
     }
 
