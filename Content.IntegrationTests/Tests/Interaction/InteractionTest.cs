f92d3d775534278cc10f28e98e9cab1d420597d6
slarticodefast <161409025+slarticodefast@users.noreply.github.com>
2026-01-15 18:53:30 +0100

    Add integration test for drains (#41190)
    * drain test
    
    * fix linter fail

--- Content.IntegrationTests/Tests/Interaction/InteractionTest.cs
+++ Content.IntegrationTests/Tests/Interaction/InteractionTest.cs
@@ -47,6 +47,7 @@ using System.Numerics;
 using Content.Client.Construction;
 using Content.Client.Examine;
 using Content.Client.Gameplay;
+using Content.Client.Interaction;
 using Content.IntegrationTests.Pair;
 using Content.Server.Hands.Systems;
 using Content.Server.Stack;
@@ -178,6 +179,7 @@ public abstract partial class InteractionTest
     protected InteractionTestSystem CTestSystem = default!;
     protected ISawmill CLogger = default!;
     protected SharedUserInterfaceSystem CUiSys = default!;
+    protected DragDropSystem CDragDropSys = default!;
 
     // player components
     protected HandsComponent? Hands;
@@ -252,6 +254,7 @@ public abstract partial class InteractionTest
         CConSys = CEntMan.System<ConstructionSystem>();
         ExamineSys = CEntMan.System<ExamineSystem>();
         CUiSys = CEntMan.System<SharedUserInterfaceSystem>();
+        CDragDropSys = CEntMan.System<DragDropSystem>();
 
         // Setup map.
         if (TestMapPath == null)
