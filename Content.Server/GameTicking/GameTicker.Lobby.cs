a2a2a555a779da6de59a928090b60b12b61a71c1
Airi Sezaki <hina@masquerading.agency>
2026-02-04 00:09:50 +0200

    nya
    pukes on your carpet

--- Content.Server/GameTicking/GameTicker.Lobby.cs
+++ Content.Server/GameTicking/GameTicker.Lobby.cs
@@ -155,7 +155,15 @@ namespace Content.Server.GameTicking
         private TickerLobbyStatusEvent GetStatusMsg(ICommonSession session)
         {
             _playerGameStatuses.TryGetValue(session.UserId, out var status);
-            return new TickerLobbyStatusEvent(RunLevel != GameRunLevel.PreRoundLobby, LobbyBackground, status == PlayerGameStatus.ReadyToPlay, _roundStartTime, RoundPreloadTime, RoundStartTimeSpan, Paused);
+            return new TickerLobbyStatusEvent(
+                RunLevel != GameRunLevel.PreRoundLobby,
+                LobbyBackground,
+                status == PlayerGameStatus.ReadyToPlay,
+                _roundStartTime,
+                RoundPreloadTime,
+                RoundStartTimeSpan,
+                Paused,
+                GetActivePlayerCount());
         }
 
         private void SendStatusToAll()
---
+++ Resources/Locale/en-US/_goobstation/MisandryBox/pop-requirement.ftl
@@ -0,0 +1,2 @@
+role-timer-players-insufficient = Requires [color=yellow]{$players}[/color] ready players
+role-timer-players-too-high = Requires fewer than [color=yellow]{$players}[/color] players
