0f7b64f506f83a5a36ca3be4d162b54921ff7ce0
Fishbait <Fishbait@git.ml>
2026-02-05 01:20:23 +0100

    memento mori now only works in lavaland presseure

--- Content.Server/_Lavaland/Damage/PreventDamageSystem .cs
+++ Content.Server/_Lavaland/Damage/PreventDamageSystem .cs
@@ -1,20 +1,24 @@
+using Content.Server._Lavaland.Damage.Components;
 using Content.Shared.Damage;
 using Content.Shared.Mobs;
 using Content.Shared.Mobs.Systems;
+using Content.Server.Atmos.EntitySystems;
 
 namespace Content.Server._Lavaland.Damage;
 
 /// <summary>
-/// This stops a mob from taking damage if that damge leads to its death
+/// This stops a mob from taking damage if that damage leads to its death
 /// </summary>
-public sealed class PreventDamageSystem : EntitySystem
+public sealed class PreventDamageSystem  : EntitySystem
 {
-    [Dependency] private readonly MobThresholdSystem _mobThreshold= default!;
+
+    [Dependency] private readonly MobThresholdSystem _mobThreshold = default!;
+    [Dependency] private readonly AtmosphereSystem _atmos = default!;
 
     public override void Initialize()
     {
         base.Initialize();
-        
+
         SubscribeLocalEvent<PreventDamageComponent, BeforeDamageChangedEvent>(OnBeforeDamageChanged);
     }
 
@@ -24,18 +28,34 @@ public sealed class PreventDamageSystem : EntitySystem
            return;
 
         if (!_mobThreshold.TryGetThresholdForState(entity.Owner,MobState.Critical, out var critThreshold))
-            return; // systems dont work if there is no critical threshold, this is to prevent immortality
+            return; // systems don't work if there is no critical threshold, this is to prevent immortality
 
         if (!_mobThreshold.TryGetThresholdForState(entity.Owner,MobState.Dead, out var deadThreshold))
             return; // no death threshold
 
+        if (entity.Comp.LockedToLavaland && !IsOnLavaland(entity))
+            return; //Lavaland lock
+
         if (deadThreshold - critThreshold < entity.Comp.DifferensMinimum) // prevents misuse if the critical and death trigger is the same value
             return;
 
-        var currentDaamage = damageable.Damage.GetTotal();
-        var newTotalDamage = currentDaamage+arg.Damage.GetTotal();
+        var currentDamage = damageable.Damage.GetTotal();
+        var newTotalDamage = currentDamage+arg.Damage.GetTotal();
 
         if(newTotalDamage > deadThreshold)
             arg.Cancelled = true;
+    }
+
+    private  bool IsOnLavaland(Entity<PreventDamageComponent> entity)
+    {
+
+        var pressure = _atmos.GetTileMixture((entity.Owner, Transform(entity)))?.Pressure ?? 0f;
+
+        if ((pressure >= entity.Comp.LowerBound &&
+             pressure <= entity.Comp.UpperBound))
+            return true;
+
+        return false;
+
     }
 }
--- Content.Shared/_Lavaland/Damage/Components/PreventDamageComponent.cs
+++ Content.Shared/_Lavaland/Damage/Components/PreventDamageComponent.cs
@@ -1,6 +1,7 @@
 using Content.Goobstation.Maths.FixedPoint;
+using Content.Shared.Atmos;
 
-namespace Content.Server._Lavaland.Damage;
+namespace Content.Server._Lavaland.Damage.Components;
 
 /// <summary>
 /// This is used for prevents damage if that damage is above death threshold.
@@ -9,8 +10,19 @@ namespace Content.Server._Lavaland.Damage;
 public sealed partial class PreventDamageComponent : Component
 {
     /// <summary>
-    ///  the minimum point diferance from crit to dead. for this to work
+    ///  the minimum point difference from crit to dead. for this to work
     /// </summary>
     [DataField]
     public  FixedPoint2 DifferensMinimum  = FixedPoint2.New(1);
+
+
+    [DataField]
+    public bool LockedToLavaland = true;
+
+    [DataField]
+    public float LowerBound = Atmospherics.OneAtmosphere * 0.2f;
+
+    [DataField]
+    public float UpperBound = Atmospherics.OneAtmosphere * 0.5f;
+
 }
