536d970564fd25ad646bdc378cdcc27472fb6f73
Dmitry <57028746+DIMMoon1@users.noreply.github.com>
2026-02-08 03:31:08 +0700

    C#12 fix

--- Content.Server/Database/EFCoreExtensions.cs
+++ Content.Server/Database/EFCoreExtensions.cs
@@ -1,4 +1,4 @@
-﻿using System.Linq;
+using System.Linq;
 using System.Linq.Expressions;
 using Microsoft.EntityFrameworkCore;
 
@@ -6,32 +6,31 @@ namespace Content.Server.Database;
 
 internal static class EFCoreExtensions
 {
-    extension<TEntity>(IQueryable<TEntity> query) where TEntity : class
+    public static IQueryable<TEntity> ApplyIncludes<TEntity>(
+        this IQueryable<TEntity> query,
+        IEnumerable<Expression<Func<TEntity, object>>> properties)
+        where TEntity : class
     {
-        public IQueryable<TEntity> ApplyIncludes(
-            IEnumerable<Expression<Func<TEntity, object>>> properties)
+        var q = query;
+        foreach (var property in properties)
         {
-            var q = query;
-            foreach (var property in properties)
-            {
-                q = q.Include(property);
-            }
-
-            return q;
+            q = q.Include(property);
         }
+        return q;
+    }
 
-        public IQueryable<TEntity> ApplyIncludes<TDerived>(
-            IEnumerable<Expression<Func<TDerived, object>>> properties,
-            Expression<Func<TEntity, TDerived>> getDerived)
-            where TDerived : class
+    public static IQueryable<TEntity> ApplyIncludes<TEntity, TDerived>(
+        this IQueryable<TEntity> query,
+        IEnumerable<Expression<Func<TDerived, object>>> properties,
+        Expression<Func<TEntity, TDerived>> getDerived)
+        where TEntity : class
+        where TDerived : class
+    {
+        var q = query;
+        foreach (var property in properties)
         {
-            var q = query;
-            foreach (var property in properties)
-            {
-                q = q.Include(getDerived).ThenInclude(property);
-            }
-
-            return q;
+            q = q.Include(getDerived).ThenInclude(property);
         }
+        return q;
     }
 }
