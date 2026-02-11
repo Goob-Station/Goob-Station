a692f1bd22eaa66a16e82fc7ad254ce2514896c3
notactuallymarty <martynashagriefer@gmail.com>
2026-02-11 19:47:47 +0200

    ass2

--- Content.Server/Explosion/Components/ProjectileGrenadeComponent.cs
+++ Content.Server/Explosion/Components/ProjectileGrenadeComponent.cs
@@ -55,5 +55,5 @@ public sealed partial class ProjectileGrenadeComponent : Component
     /// The trigger key that will activate the grenade.
     /// </summary>
     [DataField]
-    public string TriggerKey = "timer";
+    public string TriggerKey = "trigger"; // Goobstation shrapnel payload
 }
