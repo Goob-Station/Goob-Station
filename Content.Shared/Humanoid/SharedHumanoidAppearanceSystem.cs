1996b41e37089277e423b18703cb745fa5bfc9a4
Naxel <46362288+Naxel11@users.noreply.github.com>
2026-02-14 08:09:33 +0200

    Fix for incorrect pronouns (#564)
    * Fix for incorrect pronouns
    
    * bringing the remaining fields to the same standard

--- Content.Shared/Humanoid/SharedHumanoidAppearanceSystem.cs
+++ Content.Shared/Humanoid/SharedHumanoidAppearanceSystem.cs
@@ -160,9 +160,11 @@ public abstract class SharedHumanoidAppearanceSystem : EntitySystem
 
     private void OnExamined(EntityUid uid, HumanoidAppearanceComponent component, ExaminedEvent args)
     {
-        var identity = Identity.Entity(uid, EntityManager);
-        var species = GetSpeciesRepresentation(component.Species).ToLower();
-        var age = GetAgeRepresentation(component.Species, component.Age);
+		// CorvaxGoob 
+		// Fix for incorrect pronouns PR #564
+        var identity = ("user", Identity.Entity(uid, EntityManager));
+        var species = ("species", GetSpeciesRepresentation(component.Species).ToLower());
+        var age = ("age", GetAgeRepresentation(component.Species, component.Age));
 
         // WWDP EDIT
         string locale = "humanoid-appearance-component-examine";
@@ -171,8 +173,7 @@ public abstract class SharedHumanoidAppearanceSystem : EntitySystem
             locale += "-selfaware";
 
         // Goob Sanitize Text
-        var escapedIdentity = FormattedMessage.EscapeText(identity.ToString());
-        args.PushText(Loc.GetString(locale, ("user", escapedIdentity), ("age", age), ("species", species)),
+        args.PushText(Loc.GetString(locale, identity, age, species),
             100); // priority for examine
         // WWDP EDIT END
     }
