e4d63d7e63a33e3fc3e4649f951eaf25edc0efc5
notactuallymarty <martynashagriefer@gmail.com>
2026-02-21 12:26:07 +0200

    Kill stamina slowdown modifiers

--- Content.Shared/Damage/Components/StaminaComponent.cs
+++ Content.Shared/Damage/Components/StaminaComponent.cs
@@ -127,8 +127,8 @@ public sealed partial class StaminaComponent : Component
     /// <summary>
     /// Thresholds that determine an entity's slowdown as a function of stamina damage.
     /// </summary>
-    [DataField]
-    public Dictionary<FixedPoint2, float> StunModifierThresholds = new() { {0, 1f }, { 60, 0.7f }, { 80, 0.5f } };
+    [DataField] // Goob edit. No slowdown. todo goobstation refactor sprint shit so it isnt as dependent on stamina its kinda annoying to wrangle both at the same time.
+    public Dictionary<FixedPoint2, float> StunModifierThresholds = new() { {0, 1f }, { 60, 1f }, { 80, 1f } }; // Goob edit, 0.7 -> 1, 0.5 -> 1
 
     #region Animation Data
 
