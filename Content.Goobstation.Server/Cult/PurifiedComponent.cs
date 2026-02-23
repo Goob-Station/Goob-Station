34fce88d5dd6fb22056df8610986864118b58b3a
John Git <113782077+whateverusername0@users.noreply.github.com>
2026-02-23 15:24:14 +1000

    fix shitcode

--- Content.Goobstation.Server/Cult/PurifiedComponent.cs
+++
@@ -1,7 +0,0 @@
-namespace Content.Goobstation.Server.Cult;
-
-/// <summary>
-///     Indicates that some evil thing has been purified by a bible or whatever there is.
-/// </summary>
-[RegisterComponent]
-public sealed partial class PurifiedComponent : Component;
