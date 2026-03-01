21f7b852f7c45a1b59ad25b7b70325a9882b38f3
Kenkaito <240330945+Kenkaitos@users.noreply.github.com>
2026-03-02 00:24:58 +0700

    vs major

--- Content.Goobstation.Shared/Religion/Recall/RecallPrayableComponent.cs
+++ Content.Goobstation.Shared/Religion/Recall/RecallPrayableComponent.cs
@@ -1,8 +1,3 @@
-using Robust.Shared.GameStates;
-using Robust.Shared.Prototypes;
-using Robust.Shared.Utility;
-using System.ComponentModel;
-
 namespace Content.Goobstation.Shared.Religion.Recall;
 
 [RegisterComponent]
