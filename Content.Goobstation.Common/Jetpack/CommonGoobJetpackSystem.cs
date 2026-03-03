6ebca12e744e3160a43ee383e8ec99f5a6cf8a41
Airi Sezaki <hina@masquerading.agency>
2026-03-04 00:03:19 +0200

    Oh que j'aimerais danser encore une fois Mais ces jambes raides ne bougent plus du tout

--- Content.Goobstation.Common/Jetpack/CommonGoobJetpackSystem.cs
+++
@@ -1,8 +0,0 @@
-using System.Numerics;
-
-namespace Content.Goobstation.Common.Jetpack;
-
-public abstract class CommonGoobJetpackSystem : EntitySystem
-{
-    public abstract bool TryOverrideWishDir(EntityUid uid, out Vector2 wishDir);
-}
--- Content.Shared/Movement/Systems/SharedMoverController.cs
+++ Content.Shared/Movement/Systems/SharedMoverController.cs
@@ -137,7 +137,6 @@ using Content.Shared.Interaction;
 using Content.Shared.Physics;
 using Content.Shared._vg.TileMovement;
 using Content.Shared.Standing; // Goobstation - kil mofs
-using Content.Goobstation.Common.Jetpack; // Goobstation - jetpack in-hands scatter
 using Content.Goobstation.Common.MomentumSteering; // Goobstation - also kil mofs
 using PullableComponent = Content.Shared.Movement.Pulling.Components.PullableComponent;
 
@@ -166,7 +165,6 @@ public abstract partial class SharedMoverController : VirtualController
     [Dependency] private   readonly StandingStateSystem _standing = default!; // Goobstation - kil mofs
     [Dependency] private   readonly CommonMomentumSteeringSystem _momentumSteering = default!; // Goobstation - momentum steering
     [Dependency] private   readonly CommonMomentumThrustSystem _momentumThrust = default!; // Goobstation - jetpack thrust falloff
-    [Dependency] private   readonly CommonGoobJetpackSystem _goobJetpack = default!; // Goobstation - jetpack in-hands scatter
 
     protected EntityQuery<CanMoveInAirComponent> CanMoveInAirQuery;
     protected EntityQuery<FootstepModifierComponent> FootstepModifierQuery;
@@ -462,14 +460,10 @@ public abstract partial class SharedMoverController : VirtualController
         var minimumFrictionSpeed = moveSpeedComponent?.MinimumFrictionSpeed ?? MovementSpeedModifierComponent.DefaultMinimumFrictionSpeed;
         Friction(minimumFrictionSpeed, frameTime, friction, ref velocity);
 
-        // Goobstation - jetpack in-hands
-        if (weightless && _goobJetpack.TryOverrideWishDir(uid, out var overrideDir))
-            wishDir = overrideDir;
-
         // Goobstation - momentum steering
         if (weightless && touching && wishDir != Vector2.Zero
             && MomentumSteeringQuery.TryComp(uid, out var momSteer2)
-            && _momentumSteering.TryAdjustedWishDir(momSteer2, velocity, wishDir, out var adjWishDir, out var momSpeed))
+            && _momentumSteering.TryAdjustedWishDir(uid, momSteer2, velocity, wishDir, out var adjWishDir, out var momSpeed))
         {
             _momentumThrust.AdjustWishDir(uid, momSteer2, wishDir, ref adjWishDir, momSpeed); // Goobstation - jetpack thrust falloff
             Accelerate(ref velocity, in adjWishDir, accel, frameTime);
