84b21272bd8b0c2110b2e2c182e3ce7528147e36
notactuallymarty <martynashagriefer@gmail.com>
2026-03-12 00:27:29 +0200

    Fix TraitorRole

--- Content.Shared/Roles/Components/TraitorRoleComponent.cs
+++ Content.Shared/Roles/Components/TraitorRoleComponent.cs
@@ -6,4 +6,11 @@ namespace Content.Shared.Roles.Components;
 /// Added to mind role entities to tag that they are a syndicate traitor.
 /// </summary>
 [RegisterComponent, NetworkedComponent]
-public sealed partial class TraitorRoleComponent : BaseMindRoleComponent;
+public sealed partial class TraitorRoleComponent : BaseMindRoleComponent
+{
+    /// <summary>
+    ///     Goobstation - traitor flavor.
+    /// </summary>
+    [DataField] public string ObjectiveIssuer = string.Empty;
+}
+
