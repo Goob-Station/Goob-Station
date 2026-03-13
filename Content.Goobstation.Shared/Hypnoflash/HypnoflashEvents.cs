c76eb177f8e2d1668668d3e7e41ee2ed0278556b
LuciferEOS <stepanteliatnik2022@gmail.com>
2026-03-13 20:43:34 +0100

    im sick

--- Content.Goobstation.Shared/Hypnoflash/HypnoflashEvents.cs
+++ Content.Goobstation.Shared/Hypnoflash/HypnoflashEvents.cs
@@ -5,11 +5,11 @@ namespace Content.Goobstation.Shared.Hypnoflash;
 /// <summary>
 /// raised on the entities in the hypnoflash radius
 /// </summary>
-[Serializable, NetSerializable]
-public readonly record struct HypnoflashedEvent;
+[ByRefEvent]
+public readonly partial record struct HypnoflashedEvent;
 
 /// <summary>
 /// raised on the entity that originally activated the hypnoflash
 /// </summary>
-[Serializable, NetSerializable]
-public readonly record struct HypnoflashActivatedEvent;
+[ByRefEvent]
+public readonly partial record struct HypnoflashActivatedEvent;
