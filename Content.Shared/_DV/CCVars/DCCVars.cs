fa24a322a08d79e54d0785b95c2d9b964cc42b69
Kenkaito <240330945+Kenkaitos@users.noreply.github.com>
2026-03-20 15:32:19 +0700

    mrp only?

--- Content.Shared/_DV/CCVars/DCCVars.cs
+++ Content.Shared/_DV/CCVars/DCCVars.cs
@@ -86,7 +86,7 @@ public sealed partial class DCCVars
     /// Whether the no EORG popup is enabled.
     /// </summary>
     public static readonly CVarDef<bool> RoundEndNoEorgPopup =
-        CVarDef.Create("game.round_end_eorg_popup_enabled", true, CVar.SERVER | CVar.REPLICATED);
+        CVarDef.Create("game.round_end_eorg_popup_enabled", false, CVar.SERVER | CVar.REPLICATED);
 
     /// <summary>
     /// Skip the no EORG popup.
