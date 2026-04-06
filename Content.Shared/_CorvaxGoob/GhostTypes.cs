98904258d076a351023b9ab5c4e67b78667797cc
KillanGenifer <157119956+KillanGenifer@users.noreply.github.com>
2026-04-07 02:10:52 +0700

    Create GhostTypes.cs (#731)

---
+++ Content.Shared/_CorvaxGoob/GhostTypes.cs
@@ -0,0 +1,7 @@
+using Robust.Shared.GameStates;
+
+namespace Content.Shared.GhostTypes;
+
+// A stub for updating secrets. Remove it when the build is in conflict/upstream.
+[RegisterComponent, NetworkedComponent]
+public sealed partial class GhostSpriteStateComponent : Component;
