Mehonizer/Trauma-Station

Particles (#2136)
Co-authored-by: TrixxedHeart <46364955+TrixxedHeart@users.noreply.github.com>
Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

Content.Client/Options/UI/Tabs/GraphicsTab.Particles.xaml.cs
@@ -1,0 +1,60 @@
+using Content.Trauma.Common.CCVar;
+using Robust.Shared.Configuration;
+
+namespace Content.Client.Options.UI.Tabs;
+
+public sealed partial class GraphicsTab
+{
+    private sealed class OptionParticleQuality : BaseOption
+    {
+        private readonly IConfigurationManager _cfg;
+        private readonly OptionDropDown _dropDown;
+
+        private const int QualityOff    = 0;
+        private const int QualityLow    = 1;
+        private const int QualityMedium = 2;
+        private const int QualityHigh   = 3;
+        private const int QualityDefault = QualityHigh;
+
+        public OptionParticleQuality(OptionsTabControlRow controller, IConfigurationManager cfg, OptionDropDown dropDown) : base(controller)
+        {
+            _cfg = cfg;
+            _dropDown = dropDown;
+            var button = dropDown.Button;
+            button.AddItem(Loc.GetString("ui-options-particles-off"),    QualityOff);
+            button.AddItem(Loc.GetString("ui-options-particles-low"),    QualityLow);
+            button.AddItem(Loc.GetString("ui-options-particles-medium"), QualityMedium);
+            button.AddItem(Loc.GetString("ui-options-particles-high"),   QualityHigh);
+            button.OnItemSelected += args =>
+            {
+                _dropDown.Button.SelectId(args.Id);
+                ValueChanged();
+            };
+        }
+
+        public override void LoadValue()
+        {
+            _dropDown.Button.SelectId(_cfg.GetCVar(TraumaCVars.ParticleQuality));
+        }
+
+        public override void SaveValue()
+        {
+            _cfg.SetCVar(TraumaCVars.ParticleQuality, _dropDown.Button.SelectedId);
+        }
+
+        public override void ResetToDefault()
+        {
+            _dropDown.Button.SelectId(QualityDefault);
+        }
+
+        public override bool IsModified()
+        {
+            return _dropDown.Button.SelectedId != _cfg.GetCVar(TraumaCVars.ParticleQuality);
+        }
+
+        public override bool IsModifiedFromDefault()
+        {
+            return _dropDown.Button.SelectedId != QualityDefault;
+        }
+    }
+}

Content.Trauma.Client/Particles/ActiveEmitter.cs
@@ -1,0 +1,151 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+using System.Numerics;
+using Content.Trauma.Shared.Particles;
+using Robust.Client.Graphics;
+using Robust.Shared.Map;
+
+namespace Content.Trauma.Client.Particles;
+
+/// <summary>
+/// A running particle emitter and its live particle pool.
+/// Created in <see cref="ParticleSystem"/>.
+/// </summary>
+public sealed class ActiveEmitter
+{
+    public ParticleEffectPrototype Proto = default!;
+
+    /// <summary>
+    /// Current world-space origin of the emitter.
+    /// </summary>
+    public MapCoordinates MapCoords;
+
+    /// <summary>
+    /// Entity this emitter follows (if any).
+    /// </summary>
+    public EntityUid? AttachedEntity;
+
+    /// <summary>
+    /// Time elapsed since this emitter was created.
+    /// </summary>
+    public TimeSpan Age;
+
+    /// <summary>
+    /// Emission accumulator for sub-tick emission rates.
+    /// </summary>
+    public float EmitAccum;
+
+    /// <summary>
+    /// True once the emitter stops producing new particles. Existing particles live out their lifetimes.
+    /// </summary>
+    public bool Exhausted;
+
+    /// <summary>
+    /// Unique client-side handle for addressing this emitter by ID.
+    /// Prefer holding the <see cref="ActiveEmitter"/> reference directly when possible, please.
+    /// </summary>
+    public uint Handle;
+
+    /// <summary>
+    /// Color tint multiplied on top of every particle's computed color.
+    /// </summary>
+    public Color? ColorOverride;
+
+    /// <summary>
+    /// Intensity multiplier for emission rate and particle size. 1.0 = normal.
+    /// </summary>
+    public float Intensity = 1f;
+
+    /// <summary>
+    /// Live overrides shadowing individual prototype fields.
+    /// Non-null values take priority, null falls back to the prototype.
+    /// </summary>
+    public ParticleRuntimeOverrides? Overrides;
+
+    #region Velocity tracking
+
+    public Vector2 PreviousPosition;
+    public Vector2 EmitterVelocity;
+    public bool VelocityInitialized;
+
+    #endregion
+
+    #region Aim-at targeting
+
+    /// <summary>
+    /// When set, each tick the emit angle is recomputed to point toward this entity.
+    /// Falls back to <see cref="TargetPosition"/> if the entity is deleted.
+    /// </summary>
+    public EntityUid? TargetEntity;
+
+    /// <summary>
+    /// When set, the emit angle points toward this world position.
+    /// Used as a fallback when <see cref="TargetEntity"/> is unset or gone.
+    /// </summary>
+    public Vector2? TargetPosition;
+
+    /// <summary>
+    /// Resolved emit angle in radians, recomputed each tick from the target if one is set.
+    /// </summary>
+    public float EffectiveEmitAngle;
+
+    #endregion
+
+    #region Timed bursts
+
+    /// <summary>
+    /// Tracks which <see cref="ParticleEffectPrototype.Bursts"/> entries have already fired.
+    /// </summary>
+    public readonly List<bool> FiredBursts = new();
+
+    #endregion
+
+    #region Animation
+
+    /// <summary>
+    /// Resolved RSI frames. Populated on creation.
+    /// Single-frame sprites have one entry and empty Delays.
+    /// </summary>
+    public Texture[] Frames = Array.Empty<Texture>();
+
+    /// <summary>
+    /// Frame delays when an RSI defines animation.
+    /// </summary>
+    public float[] Delays = Array.Empty<float>();
+
+    public int AnimFrame;
+    public float AnimTimer;
+
+    #endregion
+
+    #region Particles
+
+    // ParticleData objects are never removed from Particles once added.
+    // When a particle dies it's marked Alive = false and pushed onto FreePool.
+    // The next emission pops from FreePool and resets the object rather than allocating a new one.
+    // This avoids GC pressure from short lived allocations during heavy emission.
+    // The simulation loop still iterates the full Particles list each frame, so a very large
+    // list with mostly dead slots can waste time, <b>emitters should keep MaxCount reasonable.</b>
+
+    /// <summary>
+    /// All particle slots, including dead ones held for pooling.
+    /// </summary>
+    public readonly List<ParticleData> Particles = new();
+
+    /// <summary>
+    /// Dead particles available for reuse.
+    /// </summary>
+    public readonly Queue<ParticleData> FreePool = new();
+
+    public bool HasLiveParticles()
+    {
+        foreach (var p in Particles)
+        {
+            if (p.Alive)
+                return true;
+        }
+        return false;
+    }
+
+    #endregion
+}

Content.Trauma.Client/Particles/ParticleEmitterSystem.cs
@@ -1,0 +1,33 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+using Content.Trauma.Shared.Particles;
+
+namespace Content.Trauma.Client.Particles;
+
+/// <summary>
+/// Spawns a particle effect on this client when an entity with
+/// <see cref="ParticleEmitterComponent"/> enters the local view (MapInitEvent).
+/// </summary>
+public sealed class ParticleEmitterSystem : EntitySystem
+{
+    [Dependency] private readonly ParticleSystem _particles = default!;
+    [Dependency] private readonly SharedTransformSystem _transform = default!;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<ParticleEmitterComponent, MapInitEvent>(OnMapInit);
+    }
+
+    private void OnMapInit(Entity<ParticleEmitterComponent> ent, ref MapInitEvent args)
+    {
+        var coords = _transform.GetMapCoordinates(ent.Owner);
+        var emitter = _particles.SpawnEffect(ent.Comp.Effect, coords, ent.Owner, ent.Comp.ColorOverride);
+        if (emitter == null)
+            return;
+
+        if (ent.Comp.Intensity != 1f)
+            emitter.Intensity = ent.Comp.Intensity;
+    }
+}

Content.Trauma.Client/Particles/ParticlePanicCommand.cs
@@ -1,0 +1,25 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+using Content.Shared.Administration;
+using Robust.Shared.Console;
+
+namespace Content.Trauma.Client.Particles;
+
+/// <summary>
+/// Immediately kills all active particle emitters and their live particles.
+/// Useful if something goes wrong and needs to be killed FAST..
+/// </summary>
+[AnyCommand]
+public sealed class ParticlePanicCommand : IConsoleCommand
+{
+    public string Command => "particlepanic";
+    public string Description => "Kills all active particle emitters and clears every live particle immediately.";
+    public string Help => $"Usage: {Command}";
+
+    public void Execute(IConsoleShell shell, string argStr, string[] args)
+    {
+        var particles = IoCManager.Resolve<IEntitySystemManager>().GetEntitySystem<ParticleSystem>();
+        var count = particles.KillAll();
+        shell.WriteLine($"Cleared {count} emitter(s)/particle(s).");
+    }
+}

Content.Trauma.Shared/Particles/SpawnParticles.cs
@@ -1,0 +1,56 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+using Content.Shared.EntityEffects;
+
+namespace Content.Trauma.Shared.Particles;
+
+/// <summary>
+/// Spawns particles at the current position of the entity.
+/// </summary>
+public sealed partial class SpawnParticles : EntityEffectBase<SpawnParticles>
+{
+    /// <summary>
+    /// The particles to spawn
+    /// </summary>
+    [DataField(required: true)]
+    public ProtoId<ParticleEffectPrototype> ParticleProto;
+
+    /// <summary>
+    /// If true, it will attach to the entity
+    /// </summary>
+    [DataField]
+    public bool Attached;
+
+    /// <summary>
+    /// Amount of particles we're spawning
+    /// </summary>
+    [DataField]
+    public int Number = 1;
+
+    /// <summary>
+    /// If set, it will override the colour of the particle
+    /// </summary>
+    [DataField]
+    public Color? Color;
+}
+
+public abstract class SharedSpawnParticlesEffectSystem : EntityEffectSystem<TransformComponent, SpawnParticles>
+{
+    protected override void Effect(Entity<TransformComponent> ent, ref EntityEffectEvent<SpawnParticles> args)
+    {
+        var effect = args.Effect.ParticleProto;
+        var quantity = args.Effect.Number * (int)Math.Floor(args.Scale);
+        var color = args.Effect.Color;
+        var attach = args.Effect.Attached;
+
+        for (int i = 0; i < quantity; i++)
+        {
+            SpawnParticles(effect, ent.Owner, color, attach);
+        }
+    }
+
+    /// <summary>
+    /// Virtual function to spawn particles via the client
+    /// </summary>
+    protected virtual void SpawnParticles(ProtoId<ParticleEffectPrototype> particleProto, EntityUid target, Color? color, bool attached) { }
+}

Goob-Station/Goob-Station

Content.Client/Options/UI/Tabs/GraphicsTab.Particles.xaml.cs
@@ -1,0 +1,60 @@
+using Content.Goobstation.Common.CCVar;
+using Robust.Shared.Configuration;
+
+namespace Content.Client.Options.UI.Tabs;
+
+public sealed partial class GraphicsTab
+{
+    private sealed class OptionParticleQuality : BaseOption
+    {
+        private readonly IConfigurationManager _cfg;
+        private readonly OptionDropDown _dropDown;
+
+        private const int QualityOff    = 0;
+        private const int QualityLow    = 1;
+        private const int QualityMedium = 2;
+        private const int QualityHigh   = 3;
+        private const int QualityDefault = QualityHigh;
+
+        public OptionParticleQuality(OptionsTabControlRow controller, IConfigurationManager cfg, OptionDropDown dropDown) : base(controller)
+        {
+            _cfg = cfg;
+            _dropDown = dropDown;
+            var button = dropDown.Button;
+            button.AddItem(Loc.GetString("ui-options-particles-off"),    QualityOff);
+            button.AddItem(Loc.GetString("ui-options-particles-low"),    QualityLow);
+            button.AddItem(Loc.GetString("ui-options-particles-medium"), QualityMedium);
+            button.AddItem(Loc.GetString("ui-options-particles-high"),   QualityHigh);
+            button.OnItemSelected += args =>
+            {
+                _dropDown.Button.SelectId(args.Id);
+                ValueChanged();
+            };
+        }
+
+        public override void LoadValue()
+        {
+            _dropDown.Button.SelectId(_cfg.GetCVar(GoobCVars.ParticleQuality));
+        }
+
+        public override void SaveValue()
+        {
+            _cfg.SetCVar(GoobCVars.ParticleQuality, _dropDown.Button.SelectedId);
+        }
+
+        public override void ResetToDefault()
+        {
+            _dropDown.Button.SelectId(QualityDefault);
+        }
+
+        public override bool IsModified()
+        {
+            return _dropDown.Button.SelectedId != _cfg.GetCVar(GoobCVars.ParticleQuality);
+        }
+
+        public override bool IsModifiedFromDefault()
+        {
+            return _dropDown.Button.SelectedId != QualityDefault;
+        }
+    }
+}

Content.Goobstation.Client/Particles/ActiveEmitter.cs
@@ -1,0 +1,151 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+using System.Numerics;
+using Content.Goobstation.Shared.Particles;
+using Robust.Client.Graphics;
+using Robust.Shared.Map;
+
+namespace Content.Goobstation.Client.Particles;
+
+/// <summary>
+/// A running particle emitter and its live particle pool.
+/// Created in <see cref="ParticleSystem"/>.
+/// </summary>
+public sealed class ActiveEmitter
+{
+    public ParticleEffectPrototype Proto = default!;
+
+    /// <summary>
+    /// Current world-space origin of the emitter.
+    /// </summary>
+    public MapCoordinates MapCoords;
+
+    /// <summary>
+    /// Entity this emitter follows (if any).
+    /// </summary>
+    public EntityUid? AttachedEntity;
+
+    /// <summary>
+    /// Time elapsed since this emitter was created.
+    /// </summary>
+    public TimeSpan Age;
+
+    /// <summary>
+    /// Emission accumulator for sub-tick emission rates.
+    /// </summary>
+    public float EmitAccum;
+
+    /// <summary>
+    /// True once the emitter stops producing new particles. Existing particles live out their lifetimes.
+    /// </summary>
+    public bool Exhausted;
+
+    /// <summary>
+    /// Unique client-side handle for addressing this emitter by ID.
+    /// Prefer holding the <see cref="ActiveEmitter"/> reference directly when possible, please.
+    /// </summary>
+    public uint Handle;
+
+    /// <summary>
+    /// Color tint multiplied on top of every particle's computed color.
+    /// </summary>
+    public Color? ColorOverride;
+
+    /// <summary>
+    /// Intensity multiplier for emission rate and particle size. 1.0 = normal.
+    /// </summary>
+    public float Intensity = 1f;
+
+    /// <summary>
+    /// Live overrides shadowing individual prototype fields.
+    /// Non-null values take priority, null falls back to the prototype.
+    /// </summary>
+    public ParticleRuntimeOverrides? Overrides;
+
+    #region Velocity tracking
+
+    public Vector2 PreviousPosition;
+    public Vector2 EmitterVelocity;
+    public bool VelocityInitialized;
+
+    #endregion
+
+    #region Aim-at targeting
+
+    /// <summary>
+    /// When set, each tick the emit angle is recomputed to point toward this entity.
+    /// Falls back to <see cref="TargetPosition"/> if the entity is deleted.
+    /// </summary>
+    public EntityUid? TargetEntity;
+
+    /// <summary>
+    /// When set, the emit angle points toward this world position.
+    /// Used as a fallback when <see cref="TargetEntity"/> is unset or gone.
+    /// </summary>
+    public Vector2? TargetPosition;
+
+    /// <summary>
+    /// Resolved emit angle in radians, recomputed each tick from the target if one is set.
+    /// </summary>
+    public float EffectiveEmitAngle;
+
+    #endregion
+
+    #region Timed bursts
+
+    /// <summary>
+    /// Tracks which <see cref="ParticleEffectPrototype.Bursts"/> entries have already fired.
+    /// </summary>
+    public readonly List<bool> FiredBursts = new();
+
+    #endregion
+
+    #region Animation
+
+    /// <summary>
+    /// Resolved RSI frames. Populated on creation.
+    /// Single-frame sprites have one entry and empty Delays.
+    /// </summary>
+    public Texture[] Frames = Array.Empty<Texture>();
+
+    /// <summary>
+    /// Frame delays when an RSI defines animation.
+    /// </summary>
+    public float[] Delays = Array.Empty<float>();
+
+    public int AnimFrame;
+    public float AnimTimer;
+
+    #endregion
+
+    #region Particles
+
+    // ParticleData objects are never removed from Particles once added.
+    // When a particle dies it's marked Alive = false and pushed onto FreePool.
+    // The next emission pops from FreePool and resets the object rather than allocating a new one.
+    // This avoids GC pressure from short lived allocations during heavy emission.
+    // The simulation loop still iterates the full Particles list each frame, so a very large
+    // list with mostly dead slots can waste time, <b>emitters should keep MaxCount reasonable.</b>
+
+    /// <summary>
+    /// All particle slots, including dead ones held for pooling.
+    /// </summary>
+    public readonly List<ParticleData> Particles = new();
+
+    /// <summary>
+    /// Dead particles available for reuse.
+    /// </summary>
+    public readonly Queue<ParticleData> FreePool = new();
+
+    public bool HasLiveParticles()
+    {
+        foreach (var p in Particles)
+        {
+            if (p.Alive)
+                return true;
+        }
+        return false;
+    }
+
+    #endregion
+}

Content.Goobstation.Client/Particles/ParticleEmitterSystem.cs
@@ -1,0 +1,33 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+using Content.Goobstation.Shared.Particles;
+
+namespace Content.Goobstation.Client.Particles;
+
+/// <summary>
+/// Spawns a particle effect on this client when an entity with
+/// <see cref="ParticleEmitterComponent"/> enters the local view (MapInitEvent).
+/// </summary>
+public sealed class ParticleEmitterSystem : EntitySystem
+{
+    [Dependency] private readonly ParticleSystem _particles = default!;
+    [Dependency] private readonly SharedTransformSystem _transform = default!;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<ParticleEmitterComponent, MapInitEvent>(OnMapInit);
+    }
+
+    private void OnMapInit(Entity<ParticleEmitterComponent> ent, ref MapInitEvent args)
+    {
+        var coords = _transform.GetMapCoordinates(ent.Owner);
+        var emitter = _particles.SpawnEffect(ent.Comp.Effect, coords, ent.Owner, ent.Comp.ColorOverride);
+        if (emitter == null)
+            return;
+
+        if (ent.Comp.Intensity != 1f)
+            emitter.Intensity = ent.Comp.Intensity;
+    }
+}

Content.Goobstation.Client/Particles/ParticlePanicCommand.cs
@@ -1,0 +1,25 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+using Content.Shared.Administration;
+using Robust.Shared.Console;
+
+namespace Content.Goobstation.Client.Particles;
+
+/// <summary>
+/// Immediately kills all active particle emitters and their live particles.
+/// Useful if something goes wrong and needs to be killed FAST..
+/// </summary>
+[AnyCommand]
+public sealed class ParticlePanicCommand : IConsoleCommand
+{
+    public string Command => "particlepanic";
+    public string Description => "Kills all active particle emitters and clears every live particle immediately.";
+    public string Help => $"Usage: {Command}";
+
+    public void Execute(IConsoleShell shell, string argStr, string[] args)
+    {
+        var particles = IoCManager.Resolve<IEntitySystemManager>().GetEntitySystem<ParticleSystem>();
+        var count = particles.KillAll();
+        shell.WriteLine($"Cleared {count} emitter(s)/particle(s).");
+    }
+}

Content.Goobstation.Shared/Particles/SpawnParticles.cs
@@ -1,0 +1,61 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+using Content.Shared.EntityEffects;
+using Robust.Shared.Prototypes;
+
+namespace Content.Goobstation.Shared.Particles;
+
+/// <summary>
+/// Spawns particles at the current position of the entity.
+/// </summary>
+public sealed partial class SpawnParticles : EntityEffectBase<SpawnParticles>
+{
+    /// <summary>
+    /// The particles to spawn
+    /// </summary>
+    [DataField(required: true)]
+    public ProtoId<ParticleEffectPrototype> ParticleProto;
+
+    /// <summary>
+    /// If true, it will attach to the entity
+    /// </summary>
+    [DataField]
+    public bool Attached;
+
+    /// <summary>
+    /// Amount of particles we're spawning
+    /// </summary>
+    [DataField]
+    public int Number = 1;
+
+    /// <summary>
+    /// If set, it will override the colour of the particle
+    /// </summary>
+    [DataField]
+    public Color? Color;
+}
+
+public class EntityEffectBase<T>
+{
+}
+
+public abstract class SharedSpawnParticlesEffectSystem : EntityEffectSystem<TransformComponent, SpawnParticles>
+{
+    protected override void Effect(Entity<TransformComponent> ent, ref EntityEffectEvent<SpawnParticles> args)
+    {
+        var effect = args.Effect.ParticleProto;
+        var quantity = args.Effect.Number * (int)Math.Floor(args.Scale);
+        var color = args.Effect.Color;
+        var attach = args.Effect.Attached;
+
+        for (int i = 0; i < quantity; i++)
+        {
+            SpawnParticles(effect, ent.Owner, color, attach);
+        }
+    }
+
+    /// <summary>
+    /// Virtual function to spawn particles via the client
+    /// </summary>
+    protected virtual void SpawnParticles(ProtoId<ParticleEffectPrototype> particleProto, EntityUid target, Color? color, bool attached) { }
+}