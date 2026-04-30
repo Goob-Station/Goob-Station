e2b603016ceb2556ccbcf72da2fcef0ec6ffd054
Murphy Ibbs <Murphyibbs@gmail.com>
2026-04-30 22:09:23 +0100

    dih hoppin

---
+++ Content.Trauma.Shared/Bouncing/BounceableComponent.cs
@@ -0,0 +1,47 @@
+using Content.Shared.EntityEffects;
+using Robust.Shared.Audio;
+
+namespace Content.Trauma.Shared.Bouncing;
+
+[RegisterComponent, NetworkedComponent]
+public sealed partial class BounceableComponent : Component
+{
+    /// <summary>
+    /// How many times has this entity bounced off another one.
+    /// </summary>
+    [DataField]
+    public int TimesBounced;
+
+    /// <summary>
+    /// How many times it needs to bounce to apply effects.
+    /// </summary>
+    [DataField]
+    public int BouncesRequired = 9;
+
+    /// <summary>
+    /// Time to add to NextValidBounceTime when entity collides.
+    /// </summary>
+    [DataField]
+    public TimeSpan GraceTime = TimeSpan.FromSeconds(2);
+
+    /// <summary>
+    /// The effects to apply when collided.
+    /// </summary>
+    [DataField]
+    public EntityEffect[] Effects;
+
+    /// <summary>
+    /// The entity to spawn when bounced enough.
+    /// </summary>
+    [DataField]
+    public EntProtoId EntityToSpawn;
+
+    /// <summary>
+    /// When should we start counting collisions as bounces again.
+    /// </summary>
+    [DataField]
+    public TimeSpan NextValidBounceTime = TimeSpan.Zero;
+
+    [DataField]
+    public SoundPathSpecifier BounceSound =  new SoundPathSpecifier("/Audio/Effects/Footsteps/bounce.ogg");
+}
---
+++ Content.Trauma.Shared/Bouncing/BouncingSystem.cs
@@ -0,0 +1,48 @@
+using Content.Shared.Coordinates;
+using Content.Shared.EntityEffects;
+using Robust.Shared.Audio.Systems;
+using Robust.Shared.Physics.Events;
+using Robust.Shared.Timing;
+
+namespace Content.Trauma.Shared.Bouncing;
+
+public sealed class BouncingSystem : EntitySystem
+{
+    [Dependency] private readonly SharedEntityEffectsSystem _effects = default!;
+    [Dependency] private readonly SharedAudioSystem _audio = default!;
+    [Dependency] private readonly IGameTiming _timing = default!;
+    [Dependency] private readonly INetManager _net = default!;
+
+    public override void Initialize()
+    {
+        SubscribeLocalEvent<BounceableComponent, StartCollideEvent>(OnCollide);
+    }
+
+    private void OnCollide(Entity<BounceableComponent> ent, ref StartCollideEvent args)
+    {
+        if (!HasComp<BounceableComponent>(args.OtherEntity))
+            return;
+
+        var curTime = _timing.CurTime;
+
+        if (ent.Comp.NextValidBounceTime > curTime)
+            return;
+
+        ent.Comp.NextValidBounceTime = curTime + ent.Comp.GraceTime;
+        ent.Comp.TimesBounced++;
+
+        if (ent.Owner.Id < args.OtherEntity.Id) // Only need to do effects and stuff on one chicken + it looks weird when both do it
+            return;
+
+        _audio.PlayPredicted(ent.Comp.BounceSound, ent.Owner, ent.Owner);
+        _effects.ApplyEffects(ent.Owner, ent.Comp.Effects);
+
+        if (ent.Comp.TimesBounced < ent.Comp.BouncesRequired)
+            return;
+
+        ent.Comp.TimesBounced = 0;
+
+        if (_net.IsServer)
+            SpawnAtPosition(ent.Comp.EntityToSpawn, ent.Owner.ToCoordinates());
+    }
+}
