2b275f321bc9fe0e98ef7f73035e7332dae96d37
Arraydeess <arraygitbash@gmail.com>
2026-05-03 09:30:13 -0400

    Removed Slopcode

--- Content.Goobstation.Shared/Doodon/Building/DoodonBuildMenuComponent.cs
+++
@@ -1,8 +0,0 @@
-using Robust.Shared.GameStates;
-
-namespace Content.Goobstation.Shared.Doodon.Building;
-
-[RegisterComponent, NetworkedComponent]
-public sealed partial class DoodonBuildMenuComponent : Component
-{
-}
