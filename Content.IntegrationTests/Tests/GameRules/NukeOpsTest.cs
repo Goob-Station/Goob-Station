fe2d6fe4446892233c2bccd457cd38927209821f
NotActuallyMarty <27968892+NotActuallyMarty@users.noreply.github.com>
2026-06-08 19:05:26 +0300

    wow

--- Content.IntegrationTests/Tests/GameRules/NukeOpsTest.cs
+++ Content.IntegrationTests/Tests/GameRules/NukeOpsTest.cs
@@ -323,7 +323,7 @@ public sealed class NukeOpsTest
                 Assert.That(resp.SuffocationCycles, Is.LessThanOrEqualTo(resp.SuffocationCycleThreshold));
             }
             Assert.That(damage.TotalDamage, Is.
-                AtMost(1.0)); // Goobstation i cant believe you've done this
+                AtMost((FixedPoint2) 1.0)); // Goobstation i cant believe you've done this
         }
 
         // Check that the round does not end prematurely when agents are deleted in the outpost
