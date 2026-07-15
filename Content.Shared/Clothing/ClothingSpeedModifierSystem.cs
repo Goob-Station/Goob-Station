787e77b00b059fca1b9725f89596bcf207ceac68
Baptr0b0t <bapt.buriot@gmail.com>
2026-07-15 15:12:04 +0200

    fix clothing speed modifier

--- Content.Shared/Clothing/ClothingSpeedModifierSystem.cs
+++ Content.Shared/Clothing/ClothingSpeedModifierSystem.cs
@@ -63,7 +63,7 @@ public sealed class ClothingSpeedModifierSystem : EntitySystem
             return;
 
         // goob edit - speed modifier immunity (1 liner)
-        if (_toggle.IsActivated(uid) && !HasComp<SpeedModifierImmunityComponent>(uid))
+        if (!_toggle.IsActivated(uid) || HasComp<SpeedModifierImmunityComponent>(uid))
             return;
 
         if (component.Standing != null && !_standing.IsMatchingState(args.Owner, component.Standing.Value))
