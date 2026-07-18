a3aabbbb1bc88f7a8675142489f15ebc82593e1b
NotActuallyMarty <27968892+NotActuallyMarty@users.noreply.github.com>
2026-07-18 16:21:21 +0300

    fixtureless

--- Content.Client/Weapons/Ranged/Systems/GunSystem.cs
+++ Content.Client/Weapons/Ranged/Systems/GunSystem.cs
@@ -538,7 +538,7 @@ public sealed partial class GunSystem : SharedGunSystem
 
     private bool CheckFixtures(Entity<FixturesComponent?> entity)
     {
-        if (!Resolve(entity, ref entity.Comp))
+        if (!Resolve(entity, ref entity.Comp, false))
             return false;
 
         foreach (var fix in entity.Comp.Fixtures)
