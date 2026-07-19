06e337c99d32243e220dcf9b6ee7c54b1210e39b
Super7522 <Jonhlybr@gmail.com>
2026-07-19 10:36:38 -0300

    merge this without looking

--- Content.Shared/Materials/OreSilo/OreSiloComponent.cs
+++ Content.Shared/Materials/OreSilo/OreSiloComponent.cs
@@ -23,7 +23,7 @@ public sealed partial class OreSiloComponent : Component
     /// Default value should be big enough to span a single large department.
     /// </remarks>
     [DataField, AutoNetworkedField]
-    public float Range = 40f; // Goob - 20->40
+    public float Range = 200f; // Goob - 20-> 200, lower this when department ore silos are mapped
 }
 
 [Serializable, NetSerializable]
