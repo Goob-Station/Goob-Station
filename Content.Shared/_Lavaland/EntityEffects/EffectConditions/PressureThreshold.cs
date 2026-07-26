6178b72ec02dac27bf8d4960aeac0e31a4e46dc4
Roudenn <romabond091@gmail.com>
2026-07-26 12:50:21 +0300

    fix

--- Content.Shared/_Lavaland/EntityEffects/EffectConditions/PressureThreshold.cs
+++ Content.Shared/_Lavaland/EntityEffects/EffectConditions/PressureThreshold.cs
@@ -2,19 +2,35 @@
 
 using Content.Shared._Lavaland.Atmos;
 using Content.Shared._Lavaland.Procedural.Components;
-using Content.Shared.EntityEffects;
 using Robust.Shared.Prototypes;
-using Content.Shared.Atmos.EntitySystems;
 using Content.Shared.EntityConditions;
-using Content.Shared.EntityConditions.Conditions;
 
 namespace Content.Shared._Lavaland.EntityEffects.EffectConditions;
 
 public sealed partial class PressureThresholdEntityConditionSystem : EntityConditionSystem<TransformComponent, PressureThresholdCondition>
 {
+    [Dependency] private readonly SharedLavalandAtmosphereSystem _atmosLavaland = default!;
+
     protected override void Condition(Entity<TransformComponent> entity, ref EntityConditionEvent<PressureThresholdCondition> args)
     {
-        args.Result = false;
+        var transform = entity.Comp;
+
+        if (args.Condition.WorksOnLavaland && HasComp<LavalandMapComponent>(transform.MapUid))
+        {
+            args.Result = true;
+            return;
+        }
+
+        // TODO this is a terrible workaround and it's fixable only by making atmos partially predicted AAAAAAAAAAAAAAA
+        var mix = _atmosLavaland.GetTileMixture(entity.AsNullable());
+        if (mix == null)
+        {
+            args.Result = false;
+            return;
+        }
+
+        var pressure = mix.Pressure;
+        args.Result = pressure >= args.Condition.Min && pressure <= args.Condition.Max;
     }
 }
 
@@ -22,30 +38,13 @@ public sealed partial class PressureThresholdEntityConditionSystem : EntityCondi
 public sealed partial class PressureThresholdCondition : EntityConditionBase<PressureThresholdCondition>
 {
     [DataField]
-    public bool WorksOnLavaland = false;
+    public bool WorksOnLavaland;
 
     [DataField]
     public float Min = float.MinValue;
 
     [DataField]
     public float Max = float.MaxValue;
-
-    public override bool Condition(EntityEffectBaseArgs args)
-    {
-        if (!args.EntityManager.TryGetComponent<TransformComponent>(args.TargetEntity, out var transform))
-            return false;
-
-        if (WorksOnLavaland && args.EntityManager.HasComponent<LavalandMapComponent>(transform.MapUid))
-            return true;
-
-        // TODO this is a terrible workaround and it's fixable only by making atmos partially predicted AAAAAAAAAAAAAAA
-        var mix = args.EntityManager.System<SharedLavalandAtmosphereSystem>().GetTileMixture((args.TargetEntity, transform));
-        if (mix == null)
-            return false;
-
-        var pressure = mix?.Pressure;
-        return pressure >= Min && pressure <= Max;
-    }
 
     public override string EntityConditionGuidebookText(IPrototypeManager prototype)
     {
