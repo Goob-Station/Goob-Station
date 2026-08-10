30fe3ff19cad8fe002e6cab6ecfbcca51280ca27
HAREL <harel8090@gmail.com>
2026-08-11 00:43:55 +0300

    Makes the SM smite

--- Content.Goobstation.Server/Supermatter/Systems/SupermatterSystem.cs
+++ Content.Goobstation.Server/Supermatter/Systems/SupermatterSystem.cs
@@ -3,6 +3,7 @@
 using System;
 using System.Linq;
 using System.Text;
+using Content.Goobstation.Shared.MisandryBox.JumpScare;
 using Content.Goobstation.Shared.Supermatter;
 using Content.Goobstation.Shared.Supermatter.Components;
 using Content.Goobstation.Shared.Supermatter.Systems;
@@ -21,24 +22,29 @@ using Content.Shared.Atmos;
 using Content.Shared.Chat;
 using Content.Shared.Database;
 using Content.Shared.DoAfter;
+using Content.Shared.Electrocution;
 using Content.Shared.Examine;
 using Content.Shared.Interaction;
 using Content.Shared.Kitchen.Components;
 using Content.Shared.Mobs.Components;
+using Content.Shared.Popups;
 using Content.Shared.Projectiles;
 using Content.Shared.Radiation.Components;
 using Content.Shared.Tag;
 using Content.Shared.Throwing;
 using Robust.Server.GameObjects;
+using Robust.Shared.Audio;
 using Robust.Shared.Audio.Systems;
 using Robust.Shared.Containers;
 using Robust.Shared.GameObjects;
 using Robust.Shared.IoC;
+using Robust.Shared.Map;
 using Robust.Shared.Maths;
 using Robust.Shared.Physics;
 using Robust.Shared.Physics.Events;
 using Robust.Shared.Timing;
 using Robust.Shared.Player;
+using Robust.Shared.Utility;
 
 namespace Content.Goobstation.Server.Supermatter.Systems;
 
@@ -58,8 +64,53 @@ public sealed class SupermatterSystem : SharedSupermatterSystem
     [Dependency] private readonly DoAfterSystem _doAfter = default!;
     [Dependency] private readonly SharedTransformSystem _transform = default!;
     [Dependency] private readonly ISharedAdminLogManager _adminLog = default!;
+    [Dependency] private readonly IFullScreenImageJumpscare _jumpscare = default!;
+    [Dependency] private readonly ISharedPlayerManager _player = default!;
+    [Dependency] private readonly SharedPointLightSystem _light = default!;
+    [Dependency] private readonly SharedElectrocutionSystem _elect = default!;
+    [Dependency] private readonly SharedPopupSystem _popup = default!;
+
+    private const string Sound = "/Audio/_Goobstation/Effects/Smites/Thunderstrike/thunderstrike.ogg";
+    private const string God = "/Textures/_Goobstation/MisandryBox/LTGSM.png";
+
+    private readonly Dictionary<EntityUid, TimeSpan> _pending = new();
+    private float _accumulator;
 
     private DelamType _delamType = DelamType.Explosion;
+
+    public void Smite(EntityUid mumu, bool kill = true, TransformComponent? transform = null)
+    {
+        if (!Resolve(mumu, ref transform))
+            return;
+
+        CreateLighting(transform.Coordinates);
+
+        _elect.TryDoElectrocution(mumu, null, 250, TimeSpan.FromSeconds(1), false, ignoreInsulation: true);
+
+        if (!kill || !_player.TryGetSessionByEntity(mumu, out var sesh))
+            return;
+
+        var text = new SpriteSpecifier.Texture(new ResPath(God));
+        _jumpscare.Jumpscare(text, sesh);
+
+        QueueDel(mumu);
+        Spawn("Ash", transform.Coordinates);
+        _popup.PopupEntity(Loc.GetString("admin-smite-turned-ash-other", ("name", mumu)), mumu, PopupType.LargeCaution);
+    }
+
+    public void CreateLighting(EntityCoordinates coordinates, int energy = 125, int radius = 15)
+    {
+        var ent = Spawn(null, coordinates);
+        var comp = _light.EnsureLight(ent);
+        _light.SetColor(ent, new Color(255, 255, 255), comp);
+        _light.SetEnergy(ent, energy, comp);
+        _light.SetRadius(ent, radius, comp);
+
+        var sound = new SoundPathSpecifier(Sound);
+        _audio.PlayPvs(sound, coordinates, AudioParams.Default.WithVolume(150f));
+
+        _pending[ent] = TimeSpan.FromSeconds(_accumulator + 0.125);
+    }
 
     public override void Initialize()
     {
@@ -96,6 +147,21 @@ public sealed class SupermatterSystem : SharedSupermatterSystem
     public override void Update(float frameTime)
     {
         base.Update(frameTime);
+
+        if (_pending.Count == 0)
+            return;
+
+        _accumulator += frameTime;
+        for (var i = _pending.Count - 1; i >= 0; i--)
+        {
+            var (entity, expiryTime) = _pending.ElementAt(i);
+
+            if (!(_accumulator >= expiryTime.TotalSeconds))
+                continue;
+
+            _pending.Remove(entity);
+            Del(entity);
+        }
 
         if (!_gameTiming.IsFirstTimePredicted)
             return;
@@ -620,8 +686,15 @@ public sealed class SupermatterSystem : SharedSupermatterSystem
         if (!HasComp<ProjectileComponent>(target))
         {
             _adminLog.Add(LogType.Supermatter, LogImpact.Medium, $"Supermatter {ToPrettyString(uid)} has consumed {ToPrettyString(target)}");
-            EntityManager.SpawnEntity("Ash", Transform(target).Coordinates);
-            _audio.PlayPvs(sm.DustSound, uid);
+            if (HasComp<ActorComponent>(target))
+            {
+                Smite(target);
+            }
+            else
+            {
+                EntityManager.SpawnEntity("Ash", Transform(target).Coordinates);
+                _audio.PlayPvs(sm.DustSound, uid);
+            }
         }
 
         EntityManager.QueueDeleteEntity(target);
