7c40ef60ac6a8d847fb0105e27e11189ecf6c86c
Aiden <aiden@djkraz.com>
2026-08-13 15:30:52 -0500

    Update ActivationRadius and StillTime values

--- Content.Goobstation.Shared/ParallaxStargaze/ParallaxStargazeComponent.cs
+++ Content.Goobstation.Shared/ParallaxStargaze/ParallaxStargazeComponent.cs
@@ -7,10 +7,10 @@ namespace Content.Goobstation.Shared.ParallaxStargaze;
 public sealed partial class ParallaxStargazeComponent : Component
 {
     [DataField]
-    public float ActivationRadius = 3f;
+    public float ActivationRadius = 5f;
 
     [DataField]
-    public float StillTime = 10f;
+    public float StillTime = 6f;
 
     [DataField]
     public float FadeTime = 2.5f;
