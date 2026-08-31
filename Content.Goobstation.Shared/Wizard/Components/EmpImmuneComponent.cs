2b790e5704cd1227af342c8bf40d80fa7813cf6a
madeline-celeste <224184098+madeline-celeste@users.noreply.github.com>
2026-08-31 18:04:55 -0400

    Move EmpImmuneSystem to predicted shared

---
+++ Content.Goobstation.Shared/Wizard/Components/EmpImmuneComponent.cs
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+namespace Content.Goobstation.Shared.Wizard.Components;
+
+[RegisterComponent]
+public sealed partial class EmpImmuneComponent : Component;
--- Content.Goobstation.Shared/Wizard/Systems/EmpImmuneSystem.cs
+++ Content.Goobstation.Shared/Wizard/Systems/EmpImmuneSystem.cs
@@ -1,10 +1,9 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-using Content.Server._Goobstation.Wizard.Components;
-using Content.Server.Emp;
+using Content.Goobstation.Shared.Wizard.Components;
 using Content.Shared.Emp;
 
-namespace Content.Server._Goobstation.Wizard.Systems;
+namespace Content.Goobstation.Shared.Wizard.Systems;
 
 public sealed class EmpImmuneSystem : EntitySystem
 {
