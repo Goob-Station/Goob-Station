fc03746d8182a13f9f68318a4f58c5a97c98469c
Marty <27968892+NotActuallyMarty@users.noreply.github.com>
2026-09-02 01:54:40 +0300

    Nuke abomination of a method.

--- Content.Server/Lathe/LatheSystem.cs
+++ Content.Server/Lathe/LatheSystem.cs
@@ -224,7 +224,7 @@ namespace Content.Server.Lathe
             {
                 // Goobstation edit start: handle special case with lots of 0-time recipes that insert into storage
                 if (component.OutputToStorage)
-                    FinishProducingManyStorage((uid, component, lathe));
+                    FinishProducing(uid, component, lathe, true);
                 // Goobstation edit end
 
                 FinishProducing(uid, component, lathe);
@@ -232,12 +232,13 @@ namespace Content.Server.Lathe
             return true;
         }
 
-        public void FinishProducing(EntityUid uid, LatheComponent? comp = null, LatheProducingComponent? prodComp = null)
+       public void FinishProducing(EntityUid uid, LatheComponent? comp = null, LatheProducingComponent? prodComp = null,
+           bool bulk = false) // Goobstation
         {
             if (!Resolve(uid, ref comp, ref prodComp, false))
                 return;
 
-            if (comp.CurrentRecipe != null)
+            while (comp.CurrentRecipe != null) // Goob, if to while for bulk recipes
             {
                 var currentRecipe = _proto.Index(comp.CurrentRecipe.Value);
                 if (currentRecipe.Result is { } resultProto)
@@ -276,10 +277,15 @@ namespace Content.Server.Lathe
                         _puddle.TrySpillAt(uid, toAdd, out _);
                     }
                 }
-            }
 
-            comp.CurrentRecipe = null;
-            prodComp.StartTime = _timing.CurTime;
+                comp.CurrentRecipe = null;
+                prodComp.StartTime = _timing.CurTime;
+
+                // Goobstation edit start, see method comment
+                if (!bulk || !TryStartNextBulkRecipe(uid, comp))
+                    break;
+                // Goobstation edit end
+            }
 
             if (!TryStartProducing(uid, comp))
             {
