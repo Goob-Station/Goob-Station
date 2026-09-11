6fcbab18016e19efaa7a9754714822e1a0d0d363
Triangle <74220687+cwestkaemper@users.noreply.github.com>
2026-09-11 18:51:07 -0500

    Added new food recipes for the Chilly Pepper, Pyrotton, and golden apple (#7170)
    * added the new foods/recipes (without sprites)
    
    * Updated to include sprites
    
    * Removed unnessecary changes to the vanilla pizza rsi
    
    * Moved recipes to goobstation namespace (yes i checked they still work)
    
    * eliminated needless newline
    
    * I hate the content freeze
    
    * it has to be the exact same??? are you kidding???

--- Content.IntegrationTests/Tests/WizdenContentFreeze/WizdenContentFreeze.cs
+++ Content.IntegrationTests/Tests/WizdenContentFreeze/WizdenContentFreeze.cs
@@ -24,7 +24,7 @@ public sealed class WizdenContentFreeze
         var protoMan = server.ProtoMan;
 
         var recipesCount = protoMan.Count<FoodRecipePrototype>();
-        var recipesLimit = 224; // Goobstation lmao?
+        var recipesLimit = 230; // Goobstation lmao?
 
         if (recipesCount > recipesLimit)
         {
