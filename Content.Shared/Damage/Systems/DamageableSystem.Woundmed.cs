b020157836801e145b17ab69c9ddc9a74c39c6d1
madeline-celeste <224184098+madeline-celeste@users.noreply.github.com>
2026-09-13 17:30:29 -0400

    this error complains here too ig

--- Content.Shared/Damage/Systems/DamageableSystem.Woundmed.cs
+++ Content.Shared/Damage/Systems/DamageableSystem.Woundmed.cs
@@ -182,6 +182,7 @@ public sealed partial class DamageableSystem
                 // but it's reproducible consistently when you walk towards the people in the
                 // dev map Weapons room. this is bad but the error message isn't that helpful and
                 // probably metagameable idk
+                // See below too!
                 Log.Error(
                     $"Couldn't find any body parts for Body {ToPrettyString(body)} when applying damage!"
                 );
@@ -270,10 +271,17 @@ public sealed partial class DamageableSystem
             // No body parts at all?
             if (possibleTargets.Count == 0)
             {
-                throw new InvalidOperationException
-                (
+                // There is some sort of i assume Pvs-related bug here. i don't know why it happens,
+                // but it's reproducible consistently when you walk towards the people in the
+                // dev map Weapons room. this is bad but the error message isn't that helpful and
+                // probably metagameable idk
+                // See above too!
+#if DEBUG
+                Log.Error(
                     $"Couldn't find any possible target body parts for Body {ToPrettyString(body)} when applying damage!"
                 );
+#endif
+                return null;
             }
 
             var random = SharedRandomExtensions.PredictedRandom(_timing, GetNetEntity(body));
